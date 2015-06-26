2cc4c4383e067dd8e12ca5982503970857605f9a
lianwei <lianwei@microsoft.com>
2015-06-26 13:40:10 +0800

    Fix dnx version build issue

--- src/BackEnd/docfx/HelpTextGenerator.cs
+++ src/BackEnd/docfx/HelpTextGenerator.cs
@@ -6,7 +6,7 @@
     using System.Linq;
     using System.Reflection;
 
-    static class HelpTextGeneratorS
+    static class HelpTextGenerator
     {
         private static readonly HelpText HelpText = new HelpText
         {
