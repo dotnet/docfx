4836b1a214238e1d203a0d039e058fd8d2b9696a
MarzinZ <marzin.zhu@gmail.com>
2015-07-20 09:58:47 +0800

    add client search feature

--- src/docfx.website.themes/default/app/src/app.js
+++ src/docfx.website.themes/default/app/src/app.js
@@ -15,6 +15,6 @@ angular.module('docsApp', [
   'docascode.directives',
 ]);
 
-angular.module('docascode.controller', ['docascode.contentService', 'docascode.urlService', 'docascode.directives', 'docascode.util', 'docascode.constants']);
+angular.module('docascode.controller', ['docascode.contentService', 'docascode.urlService', 'docascode.directives', 'docascode.util', 'docascode.constants', 'docascode.searchService']);
 
 angular.module('docascode.directives', ['itemTypes', 'docascode.urlService', 'docascode.util', 'docascode.markdownService','docascode.mapfileService', 'docascode.csplayService', 'docascode.contentService']);
