58d9a8b56d8c7e99c05cfbb46c515cd5941eaacc
MarzinZ <marzin.zhu@gmail.com>
2015-07-30 15:41:03 +0800

    navbar bugs fixed

--- src/docfx.website.themes/default/app/src/search-worker.js
+++ src/docfx.website.themes/default/app/src/search-worker.js
@@ -27,13 +27,13 @@ searchDataRequest.onload = function() {
   }
   postMessage({ e: 'index-ready' });
 };
-searchDataRequest.open('GET', '/search-data.json');
+searchDataRequest.open('GET', '../search-data.json');
 searchDataRequest.send();
 
 // The worker receives a message everytime the web app wants to query the index
 onmessage = function(oEvent) {
-  var q = oEvent.data.q;
-  var hits = index.search(q);
+  var q = oEvent.data.q
+  ;var hits = index.search(q);
   var results = [];
   // Only return the array of paths to pages
   hits.forEach(function(hit) {
