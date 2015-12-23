c69031894a03639cc9dd5d97fbb56870d3b4256e
vwxyzh <vwxyzh@gmail.com>
2015-12-23 16:07:28 +0800

    add comments

--- src/Microsoft.DocAsCode.MarkdownLite/IMarkdownContext.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/IMarkdownContext.cs
@@ -5,10 +5,25 @@ namespace Microsoft.DocAsCode.MarkdownLite
 {
     using System.Collections.Immutable;
 
+    /// <summary>
+    /// The context for markdown parser.
+    /// </summary>
     public interface IMarkdownContext
     {
+        /// <summary>
+        /// The rule set for current context.
+        /// </summary>
         ImmutableList<IMarkdownRule> Rules { get; }
+        /// <summary>
+        /// The variables.
+        /// </summary>
         ImmutableDictionary<string, object> Variables { get; }
+
+        /// <summary>
+        /// Create a new context with different variables.
+        /// </summary>
+        /// <param name="variables">The new variables.</param>
+        /// <returns>a new instance of <see cref="IMarkdownContext"/></returns>
         IMarkdownContext CreateContext(ImmutableDictionary<string, object> variables);
     }
 }
