929c37e29e8da739cafad479f347552a4d53a9f8
vwxyzh <vwxyzh@gmail.com>
2015-12-23 14:48:22 +0800

    add comments

--- src/Microsoft.DocAsCode.MarkdownLite/IMarkdownEngine.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/IMarkdownEngine.cs
@@ -3,13 +3,36 @@
 
 namespace Microsoft.DocAsCode.MarkdownLite
 {
+    /// <summary>
+    /// Markdown engine
+    /// </summary>
     public interface IMarkdownEngine
     {
+        /// <summary>
+        /// Parser (it can read markdown text, then return markdown tokens).
+        /// </summary>
         IMarkdownParser Parser { get; }
+        /// <summary>
+        /// Renderer (it can read markdown token, then return text e.g. html).
+        /// </summary>
         IMarkdownRenderer Renderer { get; }
+        /// <summary>
+        /// Writer (it can read markdown tokens, then rewrite them and return).
+        /// </summary>
         IMarkdownRewriteEngine RewriteEngine { get; }
 
+        /// <summary>
+        /// Mark markdown text.
+        /// </summary>
+        /// <param name="markdown">The markdown text.</param>
+        /// <param name="context">The markdown context contains rules.</param>
+        /// <returns>Rendered text.</returns>
         StringBuffer Mark(string markdown, IMarkdownContext context = null);
+        /// <summary>
+        /// Mark markdown text.
+        /// </summary>
+        /// <param name="markdown">The markdown text.</param>
+        /// <returns>Rendered text.</returns>
         string Markup(string markdown);
     }
 }
--- src/Microsoft.DocAsCode.MarkdownLite/MarkdownEngineBuilder.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/MarkdownEngineBuilder.cs
@@ -5,6 +5,9 @@ namespace Microsoft.DocAsCode.MarkdownLite
 {
     using System.Collections.Immutable;
 
+    /// <summary>
+    /// A builder for create an instance of <see cref="IMarkdownEngine"/>
+    /// </summary>
     public class MarkdownEngineBuilder
     {
         public MarkdownEngineBuilder(Options options)
@@ -12,19 +15,40 @@ namespace Microsoft.DocAsCode.MarkdownLite
             Options = options;
         }
 
+        /// <summary>
+        /// The options.
+        /// </summary>
         public Options Options { get; }
 
+        /// <summary>
+        /// The block rules.
+        /// </summary>
         public ImmutableList<IMarkdownRule> BlockRules { get; set; } = ImmutableList<IMarkdownRule>.Empty;
 
+        /// <summary>
+        /// The inline rules.
+        /// </summary>
         public ImmutableList<IMarkdownRule> InlineRules { get; set; } = ImmutableList<IMarkdownRule>.Empty;
 
+        /// <summary>
+        /// The markdown token rewriter.
+        /// </summary>
         public IMarkdownRewriter Rewriter { get; set; }
 
+        /// <summary>
+        /// Create markdown paring context.
+        /// </summary>
+        /// <returns>a instance of <see cref="IMarkdownContext"/></returns>
         protected virtual IMarkdownContext CreateParseContext()
         {
             return new MarkdownBlockContext(BlockRules, new MarkdownInlineContext(InlineRules));
         }
 
+        /// <summary>
+        /// Create an instance of <see cref="IMarkdownEngine"/>
+        /// </summary>
+        /// <param name="renderer">the renderer.</param>
+        /// <returns>an instance of <see cref="IMarkdownEngine"/></returns>
         public virtual IMarkdownEngine CreateEngine(object renderer)
         {
             return new MarkdownEngine(CreateParseContext(), Rewriter, renderer, Options);
