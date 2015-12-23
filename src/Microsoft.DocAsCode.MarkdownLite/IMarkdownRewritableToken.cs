b3e648c1595888a6b8af5ca2bd6033082248cd98
vwxyzh <vwxyzh@gmail.com>
2015-12-23 16:51:41 +0800

    add comments.

--- src/Microsoft.DocAsCode.MarkdownLite/IMarkdownRewritableToken.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/IMarkdownRewritableToken.cs
@@ -3,8 +3,17 @@
 
 namespace Microsoft.DocAsCode.MarkdownLite
 {
+    /// <summary>
+    /// Markdown rewritable (for object contains <see cref="IMarkdownToken"/>).
+    /// </summary>
+    /// <typeparam name="T">The type of implement this interface.</typeparam>
     public interface IMarkdownRewritable<out T>
     {
-        T Rewrite(IMarkdownRewriteEngine rewriterEngine);
+        /// <summary>
+        /// Rewrite object with <see cref="IMarkdownRewriteEngine"/>
+        /// </summary>
+        /// <param name="rewriteEngine">The rewrite engine</param>
+        /// <returns>The rewritten object.</returns>
+        T Rewrite(IMarkdownRewriteEngine rewriteEngine);
     }
 }
