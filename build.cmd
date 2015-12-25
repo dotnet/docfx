c49e49bd9775b256e12d84f24b671c35f4ba2acb
lianwei <lianwei@microsoft.com>
2015-12-25 15:23:57 +0800

    make build.cmd clean

--- build.cmd
+++ build.cmd
@@ -17,7 +17,6 @@ IF NOT DEFINED VisualStudioVersion (
 
 :EnvSet
 SET BuildProj=%~dp0All.sln
-SET DocsProj=%~dp0Docs.sln
 SET Configuration=%1
 IF '%Configuration%'=='' (
     SET Configuration=Release
@@ -40,26 +39,11 @@ SET PATH=%PATH%;%LocalAppData%\NuGet
 
 :: Check if DNU exists globally
 :: DNU is OPTIONAL
-SET BuildDnxProjects=1
-WHERE dnvm >NUL
+WHERE dnu >NUL
 IF NOT '%ERRORLEVEL%'=='0' (
-    ECHO WARNING: DNU is not installed globally, DNX related projects will not be built!
-    SET BuildDnxProjects=0
-    SET BuildProj=%~dp0NonDnx.sln
-) ELSE (
-  WHERE dnu >NUL
-  IF NOT '!ERRORLEVEL!'=='0' (
-    ECHO WARNING: DNU is correctly set, please manually select DNU by running `dnvm list` and `dnvm use`
-    SET BuildDnxProjects=0
-    SET BuildProj=%~dp0NonDnx.sln
-  )
-)
-
-:: Check if node exists globally
-WHERE node >NUL
-
-IF NOT '%ERRORLEVEL%'=='0' (
-    ECHO ERROR: build.cmd requires node installed gloablly.
+    ECHO ERROR: DNU is not successfully configured.
+    ECHO ERROR: Please follow http://docs.asp.net/en/latest/getting-started/installing-on-windows.html#install-the-net-version-manager-dnvm to install dnvm.
+    ECHO ERROR: If dnvm is installed, use `dnvm list` to show available dnx runtime, and use `dnvm use` to select the default dnx runtime
     GOTO :Exit
 )
 
@@ -83,22 +67,6 @@ IF NOT '%ErrorLevel%'=='0' (
     GOTO :AfterBuild
 )
 
-CALL :BuildDocs %*
-IF NOT '%ErrorLevel%'=='0' (
-    GOTO :AfterBuild
-)
-
-:GenerateNuget
-PUSHD tools
-:: Install npm packages
-CALL npm install
-
-:: GRUNT to generate nuget packages
-CALL node node_modules/grunt-cli/bin/grunt --Configuration=%Configuration%
-
-IF NOT '%ErrorLevel%'=='0' (
-    SET NugetErrorLevel=1
-)
 POPD
 
 :AfterBuild
@@ -107,42 +75,25 @@ POPD
 ECHO.
 ECHO === BUILD RESULT ===
 findstr /ir /c:".*Warning(s)" /c:".*Error(s)" /c:"Time Elapsed.*" "%BuildLog%" & cd >nul
-IF '%BuildDnxProjects%'=='0' (
-   ECHO WARNING: DNX runtime is not successfully configured. Please follow http://docs.asp.net/en/latest/getting-started/installing-on-windows.html#install-the-net-version-manager-dnvm to install dnvm.
-)
 
 :: Pull xunit test result from the log file
 ECHO.
 ECHO === TEST EXECUTION SUMMARY ===
 findstr /ir /c:"Total:.*Failed.*Skipped.*Time.*" "%BuildLog%" & cd >nul
+
 ECHO Exit Code: %BuildErrorLevel%
 SET ERRORLEVEL=%BuildErrorLevel%
-
-:: Pull nuget package result
-IF '%NugetErrorLevel%'=='1' (
-    ECHO === GENERATING NUGET PACKAGES ===
-    ECHO ERROR: GENERATING NUGET PACKAGES FAILED, TRY RUNNING `grunt` UNDER `tools` FOLDER MANUALLY!
-    SET ERRORLEVEL=%NugetErrorLevel%
-)
 
 GOTO :Exit
 
 :Build
 %BuildPrefix% msbuild "%BuildProj%" /p:Configuration=%Configuration% /nologo /maxcpucount:1 /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%BuildLog%"; %BuildPostfix%
 SET BuildErrorLevel=%ERRORLEVEL%
-EXIT /B %BuildErrorLevel%
-
-:BuildDocs
-%BuildPrefix% msbuild "%DocsProj%" /p:Configuration=%Configuration% /nologo /maxcpucount:1 /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%BuildLog%";Append %BuildPostfix%
-SET DocsErrorLevel=%ERRORLEVEL%
-EXIT /B %DocsErrorLevel%
+EXIT /B %ERRORLEVEL%
 
 :RestorePackage
-:: Restore inside each subfolder
-IF '%BuildDnxProjects%'=='0' (
-GOTO :RestoreNormalPackage
-)
 
+:RestoreDnuPackage
 FOR /D %%x IN ("src","docs","test") DO (
 PUSHD %%x
 CMD /C dnu restore --parallel
