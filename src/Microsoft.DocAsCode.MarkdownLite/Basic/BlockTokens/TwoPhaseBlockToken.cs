975f6b6b522d9db7363f0d318bd19ca48b577c0c
vwxyzh <vwxyzh@gmail.com>
2016-01-19 21:45:43 -0800

    fix nolink and reflink in markdown.

---
+++ src/Microsoft.DocAsCode.MarkdownLite/Basic/BlockTokens/TwoPhaseBlockToken.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.MarkdownLite
+{
+    using System;
+
+    public sealed class TwoPhaseBlockToken : IMarkdownToken
+    {
+        private readonly Func<IMarkdownParser, TwoPhaseBlockToken, IMarkdownToken> _extractor;
+
+        public TwoPhaseBlockToken(
+            IMarkdownRule rule,
+            IMarkdownContext context,
+            string rawMarkdown,
+            Func<IMarkdownParser, TwoPhaseBlockToken, IMarkdownToken> extractor)
+        {
+            Rule = rule;
+            Context = context;
+            RawMarkdown = rawMarkdown;
+            _extractor = extractor;
+        }
+
+        public IMarkdownRule Rule { get; }
+
+        public IMarkdownContext Context { get; }
+
+        public string RawMarkdown { get; set; }
+
+        public IMarkdownToken Extract(IMarkdownParser parser)
+        {
+            var c = parser.SwitchContext(Context);
+            var result = _extractor(parser, this);
+            parser.SwitchContext(c);
+            return result;
+        }
+    }
+}
