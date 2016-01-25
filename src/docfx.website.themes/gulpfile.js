d2590e9df23dbb12b8dd623f8701cc678fd14170
lianwei <lianwei@microsoft.com>
2016-01-25 14:05:38 +0800

    Update gulpfile.js to be able to share files between templates and remove test.js, TODO add test function to docfx

--- src/docfx.website.themes/gulpfile.js
+++ src/docfx.website.themes/gulpfile.js
@@ -2,67 +2,111 @@ var path = require('path');
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 var minimist = require('minimist');
+var uniqueFiles = require('gulp-unique-files');
+var streamqueue = require('streamqueue');
 
 var knownOptions = {
-  string: 'dir',
-  default: { dir : '../docfx/Template/' }
+    string: 'dir',
+    default: { dir: '../docfx/Template/', t: '' }
 };
+
 var options = minimist(process.argv.slice(2), knownOptions);
+var files = [
+    "fonts/*",
+    "partials/*",
+    "styles/*",
+    "*.js",
+    "*.tmpl",
+    "favicon.ico",
+    "logo.svg"];
 
 var pack = {
-  "default": [
-    "fonts/*",
-    "partials/*",
-    "styles/*",
-    "*.js",
-    "*.tmpl",
-    "favicon.ico",
-    "logo.svg"
-  ],
-  "iframe.html": [
-    "fonts/*",
-    "partials/*",
-    "styles/*",
-    "*.js",
-    "*.tmpl",
-    "favicon.ico",
-    "logo.svg"
-  ],
-  "msdn.html": [
-    "partials/*",
-    "*.js",
-    "*.tmpl"
-  ],
-  "op.html": [
-    "partials/*",
-    "*.js",
-    "*.tmpl"
-  ],
-  "vs.html": [
-    "partials/*",
-    "*.js",
-    "*.tmpl"
-  ],
-  "docs.html": [
-    "partials/*",
-    "*.js",
-    "*.tmpl"
-  ]
+    "default": [
+        {
+            "files": files,
+        }
+    ],
+    "iframe.html": [
+        {
+            "files": files,
+            "cwd": "default",
+        },
+        {
+            "files": files, // Overrides the former one if file name is the same
+        }
+    ],
+    "msdn.html": [
+        {
+            "files": files
+        }
+    ],
+    "op.html": [
+        {
+            "files": files
+        }
+    ],
+    "vs.html": [
+        {
+            "files": files
+        }
+    ],
+    "docs.html": [
+        {
+            "files": files
+        }
+    ]
 };
 
+gulp.task('help', function () {
+    console.log("Usage:");
+    console.log("gulp pack [-t <templateName>] [-dir <outputFolder>]");
+});
+
 gulp.task('pack', function () {
-  var dirname = options.dir;
-  for (var key in pack) {
-    if (pack.hasOwnProperty(key)) {
-      var files = pack[key];
-      var filename = key + ".zip";
-      console.log("Pack files to " + dirname + filename);
-      gulp.src(files, { base: key, cwd: key })
-        .pipe(zip(filename))
-        .pipe(gulp.dest(dirname))
-      ;
+    var dirname = options.dir;
+    if (options.t) {
+        var name = options.t;
+        if (name) {
+            if (pack.hasOwnProperty(name)) {
+                packFiles(name, dirname);
+            }
+            else {
+                console.error("No folder with name " + name + " is found.");
+            }
+        }
+    } else {
+        for (var key in pack) {
+            if (pack.hasOwnProperty(key)) {
+                packFiles(key, dirname);
+            }
+        }
     }
-  }
+
+    function packFiles(key, dirname) {
+        var files = pack[key];
+        var filename = key + ".zip";
+        if (!files || files.length === 0) {
+            console.warn("no files to zip for " + key);
+            return;
+        }
+
+        console.log("Pack files to " + dirname + filename);
+        var streams = files.map(function (file) {
+            return getStream(file, key);
+        });
+        var stream = streamqueue.apply(this, [{ objectMode: true }].concat(streams));
+        stream
+            .pipe(uniqueFiles())
+            .pipe(zip(filename))
+            .pipe(gulp.dest(dirname))
+        ;
+    }
+
+    function getStream(file, key) {
+        var cwd = file.cwd || key;
+        var base = file.base || cwd;
+        return gulp.src(file.files, { base: base, cwd: cwd });
+    }
 });
 
 gulp.task('default', ['pack']);
