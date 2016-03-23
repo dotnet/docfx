d9388fcba6bf72bd0ddbd8356ca806909b0e6a32
vwxyzh <vwxyzh@gmail.com>
2016-03-23 00:52:52 -0700

    migrate build conceptual documents.

---
+++ src/Microsoft.DocAsCode.Build.Common/BaseDocumentBuildStep.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Common
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+
+    using Microsoft.DocAsCode.Plugins;
+
+    public abstract class BaseDocumentBuildStep : IDocumentBuildStep
+    {
+        public abstract string Name { get; }
+
+        public abstract int BuildOrder { get; }
+
+        public virtual IEnumerable<FileModel> Prebuild(ImmutableList<FileModel> models, IHostService host)
+        {
+            return models;
+        }
+
+        public virtual void Build(FileModel model, IHostService host)
+        {
+        }
+
+        public virtual void Postbuild(ImmutableList<FileModel> models, IHostService host)
+        {
+        }
+    }
+}
---
+++ src/Microsoft.DocAsCode.Build.ConceptualDocuments/ConceptualDocumentProcessor.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.ConceptualDocuments
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+
+    using Microsoft.DocAsCode.Build.Common;
+    using Microsoft.DocAsCode.DataContracts.Common;
+    using Microsoft.DocAsCode.Plugins;
+
+    [Export(typeof(IDocumentProcessor))]
+    public class ConceptualDocumentProcessor : DisposableDocumentProcessor
+    {
+        [ImportMany(nameof(ConceptualDocumentProcessor))]
+        public override IEnumerable<IDocumentBuildStep> BuildSteps { get; set; }
+
+        public override string Name => nameof(ConceptualDocumentProcessor);
+
+        public override ProcessingPriority GetProcessingPriority(FileAndType file)
+        {
+            if (file.Type != DocumentType.Article)
+            {
+                return ProcessingPriority.NotSupported;
+            }
+            if (".md".Equals(Path.GetExtension(file.File), StringComparison.OrdinalIgnoreCase))
+            {
+                return ProcessingPriority.Normal;
+            }
+            return ProcessingPriority.NotSupported;
+        }
+
+        public override FileModel Load(FileAndType file, ImmutableDictionary<string, object> metadata)
+        {
+            if (file.Type != DocumentType.Article)
+            {
+                throw new NotSupportedException();
+            }
+            var content = MarkdownReader.ReadMarkdownAsConceptual(file.BaseDir, file.File);
+            foreach (var item in metadata)
+            {
+                if (!content.ContainsKey(item.Key))
+                {
+                    content[item.Key] = item.Value;
+                }
+            }
+            return new FileModel(
+                file,
+                content,
+                serializer: new BinaryFormatter())
+            {
+                LocalPathFromRepoRoot = (content["source"] as SourceDetail)?.Remote?.RelativePath
+            };
+        }
+
+        public override SaveResult Save(FileModel model)
+        {
+            if (model.Type != DocumentType.Article)
+            {
+                throw new NotSupportedException();
+            }
+
+            var result = new SaveResult
+            {
+                DocumentType = model.DocumentType ?? "Conceptual",
+                ModelFile = model.File,
+                LinkToFiles = model.Properties.LinkToFiles,
+                LinkToUids = model.Properties.LinkToUids,
+            };
+            if (model.Properties.XrefSpec != null)
+            {
+                result.XRefSpecs = ImmutableArray.Create(model.Properties.XrefSpec);
+            }
+
+            return result;
+        }
+    }
+}
