9cf28bade5ed2ffeaa083fd5e4a907fa2a7b0efb
lianwei <lianwei@microsoft.com>
2016-03-31 15:51:43 +0800

    Fix merge condition

--- src/Microsoft.DocAsCode.Utility/Git/RepositoryWalker.cs
+++ src/Microsoft.DocAsCode.Utility/Git/RepositoryWalker.cs
@@ -38,61 +38,83 @@ namespace Microsoft.DocAsCode.Utility.Git
             }
         }
 
+        private CommitWrapper GetCommit(RevCommit currentRev)
+        {
+            return Map.GetOrAdd(currentRev, (s) =>
+            {
+                var commit = currentRev.AsCommit(_walker);
+                return new CommitWrapper
+                {
+                    Detail = CommitDetail.FromCommit(commit),
+                    TreeEntry = commit.TreeEntry,
+                    ParentCount = commit.ParentIds.Length
+                };
+            });
+        }
+
         private CommitDetail GetCommitDetailNoLock(string path)
         {
             // REset must be called according to https://github.com/henon/GitSharp/blob/master/GitSharp.Core/RevWalk/RevWalk.cs#L55
             _walker.reset();
             _walker.markStart(_initCommit);
             var currentRev = _walker.next(); // Use next to fetch the actual RevCommit
+            var currentCommit = GetCommit(currentRev);
+            var currentEntry = currentCommit.TreeEntry.FindBlobMember(path);
             while (currentRev != null)
             {
-                var currentCommit = Map.GetOrAdd(currentRev, (s) =>
-                {
-                    var commit = currentRev.AsCommit(_walker);
-                    return new CommitWrapper
-                    {
-                        Detail = CommitDetail.FromCommit(commit),
-                        TreeEntry = commit.TreeEntry,
-                        ParentCount = commit.ParentIds.Length
-                    };
-                });
-
-                currentRev = _walker.next();
-                var entry = currentCommit.TreeEntry.FindBlobMember(path);
-                if (entry == null)
+                if (currentEntry == null)
                 {
                     return null;
                 }
 
-                if (currentRev == null || currentCommit.ParentCount == 0)
+                if (currentCommit.ParentCount == 0)
                 {
                     return currentCommit.Detail;
                 }
 
                 if (currentCommit.ParentCount > 1)
                 {
-                    continue;
-                }
-
-                var parentCommit = Map.GetOrAdd(currentRev, (s) =>
-                {
-                    var commit = currentRev.AsCommit(_walker);
-                    return new CommitWrapper
+                    // For commit that contains multiple parents, e.g. when merge takes place, find the previous commit with only one parent that contains the same file
+                    var parentRev = _walker.next();
+                    TreeEntry parentEntry = null;
+                    CommitWrapper parentCommit = null;
+                    while (parentRev != null)
                     {
-                        Detail = CommitDetail.FromCommit(commit),
-                        TreeEntry = commit.TreeEntry,
-                        ParentCount = commit.ParentIds.Length
-                    };
-                });
-                var parentEntry = parentCommit.TreeEntry.FindBlobMember(path);
-                if (parentEntry == null)
-                {
-                    return currentCommit.Detail;
-                }
+                        parentCommit = GetCommit(parentRev);
+                        parentEntry = parentCommit.TreeEntry.FindBlobMember(path);
+                        if (parentEntry == null || parentEntry.Id != currentEntry.Id)
+                        {
+                            parentRev = _walker.next();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
 
-                if (entry.Id != parentEntry.Id)
+                    if (parentRev == null)
+                    {
+                        return currentCommit.Detail;
+                    }
+
+                    currentCommit = parentCommit;
+                    currentRev = parentRev;
+                    currentEntry = parentEntry;
+                }
+                else
                 {
-                    return parentCommit.Detail;
+                    var parentRev = _walker.next();
+
+                    var parentCommit = GetCommit(parentRev);
+                    var parentEntry = parentCommit.TreeEntry.FindBlobMember(path);
+                    if (parentEntry == null || currentEntry.Id != parentEntry.Id)
+                    {
+                        return currentCommit.Detail;
+                    }
+
+                    currentCommit = parentCommit;
+                    currentRev = parentRev;
+                    currentEntry = parentEntry;
                 }
             }
 
