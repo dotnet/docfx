72ac81ae8ae8d9da8b59160cda251fa8fa96120e
xuzho <xuzho@microsoft.com>
2016-04-07 21:09:44 +0800

    #215 support other triple slash comments

--- src/Microsoft.DocAsCode.Metadata.ManagedReference/Parsers/TripleSlashCommentModel.cs
+++ src/Microsoft.DocAsCode.Metadata.ManagedReference/Parsers/TripleSlashCommentModel.cs
@@ -24,6 +24,7 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference
         private static Regex CommentIdRegex = new Regex(@"^(?<type>N|T|M|P|F|E):(?<id>" + idSelector + ")$", RegexOptions.Compiled);
 
         private readonly ITripleSlashCommentParserContext _context;
+        private readonly TripleSlashCommentTransformer _transformer;
 
         public string Summary { get; private set; }
         public string Remarks { get; private set; }
@@ -39,6 +40,10 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference
         {
             // Normalize xml line ending before load into xml
             XDocument doc = XDocument.Parse(xml, LoadOptions.SetLineInfo | LoadOptions.PreserveWhitespace);
+            // Transform triple slash comment
+            _transformer = new TripleSlashCommentTransformer(this.GetType().Assembly, "Transform", "TripleSlashCommentTransform.xsl");
+            doc = _transformer.Transform(doc);
+
             _context = context;
             if (!context.PreserveRawInlineComments)
             {
---
+++ src/Microsoft.DocAsCode.Metadata.ManagedReference/Parsers/TripleSlashCommentTransformer.cs
@@ -0,0 +1,43 @@
+﻿namespace Microsoft.DocAsCode.Metadata.ManagedReference
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.XPath;
+    using System.Xml.Xsl;
+
+    public class TripleSlashCommentTransformer
+    {
+        private readonly XslCompiledTransform _transform;
+
+        public TripleSlashCommentTransformer(Assembly assembly, string dir, string xsltPath)
+        {
+            if (assembly == null)
+            {
+                throw new ArgumentNullException("assembly");
+            }
+            var resourceName = $"{assembly.GetName().Name}.{dir}.{xsltPath}";
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = XmlReader.Create(stream))
+            {
+                var xsltSettings = new XsltSettings(true, true);
+                _transform = new XslCompiledTransform();
+                _transform.Load(reader, xsltSettings, new XmlUrlResolver());
+            }
+
+        }
+
+        public XDocument Transform(XDocument doc)
+        {
+            using (var ms = new MemoryStream())
+            using (var writer = XmlWriter.Create(ms))
+            {
+                _transform.Transform(doc.CreateNavigator(), writer);
+                ms.Seek(0, SeekOrigin.Begin);
+                return XDocument.Load(ms, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+            }
+        }
+    }
+}
