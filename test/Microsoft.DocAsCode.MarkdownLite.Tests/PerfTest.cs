e074b9c81bb2e1ab765795732164ed8876aa7c29
Qinen Zhu <qinzhu@microsoft.com>
2016-05-07 11:52:52 +0800

    Update unit test

--- test/Microsoft.DocAsCode.MarkdownLite.Tests/PerfTest.cs
+++ test/Microsoft.DocAsCode.MarkdownLite.Tests/PerfTest.cs
@@ -4,6 +4,7 @@
 namespace Microsoft.DocAsCode.MarkdownLite.Tests
 {
     using System.Linq;
+    using System.Collections.Generic;
     using System.Text.RegularExpressions;
 
     using Microsoft.DocAsCode.MarkdownLite;
@@ -49,9 +50,9 @@ Numbered list:
 2. oranges
 3. pears
 ";
-            const string expected = @"<h1 id=""heading"">Heading</h1>
-<h2 id=""sub-heading"">Sub-heading</h2>
-<h3 id=""another-deeper-heading"">Another deeper heading</h3>
+            const string expectedTemplate = @"<h1 id=""heading{0}"">Heading</h1>
+<h2 id=""sub-heading{0}"">Sub-heading</h2>
+<h3 id=""another-deeper-heading{0}"">Another deeper heading</h3>
 <p>Paragraphs are separated
 by a blank line.</p>
 <p>Leave 2 spaces at the end of a line to do a<br>line break</p>
@@ -74,13 +75,15 @@ by a blank line.</p>
 
             var builder = new GfmEngineBuilder(new Options());
             var source1000 = string.Concat(Enumerable.Repeat(source, 1000));
-            var expectedArray = Enumerable.Repeat(expected.Replace("\r\n", "\n"), 1000).ToArray();
-            var rewriteIdRegex = new Regex(@"(id=)""([\p{L}\p{Nd}-]+)""", RegexOptions.Compiled);
-            for (var i = 1; i < expectedArray.Length; i++)
+            var template = Regex.Replace(expectedTemplate, "\r?\n", "\n");
+            var first = string.Format(template, "");
+            var expectedList = new List<string> { first };
+            for (var i = 1; i < 1000; i++)
             {
-                expectedArray[i] = rewriteIdRegex.Replace(expectedArray[i], string.Concat(@"$1""$2-", i - 1, @""""));
+                var content = string.Concat("-", i - 1);
+                expectedList.Add(string.Format(template, content));
             }
-            var expected1000 = string.Concat(expectedArray);
+            var expected1000 = string.Concat(expectedList);
             var engine = builder.CreateEngine(new HtmlRenderer());
             for (int i = 0; i < 2; i++)
             {
