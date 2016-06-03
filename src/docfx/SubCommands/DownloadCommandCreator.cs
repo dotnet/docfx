c046dc0914d530a3c3eaee56f81d735c4d830253
vwxyzh <vwxyzh@gmail.com>
2016-06-03 15:17:28 +0800

    add download subcommand for xref. (#351)
    * add download subcommand for xref.
    * modify help text.

---
+++ src/docfx/SubCommands/DownloadCommandCreator.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.SubCommands
+{
+    using Microsoft.DocAsCode;
+    using Microsoft.DocAsCode.Plugins;
+
+    [CommandOption("download", "Download remote xref map file and create an xref archive in local.")]
+    internal sealed class DownloadCommandCreator : CommandCreator<DownloadCommandOptions, DownloadCommand>
+    {
+        public override DownloadCommand CreateCommand(DownloadCommandOptions options, ISubCommandController controller)
+        {
+            return new DownloadCommand(options);
+        }
+    }
+}
