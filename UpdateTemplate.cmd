85130d0933e3ce3c0393b7adedf38fa84c0a4ea9
Qinen Zhu <qinzhu@microsoft.com>
2016-06-13 14:47:08 +0800

    Add step install gulp globally

--- UpdateTemplate.cmd
+++ UpdateTemplate.cmd
@@ -14,6 +14,8 @@ IF NOT '%ERRORLEVEL%'=='0' (
 SET TemplateHome=%~dp0src\docfx.website.themes\
 SET DefaultTemplate=%TemplateHome%default
 
+CALL npm install -g gulp
+
 PUSHD %DefaultTemplate%
 CALL npm install
 CALL gulp
