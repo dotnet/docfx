6ef3bf95ecd44f88b84b488582b3c3beeae51ffa
Qinen Zhu <qinzhu@microsoft.com>
2016-06-13 12:12:44 +0800

    Auto pack template in build.cmd

---
+++ UpdateTemplate.cmd
@@ -0,0 +1,25 @@
+@ECHO OFF
+PUSHD %~dp0
+
+SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Check if node exists globally
+WHERE node >NUL
+IF NOT '%ERRORLEVEL%'=='0' (
+    ECHO ERROR: UpdateTemplate.cmd requires node installed globally.
+    GOTO :Exit
+)
+
+SET TemplateHome=%~dp0src\docfx.website.themes\
+SET DefaultTemplate=%TemplateHome%default
+
+PUSHD %DefaultTemplate%
+CALL npm install
+CALL gulp
+POPD
+
+PUSHD %TemplateHome%
+CALL npm install
+CALL gulp
+POPD
