6d8ee190ce05f2ab177e020ac862354f324dfa8b
Huang Xueyuan <hxyjessie@gmail.com>
2016-06-24 10:23:46 +0800

    Fix issue 359 - unescape definition in swagger (#422)
    * Fix issue 359 - unescape definition in swagger
    
    * For json pointer, decode for uri format and not decode for string representation

---
+++ src/Microsoft.DocAsCode.Build.RestApi/RestApiHelper.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+namespace Microsoft.DocAsCode.Build.RestApi
+{
+    using System;
+
+    using Microsoft.DocAsCode.Utility;
+
+    public static class RestApiHelper
+    {
+        /// <summary>
+        /// Reverse to reference unescape described in http://tools.ietf.org/html/rfc6901#section-4
+        /// </summary>
+        /// <param name="reference"></param>
+        /// <returns></returns>
+        public static string FormatDefinitionSinglePath(string reference)
+        {
+            return reference.Replace("~", "~0").Replace("/", "~1");
+        }
+
+        /// <summary>
+        /// When the reference starts with '#/', treat it as URI Fragment Identifier Representation and decode.
+        /// When the reference starts with '/', treat it as JSON String Representation and keep it as.
+        /// Refer to: https://tools.ietf.org/html/rfc6901#section-5
+        /// </summary>
+        /// <param name="reference"></param>
+        /// <returns></returns>
+        public static string FormatReferenceFullPath(string reference)
+        {
+            // Decode for URI Fragment Identifier Representation
+            if (reference.StartsWith("#/"))
+            {
+                // Reuse relative path, to decode the values inside '/'.
+                var path = reference.Substring(2);
+                return "/" + ((RelativePath)path).UrlDecode();
+            }
+
+            // Not decode for JSON String Representation
+            if (reference.StartsWith("/"))
+            {
+                return reference;
+            }
+
+            throw new InvalidOperationException($"Full reference path \"{reference}\" must start with '/' or '#/'");
+        }
+    }
+}
