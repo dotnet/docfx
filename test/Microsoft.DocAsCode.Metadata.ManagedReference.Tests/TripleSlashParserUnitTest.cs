8acac9148ebe228e2c93255b4a7fcd66a4ed3bde
xuzho <xuzho@microsoft.com>
2016-06-27 15:34:35 +0800

    fix issue 428

--- test/Microsoft.DocAsCode.Metadata.ManagedReference.Tests/TripleSlashParserUnitTest.cs
+++ test/Microsoft.DocAsCode.Metadata.ManagedReference.Tests/TripleSlashParserUnitTest.cs
@@ -119,7 +119,7 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference.Tests
 <pre><code class=""c#"">public class XmlElement
     : XmlLinkedNode</code></pre>
 <ol><li>
-            word inside list->listItem->list->listItem->para.>
+            word inside list-&gt;listItem-&gt;list-&gt;listItem-&gt;para.&gt;
             the second line.
 </li><li>item2 in numbered list</li></ol>
 </li><li>item2 in bullet list</li></ul>
