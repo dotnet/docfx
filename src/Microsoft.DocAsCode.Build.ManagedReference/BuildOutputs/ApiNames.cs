bf1fca9f912b9a80bb804ece729d8174ea02f84a
Renze Yu <renzeyu@microsoft.com>
2016-06-29 11:52:40 +0800

    fix typo in json viewmodel (#440)

--- src/Microsoft.DocAsCode.Build.ManagedReference/BuildOutputs/ApiNames.cs
+++ src/Microsoft.DocAsCode.Build.ManagedReference/BuildOutputs/ApiNames.cs
@@ -24,9 +24,9 @@ namespace Microsoft.DocAsCode.Build.ManagedReference.BuildOutputs
         [JsonProperty("name")]
         public List<ApiLanguageValuePair> Name { get; set; }
 
-        [YamlMember(Alias = "nameWithTpye")]
-        [JsonProperty("nameWithTpye")]
-        public List<ApiLanguageValuePair> NameWithTpye { get; set; }
+        [YamlMember(Alias = "nameWithType")]
+        [JsonProperty("nameWithType")]
+        public List<ApiLanguageValuePair> NameWithType { get; set; }
 
         [YamlMember(Alias = "fullName")]
         [JsonProperty("fullName")]
