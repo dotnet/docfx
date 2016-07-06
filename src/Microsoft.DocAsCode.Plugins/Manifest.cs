286d1dbcf6d5c5ad2ed818fbdff3a11a586cfd43
Qinen Zhu <qinzhu@microsoft.com>
2016-07-06 10:39:20 +0800

    Move class Manifest and HomepageInfo to Plugin

--- src/Microsoft.DocAsCode.Plugins/Manifest.cs
+++ src/Microsoft.DocAsCode.Plugins/Manifest.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.Build.Engine
+namespace Microsoft.DocAsCode.Plugins
 {
     using System.Collections.Generic;
-
-    using Microsoft.DocAsCode.Plugins;
 
     using Newtonsoft.Json;
     using YamlDotNet.Serialization;
