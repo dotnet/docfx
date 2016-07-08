e21280fd81d84c9c17bade363805227c6a694e89
vwxyzh <vwxyzh@gmail.com>
2016-07-07 20:28:41 -0700

    check arguments.

--- src/Microsoft.DocAsCode.Common/YamlMime.cs
+++ src/Microsoft.DocAsCode.Common/YamlMime.cs
@@ -3,27 +3,36 @@
 
 namespace Microsoft.DocAsCode.Common
 {
+    using System;
     using System.IO;
 
     public static class YamlMime
     {
-        public const string ManagedReference = nameof(ManagedReference);
-        public const string TableOfContent = nameof(TableOfContent);
-        public const string XRefMap = nameof(XRefMap);
+        public const string ManagedReference = nameof(YamlMime) + ":" + nameof(ManagedReference);
+        public const string TableOfContent = nameof(YamlMime) + ":" + nameof(TableOfContent);
+        public const string XRefMap = nameof(YamlMime) + ":" + nameof(XRefMap);
 
         public static string ReadMime(TextReader reader)
         {
+            if (reader == null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
             var line = reader.ReadLine();
             if (!line.StartsWith("#"))
             {
                 return null;
             }
-            return line.TrimStart('#', ' ');
+            return line.TrimStart('#').TrimStart(' ');
         }
 
 #if !NetCore
         public static string ReadMime(string file)
         {
+            if (file == null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
             using (var reader = File.OpenText(file))
             {
                 return ReadMime(reader);
