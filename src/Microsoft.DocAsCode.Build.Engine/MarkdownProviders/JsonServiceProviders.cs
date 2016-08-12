25314a06625781176ede5cd362be68a9b0a642bf
t-jipe@microsoft.com <t-jipe@microsoft.com>
2016-08-12 12:04:56 +0800

    JsonRenderer and JsonServiceProvider

---
+++ src/Microsoft.DocAsCode.Build.Engine/MarkdownProviders/JsonServiceProviders.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Engine.MarkdownProviders
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Composition;
+
+    using Microsoft.DocAsCode.Dfm;
+    using Microsoft.DocAsCode.MarkdownLite;
+    using Microsoft.DocAsCode.Plugins;
+
+    [Export("json", typeof(IMarkdownServiceProvider))]
+    public class JsonServiceProviders
+    {
+        public IMarkdownService CreateMarkdownService(MarkdownServiceParameters parameters)
+        {
+            return new JsonRenderService(
+                parameters.BasePath,
+                parameters.Tokens,
+                MarkdownTokenTreeValidatorFactory.Combine(TokenTreeValidator));
+        }
+
+        [ImportMany]
+        public IEnumerable<IMarkdownTokenTreeValidator> TokenTreeValidator { get; set; }
+
+        private sealed class JsonRenderService : IMarkdownService
+        {
+            private readonly DfmEngineBuilder _builder;
+
+            private readonly ImmutableDictionary<string, string> _tokens;
+
+            public JsonRenderService(string baseDir, ImmutableDictionary<string, string> tokens, IMarkdownTokenTreeValidator tokenTreeValidator)
+            {
+                _builder = DocfxFlavoredMarked.CreateBuilder(baseDir);
+                _builder.TokenTreeValidator = tokenTreeValidator;
+                _tokens = tokens;
+            }
+
+            public MarkupResult Markup(string src, string path)
+            {
+                var dependency = new HashSet<string>();
+                var json = _builder.CreateDfmEngine(new JsonRenderer() { Tokens = _tokens }).Markup(src, path, dependency);
+                var result = new MarkupResult
+                {
+                    // I think it should be rename 
+                    Html = json,
+                };
+                if (dependency.Count > 0)
+                {
+                    result.Dependency = dependency.ToImmutableArray();
+                }
+                return result;
+            }
+        }
+    }
+}
