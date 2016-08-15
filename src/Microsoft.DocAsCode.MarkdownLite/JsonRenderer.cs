f053139cc6b316553477e8aa3b96669d1404913b
t-jipe@microsoft.com <t-jipe@microsoft.com>
2016-08-15 13:15:54 +0800

    Jsonrenderer and test

---
+++ src/Microsoft.DocAsCode.MarkdownLite/JsonRenderer.cs
@@ -0,0 +1,305 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.MarkdownLite
+{
+    using System.Collections.Immutable;
+
+    public class JsonRenderer
+    {
+        private enum Type
+        {
+            Size,
+            Child
+        }
+
+        public ImmutableDictionary<string, string> Tokens { get; set; }
+
+        #region Block
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownNewLineBlockToken token, MarkdownBlockContext context)
+        {
+            return this.Insert("NewLine", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownCodeBlockToken token, MarkdownBlockContext context)
+        {
+            return this.Insert("Code(" + token.Lang + ")", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownHeadingBlockToken token, MarkdownBlockContext context)
+        {
+            string level = token.Depth.ToString();
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content.Tokens)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Heading" + level, childContent, Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownHrBlockToken token, MarkdownBlockContext context)
+        {
+            return this.Insert("Hr", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownBlockquoteBlockToken token, MarkdownBlockContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Tokens)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Blockquote", childContent, Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownListBlockToken token, MarkdownBlockContext context)
+        {
+            var type = token.Ordered ? "ol" : "ul";
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Tokens)
+            {
+                childContent += Render(renderer, (MarkdownListItemBlockToken) item);
+            }
+            return this.Insert(type, childContent, Type.Child);
+        }
+
+        protected virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownListItemBlockToken token)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Tokens)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("li", childContent, Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownHtmlBlockToken token, MarkdownBlockContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content.Tokens)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Html", childContent.ToString(), Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownParagraphBlockToken token, MarkdownBlockContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.InlineTokens.Tokens)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Paragraph", childContent, Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownTextToken token, MarkdownBlockContext context)
+        {
+            return this.Insert("Text:" + token.Content, this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownTableBlockToken token, MarkdownBlockContext context)
+        {
+            var content = StringBuffer.Empty;
+
+            // header
+            var headerContent = StringBuffer.Empty;
+            for (int i = 0; i < token.Header.Length; i++)
+            {
+                var childContent = StringBuffer.Empty;
+                foreach (var item in token.Header[i].Content.Tokens)
+                {
+                    childContent += renderer.Render(item);
+                }
+                headerContent += this.Insert("headerItem", childContent.ToString(), Type.Child);
+            }
+            content += this.Insert("Header", headerContent.ToString(), Type.Child);
+
+            // Body
+            var bodyContent = StringBuffer.Empty;
+            for (int i = 0; i < token.Cells.Length; i++)
+            {
+                var rowContent = StringBuffer.Empty;
+                var row = token.Cells[i];
+                for (int j = 0; j < row.Length; j++)
+                {
+                    var childContent = StringBuffer.Empty;
+                    foreach (var item in row[j].Content.Tokens)
+                    {
+                        childContent += renderer.Render(item);
+                    }
+                    rowContent += this.Insert("RowItem", childContent.ToString(), Type.Child);
+                }
+                bodyContent += this.Insert("Row", rowContent.ToString(), Type.Child);
+            }
+            content += this.Insert("Body", bodyContent.ToString(), Type.Child);
+            return this.Insert("Table", content.ToString(), Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownNonParagraphBlockToken token, MarkdownBlockContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content.Tokens)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("NonParagraph", childContent.ToString(), Type.Child);
+        }
+
+        #endregion
+
+        #region Inline
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownEscapeInlineToken token, MarkdownInlineContext context)
+        {
+            return this.Insert("Escape:" + token.Content, this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownLinkInlineToken token, MarkdownInlineContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Link", childContent.ToString(), Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownImageInlineToken token, MarkdownInlineContext context)
+        {
+            return this.Insert("Image", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownStrongInlineToken token, MarkdownInlineContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Strong", childContent.ToString(), Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownEmInlineToken token, MarkdownInlineContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Em", childContent.ToString(), Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownCodeInlineToken token, MarkdownInlineContext context)
+        {
+            return this.Insert("InLineCode", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, GfmDelInlineToken token, MarkdownInlineContext context)
+        {
+            var childContent = StringBuffer.Empty;
+            foreach (var item in token.Content)
+            {
+                childContent += renderer.Render(item);
+            }
+            return this.Insert("Del", childContent.ToString(), Type.Child);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownTagInlineToken token, MarkdownInlineContext context)
+        {
+            return this.Insert("Tag", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownBrInlineToken token, MarkdownInlineContext context)
+        {
+            return this.Insert("Br", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownTextToken token, MarkdownInlineContext context)
+        {
+            return this.Insert("InLineText(" + token.Content.Replace("\n", "\\n") + ")", this.GetSize(), Type.Size);
+        }
+
+        #endregion
+
+        #region Misc
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownIgnoreToken token, IMarkdownContext context)
+        {
+            return this.Insert("Ignore", this.GetSize(), Type.Size);
+        }
+
+        public virtual StringBuffer Render(IMarkdownRenderer renderer, MarkdownRawToken token, IMarkdownContext context)
+        {
+            switch (token.Rule.Name)
+            {
+                case ("Html"):
+                {
+                    return this.Insert("Raw(FromHtml)", this.GetSize(), Type.Size);
+                }
+                case ("Inline.AutoLink"):
+                {
+                    return this.Insert("Raw(FromInLineAutoLink)", this.GetSize(), Type.Size);
+                }
+                case ("Inline.CodeElement"):
+                {
+                    return this.Insert("Raw(FromInLineCodeElement)", this.GetSize(), Type.Size);
+                }
+                case ("Inline.Comment"):
+                {
+                    return this.Insert("Raw(FromInLineComment)", this.GetSize(), Type.Size);
+                }
+                case ("GfmHtmlComment"):
+                {
+                    return this.Insert("Raw(FromGfmHtmlComment)", this.GetSize(), Type.Size);
+                }
+                case ("Inline.Gfm.Url"):
+                {
+                    return this.Insert("Raw(FromGfmUrl)", this.GetSize(), Type.Size);
+                }
+                default:
+                    return this.Insert("Raw", this.GetSize(), Type.Size);
+            }
+        }
+
+        #endregion
+
+        #region Private
+
+        private StringBuffer Insert(string name, string tokenContent, Type tokenType)
+        {
+            var result = (StringBuffer) $"{{\"name\":\"{name}\"";
+            switch (tokenType)
+            {
+                case Type.Child:
+                {
+                    // If tokenContent is not empty ,should remove the last character(',')
+                    if (tokenContent.Length > 0)
+                    {
+                        tokenContent = tokenContent.Remove(tokenContent.Length - 1);
+                    }
+                    result += $",\"children\":[{tokenContent}]}},";
+                    break;
+                }
+                case Type.Size:
+                {
+                    // size will be needed later
+                    // result += "\"size\":" + tokenContent + "\n},";
+                    result += "},";
+                    break;
+                }
+                default:
+                    return StringBuffer.Empty;
+            }
+            return result;
+        }
+
+        private string GetSize()
+        {
+            return "1";
+        }
+
+        #endregion
+    }
+}
---
+++ test/Microsoft.DocAsCode.MarkdownLite.Tests/JsonRendererTest.cs
@@ -0,0 +1,133 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.MarkdownLite.Tests
+{
+    using Microsoft.DocAsCode.Build.Engine;
+    using Microsoft.DocAsCode.Plugins;
+
+    using Xunit;
+
+    public class JsonRendererTest
+    {
+        public string JsonRenderer(string content)
+        {
+            JsonServiceProvider jsonServiceProvider = new JsonServiceProvider();
+            IMarkdownService jsonService = jsonServiceProvider.CreateMarkdownService(new MarkdownServiceParameters());
+            return jsonService.Markup(content, null).Html;
+        }
+
+        [Fact]
+        [Trait("Related", "Markdown")]
+        public void TestTable_WithEmptyCell()
+        {
+            var source = @"# hello
+|  Name |  Type |  Notes |  Read/Write |  Description |
+|:-------|:-------|:-------|:-------|:-------|
+| value | Edm.String |  |  |
+| endDate | Edm.DateTime |  |  | The date and time at which the password expires. |
+| value | Edm.String |  |  |  |
+";
+            var expected =
+                "{\"name\":\"markdown\",\"children\":[{\"name\":\"Heading1\",\"children\":[{\"name\":\"InLineText(hello)\"}]},{\"name\":\"Table\",\"children\":[{\"name\":\"Header\",\"children\":[{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(Name)\"}]},{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(Type)\"}]},{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(Notes)\"}]},{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(Read/Write)\"}]},{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(Description)\"}]}]},{\"name\":\"Body\",\"children\":[{\"name\":\"Row\",\"children\":[{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(value)\"}]},{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(Edm.String)\"}]},{\"name\":\"RowItem\",\"children\":[]},{\"name\":\"RowItem\",\"children\":[]},{\"name\":\"RowItem\",\"children\":[]}]},{\"name\":\"Row\",\"children\":[{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(endDate)\"}]},{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(Edm.DateTime)\"}]},{\"name\":\"RowItem\",\"children\":[]},{\"name\":\"RowItem\",\"children\":[]},{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(The date and time at which the password expires.)\"}]}]},{\"name\":\"Row\",\"children\":[{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(value)\"}]},{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(Edm.String)\"}]},{\"name\":\"RowItem\",\"children\":[]},{\"name\":\"RowItem\",\"children\":[]},{\"name\":\"RowItem\",\"children\":[]}]}]}]}]}";
+
+            Assert.Equal(expected, JsonRenderer(source));
+        }
+
+        [Fact]
+        [Trait("Related", "Markdown")]
+        public void TestGfmBuilder_CommentRuleShouldBeforeAutoLink()
+        {
+            var source = @"<!--
+https://en.wikipedia.org/wiki/Draft:Microsoft_SQL_Server_Libraries/Drivers
+-->";
+            var expected =
+                "{\"name\":\"markdown\",\"children\":[{\"name\":\"Html\",\"children\":[{\"name\":\"Raw(FromGfmHtmlComment)\"}]}]}";
+            Assert.Equal(expected.Replace("\r\n", "\n"), JsonRenderer(source));
+        }
+
+        [Fact]
+        [Trait("Related", "Markdown")]
+        public void TestGfmBuilder_CodeTag()
+        {
+            var source = @"<pre><code>//*************************************************
+        // Test!
+        //*************************************************</code></pre>
+";
+            var expected =
+                "{\"name\":\"markdown\",\"children\":[{\"name\":\"Html\",\"children\":[{\"name\":\"Raw(FromHtml)\"}]}]}";
+            Assert.Equal(expected.Replace("\r\n", "\n"), JsonRenderer(source));
+        }
+
+        [Fact]
+        [Trait("Related", "Markdown")]
+        public void TestTable_WithRefLink()
+        {
+            var source = @"# Test table
+| header-1 | header-2 | header-3 |
+|:-------- |:--------:| --------:|
+| *1-1* | [User] | test |
+
+[User]: ./entity-and-complex-type-reference.md#UserEntity";
+
+            var expected =
+                "{\"name\":\"markdown\",\"children\":[{\"name\":\"Heading1\",\"children\":[{\"name\":\"InLineText(Test table)\"}]},{\"name\":\"Table\",\"children\":[{\"name\":\"Header\",\"children\":[{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(header-1)\"}]},{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(header-2)\"}]},{\"name\":\"headerItem\",\"children\":[{\"name\":\"InLineText(header-3)\"}]}]},{\"name\":\"Body\",\"children\":[{\"name\":\"Row\",\"children\":[{\"name\":\"RowItem\",\"children\":[{\"name\":\"Em\",\"children\":[{\"name\":\"InLineText(1-1)\"}]}]},{\"name\":\"RowItem\",\"children\":[{\"name\":\"Link\",\"children\":[{\"name\":\"InLineText(User)\"}]}]},{\"name\":\"RowItem\",\"children\":[{\"name\":\"InLineText(test)\"}]}]}]}]},{\"name\":\"Ignore\"}]}";
+            Assert.Equal(expected.Replace("\r\n", "\n"), JsonRenderer(source));
+        }
+
+        [Fact]
+        [Trait("Related", "Markdown")]
+        public void TestGfmImageLink_WithSpecialCharactorsInAltText()
+        {
+            var source = @"![This is image alt text with quotation ' and double quotation ""hello"" world](girl.png)";
+
+            var expected =
+                "{\"name\":\"markdown\",\"children\":[{\"name\":\"Paragraph\",\"children\":[{\"name\":\"Image\"}]}]}";
+            Assert.Equal(expected.Replace("\r\n", "\n"), JsonRenderer(source));
+        }
+
+        [Fact]
+        [Trait("Related", "Markdown")]
+        public void TestPref()
+        {
+            var source = @"Heading
+=======
+ 
+Sub-heading
+-----------
+  
+### Another deeper heading
+  
+Paragraphs are separated
+by a blank line.
+ 
+Leave 2 spaces at the end of a line to do a  
+line break
+ 
+Text attributes *italic*, **bold**, 
+`monospace`
+
+~~strikethrough~~
+ 
+A [link](http://example.com).
+
+Shopping list
+ 
+* apples
+* oranges
+* pears
+ 
+Numbered list
+ 
+1. apples
+2. oranges
+3. pears
+
+<address@example.com>
+";
+            var expected =
+                "{\"name\":\"markdown\",\"children\":[{\"name\":\"Heading1\",\"children\":[{\"name\":\"InLineText(Heading)\"}]},{\"name\":\"Heading2\",\"children\":[{\"name\":\"InLineText(Sub-heading)\"}]},{\"name\":\"Heading3\",\"children\":[{\"name\":\"InLineText(Another deeper heading)\"}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"InLineText(Paragraphs are separated\\nby a blank line.)\"}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"InLineText(Leave 2 spaces at the end of a line to do a)\"},{\"name\":\"Br\"},{\"name\":\"InLineText(line break)\"}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"InLineText(Text attributes )\"},{\"name\":\"Em\",\"children\":[{\"name\":\"InLineText(italic)\"}]},{\"name\":\"InLineText(, )\"},{\"name\":\"Strong\",\"children\":[{\"name\":\"InLineText(bold)\"}]},{\"name\":\"InLineText(, \\n)\"},{\"name\":\"InLineCode\"}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"Del\",\"children\":[{\"name\":\"InLineText(strikethrough)\"}]}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"InLineText(A )\"},{\"name\":\"Link\",\"children\":[{\"name\":\"InLineText(link)\"}]},{\"name\":\"InLineText(.)\"}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"InLineText(Shopping list)\"}]},{\"name\":\"ul\",\"children\":[{\"name\":\"li\",\"children\":[{\"name\":\"NonParagraph\",\"children\":[{\"name\":\"InLineText(apples)\"}]}]},{\"name\":\"li\",\"children\":[{\"name\":\"NonParagraph\",\"children\":[{\"name\":\"InLineText(oranges)\"}]}]},{\"name\":\"li\",\"children\":[{\"name\":\"NonParagraph\",\"children\":[{\"name\":\"InLineText(pears)\"}]}]}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"InLineText(Numbered list)\"}]},{\"name\":\"ol\",\"children\":[{\"name\":\"li\",\"children\":[{\"name\":\"NonParagraph\",\"children\":[{\"name\":\"InLineText(apples)\"}]}]},{\"name\":\"li\",\"children\":[{\"name\":\"NonParagraph\",\"children\":[{\"name\":\"InLineText(oranges)\"}]}]},{\"name\":\"li\",\"children\":[{\"name\":\"NonParagraph\",\"children\":[{\"name\":\"InLineText(pears)\"}]}]}]},{\"name\":\"Paragraph\",\"children\":[{\"name\":\"Link\",\"children\":[{\"name\":\"Raw(FromInLineAutoLink)\"}]}]}]}";
+            Assert.Equal(expected.Replace("\r\n", "\n"), JsonRenderer(source));
+        }
+    }
+}
