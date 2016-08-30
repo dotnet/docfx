c5bea1a3b7a5effcfa5903a419f0f1495fae5ab8
t-jipe@microsoft.com <t-jipe@microsoft.com>
2016-08-30 14:17:16 +0800

    add validLineCount in sourceinfo of token

--- src/Microsoft.DocAsCode.MarkdownLite/SourceInfo.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/SourceInfo.cs
@@ -5,11 +5,12 @@ namespace Microsoft.DocAsCode.MarkdownLite
 {
     public struct SourceInfo
     {
-        private SourceInfo(string markdown, string file, int lineNumber)
+        private SourceInfo(string markdown, string file, int lineNumber, int validLineCount)
         {
             Markdown = markdown;
             File = file;
             LineNumber = lineNumber;
+            ValidLineCount = validLineCount;
         }
 
         public string Markdown { get; }
@@ -18,14 +19,33 @@ namespace Microsoft.DocAsCode.MarkdownLite
 
         public int LineNumber { get; }
 
+        public int ValidLineCount { get; }
+
         public static SourceInfo Create(string markdown, string file, int lineNumber = 1)
         {
-            return new SourceInfo(markdown, file, lineNumber);
+            return new SourceInfo(markdown, file, lineNumber, GetValidLineCount(markdown));
         }
 
         public SourceInfo Copy(string markdown, int lineOffset = 0)
         {
-            return new SourceInfo(markdown, File, LineNumber + lineOffset);
+            return new SourceInfo(markdown, File, LineNumber + lineOffset, GetValidLineCount(markdown) + lineOffset);
+        }
+
+        private static int GetValidLineCount(string markdown)
+        {
+            var indexOfLastChar = markdown.Length - 1;
+            var validLineCount = 0;
+
+            while (indexOfLastChar >= 0 && markdown[indexOfLastChar] == '\n')
+                indexOfLastChar--;
+
+            for (var i = indexOfLastChar - 1; i >= 0; i--)
+            {
+                if (markdown[i] == '\n')
+                    validLineCount++;
+            }
+
+            return validLineCount;
         }
     }
 }
