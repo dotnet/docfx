ecb81bf786c685c46756f5c6ca82e1ca7965070e
vwxyzh <vwxyzh@gmail.com>
2016-09-01 19:20:59 -0700

    fix bugs in markdown table. (#658)

--- src/Microsoft.DocAsCode.MarkdownLite/Tables/MarkdownTableBlockRule.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/Tables/MarkdownTableBlockRule.cs
@@ -20,22 +20,45 @@ namespace Microsoft.DocAsCode.MarkdownLite
             {
                 return null;
             }
-            var sourceInfo = context.Consume(match.Length);
             var header = match.Groups[1].Value.ReplaceRegex(Regexes.Lexers.UselessTableHeader, string.Empty).SplitRegex(Regexes.Lexers.TableSplitter);
             var align = ParseAligns(match.Groups[2].Value.ReplaceRegex(Regexes.Lexers.UselessTableAlign, string.Empty).SplitRegex(Regexes.Lexers.TableSplitter));
-            var cells = match.Groups[3].Value.ReplaceRegex(Regexes.Lexers.UselessGfmTableCell, string.Empty).Split('\n').Select(x => new string[] { x }).ToArray();
-            for (int i = 0; i < cells.Length; i++)
+            if (header.Length > align.Length)
             {
-                cells[i] = cells[i][0]
+                return null;
+            }
+            var sourceInfo = context.Consume(match.Length);
+
+            var rows = match.Groups[3].Value.ReplaceRegex(Regexes.Lexers.UselessGfmTableCell, string.Empty).Split('\n').Select(x => x).ToArray();
+            var cells = new string[rows.Length][];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                var columns = rows[i]
                   .ReplaceRegex(Regexes.Lexers.EmptyGfmTableCell, string.Empty)
                   .SplitRegex(Regexes.Lexers.TableSplitter);
-
-                var cellList = cells[i].ToList();
-                while (cellList.Count < header.Length)
+                if (columns.Length == header.Length)
                 {
-                    cellList.Add(string.Empty);
+                    cells[i] = columns;
                 }
-                cells[i] = cellList.ToArray();
+                else if (columns.Length < header.Length)
+                {
+                    cells[i] = new string[header.Length];
+                    for (int j = 0; j < columns.Length; j++)
+                    {
+                        cells[i][j] = columns[j];
+                    }
+                    for (int j = columns.Length; j < cells[i].Length; j++)
+                    {
+                        cells[i][j] = string.Empty;
+                    }
+                }
+                else // columns.Length > header.Length
+                {
+                    cells[i] = new string[header.Length];
+                    for (int j = 0; j < header.Length; j++)
+                    {
+                        cells[i][j] = columns[j];
+                    }
+                }
             }
 
             return new TwoPhaseBlockToken(
