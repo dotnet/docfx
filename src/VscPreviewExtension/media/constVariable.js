6ab74fd7eda6358ce8999c39d09b0a3d10df85a0
Gizmos <928PJY@users.noreply.github.com>
2016-09-28 09:36:08 +0800

    add meida files (#693)
    * add meida files
    
    * use npm package instead of local file
    
    * add some dependencies
    
    * fix comments
    
    * fix comments
    
    * fix comments

---
+++ src/VscPreviewExtension/media/constVariable.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+const minHorizontalSpace = 6;
+const maxHorizontalSpace = 12;
+const selectedGold = "#EDE68A";
+const unSelectedGray = "#d4d4d4";
