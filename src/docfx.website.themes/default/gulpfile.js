c574a911be300c2b05d0fff9d3afcf8a578595f7
Qinen Zhu <qinezh@users.noreply.github.com>
2016-10-11 14:45:34 +0800

    Use mark.js to highlight keywords (#770)

--- src/docfx.website.themes/default/gulpfile.js
+++ src/docfx.website.themes/default/gulpfile.js
@@ -15,7 +15,8 @@ var vendor = {
        'bower_components/highlightjs/highlight.pack.min.js',
        'bower_components/lunr.js/lunr.min.js',
        'bower_components/js-url/url.min.js',
-       'bower_components/twbs-pagination/jquery.twbsPagination.min.js'
+       'bower_components/twbs-pagination/jquery.twbsPagination.min.js',
+       "bower_components/mark.js/dist/jquery.mark.min.js"
       ],
   webWorker: {
     src: ['lunr.min.js'],
