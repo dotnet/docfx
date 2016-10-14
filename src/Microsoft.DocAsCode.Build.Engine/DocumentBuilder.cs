ccd1946c24a3868bbec09136cec2c51e7d167375
Qinen Zhu <qinzhu@microsoft.com>
2016-10-14 11:40:28 +0800

    Resolve comments

--- src/Microsoft.DocAsCode.Build.Engine/DocumentBuilder.cs
+++ src/Microsoft.DocAsCode.Build.Engine/DocumentBuilder.cs
@@ -179,12 +179,12 @@ namespace Microsoft.DocAsCode.Build.Engine
             foreach (var duplicates in (from m in manifestItems
                                         from output in m.OutputFiles.Values
                                         let relativePath = output?.RelativePath
-                                        select new { item = m, relativeath = relativePath })
-                              .GroupBy(obj => obj.relativeath, FilePathComparer.OSPlatformSensitiveStringComparer)
+                                        select new { item = m, relativePath = relativePath })
+                              .GroupBy(obj => obj.relativePath, FilePathComparer.OSPlatformSensitiveStringComparer)
                               .Where(g => g.Count() > 1))
             {
-                Logger.LogWarning($"Overwrite occurs while input files \"{string.Join(", ", duplicates.Select(duplicate => duplicate.relativeath))}\" writing to the same output file \"{duplicates.Key}\"");
-                itemsToRemove.UnionWith(duplicates.Skip(1).Select(duplicate => duplicate.relativeath));
+                Logger.LogWarning($"Overwrite occurs while input files \"{string.Join(", ", duplicates.Select(duplicate => duplicate.item.SourceRelativePath))}\" writing to the same output file \"{duplicates.Key}\"");
+                itemsToRemove.UnionWith(duplicates.Skip(1).Select(duplicate => duplicate.item.SourceRelativePath));
             }
             manifestItems.RemoveAll(m => itemsToRemove.Contains(m.SourceRelativePath));
         }
