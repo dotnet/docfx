0ad125a1371d7f539a61b78206263b0fe6620cb9
Renze Yu <renzeyu@microsoft.com>
2016-10-21 11:41:26 +0800

    displayProperty/altProperty needn't be encoded

--- src/Microsoft.DocAsCode.Build.Engine/XrefDetails.cs
+++ src/Microsoft.DocAsCode.Build.Engine/XrefDetails.cs
@@ -71,8 +71,8 @@ namespace Microsoft.DocAsCode.Build.Engine
             }
 
             xref.InnerHtml = node.InnerHtml;
-            xref.DisplayProperty = node.GetAttributeValue("displayProperty", StringHelper.HtmlEncode(queryString?.Get("displayProperty")) ?? XRefSpec.NameKey);
-            xref.AltProperty = node.GetAttributeValue("altProperty", StringHelper.HtmlEncode(queryString?.Get("altProperty")) ?? "fullname");
+            xref.DisplayProperty = node.GetAttributeValue("displayProperty", queryString?.Get("displayProperty") ?? XRefSpec.NameKey);
+            xref.AltProperty = node.GetAttributeValue("altProperty", queryString?.Get("altProperty") ?? "fullname");
             xref.Text = node.GetAttributeValue("text", node.GetAttributeValue("name", StringHelper.HtmlEncode(queryString?.Get("text"))));
             xref.Alt = node.GetAttributeValue("alt", node.GetAttributeValue("fullname", StringHelper.HtmlEncode(queryString?.Get("alt"))));
 
