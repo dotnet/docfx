17c63eb56a2195beaa1bb4a9ec8830d8fbb10485
Renze Yu <renzeyu@microsoft.com>
2016-10-21 17:09:30 +0800

    separate default template for easy customization

--- src/docfx.website.themes/default/ManagedReference.html.primary.js
+++ src/docfx.website.themes/default/ManagedReference.html.primary.js
@@ -1,14 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-var mrefCommon = require('./ManagedReference.common.js');
+var core = require('./ManagedReference.html.primary.core.js');
 
 exports.transform = function (model) {
-  model = mrefCommon.transform(model);
-  if (model.type.toLowerCase() === "enum") {
-    model.isClass = false;
-    model.isEnum = true;
-  }
-
-  model._disableToc = model._disableToc || !model._tocPath || (model._navPath === model._tocPath);
+  model = core.transform(model);
   return {item: model};
 }
--- src/docfx.website.themes/default/conceptual.html.primary.core.js
+++ src/docfx.website.themes/default/conceptual.html.primary.core.js
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+var common = require('./common.js');
 
-var mrefCommon = require('./ManagedReference.common.js');
-
-exports.transform = function (model)  {
-  model = mrefCommon.transform(model);
+exports.transform = function (model) {
   model._disableToc = model._disableToc || !model._tocPath || (model._navPath === model._tocPath);
-  return {item: model};
+  model.docurl = model.docurl || common.getViewSourceHref(model, model._gitContribute, model._gitUrlPattern);
+  return model;
 }
