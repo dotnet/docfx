479420929a397e58210645e4f87cbcc564000a37
vwxyzh <vwxyzh@gmail.com>
2016-10-24 10:41:42 +0800

    add html post processor. (#810)

---
+++ src/Microsoft.DocAsCode.Build.Common/IHtmlDocumentHandler.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Common
+{
+    using HtmlAgilityPack;
+
+    using Microsoft.DocAsCode.Plugins;
+
+    internal interface IHtmlDocumentHandler
+    {
+        void Handle(HtmlDocument document, ManifestItem manifestItem, string inputFile, string outputFile);
+        Manifest Complete(Manifest manifest);
+    }
+}
