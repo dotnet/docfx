2b05231022b5978600b8535a8409921a1bf4e12b
vwxyzh <vwxyzh@gmail.com>
2016-10-24 13:43:54 +0800

    update html post processor. (#812)

--- src/Microsoft.DocAsCode.Build.Engine/HtmlPostProcessor.cs
+++ src/Microsoft.DocAsCode.Build.Engine/HtmlPostProcessor.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.Build.Common
+namespace Microsoft.DocAsCode.Build.Engine
 {
     using System;
     using System.Collections.Generic;
@@ -12,8 +12,9 @@ namespace Microsoft.DocAsCode.Build.Common
 
     using HtmlAgilityPack;
 
-    using Microsoft.DocAsCode.Plugins;
     using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Build.Common;
+    using Microsoft.DocAsCode.Plugins;
 
     internal sealed class HtmlPostProcessor : IPostProcessor
     {
@@ -34,40 +35,44 @@ namespace Microsoft.DocAsCode.Build.Common
             {
                 throw new ArgumentNullException("Base directory can not be null");
             }
+            foreach (var handler in Handlers)
+            {
+                manifest = handler.PreHandle(manifest);
+            }
             foreach (var tuple in from item in manifest.Files ?? Enumerable.Empty<ManifestItem>()
                                   from output in item.OutputFiles
                                   where output.Key.Equals(".html", StringComparison.OrdinalIgnoreCase)
                                   select new
                                   {
-                                      RelativePath = output.Value.RelativePath,
-                                      SrcRelativePath = item.SourceRelativePath,
                                       Item = item,
+                                      InputFile = item.SourceRelativePath,
+                                      OutputFile = output.Value.RelativePath,
                                   })
             {
-                var filePath = Path.Combine(outputFolder, tuple.RelativePath);
-                var document = new HtmlDocument();
-
-                if (File.Exists(filePath))
+                var filePath = Path.Combine(outputFolder, tuple.OutputFile);
+                if (!File.Exists(filePath))
                 {
-                    try
-                    {
-                        document.Load(filePath, Encoding.UTF8);
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.LogWarning($"Warning: Can't load content from {filePath}: {ex.Message}");
-                        continue;
-                    }
-                    foreach (var handler in Handlers)
-                    {
-                        handler.Handle(document, tuple.Item, tuple.SrcRelativePath, tuple.RelativePath);
-                    }
-                    document.Save(filePath, Encoding.UTF8);
+                    continue;
                 }
+                var document = new HtmlDocument();
+                try
+                {
+                    document.Load(filePath, Encoding.UTF8);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogWarning($"Warning: Can't load content from {filePath}: {ex.Message}");
+                    continue;
+                }
+                foreach (var handler in Handlers)
+                {
+                    handler.Handle(document, tuple.Item, tuple.InputFile, tuple.OutputFile);
+                }
+                document.Save(filePath, Encoding.UTF8);
             }
             foreach (var handler in Handlers)
             {
-                manifest = handler.Complete(manifest);
+                manifest = handler.PostHandle(manifest);
             }
             return manifest;
         }
