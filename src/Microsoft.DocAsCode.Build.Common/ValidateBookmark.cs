1c9df55b119f1b791539a02038df0729aec91128
vwxyzh <vwxyzh@gmail.com>
2016-10-24 19:50:51 -0700

    rename variables.

--- src/Microsoft.DocAsCode.Build.Common/ValidateBookmark.cs
+++ src/Microsoft.DocAsCode.Build.Common/ValidateBookmark.cs
@@ -16,26 +16,26 @@ namespace Microsoft.DocAsCode.Build.Common
 
     public class ValidateBookmark : IHtmlDocumentHandler
     {
-        private static readonly string XpathTemplate = "//*/@{0}";
+        private static readonly string XPathTemplate = "//*/@{0}";
         private static readonly HashSet<string> WhiteList = new HashSet<string> { "top" };
-        private Dictionary<string, HashSet<string>> registeredBookmarks;
-        private Dictionary<string, List<Tuple<string, string>>> bookmarks;
-        private Dictionary<string, string> fileMapping;
+        private Dictionary<string, HashSet<string>> _registeredBookmarks;
+        private Dictionary<string, List<Tuple<string, string>>> _bookmarks;
+        private Dictionary<string, string> _fileMapping;
 
         #region IHtmlDocumentHandler members
 
         public Manifest PreHandle(Manifest manifest)
         {
-            registeredBookmarks = new Dictionary<string, HashSet<string>>(FilePathComparer.OSPlatformSensitiveStringComparer);
-            bookmarks = new Dictionary<string, List<Tuple<string, string>>>(FilePathComparer.OSPlatformSensitiveStringComparer);
-            fileMapping = new Dictionary<string, string>(FilePathComparer.OSPlatformSensitiveStringComparer);
+            _registeredBookmarks = new Dictionary<string, HashSet<string>>(FilePathComparer.OSPlatformSensitiveStringComparer);
+            _bookmarks = new Dictionary<string, List<Tuple<string, string>>>(FilePathComparer.OSPlatformSensitiveStringComparer);
+            _fileMapping = new Dictionary<string, string>(FilePathComparer.OSPlatformSensitiveStringComparer);
             return manifest;
         }
 
         public void Handle(HtmlDocument document, ManifestItem manifestItem, string inputFile, string outputFile)
         {
-            fileMapping[outputFile] = inputFile;
-            bookmarks[outputFile] =
+            _fileMapping[outputFile] = inputFile;
+            _bookmarks[outputFile] =
                 (from link in GetNodeAttribute(document, "src").Concat(GetNodeAttribute(document, "href"))
                  let index = link.IndexOf("#")
                  where index != -1 && PathUtility.IsRelativePath(link)
@@ -43,12 +43,12 @@ namespace Microsoft.DocAsCode.Build.Common
                  where !WhiteList.Contains(pair.Item2)
                  select pair).ToList();
             var anchors = GetNodeAttribute(document, "id").Concat(GetNodeAttribute(document, "name"));
-            registeredBookmarks[outputFile] = new HashSet<string>(anchors);
+            _registeredBookmarks[outputFile] = new HashSet<string>(anchors);
         }
 
         public Manifest PostHandle(Manifest manifest)
         {
-            foreach (var item in bookmarks)
+            foreach (var item in _bookmarks)
             {
                 string path = item.Key;
                 foreach (var b in item.Value)
@@ -56,10 +56,10 @@ namespace Microsoft.DocAsCode.Build.Common
                     string linkedToFile = b.Item1 == string.Empty ? path : b.Item1;
                     string anchor = b.Item2;
                     HashSet<string> anchors;
-                    if (registeredBookmarks.TryGetValue(linkedToFile, out anchors) && !anchors.Contains(anchor))
+                    if (_registeredBookmarks.TryGetValue(linkedToFile, out anchors) && !anchors.Contains(anchor))
                     {
-                        string currentFileSrc = fileMapping[path];
-                        string linkedToFileSrc = fileMapping[linkedToFile];
+                        string currentFileSrc = _fileMapping[path];
+                        string linkedToFileSrc = _fileMapping[linkedToFile];
                         Logger.LogWarning($"Output file {path} which is built from src file {currentFileSrc} contains illegal link {linkedToFile}#{anchor}: the file {linkedToFile} which is built from src {linkedToFileSrc} doesn't contain a bookmark named {anchor}.");
                     }
                 }
@@ -71,7 +71,7 @@ namespace Microsoft.DocAsCode.Build.Common
 
         private static IEnumerable<string> GetNodeAttribute(HtmlDocument html, string attribute)
         {
-            var nodes = html.DocumentNode.SelectNodes(string.Format(XpathTemplate, attribute));
+            var nodes = html.DocumentNode.SelectNodes(string.Format(XPathTemplate, attribute));
             if (nodes == null)
             {
                 return Enumerable.Empty<string>();
