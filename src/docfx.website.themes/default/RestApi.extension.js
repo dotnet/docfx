e292cd36e46b9b22628c8281eb97ff9a4803db02
Renze Yu <renzeyu@microsoft.com>
2016-10-25 11:47:31 +0800

    add doc for template preprocessor extension

--- src/docfx.website.themes/default/RestApi.extension.js
+++ src/docfx.website.themes/default/RestApi.extension.js
@@ -1,9 +1,15 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+/**
+ * This method will be called at the start of exports.transform in RestApi.html.primary.js
+ */
 exports.preSteps = function (model) {
   return model;
 }
 
+/**
+ * This method will be called at the end of exports.transform in RestApi.html.primary.js
+ */
 exports.postSteps = function (model) {
   return model;
 }
