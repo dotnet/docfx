599a090d0a53f4aa18acf06b87b35f0cb37bbe19
xuzho <xuzho@microsoft.com>
2016-11-03 18:23:55 +0800

    minor fix case

--- test/Microsoft.DocAsCode.Build.Engine.Tests/ValidateBookmarkTest.cs
+++ test/Microsoft.DocAsCode.Build.Engine.Tests/ValidateBookmarkTest.cs
@@ -35,7 +35,7 @@ namespace Microsoft.DocAsCode.Build.Engine.Tests
             };
 
             File.WriteAllText(Path.Combine(outputFolder, "a.html"), @"<a href='http://bing.com#top'>Microsoft Bing</a> <p id='b1'>section</p><a href='#b1'/>");
-            File.WriteAllText(Path.Combine(outputFolder, "b.html"), @"<a href='a.html#b1' sourceFile='a.md' sourceStartLineNumber='1'>bookmark existed</a><a href='a.html#b2' sourceFile='a.md' sourceStartLineNumber='1'>link with source info</a> <a href='a.html#b3' sourceFile='token.md' sourceStartLineNumber='1'>link in token file</a><a href='a.html#b4'>link without source info</a>");
+            File.WriteAllText(Path.Combine(outputFolder, "b.html"), @"<a href='a.html#b1' sourceFile='b.md' sourceStartLineNumber='1'>bookmark existed</a><a href='a.html#b2' sourceFile='b.md' sourceStartLineNumber='1'>link with source info</a> <a href='a.html#b3' sourceFile='token.md' sourceStartLineNumber='1'>link in token file</a><a href='a.html#b4'>link without source info</a>");
 
             Logger.RegisterListener(_listener);
             using (new LoggerPhaseScope("validate_bookmark"))
@@ -51,7 +51,7 @@ namespace Microsoft.DocAsCode.Build.Engine.Tests
             Assert.Equal(3, logs.Count);
             var expected = new[]
             {
-                @"a.md contains illegal link: a.md#b2. Link text is `link with source info`. The file a.md doesn't contain a bookmark named b2.",
+                @"b.md contains illegal link: a.md#b2. Link text is `link with source info`. The file a.md doesn't contain a bookmark named b2.",
                 @"token.md contains illegal link: a.md#b3. Link text is `link in token file`. The file a.md doesn't contain a bookmark named b3.",
                 @"b.html contains illegal link: a.html#b4. Link text is `link without source info`. The file a.html doesn't contain a bookmark named b4, please check the src file b.md and src linkedTo file a.md or the template you applied."
             };
