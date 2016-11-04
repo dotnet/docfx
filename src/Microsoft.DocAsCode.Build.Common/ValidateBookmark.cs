ca42882dee4b3a16ffab07495246ebf9f2296f11
xuzho <xuzho@microsoft.com>
2016-11-04 16:57:23 +0800

    resolve comments

--- src/Microsoft.DocAsCode.Build.Common/ValidateBookmark.cs
+++ src/Microsoft.DocAsCode.Build.Common/ValidateBookmark.cs
@@ -39,14 +39,14 @@ namespace Microsoft.DocAsCode.Build.Common
             // RFC 3986: relative-ref = relative-part [ "?" query ] [ "#" fragment ]
             _linksWithBookmark[outputFile] =
                 (from node in GetNodesWithAttribute(document, "href")
-                 let link = HttpUtility.UrlDecode(node.GetAttributeValue("href", null))
-                 let title = node.InnerText
+                 let link = node.GetAttributeValue("href", null)
                  let bookmarkIndex = link.IndexOf("#")
-                 where bookmarkIndex != -1 && PathUtility.IsRelativePath(link)
+                 where bookmarkIndex != -1
+                 let bookmark = link.Substring(bookmarkIndex + 1)
                  let index = link.IndexOfAny(new[] { '?', '#' })
-                 select new LinkItem { Title = title, Href = TransformPath(outputFile, link.Remove(index)), Bookmark = link.Substring(bookmarkIndex + 1), SourceFile = node.GetAttributeValue("sourceFile", null), SourceLineNumber = node.GetAttributeValue("sourceStartLineNumber", 0), TargetLineNumber = node.Line } into item
-                 where !WhiteList.Contains(item.Bookmark)
-                 select item).ToList();
+                 let decodedLink = HttpUtility.UrlDecode(link.Remove(index))
+                 where !WhiteList.Contains(bookmark) && PathUtility.IsRelativePath(decodedLink)
+                 select new LinkItem { Title = node.InnerText, Href = TransformPath(outputFile, decodedLink), Bookmark = bookmark, SourceFile = node.GetAttributeValue("sourceFile", null), SourceLineNumber = node.GetAttributeValue("sourceStartLineNumber", 0), TargetLineNumber = node.Line }).ToList();
             var anchors = GetNodeAttribute(document, "id").Concat(GetNodeAttribute(document, "name"));
             _registeredBookmarks[outputFile] = new HashSet<string>(anchors);
         }
