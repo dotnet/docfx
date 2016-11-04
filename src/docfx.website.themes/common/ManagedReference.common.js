a35f303a82d79e706d70bc378f6e8782df099d29
Renze Yu <renzeyu@microsoft.com>
2016-11-04 17:44:42 +0800

    fix comments

--- src/docfx.website.themes/common/ManagedReference.common.js
+++ src/docfx.website.themes/common/ManagedReference.common.js
@@ -63,6 +63,8 @@ exports.getBookmarks = function (model)  {
       bookmarks[item.uid] = common.getHtmlId(item.uid);
     });
   }
+
+  return bookmarks;
 }
 
 function groupChildren(model, typeChildrenItems) {
