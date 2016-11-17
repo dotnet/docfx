677e9b2127d63d2c69b7fc2b324bfc271887c5f4
Feng Xu <fenxu@microsoft.com>
2016-11-17 13:53:30 +0800

    Video token should be only parsed when it's in blockquote (#903)

--- src/Microsoft.DocAsCode.Dfm/Rules/DfmVideoBlockRule.cs
+++ src/Microsoft.DocAsCode.Dfm/Rules/DfmVideoBlockRule.cs
@@ -16,6 +16,11 @@ namespace Microsoft.DocAsCode.Dfm
 
         public IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
         {
+            if (!parser.Context.Variables.ContainsKey(MarkdownBlockContext.IsBlockQuote) || !(bool)parser.Context.Variables[MarkdownBlockContext.IsBlockQuote])
+            {
+                return null;
+            }
+
             var match = VideoRegex.Match(context.CurrentMarkdown);
             if (match.Length == 0)
             {
