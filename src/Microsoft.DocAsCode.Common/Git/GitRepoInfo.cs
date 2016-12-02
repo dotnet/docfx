83e2d511ae9f892a12750127ddbf714059277850
Qinen Zhu <qinezh@users.noreply.github.com>
2016-12-02 13:43:23 +0800

    Remove unneeded step in GitUtility (#946)

--- src/Microsoft.DocAsCode.Common/Git/GitRepoInfo.cs
+++ src/Microsoft.DocAsCode.Common/Git/GitRepoInfo.cs
@@ -16,14 +16,6 @@ namespace Microsoft.DocAsCode.Common.Git
 
         public string RemoteOriginUrl { get; set; }
 
-        public string RawRemoteOriginUrl { get; set; }
-
-        public RepoType Type { get; set; }
-
-        public string Account { get; set; }
-
-        public string RemoteRepoName { get; set; }
-
         public string RemoteHeadCommitId { get; set; }
 
         public string LocalHeadCommitId { get; set; }
