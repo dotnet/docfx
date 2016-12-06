85f91d12de181d0f60251bc4c43512d282a3536e
jehuan <jehuan@microsoft.com>
2016-12-06 14:37:11 +0800

    Add case and fix comments

--- src/Microsoft.DocAsCode.Common/EntityMergers/JObjectMerger.cs
+++ src/Microsoft.DocAsCode.Common/EntityMergers/JObjectMerger.cs
@@ -17,47 +17,43 @@ namespace Microsoft.DocAsCode.Common.EntityMergers
 
         public override void Merge(ref object source, object overrides, Type type, IMergeContext context)
         {
-            var stopMerge = false;
             var sourceJObj = source as JObject;
-            if (sourceJObj != null)
+            if (sourceJObj != null && type == typeof(object))
             {
                 var overridesDict = overrides as Dictionary<object, object>;
                 if (overridesDict != null)
                 {
-                    foreach (var or in overridesDict)
-                    {
-                        var strKey = or.Key as string;
-                        if (strKey != null)
-                        {
-                            Merge(sourceJObj, strKey, or.Value, context);
+                    Merge(sourceJObj, overridesDict, context);
 
-                            // Stop merging since already merged by context merger
-                            stopMerge = true;
-                        }
-                    }
+                    // Stop merging since already merged by context merger
+                    return;
                 }
             }
 
-            if (!stopMerge)
-            {
-                base.Merge(ref source, overrides, type, context);
-            }
+            base.Merge(ref source, overrides, type, context);
         }
 
-        private static void Merge(JObject source, string overrideKey, object overrideValue, IMergeContext context)
+        private static void Merge(JObject source, Dictionary<object, object> overridesDict, IMergeContext context)
         {
-            object obj;
-            JToken jToken;
-            if (source.TryGetValue(overrideKey, out jToken))
+            foreach (var or in overridesDict)
             {
-                obj = jToken;
-                context.Merger.Merge(ref obj, overrideValue, typeof(object), context);
+                var overrideKey = or.Key as string;
+                if (overrideKey != null)
+                {
+                    object obj;
+                    JToken jToken;
+                    if (source.TryGetValue(overrideKey, out jToken))
+                    {
+                        obj = jToken;
+                        context.Merger.Merge(ref obj, or.Value, typeof(object), context);
+                    }
+                    else
+                    {
+                        obj = or.Value;
+                    }
+                    source[overrideKey] = JToken.FromObject(obj);
+                }
             }
-            else
-            {
-                obj = overrideValue;
-            }
-            source[overrideKey] = JToken.FromObject(obj);
         }
     }
 }
