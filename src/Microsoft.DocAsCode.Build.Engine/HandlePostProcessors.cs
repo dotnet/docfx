53cdd7917cbf8db5fec133cdc8d2f3ea75195066
jehuan <jehuan@microsoft.com>
2016-12-26 14:32:52 +0800

    Extract post processor logic out of document builder

---
+++ src/Microsoft.DocAsCode.Build.Engine/HandlePostProcessors.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Composition.Hosting;
+
+    using Microsoft.DocAsCode.Build.Common;
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Exceptions;
+    using Microsoft.DocAsCode.Plugins;
+
+    public class HandlePostProcessors : IDisposable
+    {
+        private readonly List<PostProcessor> _postProcessors;
+
+        public HandlePostProcessors(CompositionHost container, ImmutableArray<string> postProcessorNames)
+        {
+            _postProcessors = GetPostProcessor(container, postProcessorNames);
+        }
+
+        public void PrepareMetadata(ImmutableDictionary<string, object> metadata)
+        {
+            foreach (var postProcessor in _postProcessors)
+            {
+                using (new LoggerPhaseScope($"Prepare metadata in post processor {postProcessor.ContractName}", false))
+                using (new PerformanceScope($"Prepare metadata in post processor {postProcessor.ContractName}", LogLevel.Verbose))
+                {
+                    metadata = postProcessor.Processor.PrepareMetadata(metadata);
+                    if (metadata == null)
+                    {
+                        throw new DocfxException($"Plugin {postProcessor.ContractName} should not return null metadata");
+                    }
+                }
+            }
+        }
+
+        public void PostProcess(Manifest manifest, string outputFolder)
+        {
+            // post process
+            foreach (var postProcessor in _postProcessors)
+            {
+                using (new LoggerPhaseScope($"Process in post processor {postProcessor.ContractName}", false))
+                using (new PerformanceScope($"Process in post processor {postProcessor.ContractName}"))
+                {
+                    manifest = postProcessor.Processor.Process(manifest, outputFolder);
+                    if (manifest == null)
+                    {
+                        throw new DocfxException($"Plugin {postProcessor.ContractName} should not return null manifest");
+                    }
+
+                    // To make sure post processor won't generate duplicate output files
+                    ManifestUtility.RemoveDuplicateOutputFiles(manifest.Files);
+                }
+            }
+        }
+
+        private static List<PostProcessor> GetPostProcessor(CompositionHost container, ImmutableArray<string> processors)
+        {
+            var processorList = new List<PostProcessor>();
+            AddBuildInPostProcessor(processorList);
+            foreach (var processor in processors)
+            {
+                var p = CompositionUtility.GetExport<IPostProcessor>(container, processor);
+                if (p != null)
+                {
+                    processorList.Add(new PostProcessor
+                    {
+                        ContractName = processor,
+                        Processor = p
+                    });
+                    Logger.LogInfo($"Post processor {processor} loaded.");
+                }
+                else
+                {
+                    Logger.LogWarning($"Can't find the post processor: {processor}");
+                }
+            }
+            return processorList;
+        }
+
+        private static void AddBuildInPostProcessor(List<PostProcessor> processorList)
+        {
+            processorList.Add(
+                new PostProcessor
+                {
+                    ContractName = "html",
+                    Processor = new HtmlPostProcessor
+                    {
+                        Handlers =
+                        {
+                            new ValidateBookmark(),
+                            new RemoveDebugInfo(),
+                        },
+                    }
+                });
+        }
+
+        private sealed class PostProcessor
+        {
+            public string ContractName { get; set; }
+            public IPostProcessor Processor { get; set; }
+        }
+
+        public void Dispose()
+        {
+            foreach (var processor in _postProcessors)
+            {
+                Logger.LogVerbose($"Disposing processor {processor.ContractName} ...");
+                (processor.Processor as IDisposable)?.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Microsoft.DocAsCode.Common/ManifestUtility.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.DocAsCode.Plugins;
+
+    public static class ManifestUtility
+    {
+        public static void RemoveDuplicateOutputFiles(List<ManifestItem> manifestItems)
+        {
+            if (manifestItems == null)
+            {
+                throw new ArgumentNullException(nameof(manifestItems));
+            }
+
+            var itemsToRemove = new HashSet<string>();
+            foreach (var duplicates in (from m in manifestItems
+                                        from output in m.OutputFiles.Values
+                                        let relativePath = output?.RelativePath
+                                        select new { item = m, relativePath })
+                              .GroupBy(obj => obj.relativePath, FilePathComparer.OSPlatformSensitiveStringComparer)
+                              .Where(g => g.Count() > 1))
+            {
+                Logger.LogWarning($"Overwrite occurs while input files \"{string.Join(", ", duplicates.Select(duplicate => duplicate.item.SourceRelativePath))}\" writing to the same output file \"{duplicates.Key}\"");
+                itemsToRemove.UnionWith(duplicates.Skip(1).Select(duplicate => duplicate.item.SourceRelativePath));
+            }
+            manifestItems.RemoveAll(m => itemsToRemove.Contains(m.SourceRelativePath));
+        }
+
+        public static Manifest MergeManifest(List<Manifest> manifests)
+        {
+            if (manifests == null)
+            {
+                throw new ArgumentNullException(nameof(manifests));
+            }
+
+            var xrefMaps = (from manifest in manifests
+                            where manifest.XRefMap != null
+                            select manifest.XRefMap).ToList();
+            var incrementalInfos = (from manifest in manifests
+                                    from i in manifest.IncrementalInfo ?? Enumerable.Empty<IncrementalInfo>()
+                                    select i).ToList();
+            return new Manifest
+            {
+                Homepages = (from manifest in manifests
+                             from homepage in manifest.Homepages ?? Enumerable.Empty<HomepageInfo>()
+                             select homepage).Distinct().ToList(),
+                Files = (from manifest in manifests
+                         from file in manifest.Files ?? Enumerable.Empty<ManifestItem>()
+                         select file).Distinct().ToList(),
+                XRefMap = xrefMaps.Count <= 1 ? xrefMaps.FirstOrDefault() : xrefMaps,
+                SourceBasePath = manifests.FirstOrDefault()?.SourceBasePath,
+                IncrementalInfo = incrementalInfos.Count > 0 ? incrementalInfos : null,
+            };
+
+        }
+    }
+}
