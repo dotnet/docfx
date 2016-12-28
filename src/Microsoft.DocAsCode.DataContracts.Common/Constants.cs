ad837decd6a194590247a0386ef04423610e028b
Renze Yu <renzeyu@microsoft.com>
2016-12-28 10:38:31 +0800

    expand js yml using AutoMapper (#1061)
    * expand js yml using AutoMapper
    
    * resolve comments
    
    * resolve comments

--- src/Microsoft.DocAsCode.DataContracts.Common/Constants.cs
+++ src/Microsoft.DocAsCode.DataContracts.Common/Constants.cs
@@ -30,6 +30,7 @@ namespace Microsoft.DocAsCode.DataContracts.Common
         {
             public const string CSharp = "csharp";
             public const string VB = "vb";
+            public const string JavaScript = "js";
         }
     }
 }
