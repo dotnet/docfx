2bfbc7a9802f1533d9e9e1178579c05dddadb674
Liangying <lianwei@microsoft.com>
2016-12-28 16:01:34 +0800

    Add MethodLevelMREF project (#1073)

---
+++ plugins/Microsoft.DocAsCode.Build.MethodLevelManagedReference/SplitClassPageIntoMethodPages.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.ManagedReference
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.IO;
+    using System.Collections.Immutable;
+
+    using Microsoft.DocAsCode.Build.Common;
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.DataContracts.ManagedReference;
+    using Microsoft.DocAsCode.Plugins;
+
+    [Export(nameof(ManagedReferenceDocumentProcessor), typeof(IDocumentBuildStep))]
+    public class SplitClassPageIntoMethodPages : BaseDocumentBuildStep
+    {
+        public override string Name => nameof(SplitClassPageIntoMethodPages);
+
+        public override int BuildOrder => 1;
+
+        /// <summary>
+        /// Extract: group with overload
+        /// </summary>
+        /// <param name="models"></param>
+        /// <param name="host"></param>
+        /// <returns></returns>
+        public override IEnumerable<FileModel> Prebuild(ImmutableList<FileModel> models, IHostService host)
+        {
+            // TODO: split class page into method pages
+            return models;
+        }
+    }
+}
