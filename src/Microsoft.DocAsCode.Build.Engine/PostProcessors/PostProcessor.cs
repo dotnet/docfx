e92dc8965efd89cbd1abbefa48e1e9fecdb76bf2
jehuan <jehuan@microsoft.com>
2016-12-28 14:22:47 +0800

    Move post processor logic into one folder

---
+++ src/Microsoft.DocAsCode.Build.Engine/PostProcessors/PostProcessor.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Engine
+{
+    using Microsoft.DocAsCode.Plugins;
+
+    internal sealed class PostProcessor
+    {
+        public string ContractName { get; set; }
+
+        public IPostProcessor Processor { get; set; }
+    }
+}
