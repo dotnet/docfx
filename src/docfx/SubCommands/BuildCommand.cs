aaea3dc7e8f45573b51ef7ad47d85844281ae9fb
jehuan <jehuan@microsoft.com>
2016-12-30 15:04:07 +0800

    should merge with json config's force

--- src/docfx/SubCommands/BuildCommand.cs
+++ src/docfx/SubCommands/BuildCommand.cs
@@ -209,7 +209,7 @@ namespace Microsoft.DocAsCode.SubCommands
             }
             config.Force |= options.ForceRebuild;
             // When force to rebuild, should force to post process as well
-            config.ForcePostProcess = config.ForcePostProcess | options.ForcePostProcess | options.ForceRebuild;
+            config.ForcePostProcess = config.ForcePostProcess | options.ForcePostProcess | config.Force;
             config.ExportRawModel |= options.ExportRawModel;
             config.ExportViewModel |= options.ExportViewModel;
             if (!string.IsNullOrEmpty(options.RawModelOutputFolder))
