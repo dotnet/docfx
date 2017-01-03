65af622decd52c3018a2f5f00e907b57892186f5
vwxyzh <vwxyzh@gmail.com>
2017-01-02 22:30:19 -0800

    add method in uri utility.

--- src/Microsoft.DocAsCode.Common/UriUtility.cs
+++ src/Microsoft.DocAsCode.Common/UriUtility.cs
@@ -7,27 +7,76 @@ namespace Microsoft.DocAsCode.Common
 
     public static class UriUtility
     {
-        private const string QueryMarker = "?";
-        private const string FragmentMarker = "#";
+        private const char QueryMarker = '?';
+        private const char FragmentMarker = '#';
+        private static readonly char[] QueryAndFragmentMarkers = { QueryMarker, FragmentMarker };
 
         public static bool HasFragment(string uriString)
         {
-            if (string.IsNullOrEmpty(uriString)) throw new ArgumentNullException(nameof(uriString));
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
             return uriString.IndexOf(FragmentMarker) != -1;
+        }
+
+        public static bool HasQueryString(string uriString)
+        {
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
+            return uriString.IndexOf(QueryMarker) != -1;
         }
 
         public static string GetFragment(string uriString)
         {
-            if (string.IsNullOrEmpty(uriString)) throw new ArgumentNullException(nameof(uriString));
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
             var index = uriString.IndexOf(FragmentMarker);
             return index == -1 ? string.Empty : uriString.Substring(index);
         }
 
         public static string GetNonFragment(string uriString)
         {
-            if (string.IsNullOrEmpty(uriString)) throw new ArgumentNullException(nameof(uriString));
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
             var index = uriString.IndexOf(FragmentMarker);
             return index == -1 ? uriString : uriString.Remove(index);
+        }
+
+        public static string GetQueryString(string uriString)
+        {
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
+            var index = uriString.IndexOf(QueryMarker);
+            return index == -1 ? string.Empty : GetNonFragment(uriString.Substring(index));
+        }
+
+        public static string GetPath(string uriString)
+        {
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
+            var index = uriString.IndexOfAny(QueryAndFragmentMarkers);
+            return index == -1 ? uriString : uriString.Remove(index);
+        }
+
+        public static string GetQueryStringAndFragment(string uriString)
+        {
+            if (uriString == null)
+            {
+                throw new ArgumentNullException(nameof(uriString));
+            }
+            var index = uriString.IndexOfAny(QueryAndFragmentMarkers);
+            return index == -1 ? uriString : uriString.Substring(index);
         }
     }
 }
