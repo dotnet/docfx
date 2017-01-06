09a55d3f9d3e1710dd13bac8e5fe7c9443a96547
Yufei Huang <yufeih@live.com>
2017-01-06 12:09:15 +0800

    Initial MergeOverride plugin project setup.

---
+++ plugins/Microsoft.DocAsCode.Build.MergeOverride/MergeOverrideMarkdownServiceProvider.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.MergeOverride
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Linq;
+
+    using Microsoft.DocAsCode.Dfm;
+    using Microsoft.DocAsCode.MarkdownLite;
+    using Microsoft.DocAsCode.Plugins;
+
+    [Export("momd", typeof(IMarkdownServiceProvider))]
+    public class MergeOverrideMarkdownServiceProvider : IMarkdownServiceProvider
+    {
+        public IMarkdownService CreateMarkdownService(MarkdownServiceParameters parameters)
+        {
+            IReadOnlyList<string> fallbackFolders = null;
+            object obj;
+            if (parameters.Extensions != null && parameters.Extensions.TryGetValue("fallbackFolders", out obj))
+            {
+                try
+                {
+                    fallbackFolders = ((IEnumerable)obj).Cast<string>().ToList();
+                }
+                catch
+                {
+                    // Swallow cast exception. 
+                }
+            }
+
+            return new DfmService(
+                parameters.BasePath,
+                parameters.TemplateDir,
+                parameters.Tokens,
+                TokenTreeValidator,
+                fallbackFolders);
+        }
+
+        [ImportMany]
+        public IEnumerable<IMarkdownTokenTreeValidator> TokenTreeValidator { get; set; }
+
+        private sealed class DfmService : IMarkdownService
+        {
+            private readonly DfmEngineBuilder _builder;
+
+            private readonly ImmutableDictionary<string, string> _tokens;
+
+            private readonly YamlHeaderMarkdownRenderer _renderer;
+
+            public DfmService(string baseDir, string templateDir, ImmutableDictionary<string, string> tokens, IEnumerable<IMarkdownTokenTreeValidator> tokenTreeValidator, IReadOnlyList<string> fallbackFolders = null)
+            {
+                var options = DocfxFlavoredMarked.CreateDefaultOptions();
+                _builder = DocfxFlavoredMarked.CreateBuilder(baseDir, templateDir, options, fallbackFolders);
+                _builder.TokenTreeValidator = MarkdownTokenTreeValidatorFactory.Combine(tokenTreeValidator);
+                _tokens = tokens;
+                _renderer = new YamlHeaderMarkdownRenderer();
+            }
+
+            public MarkupResult Markup(string src, string path)
+            {
+                var dependency = new HashSet<string>();
+                var html = _builder.CreateDfmEngine(_renderer).Markup(src, path, dependency);
+                var result = new MarkupResult
+                {
+                    Html = html,
+                };
+                if (dependency.Count > 0)
+                {
+                    result.Dependency = dependency.ToImmutableArray();
+                }
+                return result;
+            }
+
+            private sealed class YamlHeaderMarkdownRenderer : DfmMarkdownRenderer
+            {
+                public override StringBuffer Render(IMarkdownRenderer render, DfmYamlHeaderBlockToken token, MarkdownBlockContext context)
+                {
+                    if (string.IsNullOrEmpty(token.Content))
+                    {
+                        return StringBuffer.Empty;
+                    }
+                    var startLine = token.SourceInfo.LineNumber;
+                    var endLine = startLine + token.Content.Count(ch => ch == '\n') + 2;
+                    var sourceFile = token.SourceInfo.File;
+
+                    StringBuffer result = $"<yamlheader start=\"{startLine}\" end=\"{endLine}\"";
+                    if (!string.IsNullOrEmpty(sourceFile))
+                    {
+                        sourceFile = StringHelper.HtmlEncode(sourceFile);
+                        result += $" sourceFile=\"{sourceFile}\"";
+                    }
+                    result += ">";
+                    result += StringHelper.HtmlEncode(token.Content);
+                    return result + "</yamlheader>";
+                }
+
+                // TODO : override other method, and html encode it.
+            }
+        }
+    }
+}
---
+++ test/Microsoft.DocAsCode.Build.MergeOverride.Tests/MergeOverrideTest.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.MergeOverride.Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.DocAsCode.Build.MergeOverride;
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Common.Git;
+    using Microsoft.DocAsCode.DataContracts.Common;
+
+    using Xunit;
+
+    [Trait("Owner", "yufeih")]
+    [Trait("EntityType", "MergeOverrideProcessor")]
+    public class MergeOverrideTest
+    {
+    }
+}
