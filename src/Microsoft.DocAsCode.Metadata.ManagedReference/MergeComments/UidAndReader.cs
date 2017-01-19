a5a51b3c77d7c6f79d97da68cb090affb23a8ae4
Qinen Zhu <qinzhu@microsoft.com>
2017-01-19 11:49:52 +0800

    Merge comments while generating metadata from assembly

---
+++ src/Microsoft.DocAsCode.Metadata.ManagedReference/MergeComments/UidAndReader.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Metadata.ManagedReference
+{
+    using System.Xml;
+
+    internal sealed class UidAndReader
+    {
+        public string CommentId { get; set; }
+        public XmlReader Reader { get; set; }
+        public UidAndComment ToUidAndElement() => new UidAndComment { CommentId = CommentId, Comment = Reader.ReadOuterXml() };
+    }
+}
