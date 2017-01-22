c12acf127cc8dd5fe9902a5b961d5e34f51c1019
vwxyzh <vwxyzh@gmail.com>
2017-01-21 22:00:13 -0800

    add CompositeDictionary.

---
+++ src/Microsoft.DocAsCode.Common/CompositeDictionary.cs
@@ -0,0 +1,245 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Common
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    public class CompositeDictionary
+        : IDictionary<string, object>
+    {
+
+        private readonly ImmutableArray<Entry> _entries;
+
+        private CompositeDictionary(ImmutableArray<Entry> entries)
+        {
+            _entries = entries;
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException(nameof(key));
+                }
+                foreach (var entry in _entries)
+                {
+                    if (key.StartsWith(entry.Prefix))
+                    {
+                        return entry.GetValue(key.Substring(entry.Prefix.Length));
+                    }
+                }
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException(nameof(key));
+                }
+                foreach (var entry in _entries)
+                {
+                    if (key.StartsWith(entry.Prefix))
+                    {
+                        entry.SetValue(key.Substring(entry.Prefix.Length), value);
+                        return;
+                    }
+                }
+            }
+        }
+
+        public int Count => _entries.Sum(e => e.Count());
+
+        public bool IsReadOnly => false;
+
+        public ICollection<string> Keys => (from pair in this select pair.Key).ToList();
+
+        public ICollection<object> Values => (from pair in this select pair.Value).ToList();
+
+        void ICollection<KeyValuePair<string, object>>.Add(KeyValuePair<string, object> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Add(string key, object value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            foreach (var entry in _entries)
+            {
+                if (key.StartsWith(entry.Prefix))
+                {
+                    entry.AddValue(key.Substring(entry.Prefix.Length), value);
+                    return;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (var entry in _entries)
+            {
+                entry.Clear();
+            }
+        }
+
+        bool ICollection<KeyValuePair<string, object>>.Contains(KeyValuePair<string, object> item)
+        {
+            object value;
+            return TryGetValue(item.Key, out value) && object.Equals(item.Value, value);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            object _;
+            return TryGetValue(key, out _);
+        }
+
+        void ICollection<KeyValuePair<string, object>>.CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            int count = 0;
+            foreach (var item in this)
+            {
+                array[arrayIndex + count] = item;
+                count++;
+            }
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            foreach (var entry in _entries)
+            {
+                foreach (var item in entry.Enumerate())
+                {
+                    yield return new KeyValuePair<string, object>(entry.Prefix + item.Key, item.Value);
+                }
+            }
+        }
+
+        bool ICollection<KeyValuePair<string, object>>.Remove(KeyValuePair<string, object> item)
+        {
+            if (!((ICollection<KeyValuePair<string, object>>)this).Contains(item))
+            {
+                return false;
+            }
+            return Remove(item.Key);
+        }
+
+        public bool Remove(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            foreach (var entry in _entries)
+            {
+                if (key.StartsWith(entry.Prefix))
+                {
+                    return entry.Remove(key.Substring(entry.Prefix.Length));
+                }
+            }
+            return false;
+        }
+
+        public bool TryGetValue(string key, out object value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            foreach (var entry in _entries)
+            {
+                if (key.StartsWith(entry.Prefix))
+                {
+                    var pair = entry.TryGetValue(key.Substring(entry.Prefix.Length));
+                    value = pair.Value;
+                    return pair.Key;
+                }
+            }
+            value = null;
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public static Builder CreateBuilder()
+        {
+            return new Builder();
+        }
+
+        private sealed class Entry
+        {
+            public string Prefix { get; set; }
+            public Func<IEnumerable<KeyValuePair<string, object>>> Enumerate { get; set; }
+            public Func<string, object> GetValue { get; set; }
+            public Action<string, object> AddValue { get; set; }
+            public Action<string, object> SetValue { get; set; }
+            public Func<string, KeyValuePair<bool, object>> TryGetValue { get; set; }
+            public Func<string, bool> Remove { get; set; }
+            public Action Clear { get; set; }
+            public Func<int> Count { get; set; }
+        }
+
+        public sealed class Builder
+        {
+            private readonly List<Entry> _entries = new List<Entry>();
+
+            internal Builder() { }
+
+            public Builder Add<TValue>(string prefix, IDictionary<string, TValue> dict, Func<object, TValue> valueConverter)
+            {
+                _entries.Add(new Entry
+                {
+                    Prefix = prefix,
+                    Enumerate = () => Enumerate(dict),
+                    AddValue = (k, v) => dict.Add(k, valueConverter(v)),
+                    GetValue = key => dict[key],
+                    SetValue = (k, v) => dict[k] = valueConverter(v),
+                    TryGetValue =
+                        key =>
+                        {
+                            TValue value;
+                            var getted = dict.TryGetValue(key, out value);
+                            return new KeyValuePair<bool, object>(getted, (object)value);
+                        },
+                    Remove = key => dict.Remove(key),
+                    Clear = () => dict.Clear(),
+                    Count = () => dict.Count,
+                });
+                return this;
+            }
+
+            private static IEnumerable<KeyValuePair<string, object>> Enumerate<TValue>(IEnumerable<KeyValuePair<string, TValue>> source)
+            {
+                foreach (var item in source)
+                {
+                    yield return new KeyValuePair<string, object>(item.Key, item.Value);
+                }
+            }
+
+            public CompositeDictionary Create()
+            {
+                return new CompositeDictionary(_entries.OrderByDescending(x => x.Prefix.Length).ToImmutableArray());
+            }
+        }
+    }
+}
