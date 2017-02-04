d30a7a4af4d088e5e360a7a990e855a2d35a2cb1
vwxyzh <vwxyzh@gmail.com>
2017-02-04 15:35:11 +0800

    update ComputerResource. (#1263)

--- src/Microsoft.DocAsCode.Common/ComputerResource.cs
+++ src/Microsoft.DocAsCode.Common/ComputerResource.cs
@@ -10,32 +10,36 @@ namespace Microsoft.DocAsCode.Common
     [Serializable]
     public sealed class ComputerResource : IDisposable
     {
-        public const int DefaultDiskIOThreshold = 2;
-        public const int DefaultNetworkIOThreshold = 16;
-
-        private static readonly SemaphoreSlim CpuResource = new SemaphoreSlim(Environment.ProcessorCount);
-        private static readonly SemaphoreSlim DiskIOResource = new SemaphoreSlim(DefaultDiskIOThreshold);
-        private static readonly SemaphoreSlim NetworkIOResource = new SemaphoreSlim(DefaultNetworkIOThreshold);
+        private static ComputerResourceCollection _resources = new ComputerResourceCollection();
 
         private readonly ComputerResource _outer;
         private readonly ComputerResourceType _type;
 
-        private ComputerResource(ComputerResourceType type)
+        private ComputerResource(ComputerResourceType type, bool freeSlot)
         {
             _type = type;
             _outer = GetResourceType();
-            SetResourceType(this);
-            RequireResource(_outer?._type ?? ComputerResourceType.None, type);
+            SetResourceType(freeSlot ? null : this);
+            _resources.RequireResource(_outer?._type ?? ComputerResourceType.None, type);
+        }
+
+        public static void SetResources(ComputerResourceCollection resources)
+        {
+            if (resources == null)
+            {
+                throw new ArgumentNullException(nameof(resources));
+            }
+            _resources = resources;
         }
 
         public static ComputerResource Require(ComputerResourceType type)
         {
-            return new ComputerResource(type);
+            return new ComputerResource(type, false);
         }
 
         public static ComputerResource NewThread()
         {
-            return new ComputerResource(ComputerResourceType.None);
+            return new ComputerResource(ComputerResourceType.None, true);
         }
 
         private static void SetResourceType(ComputerResource resource)
@@ -55,57 +59,16 @@ namespace Microsoft.DocAsCode.Common
             return (ComputerResource)CallContext.LogicalGetData(nameof(ComputerResource));
         }
 
-        private static void RequireResource(ComputerResourceType current, ComputerResourceType target)
-        {
-            if (target == current)
-            {
-                return;
-            }
-            switch ((target & ComputerResourceType.Cpu) - (current & ComputerResourceType.Cpu))
-            {
-                case (int)ComputerResourceType.Cpu:
-                    CpuResource.Wait();
-                    break;
-                case -(int)ComputerResourceType.Cpu:
-                    CpuResource.Release();
-                    break;
-                default:
-                    break;
-            }
-            switch ((target & ComputerResourceType.DiskIO) - (current & ComputerResourceType.DiskIO))
-            {
-                case (int)ComputerResourceType.DiskIO:
-                    DiskIOResource.Wait();
-                    break;
-                case -(int)ComputerResourceType.DiskIO:
-                    DiskIOResource.Release();
-                    break;
-                default:
-                    break;
-            }
-            switch ((target & ComputerResourceType.NetworkIO) - (current & ComputerResourceType.NetworkIO))
-            {
-                case (int)ComputerResourceType.NetworkIO:
-                    NetworkIOResource.Wait();
-                    break;
-                case -(int)ComputerResourceType.NetworkIO:
-                    NetworkIOResource.Release();
-                    break;
-                default:
-                    break;
-            }
-        }
+        public static int GetAvailableCpuResource() => _resources.CpuResource.CurrentCount;
 
-        public static int GetAvailableCpuResource() => CpuResource.CurrentCount;
+        public static int GetAvailableDiskIOResource() => _resources.DiskIOResource.CurrentCount;
 
-        public static int GetAvailableDiskIOResource() => DiskIOResource.CurrentCount;
-
-        public static int GetAvailableNetworkIOResource() => NetworkIOResource.CurrentCount;
+        public static int GetAvailableNetworkIOResource() => _resources.NetworkIOResource.CurrentCount;
 
         public void Dispose()
         {
             SetResourceType(_outer);
-            RequireResource(_type, _outer?._type ?? ComputerResourceType.None);
+            _resources.RequireResource(_type, _outer?._type ?? ComputerResourceType.None);
         }
     }
 }
