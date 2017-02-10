fa03d0dec776f97fa980acefd97c13c31b64397f
Huang Xueyuan <hxyjessie@gmail.com>
2017-02-10 16:27:07 +0800

    Rest - minor refinement and reorg for json location (#1292)
    * Rest - minor refinement and reorg for json location
    
    * rename folder

--- src/Microsoft.DocAsCode.Build.RestApi/Swagger/Internals/SwaggerObjectBase.cs
+++ src/Microsoft.DocAsCode.Build.RestApi/Swagger/Internals/SwaggerObjectBase.cs
@@ -3,8 +3,6 @@
 
 namespace Microsoft.DocAsCode.Build.RestApi.Swagger.Internals
 {
-    using Microsoft.DocAsCode.Build.Common;
-
     internal abstract class SwaggerObjectBase
     {
         public abstract SwaggerObjectType ObjectType { get; }
