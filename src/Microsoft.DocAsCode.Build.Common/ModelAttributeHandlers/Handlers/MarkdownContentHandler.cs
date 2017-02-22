58e5f525c19c2fa8215bee93c74a044f6e86da34
Renze Yu <renzeyu@microsoft.com>
2017-02-22 09:18:39 +0800

    support IList for MdContentHandler (#1327)
    * support IList for MdContentHandler
    
    * use IList<string>

--- src/Microsoft.DocAsCode.Build.Common/ModelAttributeHandlers/Handlers/MarkdownContentHandler.cs
+++ src/Microsoft.DocAsCode.Build.Common/ModelAttributeHandlers/Handlers/MarkdownContentHandler.cs
@@ -6,7 +6,6 @@ namespace Microsoft.DocAsCode.Build.Common
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Collections.Immutable;
     using System.Linq;
     using System.Reflection;
 
@@ -87,22 +86,32 @@ namespace Microsoft.DocAsCode.Build.Common
                     }
                     return marked;
                 }
-                else
+
+                var list = currentObj as IList<string>;
+                if (list != null)
                 {
-                    throw new NotSupportedException($"Type {currentObj.GetType()} is NOT a supported type for {nameof(MarkdownContentAttribute)}");
+                    for (var i = 0; i < list.Count; i++)
+                    {
+                        var item = list[i];
+                        if (item != null)
+                        {
+                            list[i] = Markup(item, context);
+                        }
+                    }
+                    return list;
                 }
+
+                throw new NotSupportedException($"Type {currentObj.GetType()} is NOT a supported type for {nameof(MarkdownContentAttribute)}");
             }
 
             protected override object HandleDictionaryType(object currentObj, HandleModelAttributesContext context)
             {
-                Func<object, object> handler = s => Handler.Handle(s, context);
                 HandleGenericItemsHelper.HandleIDictionary(currentObj, s => Handler.Handle(s, context));
                 return currentObj;
             }
 
             protected override object HandleIEnumerableType(object currentObj, HandleModelAttributesContext context)
             {
-                Func<object, object> handler = s => Handler.Handle(s, context);
                 HandleGenericItemsHelper.HandleIList(currentObj, s => Handler.Handle(s, context));
                 return currentObj;
             }
@@ -144,12 +153,12 @@ namespace Microsoft.DocAsCode.Build.Common
                 return false;
             }
 
-            private bool IsPlaceholderContent(string content)
+            private static bool IsPlaceholderContent(string content)
             {
                 return content != null && content.Trim() == ContentPlaceholder;
             }
 
-            private string MarkupCore(string content, HandleModelAttributesContext context)
+            private static string MarkupCore(string content, HandleModelAttributesContext context)
             {
                 var host = context.Host;
                 var mr = host.Markup(content, context.FileAndType);
