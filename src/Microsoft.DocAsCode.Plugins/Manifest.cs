fa4f7b121ad3c7ffd6eedd53ba9eed5318503392
V <vwxyzh@gmail.com>
2017-02-24 17:22:45 +0800

    Add index for manifest. (#1353)

--- src/Microsoft.DocAsCode.Plugins/Manifest.cs
+++ src/Microsoft.DocAsCode.Plugins/Manifest.cs
@@ -4,14 +4,25 @@
 namespace Microsoft.DocAsCode.Plugins
 {
     using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.ComponentModel;
+    using System.Threading;
 
     using Newtonsoft.Json;
 
     public class Manifest
     {
-        public Manifest() { }
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private readonly Dictionary<string, OutputFileInfo> _index = new Dictionary<string, OutputFileInfo>();
+
+        public Manifest()
+        {
+            Files = new ManifestItemCollection();
+            Files.CollectionChanged += FileCollectionChanged;
+        }
 
         public Manifest(IEnumerable<ManifestItem> files)
+            : this()
         {
             Files.AddRange(files);
         }
@@ -29,9 +40,129 @@ namespace Microsoft.DocAsCode.Plugins
         public object XRefMap { get; set; }
 
         [JsonProperty("files")]
-        public ManifestItemCollection Files { get; } = new ManifestItemCollection();
+        public ManifestItemCollection Files { get; }
 
         [JsonProperty("incremental_info")]
         public List<IncrementalInfo> IncrementalInfo { get; set; }
+
+        #region Public Methods
+
+        public OutputFileInfo FindOutputFileInfo(string relativePath)
+        {
+            OutputFileInfo result;
+            _lock.EnterReadLock();
+            try
+            {
+                _index.TryGetValue(relativePath, out result);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region EventHandlers
+
+        private void FileCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                if (e.NewItems != null)
+                {
+                    foreach (ManifestItem item in e.NewItems)
+                    {
+                        foreach (var ofi in item.OutputFiles.Values)
+                        {
+                            _index[ofi.RelativePath] = ofi;
+                            ofi.PropertyChanged += OutputFileInfoPropertyChanged;
+                        }
+                        ((INotifyCollectionChanged)item.OutputFiles).CollectionChanged += ManifestItemOutputChanged;
+                    }
+                }
+                if (e.OldItems != null)
+                {
+                    foreach (ManifestItem item in e.OldItems)
+                    {
+                        foreach (var ofi in item.OutputFiles.Values)
+                        {
+                            OutputFileInfo value;
+                            _index.TryGetValue(ofi.RelativePath, out value);
+                            if (value == ofi)
+                            {
+                                _index.Remove(ofi.RelativePath);
+                                ofi.PropertyChanged -= OutputFileInfoPropertyChanged;
+                            }
+                        }
+                       ((INotifyCollectionChanged)item.OutputFiles).CollectionChanged -= ManifestItemOutputChanged;
+                    }
+                }
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        private void ManifestItemOutputChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                if (e.NewItems != null)
+                {
+                    foreach (KeyValuePair<string, OutputFileInfo> item in e.NewItems)
+                    {
+                        _index[item.Value.RelativePath] = item.Value;
+                        item.Value.PropertyChanged += OutputFileInfoPropertyChanged;
+                    }
+                }
+                if (e.OldItems != null)
+                {
+                    foreach (KeyValuePair<string, OutputFileInfo> item in e.OldItems)
+                    {
+                        OutputFileInfo value;
+                        _index.TryGetValue(item.Value.RelativePath, out value);
+                        if (value == item.Value)
+                        {
+                            _index.Remove(item.Value.RelativePath);
+                            item.Value.PropertyChanged -= OutputFileInfoPropertyChanged;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        private void OutputFileInfoPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var args = e as PropertyChangedEventArgs<string>;
+            if (args == null)
+            {
+                return;
+            }
+            if (args.PropertyName != nameof(OutputFileInfo.RelativePath))
+            {
+                return;
+            }
+            _lock.EnterWriteLock();
+            try
+            {
+                _index.Remove(args.Original);
+                _index.Add(args.Current, (OutputFileInfo)sender);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        #endregion
     }
 }
--- src/Microsoft.DocAsCode.Plugins/OutputFileCollection.cs
+++ src/Microsoft.DocAsCode.Plugins/OutputFileCollection.cs
@@ -3,37 +3,8 @@
 
 namespace Microsoft.DocAsCode.Plugins
 {
-    using System;
-    using System.Collections.Generic;
-
-    public class OutputFileCollection : Dictionary<string, OutputFileInfo>
+    public class OutputFileCollection : ObservableDictionary<string, OutputFileInfo>
     {
-        public new OutputFileInfo this[string key]
-        {
-            get { return base[key]; }
-            set { base[key] = value; }
-        }
-
-        public new IEqualityComparer<string> Comparer => base.Comparer;
-
-        public new int Count => base.Count;
-
-        public new ICollection<string> Keys => base.Keys;
-
-        public new ICollection<OutputFileInfo> Values => base.Values;
-
-        public new void Add(string key, OutputFileInfo value) => base.Add(key, value);
-
-        public new void Clear() => base.Clear();
-
-        public new bool ContainsKey(string key) => base.ContainsKey(key);
-
-        public new bool ContainsValue(OutputFileInfo value) => base.ContainsValue(value);
-
-        public new IEnumerator<KeyValuePair<string, OutputFileInfo>> GetEnumerator() => base.GetEnumerator();
-
-        public new bool Remove(string key) => base.Remove(key);
-
-        public new bool TryGetValue(string key, out OutputFileInfo value) => base.TryGetValue(key, out value);
+        public bool ContainsValue(OutputFileInfo value) => Values.Contains(value);
     }
 }
