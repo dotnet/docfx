6bba2aee32193919f46f563322fcfb9597ab07c2
lianwei <lianwei@microsoft.com>
2017-03-10 16:08:07 +0800

    resolve comments

--- plugins/Microsoft.DocAsCode.Build.MemberLevelManagedReference/resources/ManagedReference.overwrite.js
+++ plugins/Microsoft.DocAsCode.Build.MemberLevelManagedReference/resources/ManagedReference.overwrite.js
@@ -2,7 +2,7 @@
 var common = require('./ManagedReference.common.js');
 
 exports.getOptions = function (model) {
-  var ignoreChildrenBookmarks = model._splitReference && model.type && common.getCategory(model.type) == 'ns';
+  var ignoreChildrenBookmarks = model._splitReference && model.type && common.getCategory(model.type) === 'ns';
 
   return {
     "bookmarks": common.getBookmarks(model, ignoreChildrenBookmarks)
