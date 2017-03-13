b8363e90c28238b5aae92b7077e6bd993c1f608a
xuzho <xuzho@microsoft.com>
2017-03-13 13:37:00 +0800

    resolve comments

--- src/Microsoft.DocAsCode.Build.Engine/Incrementals/DependencyGraph.cs
+++ src/Microsoft.DocAsCode.Build.Engine/Incrementals/DependencyGraph.cs
@@ -406,7 +406,7 @@ namespace Microsoft.DocAsCode.Build.Engine.Incrementals
 
             if (unresolved.Count > 0)
             {
-                Logger.LogVerbose($"Dependency graph Failed to resolve references: {JsonUtility.Serialize(unresolved)}.");
+                Logger.LogVerbose($"Dependency graph failed to resolve references: {string.Join(";", unresolved)}.");
             }
         }
 
