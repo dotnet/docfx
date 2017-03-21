76b5554322f41555cf262708e42eecf30cdf10f4
V <vwxyzh@gmail.com>
2017-03-21 17:16:58 +0800

    update block text in markdown. (#1450)

--- src/Microsoft.DocAsCode.MarkdownLite/Basic/BlockRules/MarkdownTextBlockRule.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/Basic/BlockRules/MarkdownTextBlockRule.cs
@@ -3,15 +3,40 @@
 
 namespace Microsoft.DocAsCode.MarkdownLite
 {
+    using System;
     using System.Text.RegularExpressions;
+
+    using Microsoft.DocAsCode.MarkdownLite.Matchers;
 
     public class MarkdownTextBlockRule : IMarkdownRule
     {
+        // @"^[^\n]+\n?"
+        private static readonly Matcher _TextMatcher =
+            Matcher.AnyStringInSingleLine + Matcher.NewLine.Maybe();
+
         public virtual string Name => "Text";
 
+        [Obsolete("Please use LHeadingMatcher.")]
         public virtual Regex Text => Regexes.Block.Text;
 
+        public virtual Matcher TextMatcher => _TextMatcher;
+
         public virtual IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
+        {
+            if (Text != Regexes.Block.Text)
+            {
+                return TryMatchOld(parser, context);
+            }
+            var match = context.Match(TextMatcher);
+            if (match?.Length > 0)
+            {
+                var sourceInfo = context.Consume(match.Length);
+                return new MarkdownTextToken(this, parser.Context, sourceInfo.Markdown, sourceInfo);
+            }
+            return null;
+        }
+
+        public virtual IMarkdownToken TryMatchOld(IMarkdownParser parser, IMarkdownParsingContext context)
         {
             var match = Text.Match(context.CurrentMarkdown);
             if (match.Length == 0)
