820f8affbf7172dc3527c2b568eb6b992257c171
xuzho <xuzho@microsoft.com>
2017-03-22 19:25:48 +0800

    addlog

--- src/Microsoft.DocAsCode.Build.Engine/LinkPhaseHandlerWithIncremental.cs
+++ src/Microsoft.DocAsCode.Build.Engine/LinkPhaseHandlerWithIncremental.cs
@@ -210,7 +210,11 @@ namespace Microsoft.DocAsCode.Build.Engine
         private void ProcessUnloadedTemplateDependency(IEnumerable<HostService> hostServices)
         {
             var loaded = Context.ManifestItems;
-            var unloaded = GetUnloadedManifestItems(hostServices);
+            IEnumerable<ManifestItem> unloaded;
+            using (new LoggerPhaseScope("GetUnloadedManifestItems", LogLevel.Verbose))
+            {
+                unloaded = GetUnloadedManifestItems(hostServices);
+            }
             var types = new HashSet<string>(unloaded.Select(m => m.DocumentType).Except(loaded.Select(m => m.DocumentType)));
             if (types.Count > 0)
             {
