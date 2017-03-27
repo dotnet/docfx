f5c00517810453f94cf21c6a590a57241cdd0776
V <vwxyzh@gmail.com>
2017-03-27 17:16:05 +0800

    support escaped pipe in table. (#1493)

--- src/Microsoft.DocAsCode.MarkdownLite/Regexes.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/Regexes.cs
@@ -155,7 +155,7 @@ namespace Microsoft.DocAsCode.MarkdownLite
             public static readonly Regex UselessTableAlign = new Regex(@"^ *|\|? *$", RegexOptionCompiled);
             public static readonly Regex UselessTableRow = new Regex(@"^ *\| *| *\|? *$", RegexOptionCompiled);
             public static readonly Regex UselessGfmTableCell = new Regex(@"(?: *\| *)?\n$", RegexOptionCompiled);
-            public static readonly Regex TableSplitter = new Regex(@" *\| *", RegexOptionCompiled);
+            public static readonly Regex TableSplitter = new Regex(@" *(?<!\\)\| *", RegexOptionCompiled);
             public static readonly Regex EndWithNewLine = new Regex(@"\n$", RegexOptionCompiled);
             public static readonly Regex TableAlignRight = new Regex(@"^ *-+: *$", RegexOptionCompiled);
             public static readonly Regex TableAlignCenter = new Regex(@"^ *:-+: *$", RegexOptionCompiled);
