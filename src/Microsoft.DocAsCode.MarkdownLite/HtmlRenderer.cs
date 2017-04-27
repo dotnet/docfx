55b218f12375fe0ce91c906649c72a88de8ef0a3
V <vwxyzh@gmail.com>
2017-04-27 14:52:26 +0800

    public AppendSourceInfo. (#1607)

--- src/Microsoft.DocAsCode.MarkdownLite/HtmlRenderer.cs
+++ src/Microsoft.DocAsCode.MarkdownLite/HtmlRenderer.cs
@@ -366,13 +366,35 @@ namespace Microsoft.DocAsCode.MarkdownLite
 
         #endregion
 
-        #region Protected Methods
+        #region Static Methods
+
+        public static StringBuffer AppendSourceInfo(StringBuffer result, Options options, IMarkdownToken token)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+            if (options.ShouldExportSourceInfo)
+            {
+                result = AppendSourceInfoCore(result, token);
+            }
+            return result;
+        }
+
+        private static StringBuffer AppendSourceInfoCore(StringBuffer result, IMarkdownToken token)
+        {
+            return result + " sourceFile=\"" + StringHelper.HtmlEncode(token.SourceInfo.File) + "\" sourceStartLineNumber=\"" + token.SourceInfo.LineNumber.ToString() + "\" sourceEndLineNumber=\"" + (token.SourceInfo.LineNumber + token.SourceInfo.ValidLineCount - 1).ToString() + "\"";
+        }
 
         protected static StringBuffer AppendSourceInfo(StringBuffer result, IMarkdownRenderer renderer, IMarkdownToken token)
         {
             if (renderer.Options.ShouldExportSourceInfo)
             {
-                result = result + " sourceFile=\"" + StringHelper.HtmlEncode(token.SourceInfo.File) + "\" sourceStartLineNumber=\"" + token.SourceInfo.LineNumber.ToString() + "\" sourceEndLineNumber=\"" + (token.SourceInfo.LineNumber + token.SourceInfo.ValidLineCount - 1).ToString() + "\"";
+                result = AppendSourceInfoCore(result, token);
             }
             return result;
         }
