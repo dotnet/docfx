dfe5497d0a6a135f5220c3c4df9d12e2f1bcfd49
vwxyzh <vwxyzh@gmail.com>
2017-06-02 17:27:47 +0800

    update test case.

--- test/Microsoft.DocAsCode.Common.Tests/SharedObjectTest.cs
+++ test/Microsoft.DocAsCode.Common.Tests/SharedObjectTest.cs
@@ -18,16 +18,29 @@ namespace Microsoft.DocAsCode.Common.Tests
             var m = new SharedObjectManager<string, string>(
                 string.Empty,
                 (s, o) => new string(s.ToCharArray().Concat(o.ToCharArray()).OrderBy(c => c).ToArray()));
+
+            Assert.Same(string.Empty, m.RootState.Value);
+            Assert.Same(m.RootState, m.RootState.Modify(string.Empty));
+
             var a = m.RootState.Modify("A");
+            Assert.Equal("A", a.Value);
             Assert.Same(a, m.RootState.Modify("A"));
-            var ab = a.Modify("B");
+
             var b = m.RootState.Modify("B");
+            Assert.Equal("B", b.Value);
+
+            var ab = m.RootState.Modify("AB");
+            Assert.Equal("AB", ab.Value);
+
+            Assert.Same(ab, a.Modify("B"));
             Assert.Same(ab, b.Modify("A"));
-            Assert.Same(ab, b.Modify("A"));
+
             Assert.Same(ab, m.RootState.Modify("AB"));
-            Assert.Same(ab, m.RootState.Modify("AB"));
+            Assert.Same(ab, m.RootState.Modify("AB").Modify(string.Empty));
             Assert.Same(ab, m.RootState.Modify("BA"));
-            Assert.Same(ab, m.RootState.Modify("BA"));
+            Assert.Same(ab, m.RootState.Modify("BA").Modify(string.Empty));
+            Assert.Same(ab, m.RootState.Modify("A").Modify(string.Empty).Modify("B"));
+            Assert.Same(ab, m.RootState.Modify("B").Modify(string.Empty).Modify("A"));
         }
     }
 }
