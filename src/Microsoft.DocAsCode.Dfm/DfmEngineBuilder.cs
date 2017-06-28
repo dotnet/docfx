d0b4c0eba0dc76a13d98572edaac1eb3f27d09c2
vwxyzh <vwxyzh@gmail.com>
2017-06-28 15:43:53 +0800

    add log code, no visible tab.

--- src/Microsoft.DocAsCode.Dfm/DfmEngineBuilder.cs
+++ src/Microsoft.DocAsCode.Dfm/DfmEngineBuilder.cs
@@ -161,7 +161,7 @@ namespace Microsoft.DocAsCode.Dfm
                 {
                     active = firstVisibleTab;
                     selectedTabIds.Add(items[firstVisibleTab].Id);
-                    Logger.LogWarning("No visible tab.", file: token.SourceInfo.File, line: token.SourceInfo.LineNumber.ToString(), code: "aaa");
+                    Logger.LogWarning("No visible tab.", file: token.SourceInfo.File, line: token.SourceInfo.LineNumber.ToString(), code: WarningCodes.Markdown.NoVisibleTab);
                 }
                 else
                 {
