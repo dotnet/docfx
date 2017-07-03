6e204c462a1353b2b0c64c29ace13f7bf8d12f7e
jehuan <jehuan@microsoft.com>
2017-07-03 19:06:03 +0800

    Add plugins to split rest into tag pages

--- plugins/Microsoft.DocAsCode.Build.TagsLevelRestApi/SplitRestApiToTagsLevel.cs
+++ plugins/Microsoft.DocAsCode.Build.TagsLevelRestApi/SplitRestApiToTagsLevel.cs
@@ -7,11 +7,16 @@ namespace Microsoft.DocAsCode.Build.RestApi
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Composition;
+    using System.IO;
+    using System.Linq;
 
     using Microsoft.DocAsCode.Build.Common;
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.DataContracts.Common;
+    using Microsoft.DocAsCode.DataContracts.RestApi;
     using Microsoft.DocAsCode.Plugins;
 
-    //[Export("RestApiDocumentProcessor", typeof(IDocumentBuildStep))]
+    [Export("RestApiDocumentProcessor", typeof(IDocumentBuildStep))]
     public class SplitRestApiToTagsLevel : BaseDocumentBuildStep
     {
         public override string Name => nameof(SplitRestApiToTagsLevel);
@@ -20,7 +25,174 @@ namespace Microsoft.DocAsCode.Build.RestApi
 
         public override IEnumerable<FileModel> Prebuild(ImmutableList<FileModel> models, IHostService host)
         {
-            throw new NotImplementedException();
+            var collection = new List<FileModel>(models);
+
+            var treeMapping = new Dictionary<string, Tuple<FileAndType, IEnumerable<TreeItem>>>();
+            foreach (var model in models)
+            {
+                var result = SplitModelToOperationGroup(model);
+                if (result != null)
+                {
+                    if (treeMapping.ContainsKey(result.Key))
+                    {
+                        Logger.LogWarning($"Model with the key {result.Key} already exists. '{model.OriginalFileAndType?.FullPath ?? model.FileAndType.FullPath}' is ignored.");
+                    }
+                    else
+                    {
+                        treeMapping.Add(result.Key, Tuple.Create(model.OriginalFileAndType, result.TreeItems));
+                        collection.AddRange(result.Models);
+                    }
+                }
+            }
+
+            host.TableOfContentRestructions =
+                (from item in treeMapping
+                 select new TreeItemRestructure
+                 {
+                     ActionType = TreeItemActionType.AppendChild,
+                     Key = item.Key,
+                     TypeOfKey = TreeItemKeyType.TopicHref,
+                     RestructuredItems = item.Value.Item2.ToImmutableList(),
+                     SourceFiles = new FileAndType[] { item.Value.Item1 }.ToImmutableList(),
+                 }).ToImmutableList();
+
+            return collection;
+        }
+
+        private SplittedResult SplitModelToOperationGroup(FileModel model)
+        {
+            if (model.Type != DocumentType.Article)
+            {
+                return null;
+            }
+
+            var content = (RestApiRootItemViewModel)model.Content;
+
+            if (content.Tags.Count == 0 || content.Children.Count == 0)
+            {
+                return null;
+            }
+
+            var tagModels = GenerateTagModels(content).ToList();
+            if (tagModels.Count == 0)
+            {
+                return null;
+            }
+
+            var treeItems = new List<TreeItem>();
+            var splittedModels = new List<FileModel>();
+            foreach (var tagModel in tagModels)
+            {
+                var newModel = GenerateNewFileModel(model, tagModel);
+                splittedModels.Add(newModel);
+                treeItems.Add(ConvertToTreeItem(tagModel, newModel.Key));
+            }
+
+            // Only keep not tagged children in root model
+            var groupedUids = splittedModels.SelectMany(m => m.Uids).Select(u => u.Name).ToList();
+            content.Tags = new List<RestApiTagViewModel>();
+            content.Children = content.Children.Where(child => !groupedUids.Contains(child.Uid)).ToList();
+            model.Content = content;
+
+            return new SplittedResult(model.Key, treeItems.OrderBy(s => s.Metadata[Constants.PropertyName.Name]), splittedModels);
+        }
+
+        private IEnumerable<RestApiRootItemViewModel> GenerateTagModels(RestApiRootItemViewModel root)
+        {
+            foreach (var tag in root.Tags)
+            {
+                var tagChildren = GetChildrenByTag(root, tag.Name).ToList();
+                if (tagChildren.Count > 0)
+                {
+                    yield return new RestApiRootItemViewModel
+                    {
+                        Uid = tag.Uid,
+                        HtmlId = tag.HtmlId,
+                        Name = tag.Name,
+                        Conceptual = tag.Conceptual,
+                        Description = tag.Description,
+                        Documentation = tag.Documentation,
+                        Children = tagChildren,
+                        Tags = new List<RestApiTagViewModel>(),
+                        Metadata = MergeMetadata(root.Metadata, tag.Metadata)
+                    };
+                }
+            }
+        }
+
+        private IEnumerable<RestApiChildItemViewModel> GetChildrenByTag(RestApiRootItemViewModel root, string tagName)
+        {
+            // Only group children into first tag, to keep cross reference unique
+            return root.Children.Where(child => child.Tags != null && tagName == child.Tags.FirstOrDefault());
+        }
+
+        private FileModel GenerateNewFileModel(FileModel model, RestApiRootItemViewModel tagModel)
+        {
+            var originalFile = model.FileAndType.File;
+            var fileExtension = Path.GetExtension(originalFile);
+
+            // When handlering tags in petstore.swagger.json, the tag file path should be petstore/tag.json, to prevent tag name confliction
+            var originalFileName = Path.GetFileName(originalFile);
+            var subDirectory = originalFileName.Remove(originalFileName.IndexOf('.'));
+            var directory = Path.GetDirectoryName(originalFile);
+            var filePath = Path.Combine(directory, subDirectory, tagModel.Name + fileExtension);
+
+            var newFileAndType = new FileAndType(model.FileAndType.BaseDir, filePath, model.FileAndType.Type, model.FileAndType.SourceDir, model.FileAndType.DestinationDir);
+            var newKey = "~/" + RelativePath.GetPathWithoutWorkingFolderChar(filePath);
+            var newModel = new FileModel(newFileAndType, tagModel, model.OriginalFileAndType, model.Serializer, newKey)
+            {
+                LocalPathFromRoot = model.LocalPathFromRoot,
+                Uids = CalculateUids(tagModel, model.LocalPathFromRoot)
+            };
+
+            return newModel;
+        }
+
+        private ImmutableArray<UidDefinition> CalculateUids(RestApiRootItemViewModel root, string file)
+        {
+            return new[] { new UidDefinition(root.Uid, file) }
+                   .Concat(from item in root.Children select new UidDefinition(item.Uid, file)).ToImmutableArray();
+        }
+
+        private TreeItem ConvertToTreeItem(RestApiRootItemViewModel root, string fileKey)
+        {
+            return new TreeItem()
+            {
+                Metadata = new Dictionary<string, object>()
+                {
+                    [Constants.PropertyName.Name] = root.Name,
+                    [Constants.PropertyName.Href] = fileKey,
+                    [Constants.PropertyName.TopicHref] = fileKey
+                }
+            };
+        }
+
+        private Dictionary<string, object> MergeMetadata(Dictionary<string, object> rootMetadata, Dictionary<string, object> tagMetadata)
+        {
+            var result = new Dictionary<string, object>(rootMetadata);
+            foreach (var pair in tagMetadata)
+            {
+                // Root metadata wins for the same key
+                if (!result.ContainsKey(pair.Key))
+                {
+                    result[pair.Key] = tagMetadata[pair.Key];
+                }
+            }
+            return result;
+        }
+
+        private sealed class SplittedResult
+        {
+            public string Key { get; }
+            public IEnumerable<TreeItem> TreeItems { get; }
+            public IEnumerable<FileModel> Models { get; }
+
+            public SplittedResult(string key, IEnumerable<TreeItem> items, IEnumerable<FileModel> models)
+            {
+                Key = key;
+                TreeItems = items;
+                Models = models;
+            }
         }
     }
 }
--- test/Microsoft.DocAsCode.Build.RestApi.WithPlugins.Tests/SplitRestApiToTagsLevelTest.cs
+++ test/Microsoft.DocAsCode.Build.RestApi.WithPlugins.Tests/SplitRestApiToTagsLevelTest.cs
@@ -9,6 +9,9 @@ namespace Microsoft.DocAsCode.Build.RestApi.Tests
 
     using Microsoft.DocAsCode.Build.Engine;
     using Microsoft.DocAsCode.Build.TableOfContents;
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.DataContracts.Common;
+    using Microsoft.DocAsCode.DataContracts.RestApi;
     using Microsoft.DocAsCode.Plugins;
     using Microsoft.DocAsCode.Tests.Common;
 
@@ -36,7 +39,8 @@ namespace Microsoft.DocAsCode.Build.RestApi.Tests
             _defaultFiles.Add(DocumentType.Article, new[] { "TestData/swagger/petstore.json" }, "TestData/");
             _applyTemplateSettings = new ApplyTemplateSettings(_inputFolder, _outputFolder)
             {
-                RawModelExportSettings = { Export = true }
+                RawModelExportSettings = { Export = true },
+                TransformDocument = true,
             };
             _templateManager = new TemplateManager(null, null, new List<string> { "template" }, null, "TestData/");
         }
@@ -47,7 +51,78 @@ namespace Microsoft.DocAsCode.Build.RestApi.Tests
             var files = new FileCollection(_defaultFiles);
             BuildDocument(files);
 
-            // TODO: verification
+            {
+                // Verify original petstore page
+                var outputRawModelPath = GetRawModelFilePath("petstore.json");
+                Assert.True(File.Exists(outputRawModelPath));
+                var model = JsonUtility.Deserialize<RestApiRootItemViewModel>(outputRawModelPath);
+                Assert.NotNull(model);
+                Assert.Equal("petstore.swagger.io/v2/Swagger Petstore/1.0.0", model.Uid);
+                Assert.Equal(0, model.Children.Count);
+                Assert.Equal(0, model.Tags.Count);
+            }
+            {
+                // Verify splitted tag page
+                var outputRawModelPath = GetRawModelFilePath("petstore/pet.json");
+                Assert.True(File.Exists(outputRawModelPath));
+                var model = JsonUtility.Deserialize<RestApiRootItemViewModel>(outputRawModelPath);
+                Assert.NotNull(model);
+                Assert.Equal("petstore.swagger.io/v2/Swagger Petstore/1.0.0/tag/pet", model.Uid);
+                Assert.Equal("pet", model.Name);
+                Assert.Equal("<p sourcefile=\"TestData/swagger/petstore.json\" sourcestartlinenumber=\"1\" sourceendlinenumber=\"1\">Everything about your Pets</p>\n", model.Description);
+                Assert.Equal(8, model.Children.Count);
+                Assert.Equal(0, model.Tags.Count);
+                Assert.Equal("swagger/petstore/pet.html", model.Metadata["_path"]);
+                Assert.Equal("TestData/swagger/petstore/pet.json", model.Metadata["_key"]);
+                Assert.True(model.Metadata.ContainsKey("externalDocs"));
+            }
+        }
+
+        [Fact]
+        public void ProcessRestApiWithTocShouldSucceed()
+        {
+            var files = new FileCollection(_defaultFiles);
+            files.Add(DocumentType.Article, new[] { "TestData/swagger/toc.yml" }, "TestData/");
+            BuildDocument(files);
+
+            {
+                // Verify original petstore page
+                var outputRawModelPath = GetRawModelFilePath("petstore.json");
+                Assert.True(File.Exists(outputRawModelPath));
+                var model = JsonUtility.Deserialize<RestApiRootItemViewModel>(outputRawModelPath);
+                Assert.NotNull(model);
+                Assert.Equal("petstore.swagger.io/v2/Swagger Petstore/1.0.0", model.Uid);
+                Assert.Equal(0, model.Children.Count);
+                Assert.Equal(0, model.Tags.Count);
+            }
+            {
+                // Verify splitted tag page
+                var outputRawModelPath = GetRawModelFilePath("petstore/pet.json");
+                Assert.True(File.Exists(outputRawModelPath));
+                var model = JsonUtility.Deserialize<RestApiRootItemViewModel>(outputRawModelPath);
+                Assert.NotNull(model);
+                Assert.Equal("petstore.swagger.io/v2/Swagger Petstore/1.0.0/tag/pet", model.Uid);
+                Assert.Equal("pet", model.Name);
+                Assert.Equal("<p sourcefile=\"TestData/swagger/petstore.json\" sourcestartlinenumber=\"1\" sourceendlinenumber=\"1\">Everything about your Pets</p>\n", model.Description);
+                Assert.Equal(8, model.Children.Count);
+                Assert.Equal(0, model.Tags.Count);
+                Assert.Equal("swagger/petstore/pet.html", model.Metadata["_path"]);
+                Assert.Equal("TestData/swagger/petstore/pet.json", model.Metadata["_key"]);
+                Assert.True(model.Metadata.ContainsKey("externalDocs"));
+            }
+            {
+                // Verify toc page
+                var outputRawModelPath = GetRawModelFilePath("toc.yml");
+                Assert.True(File.Exists(outputRawModelPath));
+                var model = JsonUtility.Deserialize<TocItemViewModel>(outputRawModelPath);
+                Assert.NotNull(model);
+                Assert.Equal(1, model.Items.Count);
+                var rootModel = model.Items[0];
+                Assert.Equal("petstore.html", rootModel.TopicHref);
+                Assert.Equal(3, rootModel.Items.Count);
+                Assert.Equal("petstore/pet.html", rootModel.Items[0].TopicHref);
+                Assert.Equal("pet", rootModel.Items[0].Name);
+            }
         }
 
         private void BuildDocument(FileCollection files)
@@ -60,7 +135,8 @@ namespace Microsoft.DocAsCode.Build.RestApi.Tests
                 Metadata = new Dictionary<string, object>
                 {
                     ["meta"] = "Hello world!",
-                }.ToImmutableDictionary()
+                }.ToImmutableDictionary(),
+                TemplateManager = _templateManager
             };
 
             using (var builder = new DocumentBuilder(LoadAssemblies(), ImmutableArray<string>.Empty, null))
@@ -74,6 +150,11 @@ namespace Microsoft.DocAsCode.Build.RestApi.Tests
             yield return typeof(RestApiDocumentProcessor).Assembly;
             yield return typeof(TocDocumentProcessor).Assembly;
             yield return typeof(SplitRestApiToTagsLevel).Assembly;
+        }
+
+        private string GetRawModelFilePath(string fileName)
+        {
+            return Path.GetFullPath(Path.Combine(_outputFolder, "swagger", Path.ChangeExtension(fileName, RawModelFileExtension)));
         }
     }
 }
