03b6ff9dcca6bdc99b803d33a4c7a0e16ba1e5bb
Liangying.Wei <lianwei@microsoft.com>
2017-07-03 15:14:46 +0800

    Fix #1807 that overload uids get lost (#1814)
    * Fix #1807 that overload uids get lost
    
    when incremental build saves xref specs
    
    * Update ManagedReferenceDocumentProcessor.cs
    
    * Update ManagedReferenceDocumentProcessor.cs

--- src/Microsoft.DocAsCode.Build.ManagedReference/ManagedReferenceDocumentProcessor.cs
+++ src/Microsoft.DocAsCode.Build.ManagedReference/ManagedReferenceDocumentProcessor.cs
@@ -104,7 +104,9 @@ namespace Microsoft.DocAsCode.Build.ManagedReference
 
             return new FileModel(file, page, serializer: new BinaryFormatter())
             {
-                Uids = (from item in page.Items select new UidDefinition(item.Uid, localPathFromRoot)).ToImmutableArray(),
+                Uids = (from item in page.Items select item.Uid)
+                .Concat(from item in page.Items where item.Overload != null select item.Overload)
+                .Distinct().Select(s => new UidDefinition(s, localPathFromRoot)).ToImmutableArray(),
                 LocalPathFromRoot = localPathFromRoot
             };
         }
