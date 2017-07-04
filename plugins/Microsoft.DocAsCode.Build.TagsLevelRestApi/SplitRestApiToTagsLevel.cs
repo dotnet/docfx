93a53bb0a945ec5f6ccf8d981b0dd054c6654dfd
jehuan <jehuan@microsoft.com>
2017-07-04 15:42:28 +0800

    minor: update namespace

--- plugins/Microsoft.DocAsCode.Build.TagsLevelRestApi/SplitRestApiToTagsLevel.cs
+++ plugins/Microsoft.DocAsCode.Build.TagsLevelRestApi/SplitRestApiToTagsLevel.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.Build.RestApi
+namespace Microsoft.DocAsCode.Build.TagsLevelRestApi
 {
     using System;
     using System.Collections.Generic;
