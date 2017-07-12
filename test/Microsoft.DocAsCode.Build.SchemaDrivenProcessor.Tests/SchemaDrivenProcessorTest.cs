5c2334882b1157c94c19ca394be05ed4e573a823
lianwei <lianwei@microsoft.com>
2017-07-12 11:45:02 +0800

    Add skeleton for sdp

---
+++ test/Microsoft.DocAsCode.Build.SchemaDrivenProcessor.Tests/SchemaDrivenProcessorTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.SchemaDrivenProcessor.Tests
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
+
+    using Microsoft.DocAsCode.Build.Engine;
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.DataContracts.Common;
+    using Microsoft.DocAsCode.Plugins;
+    using Microsoft.DocAsCode.Tests.Common;
+
+    using Newtonsoft.Json.Linq;
+    using Xunit;
+
+    [Trait("Owner", "lianwei")]
+    [Trait("EntityType", "SchemaDrivenProcessorTest")]
+    public class SchemaDrivenProcessorTest : TestBase
+    {
+        public SchemaDrivenProcessorTest()
+        {
+        }
+    }
+}
