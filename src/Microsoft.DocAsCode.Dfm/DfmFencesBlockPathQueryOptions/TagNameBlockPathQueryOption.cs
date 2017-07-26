3b0789eadf27e3a4dc56013c194acddb68df70cd
vwxyzh <vwxyzh@gmail.com>
2017-07-26 10:00:33 +0800

    add language support for aspx-cs/vb.

--- src/Microsoft.DocAsCode.Dfm/DfmFencesBlockPathQueryOptions/TagNameBlockPathQueryOption.cs
+++ src/Microsoft.DocAsCode.Dfm/DfmFencesBlockPathQueryOptions/TagNameBlockPathQueryOption.cs
@@ -80,7 +80,7 @@ namespace Microsoft.DocAsCode.Dfm
                 .AddAlias("fsharp", "fs", ".fs", ".fsi", ".fsx")
                 .AddAlias("go", "golang", ".go")
                 .AddAlias("haskell", ".hs")
-                .AddAlias("html", ".html", ".cshtml", "cshtml", ".vbhtml", "vbhtml", ".jsp", ".asp", ".aspx", "ascx")
+                .AddAlias("html", ".html", ".cshtml", "cshtml", ".vbhtml", "vbhtml", ".jsp", "aspx-cs", "aspx-csharp", "aspx-vb" ,".asp", ".aspx", ".ascx")
                 .AddAlias("java", ".java")
                 .AddAlias("javascript", "js", "node", ".js")
                 .AddAlias("lisp", ".lisp", ".lsp")
