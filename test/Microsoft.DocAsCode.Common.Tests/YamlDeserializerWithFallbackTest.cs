2461e6d0a704db2736cce5aa073adc1a4e234105
vwxyzh <vwxyzh@gmail.com>
2017-08-10 17:28:52 +0800

    add test case for multi fallback.

--- test/Microsoft.DocAsCode.Common.Tests/YamlDeserializerWithFallbackTest.cs
+++ test/Microsoft.DocAsCode.Common.Tests/YamlDeserializerWithFallbackTest.cs
@@ -40,5 +40,39 @@ namespace Microsoft.DocAsCode.Common.Tests
                 Assert.Equal(3, ex.Start.Column);
             }
         }
+
+        [Fact]
+        public void TestYamlDeserializerWithFallback_MultiFallback()
+        {
+            var deserialzer = YamlDeserializerWithFallback.Create<int>()
+                .WithFallback<string>()
+                .WithFallback<string[]>();
+            {
+                var obj = deserialzer.Deserialize(() => new StringReader(@"1"));
+                Assert.NotNull(obj);
+                var a = Assert.IsType<int>(obj);
+                Assert.Equal(1, a);
+            }
+            {
+                var obj = deserialzer.Deserialize(() => new StringReader(@"A"));
+                Assert.NotNull(obj);
+                var a = Assert.IsType<string>(obj);
+                Assert.Equal("A", a);
+            }
+            {
+                var obj = deserialzer.Deserialize(() => new StringReader(@"- A
+- B"));
+                Assert.NotNull(obj);
+                var a = Assert.IsType<string[]>(obj);
+                Assert.Equal("A", a[0]);
+                Assert.Equal("B", a[1]);
+            }
+            {
+                var ex = Assert.Throws<YamlException>(() => deserialzer.Deserialize(() => new StringReader(@"- A
+- A: abc")));
+                Assert.Equal(2, ex.Start.Line);
+                Assert.Equal(3, ex.Start.Column);
+            }
+        }
     }
 }
