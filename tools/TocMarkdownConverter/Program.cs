678e65ada0e7f25aa48abd84cf5c45c6163e188e
qinezh <qinzhu@microsoft.com>
2017-08-16 14:16:14 +0800

    Add converter tool for toc.md

---
+++ tools/TocMarkdownConverter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace TocMarkdownConverter
+{
+    internal sealed class Program
+    {
+        private static readonly string MarkdownExtension = ".md";
+
+        private static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                PrintUsage();
+                return 1;
+            }
+
+            var tocMarkdown = args[0];
+            var extension = Path.GetExtension(tocMarkdown);
+            if (!string.Equals(extension, MarkdownExtension, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Only support to convert toc markdown file.");
+                return 1;
+            }
+
+            var tocYml = args.Length > 1 ? args[1] : null;
+            try
+            {
+                tocYml = TocConverter.Convert(tocMarkdown, tocYml);
+                Console.WriteLine($"Successfully convert {tocMarkdown} to {tocYml}.");
+
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to convert {tocMarkdown} to {tocYml}, {ex.Message}");
+
+                return 1;
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine($"\t{AppDomain.CurrentDomain.FriendlyName} <path of toc markdown file> [path of generated toc yaml file]");
+        }
+    }
+}
---
+++ tools/TocMarkdownConverter/TocConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.DocAsCode.Build.TableOfContents;
+using Microsoft.DocAsCode.Common;
+
+namespace TocMarkdownConverter
+{
+    internal static class TocConverter
+    {
+        private static readonly string YmlExtension = ".yml";
+
+        public static string Convert(string tocMarkdownFilePath, string tocYmlPath = null)
+        {
+            if (string.IsNullOrEmpty(tocMarkdownFilePath))
+            {
+                throw new ArgumentException($"{nameof(tocMarkdownFilePath)} can't be null or empty");
+            }
+
+            if (!File.Exists(tocMarkdownFilePath))
+            {
+                throw new FileNotFoundException($"{tocMarkdownFilePath} can't be found.");
+            }
+
+            if (string.IsNullOrEmpty(tocYmlPath))
+            {
+                var tocName = Path.GetFileNameWithoutExtension(tocMarkdownFilePath);
+                var tocDir = Path.GetDirectoryName(tocMarkdownFilePath);
+
+                tocYmlPath = Path.Combine(tocDir, tocName + YmlExtension);
+            }
+
+            ConvertCore(tocMarkdownFilePath, tocYmlPath);
+
+            return tocYmlPath;
+        }
+
+        private static void ConvertCore(string tocMarkdownFilePath, string tocYmlPath)
+        {
+            using (var sr = new StreamReader(tocMarkdownFilePath))
+            {
+                var tocModel = MarkdownTocReader.LoadToc(sr.ReadToEnd(), tocMarkdownFilePath);
+                YamlUtility.Serialize(tocYmlPath, tocModel);
+            }
+        }
+    }
+}
