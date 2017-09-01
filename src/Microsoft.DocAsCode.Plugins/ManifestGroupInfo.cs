ed701340f102c7f4528a9cf67ea76f6949959c02
Renze Yu <renzeyu@microsoft.com>
2017-09-01 17:20:43 +0800

    add groups to manifest (#2019)
    * add groups to manifest
    
    * fix copyright header

---
+++ src/Microsoft.DocAsCode.Plugins/ManifestGroupInfo.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Plugins
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public class ManifestGroupInfo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("dest")]
+        public string Destination { get; set; }
+
+        [JsonProperty("xrefmap")]
+        public string XRefmap { get; set; }
+
+        [JsonExtensionData]
+        public Dictionary<string, object> Metadata { get; set; } = new Dictionary<string, object>();
+
+        public ManifestGroupInfo(GroupInfo groupInfo)
+        {
+            if (groupInfo == null)
+            {
+                return;
+            }
+
+            Name = groupInfo.Name;
+            Destination = groupInfo.Destination;
+            Metadata = groupInfo.Metadata;
+        }
+    }
+}
