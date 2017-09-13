cfc66175f1b4d6145c0377c59d21a3e9ef5c37ae
Qinen Zhu <qinezh@users.noreply.github.com>
2017-09-13 13:36:54 +0800

    Move validation config to plugin project (#2054)

--- src/Microsoft.DocAsCode.Plugins/MarkdownSytleDefinition.cs
+++ src/Microsoft.DocAsCode.Plugins/MarkdownSytleDefinition.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.Dfm.MarkdownValidators
+namespace Microsoft.DocAsCode.Plugins
 {
     using System.Collections.Generic;
 
