bacbd24f1b14c574e56271fc89e75d7a207f1e99
Jiayin Pei <jipe@microsoft.com>
2017-09-25 16:57:41 +0800

    fix

--- src/Microsoft.DocAsCode.Dfm/DfmCodeRenderer.cs
+++ src/Microsoft.DocAsCode.Dfm/DfmCodeRenderer.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-
 namespace Microsoft.DocAsCode.Dfm
 {
+    using System;
     using System.IO;
 
     using Microsoft.DocAsCode.Common;
