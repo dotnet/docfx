37fdab62019021f237a9ec22355df45d80a7332e
vwxyzh <vwxyzh@gmail.com>
2017-09-26 10:44:42 +0800

    add xref client

---
+++ src/Microsoft.DocAsCode.Build.Engine/XrefClient.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Engine
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Plugins;
+
+    public class XrefClient
+    {
+        public static readonly XrefClient Default = new XrefClient();
+        private static readonly HttpClient _sharedClient =
+            new Func<HttpClient>(() =>
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return client;
+            })();
+        private readonly HttpClient _client;
+
+        public XrefClient()
+            : this(_sharedClient) { }
+
+        public XrefClient(HttpClient client)
+        {
+            _client = client ?? _sharedClient;
+        }
+
+        public async Task<XRefSpec[]> ResloveAsync(string url)
+        {
+            using (var stream = await _client.GetStreamAsync(url))
+            using (var sr = new StreamReader(stream))
+            {
+                return JsonUtility.Deserialize<XRefSpec[]>(sr);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.DocAsCode.Build.Engine/XrefServiceResolver.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.Engine
+{
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Plugins;
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class XrefServiceResolver
+    {
+        private readonly ImmutableArray<string> _xrefServiceUrls;
+
+        public XrefServiceResolver(ImmutableArray<string> xrefServiceUrls)
+        {
+            _xrefServiceUrls = xrefServiceUrls;
+        }
+
+        private async Task<List<string>> ResolveByXRefServiceAsync(List<string> uidList, ConcurrentDictionary<string, XRefSpec> externalXRefSpec)
+        {
+            if (_xrefServiceUrls == null || _xrefServiceUrls.Length == 0)
+            {
+                return uidList;
+            }
+
+            var unresolvedUidList = new List<string>();
+            var resolve = GetResolver();
+
+            // todo : parallel.
+            foreach (var uid in uidList)
+            {
+                var result = await resolve(uid);
+                if (resolve == null)
+                {
+                    unresolvedUidList.Add(uid);
+                }
+                else
+                {
+                    externalXRefSpec.TryAdd(uid, result);
+                }
+            }
+            return unresolvedUidList;
+        }
+
+        private Func<string, Task<XRefSpec>> GetResolver()
+        {
+            var uriTemplates =
+                (from url in _xrefServiceUrls
+                 select UriTemplate.Create(url, XrefClient.Default.ResloveAsync, GetPipeline)).ToList();
+            return async uid =>
+            {
+                var d = new Dictionary<string, string> { ["uid"] = uid };
+                foreach (var t in uriTemplates)
+                {
+                    XRefSpec[] value = null;
+                    try
+                    {
+                        value = await t.Evaluate(d);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    if (value != null && value.Length > 0)
+                    {
+                        return value[0];
+                    }
+                }
+                return null;
+            };
+        }
+
+        private IUriTemplatePipeline<Task<XRefSpec[]>> GetPipeline(string name)
+        {
+            // todo : add pipeline.
+            return EmptyUriTemplatePipeline.Default;
+        }
+
+        private sealed class EmptyUriTemplatePipeline : IUriTemplatePipeline<Task<XRefSpec[]>>
+        {
+            public static readonly EmptyUriTemplatePipeline Default = new EmptyUriTemplatePipeline();
+
+            public Task<XRefSpec[]> Handle(Task<XRefSpec[]> value, string[] parameters)
+            {
+                return value;
+            }
+        }
+    }
+}
