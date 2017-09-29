8f28224a9a150c77ff4750a4184a3adb49b5522f
Liangying.Wei <lianwei@microsoft.com>
2017-09-29 15:06:18 +0800

    Add XRefService projects (#2059)
    * Add XRefService projects
    
    * Rename namespace, change API url
    
    * resolve comments
    
    * fix build failure
    
    * Resolve comments
    
    * return accepted for time consuming upload

---
+++ tools/XRefService/XRefService.Common/Models/XRefMap.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace XRefService.Common.Models
+{
+    using System.Collections.Generic;
+    using YamlDotNet.Serialization;
+
+    using Microsoft.DocAsCode.Plugins;
+
+    public class XRefMap
+    {
+        [YamlMember(Alias = "references")]
+        public List<XRefSpec> References { get; set; }
+    }
+}
---
+++ tools/XRefService/XRefService/Program.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace XRefService.Get
+{
+    using System.IO;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
