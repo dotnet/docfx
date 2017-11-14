579fb66ff387ad7105bc191962a5617b4d9452b0
vwxyzh <vwxyzh@gmail.com>
2017-11-14 14:20:21 +0800

    add support for tab combining.

--- test/Microsoft.DocAsCode.Dfm.Tests/DfmTest.cs
+++ test/Microsoft.DocAsCode.Dfm.Tests/DfmTest.cs
@@ -156,6 +156,59 @@ content-b
 
         [Fact]
         [Trait("Related", "DfmMarkdown")]
+        public void TestTabGroup_Combining()
+        {
+            var options = DocfxFlavoredMarked.CreateDefaultOptions();
+            options.ShouldExportSourceInfo = true;
+            var actual = DocfxFlavoredMarked.Markup(null, null, options, @"# [title-a or b](#tab/a+b)
+content-a or b
+# [title-c](#tab/c)
+content-c
+- - -
+# [title-a](#tab/a)
+content-a
+# [title-b or c](#tab/b+c)
+content-b or c
+- - -", "test.md");
+            var groupId = "uBn0rykxXo";
+            var expected = $@"<div class=""tabGroup"" id=""tabgroup_{groupId}"" sourceFile=""test.md"" sourceStartLineNumber=""1"" sourceEndLineNumber=""5"">
+<ul role=""tablist"">
+<li role=""presentation"">
+<a href=""#tabpanel_{groupId}_a+b"" role=""tab"" aria-controls=""tabpanel_{groupId}_a+b"" data-tab=""a+b"" tabindex=""0"" aria-selected=""true"" sourceFile=""test.md"" sourceStartLineNumber=""1"" sourceEndLineNumber=""1"">title-a or b</a>
+</li>
+<li role=""presentation"">
+<a href=""#tabpanel_{groupId}_c"" role=""tab"" aria-controls=""tabpanel_{groupId}_c"" data-tab=""c"" tabindex=""-1"" sourceFile=""test.md"" sourceStartLineNumber=""3"" sourceEndLineNumber=""3"">title-c</a>
+</li>
+</ul>
+<section id=""tabpanel_{groupId}_a+b"" role=""tabpanel"" data-tab=""a+b"">
+<p sourceFile=""test.md"" sourceStartLineNumber=""2"" sourceEndLineNumber=""2"">content-a or b</p>
+</section>
+<section id=""tabpanel_{groupId}_c"" role=""tabpanel"" data-tab=""c"" aria-hidden=""true"" hidden=""hidden"">
+<p sourceFile=""test.md"" sourceStartLineNumber=""4"" sourceEndLineNumber=""4"">content-c</p>
+</section>
+</div>
+<div class=""tabGroup"" id=""tabgroup_{groupId}-1"" sourceFile=""test.md"" sourceStartLineNumber=""6"" sourceEndLineNumber=""10"">
+<ul role=""tablist"">
+<li role=""presentation"">
+<a href=""#tabpanel_{groupId}-1_a"" role=""tab"" aria-controls=""tabpanel_uBn0rykxXo-1_a"" data-tab=""a"" tabindex=""0"" aria-selected=""true"" sourceFile=""test.md"" sourceStartLineNumber=""6"" sourceEndLineNumber=""6"">title-a</a>
+</li>
+<li role=""presentation"">
+<a href=""#tabpanel_{groupId}-1_b+c"" role=""tab"" aria-controls=""tabpanel_uBn0rykxXo-1_b+c"" data-tab=""b+c"" tabindex=""-1"" sourceFile=""test.md"" sourceStartLineNumber=""8"" sourceEndLineNumber=""8"">title-b or c</a>
+</li>
+</ul>
+<section id=""tabpanel_{groupId}-1_a"" role=""tabpanel"" data-tab=""a"">
+<p sourceFile=""test.md"" sourceStartLineNumber=""7"" sourceEndLineNumber=""7"">content-a</p>
+</section>
+<section id=""tabpanel_{groupId}-1_b+c"" role=""tabpanel"" data-tab=""b+c"" aria-hidden=""true"" hidden=""hidden"">
+<p sourceFile=""test.md"" sourceStartLineNumber=""9"" sourceEndLineNumber=""9"">content-b or c</p>
+</section>
+</div>
+";
+            Assert.Equal(expected.Replace("\r\n", "\n"), actual);
+        }
+
+        [Fact]
+        [Trait("Related", "DfmMarkdown")]
         public void TestCodeFenceWithSpaceInFileName()
         {
             var source = @"  [!code-csharp  [  Test Space  ] ( test space in\) link.cs#abc ""title test"" ) ]  ";
@@ -341,51 +394,6 @@ world</p>
 <hr/>
 ";
             var marked = DocfxFlavoredMarked.Markup(source);
-            Assert.Equal(expected.Replace("\r\n", "\n"), marked);
-        }
-
-        [Fact]
-        [Trait("Related", "DfmMarkdown")]
-        public void TestDfm_InvalidYamlHeader_YamlUtilityThrowException()
-        {
-            var source = @"---
-- Jon Schlinkert
-- Brian Woodward
-
----
-
-1
-===
-1.1
----
-blabla
-
-1.2
----
-blabla";
-            var expected = @"<hr/>
-<ul>
-<li>Jon Schlinkert</li>
-<li>Brian Woodward</li>
-</ul>
-<hr/>
-<h1 id=""1"">1</h1>
-<h2 id=""11"">1.1</h2>
-<p>blabla</p>
-<h2 id=""12"">1.2</h2>
-<p>blabla</p>
-";
-
-            var listener = TestLoggerListener.CreateLoggerListenerWithPhaseEqualFilter("YamlHeader");
-            Logger.RegisterListener(listener);
-            string marked;
-            using (new LoggerPhaseScope("YamlHeader"))
-            {
-                marked = DocfxFlavoredMarked.Markup(source);
-            }
-            Logger.UnregisterListener(listener);
-
-            Assert.Equal(1, listener.Items.Count(i => i.LogLevel == LogLevel.Warning));
             Assert.Equal(expected.Replace("\r\n", "\n"), marked);
         }
 
@@ -672,81 +680,6 @@ tag started with alphabet should not be encode: <abc> <a-hello> <AC att='bcd'></
 ";
             var marked = DocfxFlavoredMarked.Markup(source);
             Assert.Equal(expected.Replace("\r\n", "\n"), marked);
-        }
-
-        [Fact]
-        [Trait("Related", "DfmMarkdown")]
-        public void TestDfmTagValidate()
-        {
-            var builder = new DfmEngineBuilder(new Options() { Mangle = false });
-            var mrb = new MarkdownValidatorBuilder(
-                new CompositionContainer(
-                    new ContainerConfiguration()
-                        .WithAssembly(typeof(DfmTest).Assembly)
-                        .CreateContainer()));
-            mrb.AddTagValidators(new[]
-            {
-                new MarkdownTagValidationRule
-                {
-                    TagNames = new List<string> { "em", "div" },
-                    MessageFormatter = "Invalid tag({0})!",
-                    Behavior = TagValidationBehavior.Error,
-                    OpeningTagOnly = true,
-                },
-                new MarkdownTagValidationRule
-                {
-                    TagNames = new List<string> { "h1" },
-                    MessageFormatter = "Warning tag({0})!",
-                    Behavior = TagValidationBehavior.Warning,
-                },
-                new MarkdownTagValidationRule
-                {
-                    TagNames = new List<string> { "script" },
-                    MessageFormatter = "Warning tag({0})!",
-                    Behavior = TagValidationBehavior.Warning,
-                },
-                new MarkdownTagValidationRule
-                {
-                    TagNames = new List<string> { "pre" },
-                    MessageFormatter = "Warning tag({0})!",
-                    Behavior = TagValidationBehavior.Warning,
-                },
-            });
-            mrb.AddValidators(new[]
-            {
-                new MarkdownValidationRule
-                {
-                    ContractName =  HtmlMarkdownTokenValidatorProvider.ContractName,
-                }
-            });
-            builder.Rewriter = mrb.CreateRewriter();
-
-            var engine = builder.CreateDfmEngine(new DfmRenderer());
-            var listener = TestLoggerListener.CreateLoggerListenerWithPhaseEqualFilter("test!!!!" + "." + MarkdownValidatorBuilder.MarkdownValidatePhaseName);
-            Logger.RegisterListener(listener);
-            string result;
-            using (new LoggerPhaseScope("test!!!!"))
-            {
-                result = engine.Markup(@"<div><i>x</i><EM>y</EM><h1>z<pre><code>a*b*c</code></pre></h1></div>
-
-<script>alert(1);</script>", "test");
-            }
-            Logger.UnregisterListener(listener);
-            Assert.Equal(@"<div><i>x</i><EM>y</EM><h1>z<pre><code>a*b*c</code></pre></h1></div>
-
-<script>alert(1);</script>".Replace("\r\n", "\n"), result);
-            Assert.Equal(8, listener.Items.Count);
-            Assert.Equal(new[]
-            {
-                HtmlMarkdownTokenValidatorProvider.WarningMessage,
-                "Invalid tag(div)!",
-                "Invalid tag(EM)!",
-                "Warning tag(h1)!",
-                "Warning tag(pre)!",
-                "Warning tag(h1)!",
-                "Html Tag!",
-                "Warning tag(script)!",
-            }, from item in listener.Items select item.Message);
         }
     }
 }
