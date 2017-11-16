b8563c066c63888cd2f12cb1682763f37e66307a
Qinen Zhu <qinzhu@microsoft.com>
2017-11-16 14:40:34 +0800

    Add project Microsoft.DocAsCode.MarkdigMarkdownRewriters

---
+++ src/Microsoft.DocAsCode.MarkdigMarkdownRewriters/MarkdigMarkdownRenderer.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.MarkdigMarkdownRewriters
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Dfm;
+    using Microsoft.DocAsCode.MarkdownLite;
+
+    public class MarkdigMarkdownRenderer : DfmMarkdownRenderer
+    {
+    }
+}
