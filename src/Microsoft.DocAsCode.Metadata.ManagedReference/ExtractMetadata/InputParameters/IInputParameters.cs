8795612254e1570feb989a229b4da43cb5285f9f
lianwei <lianwei@microsoft.com>
2017-11-28 15:34:11 +0800

    minor fix

--- src/Microsoft.DocAsCode.Metadata.ManagedReference/ExtractMetadata/InputParameters/IInputParameters.cs
+++ src/Microsoft.DocAsCode.Metadata.ManagedReference/ExtractMetadata/InputParameters/IInputParameters.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-
 namespace Microsoft.DocAsCode.Metadata.ManagedReference
 {
+    using System.Collections.Generic;
+
     interface IInputParameters
     {
         ExtractMetadataOptions Options { get; }
