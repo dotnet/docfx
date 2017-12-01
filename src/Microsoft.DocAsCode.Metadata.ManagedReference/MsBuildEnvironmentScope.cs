c0f67f61b1365e88516ad44b7ae9c2328d31578b
lianwei <lianwei@microsoft.com>
2017-12-01 11:27:54 +0800

    Fix issue running under netcore 2.0

--- src/Microsoft.DocAsCode.Metadata.ManagedReference/MsBuildEnvironmentScope.cs
+++ src/Microsoft.DocAsCode.Metadata.ManagedReference/MsBuildEnvironmentScope.cs
@@ -5,30 +5,81 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
 
     using Microsoft.Build.MSBuildLocator;
     using Microsoft.DocAsCode.Common;
 
     public class MSBuildEnvironmentScope : IDisposable
     {
+        private static readonly Regex DotnetBasePathRegex = new Regex("Base Path:(.*)$", RegexOptions.Compiled | RegexOptions.Multiline);
         private readonly EnvironmentScope _innerScope;
 
         public MSBuildEnvironmentScope()
         {
-            // workaround for https://github.com/dotnet/docfx/issues/1969
-            // FYI https://github.com/dotnet/roslyn/issues/21799#issuecomment-343695700
-            var instances = MSBuildLocator.QueryVisualStudioInstances();
-            var latest = instances.FirstOrDefault(a => a.Version.Major == 15);
-            if (latest != null)
+            // workaround for https://github.com/dotnet/docfx/issues/1752
+            var instances = MSBuildLocator.QueryVisualStudioInstances().ToList();
+            if (instances.Count == 0)
             {
-                Logger.LogInfo($"Using msbuild {latest.MSBuildPath} as inner compiler.");
-
-                _innerScope = new EnvironmentScope(new Dictionary<string, string>
+                // when no visual studio installed, try detect dotnet
+                var dotnetBasePath = GetDotnetBasePath();
+                if (dotnetBasePath != null)
                 {
-                    ["VSINSTALLDIR"] = latest.VisualStudioRootPath,
-                    ["VisualStudioVersion"] = "15.0"
-                });
+                    Logger.LogInfo($"Using dotnet {dotnetBasePath} as inner comipiler.");
+                    _innerScope = new EnvironmentScope(new Dictionary<string, string>
+                    {
+                        ["MSBuild_EXE_PATH"] = dotnetBasePath + "MSBuild.dll",
+                        ["MSBuildExtensionsPath"] = dotnetBasePath,
+                        ["MSBuildSDKsPath"] = dotnetBasePath + "Sdks"
+                    });
+                }
+            }
+            else
+            {
+                // workaround for https://github.com/dotnet/docfx/issues/1969
+                // FYI https://github.com/dotnet/roslyn/issues/21799#issuecomment-343695700
+                var latest = instances.FirstOrDefault(a => a.Version.Major == 15);
+                if (latest != null)
+                {
+                    Logger.LogInfo($"Using msbuild {latest.MSBuildPath} as inner comipiler.");
+
+                    _innerScope = new EnvironmentScope(new Dictionary<string, string>
+                    {
+                        ["VSINSTALLDIR"] = latest.VisualStudioRootPath,
+                        ["VisualStudioVersion"] = "15.0"
+                    });
+                }
+            }
+        }
+
+        private string GetDotnetBasePath()
+        {
+            using (var outputStream = new MemoryStream())
+            {
+                using (var outputStreamWriter = new StreamWriter(outputStream))
+                {
+                        var exitCode = CommandUtility.RunCommand(new CommandInfo
+                        {
+                            Name = "dotnet",
+                            Arguments = "--info"
+                        }, outputStreamWriter, timeoutInMilliseconds: 60000);
+
+                    // writer streams have to be flushed before reading from memory streams
+                    // make sure that streamwriter is not closed before reading from memory stream
+                    outputStreamWriter.Flush();
+
+                    var outputString = System.Text.Encoding.UTF8.GetString(outputStream.ToArray());
+
+                    var matched = DotnetBasePathRegex.Match(outputString);
+                    if (matched.Success)
+                    {
+                        return matched.Groups[1].Value.Trim();
+                    }
+
+                    return null;
+                }
             }
         }
 
