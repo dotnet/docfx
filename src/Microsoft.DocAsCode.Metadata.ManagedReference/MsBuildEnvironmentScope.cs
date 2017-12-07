4d979568c4568cae2e58c1081fad10b481dd77a1
Liangying.Wei <lianwei@microsoft.com>
2017-12-07 10:52:50 +0800

    Fix cross platform issue (#2268)

--- src/Microsoft.DocAsCode.Metadata.ManagedReference/MsBuildEnvironmentScope.cs
+++ src/Microsoft.DocAsCode.Metadata.ManagedReference/MsBuildEnvironmentScope.cs
@@ -11,47 +11,105 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference
 
     using Microsoft.Build.MSBuildLocator;
     using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.Exceptions;
+
+    using AsyncGenerator.Internal;
 
     public class MSBuildEnvironmentScope : IDisposable
     {
+        private const string VSInstallDirKey = "VSINSTALLDIR";
+        private const string MSBuildExePathKey = "MSBuild_EXE_PATH";
         private static readonly Regex DotnetBasePathRegex = new Regex("Base Path:(.*)$", RegexOptions.Compiled | RegexOptions.Multiline);
         private readonly EnvironmentScope _innerScope;
 
         public MSBuildEnvironmentScope()
         {
-            // workaround for https://github.com/dotnet/docfx/issues/1752
-            var instances = MSBuildLocator.QueryVisualStudioInstances().ToList();
-            if (instances.Count == 0)
-            {
-                // when no visual studio installed, try detect dotnet
-                var dotnetBasePath = GetDotnetBasePath();
-                if (dotnetBasePath != null)
-                {
-                    Logger.LogInfo($"Using dotnet {dotnetBasePath} as inner compiler.");
-                    _innerScope = new EnvironmentScope(new Dictionary<string, string>
-                    {
-                        ["MSBuild_EXE_PATH"] = dotnetBasePath + "MSBuild.dll",
-                        ["MSBuildExtensionsPath"] = dotnetBasePath,
-                        ["MSBuildSDKsPath"] = dotnetBasePath + "Sdks"
-                    });
-                }
-            }
-            else
-            {
-                // workaround for https://github.com/dotnet/docfx/issues/1969
-                // FYI https://github.com/dotnet/roslyn/issues/21799#issuecomment-343695700
-                var latest = instances.FirstOrDefault(a => a.Version.Major == 15);
-                if (latest != null)
-                {
-                    Logger.LogInfo($"Using msbuild {latest.MSBuildPath} as inner compiler.");
+            _innerScope = GetScope();
+        }
 
-                    _innerScope = new EnvironmentScope(new Dictionary<string, string>
+        private EnvironmentScope GetScope()
+        {
+            var vsInstallDirEnv = Environment.GetEnvironmentVariable(VSInstallDirKey);
+            if (!string.IsNullOrEmpty(vsInstallDirEnv))
+            {
+                Logger.LogInfo($"Environment variable {VSInstallDirKey} is set to {vsInstallDirEnv}, it is used as the inner compiler.");
+                return null;
+            }
+
+            var msbuildExePathEnv = Environment.GetEnvironmentVariable(MSBuildExePathKey);
+
+            if (!string.IsNullOrEmpty(msbuildExePathEnv))
+            {
+                Logger.LogInfo($"Environment variable {MSBuildExePathKey} is set to {msbuildExePathEnv}, it is used as the inner compiler.");
+                return null;
+            }
+
+            if (EnvironmentHelper.IsMono)
+            {
+                string extensionPath = null;
+                string msbuildPath = null;
+                EnvironmentHelper.GetMonoMsBuildPath(monoDir =>
+                {
+                    extensionPath = Path.Combine(monoDir, "xbuild");
+                    msbuildPath = Path.Combine(monoDir, "msbuild", "15.0", "bin", "MSBuild.dll");
+                });
+
+                if (msbuildPath == null || !File.Exists(msbuildPath))
+                {
+                    var message = $"Unable to find msbuild from {msbuildPath}, please try downloading latest mono to solve the issue.";
+                    Logger.LogError(message);
+                    throw new DocfxException(message);
+                }
+
+                return new EnvironmentScope(new Dictionary<string, string>
+                {
+                    [MSBuildExePathKey] = msbuildPath,
+                    ["MSBuildExtensionsPath"] = extensionPath
+                });
+            }
+
+            try
+            {
+                var instances = MSBuildLocator.QueryVisualStudioInstances().ToList();
+                if (instances.Count == 0)
+                {
+                    // when no visual studio installed, try detect dotnet
+                    // workaround for https://github.com/dotnet/docfx/issues/1752
+                    var dotnetBasePath = GetDotnetBasePath();
+                    if (dotnetBasePath != null)
                     {
-                        ["VSINSTALLDIR"] = latest.VisualStudioRootPath,
-                        ["VisualStudioVersion"] = "15.0"
-                    });
+                        Logger.LogInfo($"Using dotnet {dotnetBasePath} as inner compiler.");
+                        return new EnvironmentScope(new Dictionary<string, string>
+                        {
+                            [MSBuildExePathKey] = dotnetBasePath + "MSBuild.dll",
+                            ["MSBuildExtensionsPath"] = dotnetBasePath,
+                            ["MSBuildSDKsPath"] = dotnetBasePath + "Sdks"
+                        });
+                    }
+                }
+                else
+                {
+                    // workaround for https://github.com/dotnet/docfx/issues/1969
+                    // FYI https://github.com/dotnet/roslyn/issues/21799#issuecomment-343695700
+                    var latest = instances.FirstOrDefault(a => a.Version.Major == 15);
+                    if (latest != null)
+                    {
+                        Logger.LogInfo($"Using msbuild {latest.MSBuildPath} as inner compiler.");
+
+                        return new EnvironmentScope(new Dictionary<string, string>
+                        {
+                            [VSInstallDirKey] = latest.VisualStudioRootPath,
+                            ["VisualStudioVersion"] = "15.0"
+                        });
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Logger.LogDiagnostic($"Have trouble locating MSBuild, if you meet issue similar to https://github.com/dotnet/docfx/issues/1969, try setting environment value VSINSTALLDIR and VisualStudioVersion as a workaround: {e.Message}");
+            }
+
+            return null;
         }
 
         private string GetDotnetBasePath()
