896efe54de741b56b41b68335abac57f01209c61
Lex Li <support@lextm.com>
2017-12-12 09:38:44 -0500

    Added a converter for MergeJsonConfig.

--- src/docfx/Models/MergeJsonConfig.cs
+++ src/docfx/Models/MergeJsonConfig.cs
@@ -3,8 +3,10 @@
 
 namespace Microsoft.DocAsCode
 {
+    using Newtonsoft.Json;
     using System.Collections.Generic;
 
+    [JsonConverter(typeof(MergeJsonConfigConverter))]
     public class MergeJsonConfig : List<MergeJsonItemConfig>
     {
         public MergeJsonConfig(IEnumerable<MergeJsonItemConfig> configs) : base(configs) { }
