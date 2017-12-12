cc99c40315fe7315083cc32dddac07ae0275e197
Renze Yu <renzeyu@microsoft.com>
2017-12-12 16:31:39 +0800

    add group to FileLinkInfo (#2300)
    * add group to FileLinkInfo
    
    * add blank line

--- src/Microsoft.DocAsCode.Plugins/IFileLinkInfo.cs
+++ src/Microsoft.DocAsCode.Plugins/IFileLinkInfo.cs
@@ -9,33 +9,45 @@ namespace Microsoft.DocAsCode.Plugins
         /// The path of link from file in source folder.
         /// </summary>
         string FromFileInSource { get; }
+
         /// <summary>
         /// The path of link from file in dest folder.
         /// </summary>
         string FromFileInDest { get; }
+
         /// <summary>
         /// The path of link to file in source folder.
         /// </summary>
         string ToFileInSource { get; }
+
         /// <summary>
         /// The path of link to file in dest folder.
         /// </summary>
         string ToFileInDest { get; }
+
         /// <summary>
         /// The file link in source folder.
         /// </summary>
         string FileLinkInSource { get; }
+
         /// <summary>
         /// The file link in dest folder.
         /// </summary>
         string FileLinkInDest { get; }
+
         /// <summary>
         /// The href.
         /// </summary>
         string Href { get; }
+
         /// <summary>
         /// Is resolved by DocFX.
         /// </summary>
         bool IsResolved { get; }
+
+        /// <summary>
+        /// The group that current link belongs to
+        /// </summary>
+        string Group { get; }
     }
 }
