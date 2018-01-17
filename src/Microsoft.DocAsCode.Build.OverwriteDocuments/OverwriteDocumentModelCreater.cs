cd652f6e90fc325212a61a6672f646227f11d4f8
Renze Yu <renzeyu@microsoft.com>
2018-01-17 09:33:57 +0800

    add MarkdownFragments project (#2354)
    * add MarkdownFragments project
    
    * preserve source info in models
    
    * updtae model
    
    * rename project

---
+++ src/Microsoft.DocAsCode.Build.OverwriteDocuments/OverwriteDocumentModelCreater.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.OverwriteDocuments
+{
+    using Microsoft.DocAsCode.Build.Common;
+
+    public class OverwriteDocumentModelCreater
+    {
+        public OverwriteDocumentModel Create(MarkdownFragmentModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
