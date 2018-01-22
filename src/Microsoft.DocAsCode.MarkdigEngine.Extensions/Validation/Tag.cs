628bbf20893bb88fd1d312ebbcf6517bb2b03929
Qinen Zhu <qinzhu@microsoft.com>
2018-01-22 11:51:35 +0800

    Update namespace of markdig engine

--- src/Microsoft.DocAsCode.MarkdigEngine.Extensions/Validation/Tag.cs
+++ src/Microsoft.DocAsCode.MarkdigEngine.Extensions/Validation/Tag.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace MarkdigEngine.Extensions
+namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
 {
     using System.Collections.Generic;
     using System.Linq;
