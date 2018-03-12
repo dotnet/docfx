93599084375930aef622a3bddce42b0f7da79ae4
Jiayin Pei <928PJY@users.noreply.github.com>
2018-03-12 15:42:01 +0800

    extract interfaces and data models for validation (#2515)
    * extract interfaces and data models for validation
    
    * rename

--- src/Microsoft.DocAsCode.MarkdigEngine.Validators/IMarkdownValidatorContext.cs
+++ src/Microsoft.DocAsCode.MarkdigEngine.Validators/IMarkdownValidatorContext.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.Plugins
+namespace Microsoft.DocAsCode.MarkdigEngine.Validators
 {
     public interface IMarkdownValidatorContext
     {
