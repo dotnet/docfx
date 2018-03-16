40ccfa0ef1cf871deae273e6f57dca3fe9901c91
Renze Yu <renzeyu@microsoft.com>
2018-03-16 16:03:35 +0800

    use editable in schema tags

---
+++ src/Microsoft.DocAsCode.Build.SchemaDriven/SchemaExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Build.SchemaDriven
+{
+    public static class SchemaExtensions
+    {
+        private const string IsRequiredInFragmentsTag = "editable";
+
+        private const string IsLegalInFragmentsTag = "editable";
+
+        /// <summary>
+        /// Return if a property is required to appear in markdown fragments
+        /// </summary>
+        public static bool IsRequiredInFragments(this BaseSchema schema)
+            => schema.Tags.IndexOf(IsRequiredInFragmentsTag) >= 0;
+
+        /// <summary>
+        /// Return if a property is legal to appear in markdown fragmetns
+        /// </summary>
+        public static bool IsLegalInFragments(this BaseSchema schema)
+            => schema.Tags.IndexOf(IsLegalInFragmentsTag) >= 0;
+    }
+}
