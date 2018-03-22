430f563be3d6a715636fbf4c7d10c96d93d6d440
Renze Yu <renzeyu@microsoft.com>
2018-03-22 11:38:50 +0800

    extract SchemaFragmentsIterator (#2537)
    * extract SchemaFragmentsIterator
    
    * fix ut
    
    * resolve comments

--- src/Microsoft.DocAsCode.Build.OverwriteDocuments/ExtendedModels/MarkdownProperty.cs
+++ src/Microsoft.DocAsCode.Build.OverwriteDocuments/ExtendedModels/MarkdownProperty.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.Tools.YamlSplitter.Models
+namespace Microsoft.DocAsCode.Build.OverwriteDocuments
 {
     using System.Collections.Generic;
     using System.Text;
@@ -14,7 +14,7 @@ namespace Microsoft.DocAsCode.Tools.YamlSplitter.Models
 
         public void SerializeTo(StringBuilder sb)
         {
-            string wrapper = FragmentModelHelper.GetUidWrapper(OPath);
+            string wrapper = OverwriteUtility.GetUidWrapper(OPath);
             sb.Append("## " + wrapper);
             sb.Append(OPath);
             sb.AppendLine(wrapper);
---
+++ tools/YamlSplitter/UpdateFragmentsHandler.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DocAsCode.Tools.YamlSplitter
+{
+    using System.Collections.Generic;
+
+    using Microsoft.DocAsCode.Build.OverwriteDocuments;
+    using Microsoft.DocAsCode.Build.SchemaDriven;
+
+    using Newtonsoft.Json.Schema;
+    using YamlDotNet.RepresentationModel;
+
+    public class UpdateFragmentsHandler : ISchemaFragmentsHandler
+    {
+        public void HandleUid(string uidKey, YamlMappingNode node, Dictionary<string, MarkdownFragment> fragments, BaseSchema schema, string oPathPrefix, string uid)
+        {
+            fragments.AddOrUpdateFragmentEntity(node.Children[uidKey].ToString());
+        }
+
+        public void HandleProperty(string propertyKey, YamlMappingNode node, Dictionary<string, MarkdownFragment> fragments, BaseSchema schema, string oPathPrefix, string uid)
+        {
+            var propSchema = schema.Properties[propertyKey];
+            // TODO: consume "editable"
+            if (propSchema.Type == JSchemaType.String && propSchema.ContentType == ContentType.Markdown)
+            {
+                var val = "";
+                if (node.Children.ContainsKey(propertyKey))
+                {
+                    val = node.Children[propertyKey].ToString();
+                    node.Children.Remove(propertyKey);
+                }
+
+                fragments[uid].AddOrUpdateFragmentProperty(oPathPrefix + propertyKey, val);
+            }
+        }
+    }
+}
