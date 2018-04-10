46d7b3c4a7959181be045d628b56f6c37bc88f5f
jipe@microsoft.com <jipe@microsoft.com>
2018-04-10 12:04:09 +0800

    add json utility

---
+++ src/docfx/lib/JsonUtililty.cs
@@ -0,0 +1,104 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace Microsoft.Docs
+{
+    /// <summary>
+    /// Provide Utilities of Json
+    /// </summary>
+    public static class JsonUtililty
+    {
+        private static readonly ThreadLocal<JsonSerializer> s_defaultSerializer = new ThreadLocal<JsonSerializer>(
+            () => new JsonSerializer
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                Converters =
+                {
+                    new StringEnumConverter { CamelCaseText = true },
+                },
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            });
+
+        private static readonly JsonMergeSettings s_jsonMergeSettings = new JsonMergeSettings
+        {
+            MergeArrayHandling = MergeArrayHandling.Replace,
+        };
+
+        /// <summary>
+        /// Serialize an object to TextWriter
+        /// </summary>
+        public static void Serialize(TextWriter writer, object graph, bool isStable = false, Formatting formatting = Formatting.None, JsonSerializer serializer = null)
+        {
+            var localSerializer = serializer ?? s_defaultSerializer.Value;
+            if (isStable)
+            {
+                localSerializer.Formatting = Formatting.Indented;
+                localSerializer.Serialize(writer, Stablize(JToken.FromObject(graph, localSerializer)));
+            }
+            else
+            {
+                localSerializer.Formatting = formatting;
+                localSerializer.Serialize(writer, graph);
+            }
+        }
+
+        /// <summary>
+        /// Serialize an object to string
+        /// </summary>
+        public static string Serialize(object graph, bool isStable = false, Formatting formatting = Formatting.None, JsonSerializer serializer = null)
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Serialize(writer, graph, isStable, formatting, serializer);
+                return writer.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Deserialize from TextReader to an object
+        /// </summary>
+        public static T Deserialize<T>(TextReader reader, JsonSerializer serializer = null)
+        {
+            using (JsonReader json = new JsonTextReader(reader))
+            {
+                return (serializer ?? s_defaultSerializer.Value).Deserialize<T>(json);
+            }
+        }
+
+        /// <summary>
+        /// Deserialize a string to an object
+        /// </summary>
+        public static T Deserialize<T>(string json, JsonSerializer serializer = null)
+        {
+            return Deserialize<T>(new StringReader(json), serializer);
+        }
+
+        private static JToken Stablize(JToken token)
+        {
+            if (token is JObject obj)
+            {
+                return new JObject(obj.Properties()
+                    .OrderBy(p => p.Name, StringComparer.Ordinal)
+                    .Select(p => new JProperty(p.Name, Stablize(p.Value))));
+            }
+
+            if (token is JArray arr)
+            {
+                return new JArray(arr.Select(i => Stablize(i)));
+            }
+
+            return token;
+        }
+    }
+}
---
+++ test/docfx.Test/lib/JsonUtilityTest.cs
@@ -0,0 +1,256 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json;
+
+using Xunit;
+
+namespace Microsoft.Docs.Test
+{
+    public class JsonUtilityTest
+    {
+        [Theory]
+        [InlineData(" Add --globalMetadata, --globalMetadataFile and --fileMetadataFile\n")]
+        [InlineData("\r\n Hello\n")]
+        [InlineData("  \r\n Hello\n")]
+        [InlineData("True")]
+        [InlineData("true")]
+        [InlineData("TRUE")]
+        [InlineData("False")]
+        [InlineData("false")]
+        [InlineData("FALSE")]
+        [InlineData("Null")]
+        [InlineData("null")]
+        [InlineData("NULL")]
+        public void TestObjectWithStringProperty(string input)
+        {
+            var sw = new StringWriter();
+            JsonUtililty.Serialize(sw, new BasicClass { C = input });
+            var json = sw.ToString();
+            var value = JsonUtililty.Deserialize<BasicClass>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(input, value.C);
+        }
+
+        [Fact]
+        public void TestNotWorkInJsonDotNet39()
+        {
+            const string Text = "😄";
+            var sw = new StringWriter();
+            JsonUtililty.Serialize(sw, new BasicClass { C = Text });
+            var json = sw.ToString();
+            var value = JsonUtililty.Deserialize<BasicClass>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(Text, value.C);
+        }
+
+        [Fact]
+        public void TestBasicClass()
+        {
+            var json = JsonUtililty.Serialize(new BasicClass { B = 1, C = "Good!", D = true }, formatting: Formatting.Indented);
+            Assert.Equal(
+                @"{
+  ""c"": ""Good!"",
+  ""b"": 1,
+  ""d"": true
+}".Replace("\r\n", "\n"),
+                json.Replace("\r\n", "\n"));
+            var value = JsonUtililty.Deserialize<BasicClass>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(1, value.B);
+            Assert.Equal("Good!", value.C);
+            Assert.True(value.D);
+        }
+
+        [Fact]
+        public void TestBasicClassWithNullCharactor()
+        {
+            var json = JsonUtililty.Serialize(new BasicClass { C = null, });
+            Assert.Equal("{\"b\":0,\"d\":false}", json);
+            var value = JsonUtililty.Deserialize<BasicClass>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(0, value.B);
+            Assert.Null(value.C);
+            Assert.False(value.D);
+        }
+
+        [Fact]
+        public void TestBoolean()
+        {
+            var sw = new StringWriter();
+            JsonUtililty.Serialize(sw, new object[] { true, false });
+            var json = sw.ToString();
+            Assert.Equal("[true,false]", json);
+            var value = JsonUtililty.Deserialize<object[]>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(2, value.Length);
+            Assert.Equal(true, value[0]);
+            Assert.Equal(false, value[1]);
+        }
+
+        [Fact]
+        public void TestListOfBasicClass()
+        {
+            var json = JsonUtililty.Serialize(
+                (from i in Enumerable.Range(0, 10)
+                 select new BasicClass { B = i, C = $"Good{i}!", D = (i % 2 == 0) ? true : false }).ToList());
+            var values = JsonUtililty.Deserialize<List<BasicClass>>(new StringReader(json));
+            Assert.NotNull(values);
+            Assert.Equal(10, values.Count);
+            for (int i = 0; i < values.Count; i++)
+            {
+                Assert.Equal(i, values[i].B);
+                Assert.Equal($"Good{i}!", values[i].C);
+                Assert.Equal((i % 2 == 0) ? true : false, values[i].D);
+            }
+        }
+
+        [Fact]
+        public void TestClassWithMoreMembersByUnstableSerializeThenDeserialize()
+        {
+            var sw = new StringWriter();
+            JsonUtililty.Serialize(
+                sw,
+                new ClassWithMoreMembers
+                {
+                    D = true,
+                    B = 1,
+                    C = "Good!",
+                    ValueDict = new Dictionary<string, object>
+                    {
+                        { "b", "valueA" },
+                        { "c", 10 },
+                        { "a", true }
+                    },
+                    ValueList = new List<string> { "b", "a", },
+                    ValueBasic = new BasicClass
+                    {
+                        D = false,
+                        B = 5,
+                        C = "Amazing!",
+                    }
+                }, formatting: Formatting.Indented);
+            var json = sw.ToString();
+            Assert.Equal(
+                @"{
+  ""valueDict"": {
+    ""b"": ""valueA"",
+    ""c"": 10,
+    ""a"": true
+  },
+  ""valueList"": [
+    ""b"",
+    ""a""
+  ],
+  ""valueBasic"": {
+    ""c"": ""Amazing!"",
+    ""b"": 5,
+    ""d"": false
+  },
+  ""c"": ""Good!"",
+  ""b"": 1,
+  ""d"": true
+}".Replace("\r\n", "\n"),
+                json.Replace("\r\n", "\n"));
+            var value = JsonUtililty.Deserialize<ClassWithMoreMembers>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(1, value.B);
+            Assert.Equal("Good!", value.C);
+            Assert.Equal(true, value.D);
+            Assert.Equal(5, value.ValueBasic.B);
+            Assert.Equal("Amazing!", value.ValueBasic.C);
+            Assert.Equal(false, value.ValueBasic.D);
+            Assert.Equal(true, value.ValueDict["a"]);
+            Assert.Equal("valueA", value.ValueDict["b"]);
+            Assert.Equal((long)10, value.ValueDict["c"]);
+            Assert.Equal("b", value.ValueList[0]);
+            Assert.Equal("a", value.ValueList[1]);
+        }
+
+        [Fact]
+        public void TestClassWithMoreMembersByStableSerializeThenDeserialize()
+        {
+            var sw = new StringWriter();
+            JsonUtililty.Serialize(
+                sw,
+                new ClassWithMoreMembers
+                {
+                    D = true,
+                    B = 1,
+                    C = "Good!",
+                    ValueDict = new Dictionary<string, object>
+                    {
+                        { "b", "valueA" },
+                        { "c", 10 },
+                        { "a", true }
+                    },
+                    ValueList = new List<string> { "b", "a", },
+                    ValueBasic = new BasicClass
+                    {
+                        D = false,
+                        B = 5,
+                        C = "Amazing!",
+                    }
+                }, true);
+            var json = sw.ToString();
+            Assert.Equal(
+                @"{
+  ""b"": 1,
+  ""c"": ""Good!"",
+  ""d"": true,
+  ""valueBasic"": {
+    ""b"": 5,
+    ""c"": ""Amazing!"",
+    ""d"": false
+  },
+  ""valueDict"": {
+    ""a"": true,
+    ""b"": ""valueA"",
+    ""c"": 10
+  },
+  ""valueList"": [
+    ""b"",
+    ""a""
+  ]
+}".Replace("\r\n", "\n"),
+                json.Replace("\r\n", "\n"));
+            var value = JsonUtililty.Deserialize<ClassWithMoreMembers>(new StringReader(json));
+            Assert.NotNull(value);
+            Assert.Equal(1, value.B);
+            Assert.Equal("Good!", value.C);
+            Assert.Equal(true, value.D);
+            Assert.Equal(5, value.ValueBasic.B);
+            Assert.Equal("Amazing!", value.ValueBasic.C);
+            Assert.Equal(false, value.ValueBasic.D);
+            Assert.Equal(true, value.ValueDict["a"]);
+            Assert.Equal("valueA", value.ValueDict["b"]);
+            Assert.Equal((long)10, value.ValueDict["c"]);
+            Assert.Equal("b", value.ValueList[0]);
+            Assert.Equal("a", value.ValueList[1]);
+        }
+
+        public class BasicClass
+        {
+            public string C { get; set; }
+
+            public int B { get; set; }
+
+            public bool D { get; set; }
+        }
+
+        public class ClassWithMoreMembers : BasicClass
+        {
+            public Dictionary<string, object> ValueDict { get; set; }
+
+            public List<string> ValueList { get; set; }
+
+            public BasicClass ValueBasic { get; set; }
+        }
+    }
+}
