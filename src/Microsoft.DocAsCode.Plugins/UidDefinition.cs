f678b57657cca1e68758cc1acfa88102e5eb370f
Renze Yu <renzeyu@microsoft.com>
2018-04-24 16:57:11 +0800

    improve error messasge (#2677)
    * improve error messasge
    
    * fix param order

--- src/Microsoft.DocAsCode.Plugins/UidDefinition.cs
+++ src/Microsoft.DocAsCode.Plugins/UidDefinition.cs
@@ -25,7 +25,7 @@ namespace Microsoft.DocAsCode.Plugins
         {
             if (string.IsNullOrEmpty(name))
             {
-                throw new ArgumentNullException(name, nameof(name));
+                throw new ArgumentException("Uid name cannot be null or empty.", nameof(name));
             }
 
             Name = name;
