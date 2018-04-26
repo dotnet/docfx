b39408b35b11006f6d28ef8173b3fa2d2c3bd36e
jipe@microsoft.com <jipe@microsoft.com>
2018-04-26 08:39:08 +0800

    use System.CommandLine to parse command line options

--- src/docfx/cli/CommandLineOptions.cs
+++ src/docfx/cli/CommandLineOptions.cs
@@ -1,11 +1,29 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
 {
     internal class CommandLineOptions
     {
+        public string Output;
+        public string Log;
+        public string BuildLocale;
+        public bool Stable;
+        public IReadOnlyList<string> GitHubTokens;
+
+        public JObject ToJObject() => new JObject
+        {
+            ["output"] = new JObject
+            {
+                ["path"] = Output != null ? (JValue)Output : JValue.CreateNull(),
+                ["logPath"] = Log != null ? (JValue)Log : JValue.CreateNull(),
+                ["stable"] = Stable ? (JValue)true : JValue.CreateNull(),
+            },
+            ["locale"] = BuildLocale != null ? (JValue)BuildLocale : JValue.CreateNull(),
+            ["github"] = new JObject { ["token"] = GitHubTokens != null ? JToken.FromObject(GitHubTokens) : JValue.CreateNull() },
+        };
     }
 }
--- src/docfx/cli/Program.cs
+++ src/docfx/cli/Program.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.CommandLine;
 using System.Threading.Tasks;
 
 namespace Microsoft.Docs.Build
@@ -9,23 +10,43 @@ namespace Microsoft.Docs.Build
     {
         internal static async Task Main(string[] args)
         {
-            var (command, docset, options) = ParseCommandLineOptions(args);
+            var (command, source, options) = ParseCommandLineOptions(args);
             var log = new ConsoleLog();
 
             switch (command)
             {
                 case "restore":
-                    await Restore.Run(docset, options, log);
+                    await Restore.Run(source, options, log);
                     break;
                 case "build":
-                    await Build.Run(docset, options, log);
+                    await Build.Run(source, options, log);
                     break;
             }
         }
 
-        private static (string command, string docset, CommandLineOptions options) ParseCommandLineOptions(string[] args)
+        private static (string command, string source, CommandLineOptions options) ParseCommandLineOptions(string[] args)
         {
-            return (args[0], args[1], default);
+            var command = string.Empty;
+            var source = ".";
+            var options = new CommandLineOptions();
+
+            ArgumentSyntax.Parse(args, syntax =>
+            {
+                // Restore command
+                syntax.DefineCommand("restore", ref command, "restores dependencies before build");
+                syntax.DefineParameter("source", ref source, "docset path that contains ops.yml");
+
+                // Build command
+                syntax.DefineCommand("build", ref command, "builds a folder containing ops.yml");
+                syntax.DefineOption("o|out", ref options.Output, "output folder");
+                syntax.DefineOption("log", ref options.Log, "path to log file");
+                syntax.DefineOption("locale", ref options.BuildLocale, "locale to build");
+                syntax.DefineOptionList("github-token", ref options.GitHubTokens, "GitHub personal access tokens to call github API");
+                syntax.DefineOption("stable", ref options.Stable, "produces stable output for comparison in a diff tool");
+                syntax.DefineParameter("source", ref source, "docset path that contains ops.yml");
+            });
+
+            return (command, source, options);
         }
     }
 }
