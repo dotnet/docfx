2e07e869fed61835c2601ab3e7a172caf36ef873
Yufei Huang <yufeih@live.com>
2018-04-27 17:39:31 +0800

    Update namespace

--- src/Microsoft.DocAsCode.MarkdigEngine.Extensions/MarkdownExtensions.cs
+++ src/Microsoft.DocAsCode.MarkdigEngine.Extensions/MarkdownExtensions.cs
@@ -1,13 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.DocAsCode.MarkdigEngine
+namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
 {
     using System.IO;
 
     using Microsoft.DocAsCode.Common;
-
-    using MarkdigEngine.Extensions;
 
     using Markdig;
     using Markdig.Extensions.AutoIdentifiers;
--- src/Microsoft.DocAsCode.MarkdigEngine/MarkdigMarkdownService.cs
+++ src/Microsoft.DocAsCode.MarkdigEngine/MarkdigMarkdownService.cs
@@ -4,7 +4,6 @@
 namespace Microsoft.DocAsCode.MarkdigEngine
 {
     using System;
-    using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.IO;
 
