c45bc3d3e5c5b11232b33b3ad974ec3a4c4f6732
Osmond Jiang <xinjiang@microsoft.com>
2018-05-07 17:04:03 +0800

    fix yml parser bug of empty string and split test cases (#2728)
    * split test cases
    
    * fix empty string bug
    
    * add unit test

--- test/docfx.Test/common/TestHelper.cs
+++ test/docfx.Test/common/TestHelper.cs
@@ -9,37 +9,41 @@ namespace Microsoft.Docs.Build
 {
     internal static class TestHelper
     {
-        public static TheoryData<TestSpec> FindTestSpecs(string path)
+        public static TheoryData<string, string> FindTestSpecs(string path)
         {
-            var result = new TheoryData<TestSpec>();
+            var result = new TheoryData<string, string>();
 
             Parallel.ForEach(
                 Directory.EnumerateFiles(Path.Combine("specs", path), "*.yml", SearchOption.AllDirectories),
                 file =>
                 {
-                    foreach (var spec in FindTestSpecsInFile(file))
+                    foreach (var spec in FindTestSpecHeadersInFile(file))
                     {
-                        result.Add(spec);
+                        result.Add(file, spec);
                     }
                 });
 
             return result;
         }
 
-        public static IEnumerable<TestSpec> FindTestSpecsInFile(string path)
+        public static TestSpec FindTestSpecInFile(string ymlPath, string header)
         {
-            var sections = File.ReadAllText(path).Split("\n---", StringSplitOptions.RemoveEmptyEntries);
+            var sections = File.ReadAllText(ymlPath).Split("\n---", StringSplitOptions.RemoveEmptyEntries);
 
             foreach (var section in sections)
             {
                 var yaml = section.Trim('\r', '\n', '-');
-                var header = YamlUtility.ReadHeader(yaml) ?? "";
-                var spec = YamlUtility.Deserialize<TestSpec>(yaml);
+                if (string.Equals(header, YamlUtility.ReadHeader(yaml) ?? "", StringComparison.OrdinalIgnoreCase))
+                {
+                    var spec = YamlUtility.Deserialize<TestSpec>(yaml);
 
-                spec.Path = Path.Combine(path.Replace("\\", "/").Replace("specs/", "").Replace(".yml", ""), header);
+                    spec.Path = Path.Combine(ymlPath.Replace("\\", "/").Replace("specs/", "").Replace(".yml", ""), header);
 
-                yield return spec;
+                    return spec;
+                }
             }
+
+            return null;
         }
 
         public static string CreateDocset(this TestSpec spec)
@@ -86,6 +90,19 @@ namespace Microsoft.Docs.Build
             else
             {
                 Assert.Equal(expected, actual);
+            }
+        }
+
+        private static IEnumerable<string> FindTestSpecHeadersInFile(string path)
+        {
+            var sections = File.ReadAllText(path).Split("\n---", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var section in sections)
+            {
+                var yaml = section.Trim('\r', '\n', '-');
+                var header = YamlUtility.ReadHeader(yaml) ?? "";
+
+                yield return header;
             }
         }
     }
