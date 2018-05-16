285c85d4d60b325e2c70ef123b6b777d8a71174e
Yufei Huang <yufeih@live.com>
2018-05-16 14:16:41 +0800

    a

---
+++ src/docfx/lib/MarkdownUtility.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using Markdig;
+using Microsoft.DocAsCode.MarkdigEngine.Extensions;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Docs.Build
+{
+    /// <summary>
+    /// Converts markdown to html
+    /// </summary>
+    internal static class MarkdownUtility
+    {
+        public static (string html, JObject metadata) Markup(string markdown, Document file, Context context)
+        {
+            using (InclusionContext.PushFile(file))
+            {
+                var pipeline = CreateMarkdownPipeline(file);
+                var html = Markdown.ToHtml(markdown, pipeline);
+                return (html, null);
+            }
+        }
+
+        private static MarkdownPipeline CreateMarkdownPipeline(Document file)
+        {
+            var markdownContext = new MarkdownContext(null, null, null, null, null);
+
+            return new MarkdownPipelineBuilder()
+                .UseDocfxExtensions(markdownContext)
+                .Build();
+        }
+    }
+}
