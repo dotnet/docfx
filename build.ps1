b78dec1117bc11612941db640d5227a179bc684a
Yufei Huang <yufeih@users.noreply.github.com>
2018-05-22 10:34:38 +0800

    Appveyor CI publishes NuGet packages to `https://ci.appveyor.com/nuget/docfx` (#2794)

--- build.ps1
+++ build.ps1
@@ -6,5 +6,9 @@ function exec([string] $cmd) {
     }
 }
 
+# After first v3 release, $version is just `git describe`
+$version = & { git describe --always }
+$version = "3.0.0-preview1-$version"
+
 exec "dotnet test test\docfx.Test -c Release"
-exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop"
+exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version"
