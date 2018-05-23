52d98bdfcfcfbc8ed7ea71349eb1c777e7f97830
Yufei Huang <yufeih@live.com>
2018-05-23 17:04:22 +0800

    Revert toc changes

--- src/docfx/build/markdown/BuildMarkdown.cs
+++ src/docfx/build/markdown/BuildMarkdown.cs
@@ -13,9 +13,11 @@ namespace Microsoft.Docs.Build
 
             var (html, yamlHeader) = MarkdownUtility.Markup(markdown, file, context);
 
+            var content = !string.IsNullOrEmpty(html) ? $"<div>{html}</div>" : "";
+
             var model = new PageModel
             {
-                Content = html,
+                Content = content,
                 Metadata = JsonUtility.Merge(Metadata.GetFromConfig(file), yamlHeader),
             };
 
