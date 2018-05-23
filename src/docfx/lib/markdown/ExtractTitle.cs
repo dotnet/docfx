953afb4bc3cadcfe6d142923a197b2e308ea3573
Yufei Huang <yufeih@live.com>
2018-05-23 17:58:15 +0800

    Skip yaml header

--- src/docfx/lib/markdown/ExtractTitle.cs
+++ src/docfx/lib/markdown/ExtractTitle.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Markdig;
+using Markdig.Extensions.Yaml;
 using Markdig.Syntax;
 
 namespace Microsoft.Docs.Build
@@ -13,11 +15,13 @@ namespace Microsoft.Docs.Build
         {
             return builder.Use(document =>
             {
-                if (document.Count > 0 && document[0] is HeadingBlock h1 && h1.Level == 1)
+                var h1 = document.SkipWhile(block => block is YamlFrontMatterBlock).FirstOrDefault() as HeadingBlock;
+
+                if (h1 != null && h1.Level == 1)
                 {
                     // TODO: H1 with markdown formats?
                     result.Value = h1.Inline.FirstChild.ToString();
-                    document.RemoveAt(0);
+                    document.Remove(h1);
                 }
             });
         }
