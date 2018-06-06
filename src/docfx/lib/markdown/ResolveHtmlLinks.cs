9f112c8830295f2d734113782518d4eb60fe9bfc
Yufei Huang <yufeih@live.com>
2018-06-06 11:41:12 +0800

    Fix resolve spec

--- src/docfx/lib/markdown/ResolveHtmlLinks.cs
+++ src/docfx/lib/markdown/ResolveHtmlLinks.cs
@@ -1,12 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Linq;
 using System.Runtime.CompilerServices;
 using Markdig;
-using Markdig.Extensions.Yaml;
 using Markdig.Helpers;
-using Markdig.Renderers;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 using Microsoft.DocAsCode.MarkdigEngine.Extensions;
