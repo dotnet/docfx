3d4c32e430962729df25427c457b6f51511500f8
Yufei Huang <yufeih@live.com>
2018-07-02 16:41:53 +0800

    Optimize glob

--- src/docfx/lib/FileGlob.cs
+++ src/docfx/lib/FileGlob.cs
@@ -2,20 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Microsoft.Docs.Build
 {
-    /// <summary>
-    /// Provide Utilities of File glob
-    /// </summary>
     internal static class FileGlob
     {
-        /// <summary>
-        /// Get files with patterns
-        /// </summary>
         public static IEnumerable<string> GetFiles(string cwd, string[] patterns, string[] excludePatterns)
         {
             if (!Directory.Exists(cwd))
@@ -23,77 +19,42 @@ namespace Microsoft.Docs.Build
                 return Array.Empty<string>();
             }
 
-            var globList = patterns.Select(s => new GlobMatcher(s)).ToArray();
-            var excludeGlobList = excludePatterns.Select(s => new GlobMatcher(s)).ToArray();
+            var includeGlobs = patterns.Select(s => new GlobMatcher(s)).ToList();
+            var excludeGlobs = excludePatterns.Select(s => new GlobMatcher(s)).ToList();
+            var result = new ConcurrentBag<string>();
+            Parallel.ForEach(Directory.EnumerateFiles(cwd, "*.*", SearchOption.AllDirectories), MatchFile);
+            return result;
 
-            return GetFilesCore(cwd, globList, excludeGlobList);
-        }
-
-        private static IEnumerable<string> GetFilesCore(string cwd, GlobMatcher[] globs, GlobMatcher[] excludeGlobs)
-        {
-            foreach (var file in GetFilesFromSubfolder(cwd, cwd, globs, excludeGlobs))
+            void MatchFile(string file)
             {
-                yield return Path.GetFullPath(file);
-            }
-        }
-
-        private static IEnumerable<string> GetFilesFromSubfolder(string baseDirectory, string cwd, GlobMatcher[] globs, GlobMatcher[] excludeGlobs)
-        {
-            foreach (var file in Directory.EnumerateFiles(baseDirectory))
-            {
-                var relativePath = GetRelativeFilePath(cwd, file);
-                if (IsMatch(relativePath, globs, excludeGlobs, directory: false))
+                // cwd could be
+                // 1. root folder, e.g. E:\ or /
+                // 2. sub folder, e.g. a or a/ or a\
+                var relativePath = file.Substring(cwd.Length).TrimStart('\\', '/');
+                if (IsMatch(relativePath))
                 {
-                    yield return file;
+                    result.Add(Path.GetFullPath(file));
                 }
             }
 
-            foreach (var dir in Directory.EnumerateDirectories(baseDirectory))
+            bool IsMatch(string path)
             {
-                var relativePath = GetRelativeDirectoryPath(cwd, dir);
-                if (IsMatch(relativePath, globs, excludeGlobs, directory: true))
+                foreach (var glob in includeGlobs)
                 {
-                    foreach (var file in GetFilesFromSubfolder(dir, cwd, globs, excludeGlobs))
+                    if (glob.Match(path, false))
                     {
-                        yield return file;
+                        foreach (var exclude in excludeGlobs)
+                        {
+                            if (exclude.Match(path, false))
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
                     }
                 }
+                return false;
             }
-        }
-
-        private static string GetRelativeFilePath(string directory, string file)
-        {
-            var subpath = file.Substring(directory.Length);
-
-            // directory could be
-            // 1. root folder, e.g. E:\ or /
-            // 2. sub folder, e.g. a or a/ or a\
-            return subpath.TrimStart('\\', '/');
-        }
-
-        private static string GetRelativeDirectoryPath(string parentDirectory, string directory)
-        {
-            var relativeDirectory = GetRelativeFilePath(parentDirectory, directory);
-            return relativeDirectory.TrimEnd('\\', '/') + "/";
-        }
-
-        private static bool IsMatch(string path, GlobMatcher[] globs, GlobMatcher[] excludeGlobs, bool directory)
-        {
-            foreach (var exclude in excludeGlobs)
-            {
-                if (exclude.Match(path, false))
-                {
-                    return false;
-                }
-            }
-            foreach (var glob in globs)
-            {
-                if (glob.Match(path, directory))
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
