b268f4a53aa22e7f170c504a544c6922c5837c17
Wei Hu <huwe@microsoft.com>
2018-07-04 13:08:02 +0800

    Fix lint error

--- src/docfx/lib/LineInfo.cs
+++ src/docfx/lib/LineInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
@@ -6,7 +6,6 @@ using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
 {
-
     internal class LineInfo
     {
         public LineInfo(int lineNumber, int linePosition)
