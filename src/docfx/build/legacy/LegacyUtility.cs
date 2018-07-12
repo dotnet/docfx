0c1d77b0e8c6041f62d9185a123d97e05306ce40
Renze Yu <renzeyu@microsoft.com>
2018-07-12 13:04:27 +0800

    merge utility and extension

--- src/docfx/build/legacy/LegacyUtility.cs
+++ src/docfx/build/legacy/LegacyUtility.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Diagnostics;
 using System.IO;
 
@@ -14,6 +17,43 @@ namespace Microsoft.Docs.Build
             Directory.CreateDirectory(Path.GetDirectoryName(destFileName));
             File.Delete(destFileName);
             File.Move(sourceFileName, destFileName);
+        }
+
+        public static string ToLegacyPathRelativeToBasePath(this Document doc, Docset docset)
+        {
+            return PathUtility.NormalizeFile(Path.GetRelativePath(docset.Config.SourceBasePath ?? string.Empty, doc.FilePath));
+        }
+
+        public static string ToLegacyOutputPathRelativeToBaseSitePath(this Document doc, Docset docset)
+        {
+            var legacyOutputFilePathRelativeToSiteBasePath = doc.OutputPath;
+            if (legacyOutputFilePathRelativeToSiteBasePath.StartsWith(docset.Config.SiteBasePath, PathUtility.PathComparison))
+            {
+                legacyOutputFilePathRelativeToSiteBasePath = Path.GetRelativePath(docset.Config.SiteBasePath, doc.OutputPath);
+            }
+
+            return PathUtility.NormalizeFile(legacyOutputFilePathRelativeToSiteBasePath);
+        }
+
+        public static string ToLegacySiteUrlRelativeToBaseSitePath(this Document doc, Docset docset)
+        {
+            var legacyOutputFilePathRelativeToSiteBasePath = doc.SiteUrl;
+            if (legacyOutputFilePathRelativeToSiteBasePath.StartsWith($"/{docset.Config.SiteBasePath}", PathUtility.PathComparison))
+            {
+                legacyOutputFilePathRelativeToSiteBasePath = Path.GetRelativePath(docset.Config.SiteBasePath, doc.SiteUrl.Substring(1));
+            }
+
+            return PathUtility.NormalizeFile(doc.FilePath.EndsWith("index.md", PathUtility.PathComparison) ? $"{legacyOutputFilePathRelativeToSiteBasePath}/index" : legacyOutputFilePathRelativeToSiteBasePath);
+        }
+
+        public static string ToLegacyOutputPath(this LegacyManifestOutputItem legacyManifestOutputItem, Docset docset)
+        {
+            return Path.Combine(docset.Config.SiteBasePath ?? string.Empty, legacyManifestOutputItem.OutputPathRelativeToSiteBasePath);
+        }
+
+        public static string GetAbsoluteOutputPathFromRelativePath(this Docset docset, string relativePath)
+        {
+            return Path.Combine(docset.DocsetPath, docset.Config.Output.Path, relativePath);
         }
     }
 }
