acf9fc15da241d98d9c42ef8eaf7658593628769
Yufei Huang <yufeih@live.com>
2018-07-16 12:02:03 +0800

    Test PR

--- build.ps1
+++ build.ps1
@@ -11,6 +11,9 @@ $commitCount = & { git rev-list --count HEAD }
 $revision = $commitCount.ToString().PadLeft(5, '0')
 $version = "3.0.0-alpha-$revision-$commitSha"
 
+exec "git rev-parse --abbrev-ref HEAD"
+exec "git config --get remote.origin.url"
+
 exec "dotnet test test\docfx.Test"
 exec "dotnet test test\docfx.Test -c Release"
 exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version /p:PackAsTool=true"
