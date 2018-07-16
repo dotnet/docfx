ce466787df3b6180e1ee01c87934b6723656a414
Yufei Huang <yufeih@live.com>
2018-07-16 12:04:47 +0800

    update bash

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+git rev-parse --abbrev-ref HEAD
+git config --get remote.origin.url
+
 dotnet test test/docfx.Test
 dotnet test test/docfx.Test -c Release
 dotnet pack src/docfx -c Release /p:PackAsTool=true
