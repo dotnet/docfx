ce933c8e1fd19a8d114bb7c8478c61b3f1f2ae41
Renze Yu <renzeyu@microsoft.com>
2018-07-16 16:10:27 +0800

    improve error msg when for empty uid in udp

--- test/Microsoft.DocAsCode.Build.UniversalReference.Tests/UniversalReferenceDocumentProcessorTest.cs
+++ test/Microsoft.DocAsCode.Build.UniversalReference.Tests/UniversalReferenceDocumentProcessorTest.cs
@@ -3,6 +3,7 @@
 
 namespace Microsoft.DocAsCode.Build.UniversalReference.Tests
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.IO;
@@ -172,6 +173,17 @@ namespace Microsoft.DocAsCode.Build.UniversalReference.Tests
         }
 
         #endregion
+
+        [Fact]
+        public void ProcessItemWithEmptyUidShouldFail()
+        {
+            var fileNames = new string[] { "invalid.yml" };
+            var files = new FileCollection(Directory.GetCurrentDirectory());
+            files.Add(DocumentType.Article, fileNames.Select(f => $"{YmlDataDirectory}/{f}"), TestDataDirectory);
+
+            var ex = Assert.Throws<ArgumentException>(() => BuildDocument(files));
+            Assert.Equal("Uid must not be null or empty", ex.Message);
+        }
 
         private void BuildDocument(FileCollection files)
         {
