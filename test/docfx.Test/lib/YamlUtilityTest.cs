3f701a6285f9ba28a2227cbc14c46bf27a0b9749
Wei Hu <huwe@microsoft.com>
2018-07-20 14:24:49 +0800

    Address PR comment

--- test/docfx.Test/lib/YamlUtilityTest.cs
+++ test/docfx.Test/lib/YamlUtilityTest.cs
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Microsoft.Docs.Build
