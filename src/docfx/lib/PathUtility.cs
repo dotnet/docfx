d8df84f21f4870ddd5bade5d14d2b489fb54373e
Yufei Huang <yufeih@users.noreply.github.com>
2018-07-20 11:33:18 +0800

    Fix NormalizeFile to ensure it does not ends with / (#3104)

--- src/docfx/lib/PathUtility.cs
+++ src/docfx/lib/PathUtility.cs
@@ -125,7 +125,7 @@ namespace Microsoft.Docs.Build
         {
             path = path.Replace('\\', '/');
 
-            if (path.IndexOf('.') == -1 && !path.Contains("//"))
+            if (path.IndexOf('.') == -1 && !path.Contains("//") && !path.EndsWith('/'))
             {
                 return path;
             }
