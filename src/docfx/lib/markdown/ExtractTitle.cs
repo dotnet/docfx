10b039b0808fdd0eb4ac5ea7dc6f591f8c912933
Jiayin Pei <jipe@microsoft.com>
2018-08-14 10:36:33 +0800

    ignore comments between yaml header and H1 when extract title

--- src/docfx/lib/markdown/ExtractTitle.cs
+++ src/docfx/lib/markdown/ExtractTitle.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using HtmlAgilityPack;
 using Markdig;
 using Markdig.Extensions.Yaml;
 using Markdig.Renderers;
@@ -16,7 +18,7 @@ namespace Microsoft.Docs.Build
         {
             return builder.Use(document =>
             {
-                var h1 = document.SkipWhile(block => block is YamlFrontMatterBlock).FirstOrDefault() as HeadingBlock;
+                var h1 = GetFirstAfterYamlNoneCommentHeadingBlock(document);
 
                 if (h1 != null && h1.Level == 1)
                 {
@@ -24,6 +26,35 @@ namespace Microsoft.Docs.Build
                     document.Remove(h1);
                 }
             });
+        }
+
+        private static HeadingBlock GetFirstAfterYamlNoneCommentHeadingBlock(MarkdownDocument document)
+        {
+            var firstAfterYamlBlock = GetAfterYamlChildren(document).SkipWhile(block => IsCommentsBlock(block)).FirstOrDefault();
+            return firstAfterYamlBlock as HeadingBlock;
+        }
+
+        private static IEnumerable<Block> GetAfterYamlChildren(MarkdownDocument document)
+        {
+            return document.FirstOrDefault() is YamlFrontMatterBlock ? document.Skip(1) : document;
+        }
+
+        private static bool IsCommentsBlock(Block block)
+        {
+            var htmlBlock = block as HtmlBlock;
+
+            if (htmlBlock != null)
+            {
+                var document = new HtmlDocument();
+                document.LoadHtml(htmlBlock.Lines.ToString());
+                var child = document.DocumentNode.FirstChild;
+                while (child != null && (child.NodeType == HtmlNodeType.Comment || string.IsNullOrWhiteSpace(child.OuterHtml)))
+                {
+                    child = child.NextSibling;
+                }
+                return child == null;
+            }
+            return false;
         }
 
         private static string RenderTitle(HeadingBlock h1)
