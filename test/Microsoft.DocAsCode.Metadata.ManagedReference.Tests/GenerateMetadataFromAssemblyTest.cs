aecba56154bfb439823a7f6c9e9a38a3bd48244e
Alexander Puzynia <werwolf.by@gmail.com>
2018-08-19 17:10:44 +0300

    Add test to load library with references

--- test/Microsoft.DocAsCode.Metadata.ManagedReference.Tests/GenerateMetadataFromAssemblyTest.cs
+++ test/Microsoft.DocAsCode.Metadata.ManagedReference.Tests/GenerateMetadataFromAssemblyTest.cs
@@ -8,6 +8,8 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference.Tests
 
     using Xunit;
 
+    using Microsoft.DocAsCode.DataContracts.ManagedReference;
+
     using static Microsoft.DocAsCode.Metadata.ManagedReference.RoslynIntermediateMetadataExtractor;
 
     [Trait("Owner", "qinezh")]
@@ -22,6 +24,18 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference.Tests
                 @"TestData\BaseClassForTestClass1.dll",
                 @"TestData\CatLibrary.dll",
                 @"TestData\CatLibrary2.dll",
+            };
+
+        public readonly string[] TupleAssemblyFiles =
+            new[]
+            {
+                @"TestData\TupleLibrary.dll",
+            };
+
+        public readonly string[] TupleReferencesFiles =
+            new[]
+            {
+                @"TestDataReferences\System.ValueTuple.dll",
             };
 
         [Fact]
@@ -45,6 +59,39 @@ namespace Microsoft.DocAsCode.Metadata.ManagedReference.Tests
                 Assert.NotNull(@class);
                 Assert.Equal("CarLibrary2.Cat2", @class.Name);
                 Assert.Equal(new[] { "System.Object", "CatLibrary.Cat{CatLibrary.Dog{System.String},System.Int32}" }, @class.Inheritance);
+            }
+        }
+
+        [Fact]
+        public void TestGenerateMetadataFromAssemblyWithReferences()
+        {
+            var compilation = CompilationUtility.CreateCompilationFromAssembly(TupleAssemblyFiles.Concat(TupleReferencesFiles));
+            var referenceAssembly = CompilationUtility.GetAssemblyFromAssemblyComplation(compilation, TupleAssemblyFiles).Select(s => s.assembly).ToList();
+
+            var output = GenerateYamlMetadata(compilation, referenceAssembly[0]);
+            var @class = output.Items[0].Items[0];
+            Assert.NotNull(@class);
+            Assert.Equal("XmlTasks", @class.DisplayNames[SyntaxLanguage.CSharp]);
+            Assert.Equal("XmlTasks", @class.DisplayNamesWithType[SyntaxLanguage.CSharp]);
+            Assert.Equal("TupleLibrary.XmlTasks", @class.DisplayQualifiedNames[SyntaxLanguage.CSharp]);
+
+            {
+                var method = @class.Items.Single(i => i.Name == "TupleLibrary.XmlTasks.ToNamespace(System.String,System.String)");
+                Assert.Equal("ToNamespace(String, String)", method.DisplayNames[SyntaxLanguage.CSharp]);
+                Assert.Equal("XmlTasks.ToNamespace(String, String)", method.DisplayNamesWithType[SyntaxLanguage.CSharp]);
+                Assert.Equal("TupleLibrary.XmlTasks.ToNamespace(System.String, System.String)", method.DisplayQualifiedNames[SyntaxLanguage.CSharp]);
+
+                // TODO: add whitespace after fix: https://github.com/dotnet/roslyn/issues/29390
+                Assert.Equal("public (string prefix, string uri)ToNamespace(string prefix, string uri)", method.Syntax.Content[SyntaxLanguage.CSharp]);
+            }
+
+            {
+                var method = @class.Items.Single(i => i.Name == "TupleLibrary.XmlTasks.XmlPeek(System.String,System.ValueTuple{System.String,System.String}[])");
+                Assert.Equal("XmlPeek(String, (String prefix, String uri)[])", method.DisplayNames[SyntaxLanguage.CSharp]);
+                Assert.Equal("XmlTasks.XmlPeek(String, (String prefix, String uri)[])", method.DisplayNamesWithType[SyntaxLanguage.CSharp]);
+                Assert.Equal("TupleLibrary.XmlTasks.XmlPeek(System.String, System.ValueTuple<System.String, System.String>[])", method.DisplayQualifiedNames[SyntaxLanguage.CSharp]);
+
+                Assert.Equal("public string XmlPeek(string xpath, params (string prefix, string uri)[] namespaces)", method.Syntax.Content[SyntaxLanguage.CSharp]);
             }
         }
     }
