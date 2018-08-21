3a2a059e2aba66ea8af217ff0246c02ce4d150f6
Yufei Huang <yufeih@live.com>
2018-08-21 10:53:01 +0800

    Provide a default config for test using DOCFX_CONFIG_EXTEND

--- test/docfx.Test/common/TestFramework.cs
+++ test/docfx.Test/common/TestFramework.cs
@@ -27,6 +27,8 @@ namespace Microsoft.Docs.Build
         protected override ITestFrameworkExecutor CreateExecutor(AssemblyName assemblyName)
         {
             Environment.SetEnvironmentVariable("DOCFX_APPDATA_PATH", Path.GetFullPath("appdata"));
+            Environment.SetEnvironmentVariable("DOCFX_CONFIG_EXTEND", Path.GetFullPath("docfx.test.yml"));
+
             MakeDebugAssertThrowException();
             return new ParallelExecutor(assemblyName, SourceInformationProvider, DiagnosticMessageSink);
         }
