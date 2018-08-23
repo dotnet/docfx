20bc1444daee029f1dc5619b58e5ae02128fccf8
Jiayin Pei <jipe@microsoft.com>
2018-08-23 13:41:06 +0800

    handle heading in include file

--- src/docfx/lib/markdown/ExtractTitle.cs
+++ src/docfx/lib/markdown/ExtractTitle.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,27 +15,67 @@ namespace Microsoft.Docs.Build
 {
     internal static class ExtractTitle
     {
+        [ThreadStatic]
+        private static bool s_headingFound = false;
+
+        [ThreadStatic]
+        private static bool s_waitingForInclusionHeading = false;
+
         public static MarkdownPipelineBuilder UseExtractTitle(this MarkdownPipelineBuilder builder)
         {
             return builder.Use(document =>
             {
-                var h1 = GetHeadingBlock(document);
+                var firstBlock = GetAfterYamlFirstNonCommentsBlock(document);
+                var heading = firstBlock as HeadingBlock;
 
-                if (h1 != null && h1.Level == 1)
+                if (InclusionContext.IsInclude)
                 {
-                    Markup.Result.HtmlTitle = RenderTitle(h1);
-                    document.Remove(h1);
+                    if (!s_headingFound && s_waitingForInclusionHeading)
+                    {
+                        if (heading != null && heading.Level == 1)
+                        {
+                            Markup.Result.HtmlTitle = RenderTitle(heading);
+                            document.Remove(heading);
+                            s_headingFound = true;
+                        }
+                        else
+                        {
+                            if (!(firstBlock is InclusionBlock))
+                            {
+                                Markup.Result.Errors.Add(Errors.HeadingNotFound((Document)InclusionContext.File));
+                                s_waitingForInclusionHeading = false;
+                            }
+                        }
+                    }
                 }
                 else
                 {
-                    Markup.Result.Errors.Add(Errors.HeadingNotFound((Document)InclusionContext.File));
+                    if (heading != null && heading.Level == 1)
+                    {
+                        Markup.Result.HtmlTitle = RenderTitle(heading);
+                        document.Remove(heading);
+                        s_headingFound = true;
+                    }
+                    else
+                    {
+                        s_headingFound = false;
+                        if (firstBlock is InclusionBlock)
+                        {
+                            s_waitingForInclusionHeading = true;
+                        }
+                        else
+                        {
+                            Markup.Result.Errors.Add(Errors.HeadingNotFound((Document)InclusionContext.File));
+                            s_waitingForInclusionHeading = false;
+                        }
+                    }
                 }
             });
         }
 
-        private static HeadingBlock GetHeadingBlock(MarkdownDocument document)
+        private static Block GetAfterYamlFirstNonCommentsBlock(MarkdownDocument document)
         {
-            return GetAfterYamlChildren(document).SkipWhile(IsCommentsBlock).FirstOrDefault() as HeadingBlock;
+            return GetAfterYamlChildren(document).SkipWhile(IsCommentsBlock).FirstOrDefault();
         }
 
         private static IEnumerable<Block> GetAfterYamlChildren(MarkdownDocument document)
