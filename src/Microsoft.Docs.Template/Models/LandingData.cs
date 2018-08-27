3fba1df72eecf4b1d16d9714276d480e89a76af0
Wei Hu <huwe@microsoft.com>
2018-08-27 16:31:46 +0800

    Add view template for LandingData

--- src/Microsoft.Docs.Template/Models/LandingData.cs
+++ src/Microsoft.Docs.Template/Models/LandingData.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
@@ -22,15 +19,48 @@ namespace Microsoft.Docs.Build
     public class LandingDataAbstract
     {
         public string Description { get; set; }
+
+        public LandingDataAside Aside { get; set; }
+
+        public LandingDataMenu Menu { get; set; }
+    }
+
+    public class LandingDataAside
+    {
+        public string Title { get; set; }
+
+        [Href]
+        public string Href { get; set; }
+
+        public LandingDataImage Image { get; set; }
+    }
+
+    public class LandingDataImage
+    {
+        [Href]
+        public string Href { get; set; }
+
+        public string Src { get; set; }
+
+        public string Alt { get; set; }
+
+        public int? Width { get; set; }
+
+        public string Role { get; set; }
+    }
+
+    public class LandingDataMenu
+    {
+        public string Title { get; set; }
     }
 
     public enum LandingDataType
     {
         Paragraph,
         List,
+        Grid,
         Table,
         Markdown,
-        Text,
     }
 
     public class LandingDataSection
@@ -49,12 +79,51 @@ namespace Microsoft.Docs.Build
         public string Style { get; set; }
 
         public string ClassName { get; set; }
+
+        public object[] Items { get; set; }
+
+        public LandingDataColumn[] Columns { get; set; }
+
+        public LandingDataRow[] Rows { get; set; }
+    }
+
+    public class LandingDataColumn
+    {
+        public LandingDataImage Image { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+    }
+
+    public class LandingDataRow
+    {
+        public string Title { get; set; }
+
+        public LandingDataRowValue[] Values { get; set; }
+    }
+
+    public class LandingDataRowValue
+    {
+        [Href]
+        public string Href { get; set; }
     }
 
     public class LandingDataListItem
     {
+        public string Text { get; set; }
+
         public string Title { get; set; }
 
+        [Markdown]
+        public string Content { get; set; }
+
         public string Html { get; set; }
+
+        [Href]
+        public string Href { get; set; }
+
+        public LandingDataImage Image { get; set; }
     }
+
 }
