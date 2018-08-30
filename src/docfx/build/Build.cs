937591b08b023c393678aa79ec5edd2ecaf4815c
Renze Yu <renzeyu@microsoft.com>
2018-08-30 17:41:42 +0800

    rename git-token to github-token for build (#3337)
    * rename git-token to github-token for build
    
    * GithubToken => GitHubToken

--- src/docfx/build/Build.cs
+++ src/docfx/build/Build.cs
@@ -26,7 +26,7 @@ namespace Microsoft.Docs.Build
             var docset = new Docset(context, docsetPath, config, options);
 
             var tocMap = await BuildTableOfContents.BuildTocMap(context, docset.BuildScope);
-            var (contributionErrors, contribution) = await ContributionInfo.Load(docset, options.GitToken);
+            var (contributionErrors, contribution) = await ContributionInfo.Load(docset, options.GitHubToken);
             errors.AddRange(contributionErrors);
 
             var (files, sourceDependencies) = await BuildFiles(context, docset.BuildScope, tocMap, contribution);
