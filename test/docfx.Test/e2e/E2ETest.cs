5f1262258b8e1900c2ffca0d414e1677892c0e5f
Yufei Huang <yufeih@live.com>
2018-09-04 15:47:14 +0800

    Address PR feedback

--- test/docfx.Test/e2e/E2ETest.cs
+++ test/docfx.Test/e2e/E2ETest.cs
@@ -175,7 +175,7 @@ namespace Microsoft.Docs.Build
             {
                 case ".json":
                 case ".manifest":
-                    TestHelper.VerifyJsonContainEquals(
+                    TestUtility.VerifyJsonContainEquals(
                         JToken.Parse(content ?? "{}"),
                         JToken.Parse(File.ReadAllText(file)));
                     break;
@@ -184,13 +184,13 @@ namespace Microsoft.Docs.Build
                     var actual = File.ReadAllLines(file).OrderBy(_ => _).ToArray();
                     // TODO: Configure github token in CI to get rid of github rate limit,
                     // then we could remove the wildcard match
-                    if (expected.Contains("*"))
+                    if (expected.Any(str => str.Contains("*")))
                     {
                         Assert.Matches("^" + Regex.Escape(string.Join("\n", expected)).Replace("\\*", ".*") + "$", string.Join("\n", actual));
                     }
                     else
                     {
-                        Assert.Equal(expected, actual);
+                        Assert.Equal(string.Join("\n", expected), string.Join("\n", actual));
                     }
                     break;
                 default:
