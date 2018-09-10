7fec62b2e04a1a1cec2eb88b5f25c445b2d51646
Yufei Huang <yufeih@users.noreply.github.com>
2018-09-10 13:20:34 +0800

    Allow mutex name to be any unique string (#3384)

--- src/docfx/docset/Document.cs
+++ src/docfx/docset/Document.cs
@@ -396,7 +396,9 @@ namespace Microsoft.Docs.Build
             var sitePathWithoutExtension = Path.Combine(Path.GetDirectoryName(SitePath), Path.GetFileNameWithoutExtension(SitePath));
             var sitePath = PathUtility.NormalizeFile(Path.GetRelativePath(Docset.Config.DocumentId.SiteBasePath, sitePathWithoutExtension));
 
-            return (HashUtility.GetMd5String($"{depotName}|{sourcePath.ToLowerInvariant()}"), HashUtility.GetMd5String($"{depotName}|{sitePath.ToLowerInvariant()}"));
+            return (
+                HashUtility.GetMd5Guid($"{depotName}|{sourcePath.ToLowerInvariant()}").ToString(),
+                HashUtility.GetMd5Guid($"{depotName}|{sitePath.ToLowerInvariant()}").ToString());
         }
     }
 }
