97c663e60b5be3ac16734a19c1fb94611f1cf025
Yufei Huang <yufeih@users.noreply.github.com>
2018-09-25 11:45:16 +0800

    Fix crash when invalid locale (#3444)

--- src/Microsoft.Docs.Template/Models/Attributes.cs
+++ src/Microsoft.Docs.Template/Models/Attributes.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 
 namespace Microsoft.Docs.Build
 {
@@ -36,21 +34,4 @@ namespace Microsoft.Docs.Build
     public class UidAttribute : DataTypeAttribute { }
 
     public class XrefAttribute : DataTypeAttribute { }
-
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-    public class LocaleAttribute : ValidationAttribute
-    {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            try
-            {
-                var culture = new CultureInfo(value.ToString());
-                return null;
-            }
-            catch (CultureNotFoundException)
-            {
-                return new ValidationResult($"Locale '{value.ToString()}' is not supported.", null);
-            }
-        }
-    }
 }
