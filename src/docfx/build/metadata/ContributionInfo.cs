33a529cda809549197858c3241e065d0e4ff83d5
Osmond Jiang <xinjiang@microsoft.com>
2018-09-29 17:38:57 +0800

    support loc config conversion (#3460)
    * support loc config conversion
    
    * fix test issue
    
    * fix test issues
    
    * address pr comments\

--- src/docfx/build/metadata/ContributionInfo.cs
+++ src/docfx/build/metadata/ContributionInfo.cs
@@ -122,7 +122,8 @@ namespace Microsoft.Docs.Build
                 return default;
 
             var branch = repo.Branch ?? "master";
-            var editRepo = document.Docset.Config.Contribution.Repository ?? $"{repo.Owner}/{repo.Name}";
+            var editRepo = LocConfigConvention.GetEditRepository(document.Docset.Config.Contribution.Repository, document.Docset.Locale, document.Docset.Config.DefaultLocale)
+                ?? $"{repo.Owner}/{repo.Name}";
             var editBranch = document.Docset.Config.Contribution.Branch ?? branch;
 
             var editUrl = document.Docset.Config.Contribution.ShowEdit
---
+++ src/docfx/config/LocConfigurationConvention.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Docs.Build
+{
+    internal static class LocConfigConvention
+    {
+        private static readonly Regex s_repoNameWithLocale = new Regex(@"^.+?(\.[a-z]{2,4}-[a-z]{2,4})?$", RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// The eidt repo name for loc follows below conventions:
+        /// source name                         -->     loc name
+        /// {org}/{repo-name}                   -->     {org}/{repo-name}.{locale}
+        /// {org}/{repo-name}.{source-locale}   -->     {org}/{repo-name}.{loc-locale}
+        /// </summary>
+        /// <param name="repository">The origin repo name</param>
+        /// <param name="locale">The current build locale</param>
+        /// <returns>The repo name with locale</returns>
+        public static string GetEditRepository(string repository, string locale, string defaultLocale)
+        {
+            if (string.Equals(locale, defaultLocale, System.StringComparison.OrdinalIgnoreCase))
+            {
+                return repository;
+            }
+
+            if (string.IsNullOrEmpty(repository))
+            {
+                return repository;
+            }
+
+            if (string.IsNullOrEmpty(locale))
+            {
+                return repository;
+            }
+
+            var repoName = repository.Split(new[] { '/', '\\' }).Last();
+            var match = s_repoNameWithLocale.Match(repoName);
+            if (match.Success && match.Groups.Count == 2 && !string.IsNullOrEmpty(match.Groups[1].Value))
+            {
+                var originLocale = match.Groups[1].Value;
+                return repository.Replace(originLocale, $".{locale}");
+            }
+
+            return $"{repository}.{locale}";
+        }
+    }
+}
