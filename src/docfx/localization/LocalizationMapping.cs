b0f20fa9e37296cdeae2d3b3c7c2abbaf995dd60
OsmondJiang <xinjiang@microsoft.com>
2018-10-15 09:50:45 +0800

    fix comments

--- src/docfx/localization/LocalizationMapping.cs
+++ src/docfx/localization/LocalizationMapping.cs
@@ -6,31 +6,31 @@ namespace Microsoft.Docs.Build
     public enum LocalizationMapping
     {
         /// <summary>
-        /// loc files are stored in an **independent repository** per locale but keep the **same folder structure**
+        /// localization files are stored in an **independent repository** per locale but keep the **same folder structure**
         /// example:
-        /// source repo         locale        loc repo
+        /// source repo         locale        localization repo
         /// dotnet/docfx        zh-cn         dotnet/docfx.zh-cn
         /// dotnet/docfx        de-de         dotnet/docfx.de-de
         /// </summary>
         Repository,
 
         /// <summary>
-        /// loc files are stored in the **same repository** with source files but under different **locale folder**
+        /// localization files are stored in the **same repository** with source files but under different **locale folder**
         /// example:
-        /// source file         -->         loc files
+        /// source file         -->         localization files
         /// /readme.md          -->         /localization/zh-cn/readme.md
         /// /files/a.md         -->         /localization/de-de/files/a.md
         /// </summary>
         Folder,
 
         /// <summary>
-        /// loc files are stored in ONE **different repository** for **all locales** under different **locale folder**
+        /// localization files are stored in ONE **different repository** for **all locales** under different **locale folder**
         /// repo mapping example:
-        /// source repo         locale        loc repo
+        /// source repo         locale        localization repo
         /// dotnet/docfx        zh-cn         dotnet/docfx.localization
         /// dotnet/docfx        de-de         dotnet/docfx.localization
         /// folder mapping example:
-        /// source repo         -->           loc repo
+        /// source repo         -->           localization repo
         /// /readme.md          -->           /zh-cn/readme.md
         /// /files/a.md         -->           /zh-cn/files/a.md
         /// </summary>
