9bc60a9589201506bd049fa769c3b653eb860ae9
OsmondJiang <xinjiang@microsoft.com>
2018-11-02 16:20:06 +0800

    add garbage collector

---
+++ src/docfx/gc/GarbageCollector.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Docs.Build
+{
+    internal static class GarbageCollector
+    {
+        public static void Collect()
+        {
+
+        }
+
+        private static void CollectGit()
+        {
+
+        }
+
+        private static void CollectUrls()
+        {
+
+        }
+
+        private static void CollectCache()
+        {
+
+        }
+    }
+}
