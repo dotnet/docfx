d8463142b542e04001f3cbc57f89558bb508c024
Renze Yu <renzeyu@microsoft.com>
2018-11-26 17:05:14 +0800

    fix

--- src/docfx/lib/github/GitHubUserCache.cs
+++ src/docfx/lib/github/GitHubUserCache.cs
@@ -141,12 +141,9 @@ namespace Microsoft.Docs.Build
                 {
                     Users = Users.ToArray(),
                 });
-               var tasks = new List<Task>
+                var tasks = new List<Task>
                 {
                     ProcessUtility.WriteFile(_cachePath, file),
-                };
-                {
-                    Task.Run(() => ProcessUtility.WriteFile(_cachePath, file)),
                 };
                 if (config.GitHub.UpdateRemoteUserCache && _isFromUrl)
                 {
