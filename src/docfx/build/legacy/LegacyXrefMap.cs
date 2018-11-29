0ecbe05f0872cdee73d63025f91cb880a8756faa
Wei Hu <huwe@microsoft.com>
2018-11-29 15:12:49 +0800

    Convert JObject to Dictionary<string, object>, so that YamlDonet serializer would not throw exception on JObject.

--- src/docfx/build/legacy/LegacyXrefMap.cs
+++ src/docfx/build/legacy/LegacyXrefMap.cs
@@ -17,24 +17,8 @@ namespace Microsoft.Docs.Build
             {
                 writer.WriteLine("### YamlMime:XRefMap");
                 var serializer = new SerializerBuilder().Build();
-                serializer.Serialize(writer, new { sorted = true, references = xrefMap.InternalReferences.Select(x => ExpandJObject(x)) });
+                serializer.Serialize(writer, new { sorted = true, references = xrefMap.InternalReferences });
             }
-        }
-
-        // YamlDotNet serializer does not like Jobject, it needs to be expanded
-        // https://github.com/aaubry/YamlDotNet/issues/254
-        private static dynamic ExpandJObject(XrefSpec spec)
-        {
-            var extensionData = spec.ExtensionData.ToObject<Dictionary<string, object>>();
-            dynamic expanedoObj = new ExpandoObject();
-            var collection = (ICollection<KeyValuePair<string, object>>)expanedoObj;
-            foreach (var kv in extensionData)
-            {
-                collection.Add(kv);
-            }
-            expanedoObj.Uid = spec.Uid;
-            expanedoObj.Href = spec.Href;
-            return expanedoObj;
         }
     }
 }
