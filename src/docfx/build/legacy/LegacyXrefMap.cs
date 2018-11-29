e060333819fdd50e3ea859334d83f7115cc3b8bc
Wei Hu <huwe@microsoft.com>
2018-11-29 10:56:27 +0800

    Fix legacy xref map output

--- src/docfx/build/legacy/LegacyXrefMap.cs
+++ src/docfx/build/legacy/LegacyXrefMap.cs
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
+using System.Linq;
 using YamlDotNet.Serialization;
 
 namespace Microsoft.Docs.Build
@@ -14,8 +17,24 @@ namespace Microsoft.Docs.Build
             {
                 writer.WriteLine("### YamlMime:XRefMap");
                 var serializer = new SerializerBuilder().Build();
-                serializer.Serialize(writer, new { sorted = true, references = xrefMap.InternalReferences });
+                serializer.Serialize(writer, new { sorted = true, references = xrefMap.InternalReferences.Select(x => ExpandJObject(x)) });
             }
+        }
+
+        // YamlDotNet serializer does not like Jobject, it needs to be expanded
+        // https://github.com/aaubry/YamlDotNet/issues/254
+        private static dynamic ExpandJObject(XrefSpec spec)
+        {
+            var extensionData = spec.ExtensionData.ToObject<Dictionary<string, object>>();
+            dynamic expanedoObj = new ExpandoObject();
+            var collection = (ICollection<KeyValuePair<string, object>>)expanedoObj;
+            foreach (var kv in extensionData)
+            {
+                collection.Add(kv);
+            }
+            expanedoObj.Uid = spec.Uid;
+            expanedoObj.Href = spec.Href;
+            return expanedoObj;
         }
     }
 }
