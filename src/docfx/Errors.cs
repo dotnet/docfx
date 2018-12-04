c262ca41c6c17b5c719eab8ae72bc6a11bf3a4b1
mingwli <mingwei.liu@microsoft.com>
2018-12-04 17:39:53 +0800

    reorder errors

--- src/docfx/Errors.cs
+++ src/docfx/Errors.cs
@@ -4,12 +4,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Microsoft.Docs.Build
 {
     internal static class Errors
     {
+#pragma warning disable SA1124 // DoNotUseRegions
+
+        #region Level Error
         public static Error RedirectionConflict(string redirectFrom)
             => new Error(ErrorLevel.Error, "redirection-conflict", $"The '{redirectFrom}' appears twice or more in the redirection mappings");
 
@@ -27,12 +29,6 @@ namespace Microsoft.Docs.Build
 
         public static Error NeedRestore(string dependenyRepoHref)
             => new Error(ErrorLevel.Error, "need-restore", $"Cannot find dependency '{dependenyRepoHref}', did you forget to run `docfx restore`?");
-
-        public static Error GitHubUserNotFound(string login)
-            => new Error(ErrorLevel.Warning, "github-user-not-found", $"Cannot find user '{login}' on GitHub");
-
-        public static Error GitHubApiFailed(string api, Exception ex)
-            => new Error(ErrorLevel.Warning, "github-api-failed", $"Failed calling GitHub API '{api}': {ex.Message}");
 
         public static Error InvalidTopicHref(Document relativeTo, string topicHref)
             => new Error(ErrorLevel.Error, "invalid-topic-href", $"The topic href '{topicHref}' can only reference to a local file or absolute path", relativeTo.ToString());
@@ -55,56 +51,20 @@ namespace Microsoft.Docs.Build
         public static Error DownloadFailed(string url, string message)
             => new Error(ErrorLevel.Error, "download-failed", $"Download '{url}' failed: {message}");
 
-        public static Error UploadFailed(string url, string message)
-            => new Error(ErrorLevel.Warning, "upload-failed", $"Upload '{url}' failed: {message}");
-
         public static Error GitCloneFailed(string url, IEnumerable<string> branches)
             => new Error(ErrorLevel.Error, "git-clone-failed", $"Cloning git repository '{url}' ({Join(branches)}) failed.");
 
-        public static Error YamlHeaderNotObject(bool isArray)
-            => new Error(ErrorLevel.Warning, "yaml-header-not-object", $"Expect yaml header to be an object, but got {(isArray ? "an array" : "a scalar")}");
-
         public static Error YamlSyntaxError(in Range range, string message)
-            => new Error(ErrorLevel.Error, "yaml-syntax-error", message, range: range);
+           => new Error(ErrorLevel.Error, "yaml-syntax-error", message, range: range);
 
         public static Error YamlDuplicateKey(in Range range, string key)
             => new Error(ErrorLevel.Error, "yaml-duplicate-key", $"Key '{key}' is already defined, remove the duplicate key.", range: range);
 
-        public static Error InvalidYamlHeader(Document file, Exception ex)
-            => new Error(ErrorLevel.Warning, "invalid-yaml-header", ex.Message, file.ToString());
-
         public static Error JsonSyntaxError(in Range range, string message, string path)
             => new Error(ErrorLevel.Error, "json-syntax-error", $"{message}", range: range, jsonPath: path);
 
-        public static Error LinkIsEmpty(Document relativeTo)
-            => new Error(ErrorLevel.Info, "link-is-empty", "Link is empty", relativeTo.ToString());
-
-        public static Error LinkOutOfScope(Document relativeTo, Document file, string href, string configFile)
-            => new Error(ErrorLevel.Warning, "link-out-of-scope", $"File '{file}' referenced by link '{href}' will not be built because it is not included in {configFile}", relativeTo.ToString());
-
-        public static Error RedirectionOutOfScope(Document redirection, string configFile)
-            => new Error(ErrorLevel.Warning, "redirection-out-of-scope", $"Redirection file '{redirection}' will not be built because it is not included in {configFile}");
-
-        public static Error LinkIsDependency(Document relativeTo, Document file, string href)
-            => new Error(ErrorLevel.Warning, "link-is-dependency", $"File '{file}' referenced by link '{href}' will not be built because it is from a dependency docset", relativeTo.ToString());
-
-        public static Error AbsoluteFilePath(Document relativeTo, string path)
-            => new Error(ErrorLevel.Warning, "absolute-file-path", $"File path cannot be absolute: '{path}'", relativeTo.ToString());
-
-        public static Error HeadingNotFound(Document file)
-            => new Error(ErrorLevel.Warning, "heading-not-found", $"The first visible block is not a heading block with `#`", file.ToString());
-
-        public static Error FileNotFound(string relativeTo, string path)
-            => new Error(ErrorLevel.Warning, "file-not-found", $"Cannot find file '{path}' relative to '{relativeTo}'", relativeTo);
-
-        public static Error UidNotFound(Document file, string uid, string rawXref)
-            => new Error(ErrorLevel.Warning, "uid-not-found", $"Cannot find uid '{uid}' using xref '{rawXref}'", file.ToString());
-
         public static Error MergeConflict(string file)
             => new Error(ErrorLevel.Error, "merge-conflict", "File contains merge conflict", file);
-
-        public static Error AtUidNotFound(Document file, string uid, string rawXref)
-            => new Error(ErrorLevel.Info, "at-uid-not-found", $"Cannot find uid '{uid}' using xref '{rawXref}'", file.ToString());
 
         public static Error PublishUrlConflict(string url, IEnumerable<Document> files, IEnumerable<string> conflictMonikers)
         {
@@ -112,14 +72,8 @@ namespace Microsoft.Docs.Build
             return new Error(ErrorLevel.Error, "publish-url-conflict", $"Two or more documents{message} publish to the same url '{url}': {Join(files, file => file.ContentType == ContentType.Redirection ? $"{file} <redirection>" : file.ToString())}");
         }
 
-        public static Error IncludeRedirection(Document relativeTo, string path)
-            => new Error(ErrorLevel.Warning, "include-is-redirection", $"Referenced inclusion {path} relative to '{relativeTo}' shouldn't belong to redirections", relativeTo.ToString());
-
         public static Error OutputPathConflict(string path, IEnumerable<Document> files)
             => new Error(ErrorLevel.Error, "output-path-conflict", $"Two or more documents output to the same path '{path}': {Join(files, file => file.ContentType == ContentType.Redirection ? $"{file} <redirection>" : file.ToString())}");
-
-        public static Error RedirectionDocumentIdConflict(IEnumerable<Document> redirectFromDocs, string redirectTo)
-            => new Error(ErrorLevel.Warning, "redirected-id-conflict", $"Multiple documents redirected to '{redirectTo}' with document id: {Join(redirectFromDocs)}");
 
         public static Error GitLogError(string repoPath, int errorCode)
             => new Error(ErrorLevel.Error, "git-log-error", $"Error computing git log [{errorCode}] for '{repoPath}', did you used a shadow clone?");
@@ -129,15 +83,6 @@ namespace Microsoft.Docs.Build
 
         public static Error CommittishNotFound(string repo, string committish)
             => new Error(ErrorLevel.Error, "committish-not-found", $"Cannot find branch, tag or commit '{committish}'for repo '{repo}'.");
-
-        public static Error BookmarkNotFound(Document relativeTo, Document reference, string bookmark, IEnumerable<string> candidateBookmarks)
-            => new Error(ErrorLevel.Warning, "bookmark-not-found", $"Cannot find bookmark '#{bookmark}' in '{reference}'{(FindBestMatch(bookmark, candidateBookmarks, out string matchedBookmark) ? $", did you mean '#{matchedBookmark}'?" : null)}", relativeTo.ToString());
-
-        public static Error NullValue(in Range range, string name, string path)
-            => new Error(ErrorLevel.Info, "null-value", $"'{name}' contains null value", range: range, jsonPath: path);
-
-        public static Error UnknownField(in Range range, string propName, string typeName, string path)
-            => new Error(ErrorLevel.Warning, "unknown-field", $"Could not find member '{propName}' on object of type '{typeName}'.", range: range, jsonPath: path);
 
         public static Error ViolateSchema(in Range range, string message, string path)
             => new Error(ErrorLevel.Error, "violate-schema", $"{message}", range: range, jsonPath: path);
@@ -162,6 +107,56 @@ namespace Microsoft.Docs.Build
 
         public static Error InvalidMonikerRange(string monikerRange, string message)
             => new Error(ErrorLevel.Error, "invalid-moniker-range", $"MonikerRange `{monikerRange}` is invalid: {message}");
+        #endregion
+
+        #region Level Warning
+        public static Error BookmarkNotFound(Document relativeTo, Document reference, string bookmark, IEnumerable<string> candidateBookmarks)
+            => new Error(ErrorLevel.Warning, "bookmark-not-found", $"Cannot find bookmark '#{bookmark}' in '{reference}'{(FindBestMatch(bookmark, candidateBookmarks, out string matchedBookmark) ? $", did you mean '#{matchedBookmark}'?" : null)}", relativeTo.ToString());
+
+        public static Error UnknownField(in Range range, string propName, string typeName, string path)
+            => new Error(ErrorLevel.Warning, "unknown-field", $"Could not find member '{propName}' on object of type '{typeName}'.", range: range, jsonPath: path);
+
+        public static Error GitHubUserNotFound(string login)
+            => new Error(ErrorLevel.Warning, "github-user-not-found", $"Cannot find user '{login}' on GitHub");
+
+        public static Error GitHubApiFailed(string api, Exception ex)
+            => new Error(ErrorLevel.Warning, "github-api-failed", $"Failed calling GitHub API '{api}': {ex.Message}");
+
+        public static Error UploadFailed(string url, string message)
+            => new Error(ErrorLevel.Warning, "upload-failed", $"Upload '{url}' failed: {message}");
+
+        public static Error YamlHeaderNotObject(bool isArray)
+            => new Error(ErrorLevel.Warning, "yaml-header-not-object", $"Expect yaml header to be an object, but got {(isArray ? "an array" : "a scalar")}");
+
+        public static Error InvalidYamlHeader(Document file, Exception ex)
+            => new Error(ErrorLevel.Warning, "invalid-yaml-header", ex.Message, file.ToString());
+
+        public static Error LinkOutOfScope(Document relativeTo, Document file, string href, string configFile)
+            => new Error(ErrorLevel.Warning, "link-out-of-scope", $"File '{file}' referenced by link '{href}' will not be built because it is not included in {configFile}", relativeTo.ToString());
+
+        public static Error RedirectionOutOfScope(Document redirection, string configFile)
+            => new Error(ErrorLevel.Warning, "redirection-out-of-scope", $"Redirection file '{redirection}' will not be built because it is not included in {configFile}");
+
+        public static Error LinkIsDependency(Document relativeTo, Document file, string href)
+            => new Error(ErrorLevel.Warning, "link-is-dependency", $"File '{file}' referenced by link '{href}' will not be built because it is from a dependency docset", relativeTo.ToString());
+
+        public static Error AbsoluteFilePath(Document relativeTo, string path)
+            => new Error(ErrorLevel.Warning, "absolute-file-path", $"File path cannot be absolute: '{path}'", relativeTo.ToString());
+
+        public static Error HeadingNotFound(Document file)
+            => new Error(ErrorLevel.Warning, "heading-not-found", $"The first visible block is not a heading block with `#`", file.ToString());
+
+        public static Error FileNotFound(string relativeTo, string path)
+            => new Error(ErrorLevel.Warning, "file-not-found", $"Cannot find file '{path}' relative to '{relativeTo}'", relativeTo);
+
+        public static Error UidNotFound(Document file, string uid, string rawXref)
+            => new Error(ErrorLevel.Warning, "uid-not-found", $"Cannot find uid '{uid}' using xref '{rawXref}'", file.ToString());
+
+        public static Error IncludeRedirection(Document relativeTo, string path)
+            => new Error(ErrorLevel.Warning, "include-is-redirection", $"Referenced inclusion {path} relative to '{relativeTo}' shouldn't belong to redirections", relativeTo.ToString());
+
+        public static Error RedirectionDocumentIdConflict(IEnumerable<Document> redirectFromDocs, string redirectTo)
+            => new Error(ErrorLevel.Warning, "redirected-id-conflict", $"Multiple documents redirected to '{redirectTo}' with document id: {Join(redirectFromDocs)}");
 
         public static Error MonikerConfigMissing()
             => new Error(ErrorLevel.Warning, "moniker-config-missing", "Moniker range missing in docfx.yml/docfx.json, user should not define it in file metadata or moniker zone.");
@@ -171,6 +166,19 @@ namespace Microsoft.Docs.Build
 
         public static Error InvalidUidMoniker(string moniker, string uid)
             => new Error(ErrorLevel.Warning, "invalid-uid-moniker", $"Moniker '{moniker}' is not defined with uid '{uid}'");
+        #endregion
+
+        #region Level Info
+        public static Error LinkIsEmpty(Document relativeTo)
+            => new Error(ErrorLevel.Info, "link-is-empty", "Link is empty", relativeTo.ToString());
+
+        public static Error AtUidNotFound(Document file, string uid, string rawXref)
+            => new Error(ErrorLevel.Info, "at-uid-not-found", $"Cannot find uid '{uid}' using xref '{rawXref}'", file.ToString());
+
+        public static Error NullValue(in Range range, string name, string path)
+            => new Error(ErrorLevel.Info, "null-value", $"'{name}' contains null value", range: range, jsonPath: path);
+        #endregion
+#pragma warning restore SA1124 // DoNotUseRegions
 
         private static string Join<T>(IEnumerable<T> source, Func<T, string> selector = null)
             => string.Join(", ", source.Select(item => $"{selector?.Invoke(item) ?? item.ToString()}").OrderBy(_ => _, StringComparer.Ordinal).Select(_ => $"'{_}'").Take(5));
