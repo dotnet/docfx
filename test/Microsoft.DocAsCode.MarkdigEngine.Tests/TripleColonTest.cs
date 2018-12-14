35de5644af09034d53ebfa4d69275edb318fb68c
Jedd Ahyoung <jeahyoun@microsoft.com>
2018-12-14 12:41:33 -0800

    Add test for closing blocks.

--- test/Microsoft.DocAsCode.MarkdigEngine.Tests/TripleColonTest.cs
+++ test/Microsoft.DocAsCode.MarkdigEngine.Tests/TripleColonTest.cs
@@ -3,8 +3,11 @@
 
 namespace Microsoft.DocAsCode.MarkdigEngine.Tests
 {
+    using Markdig.Syntax;
     using Microsoft.DocAsCode.Common;
+    using Microsoft.DocAsCode.MarkdigEngine.Extensions;
     using Microsoft.DocAsCode.Plugins;
+    using System.Collections.Generic;
     using System.Linq;
     using Xunit;
 
@@ -55,6 +58,55 @@ namespace Microsoft.DocAsCode.MarkdigEngine.Tests
 
             // Listener should have no error or warning message.
             Assert.Empty(listener.Items);
+        }
+
+        [Fact]
+        public void TripleColonTestBlockClosed()
+        {
+            var source = @"::: zone target=""chromeless""
+::: form action=""create-resource"" submitText=""Create"" :::
+::: zone-end
+";
+
+            var expected = @"<div class=""zone has-target"" data-target=""chromeless"">
+<form class=""chromeless-form"" data-action=""create-resource"">
+<div></div>
+<button class=""button is-primary"" disabled=""disabled"" type=""submit"">Create</button>
+</form>
+</div>
+".Replace("\r\n", "\n");
+
+            var listener = TestLoggerListener.CreateLoggerListenerWithPhaseEqualFilter(LoggerPhase);
+
+            Logger.RegisterListener(listener);
+            using (new LoggerPhaseScope(LoggerPhase))
+            {
+                var service = TestUtility.CreateMarkdownService();
+                var document = service.Parse(source, "fakepath.md");
+                var blocks = new List<TripleColonBlock>();
+                var stack = new Stack<ContainerBlock>();
+                stack.Push(document);
+
+                // Get all triplecolon blocks in the document using a depth-first iterative tree traversal strategy.
+                do
+                {
+                    var block = stack.Pop();
+                    var children = block.Where(x => x.GetType() == typeof(TripleColonBlock)).Select(x => x as TripleColonBlock);
+                    blocks.AddRange(children);
+                    foreach (var child in children)
+                    {
+                        stack.Push(child);
+                    }
+
+                } while (stack.Count > 0);
+                
+                foreach (var block in blocks)
+                {
+                    Assert.True(block.Closed);
+                    Assert.False(block.IsOpen);
+                }
+            }
+            Logger.UnregisterListener(listener);
         }
     }
 }
