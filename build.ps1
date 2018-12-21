54052e02f6e3ebc6e25be944fab032629e3f7373
mingwli <mingwei.liu@microsoft.com>
2018-12-21 16:59:15 +0800

    update pack version logic
    only deploy when it's a deployable branch
    set package version based on branch

--- build.ps1
+++ build.ps1
@@ -6,14 +6,26 @@ function exec([string] $cmd) {
     }
 }
 
-$commitSha = & { git describe --always }
-$commitCount = & { git rev-list --count HEAD }
-$revision = $commitCount.ToString().PadLeft(5, '0')
-$version = "3.0.0-alpha-$revision-$commitSha"
-
-Remove-Item ./drop -Force -Recurse -ErrorAction Ignore
-
+# running tests
 exec "dotnet run -p tools/CreateJsonSchema"
 exec "dotnet test test\docfx.Test"
 exec "dotnet test test\docfx.Test -c Release"
+
+# packing
+$featureBranchPrefix = "feature/"
+$branch = & {git rev-parse --abbrev-ref HEAD}
+$commitSha = & { git describe --always }
+$commitCount = & { git rev-list --count HEAD }
+$revision = $commitCount.ToString().PadLeft(5, '0')
+
+if ($branch -eq "v3") {
+    $version = "3.0.0-beta-$revision-$commitSha"
+} elseif ($branch.StartsWith($featureBranchPrefix)) {
+    $feature = $branch.SubString($featureBranchPrefix.length)
+    $version = "3.0.0-alpha-$feature-$revision-$commitSha"
+} else {
+    exit 0
+}
+
+Remove-Item ./drop -Force -Recurse -ErrorAction Ignore
 exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version"
