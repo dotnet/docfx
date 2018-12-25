3f30e5bec28ead85024540b7caf42f26bf0ef23e
mingwli <mingwei.liu@microsoft.com>
2018-12-25 16:47:31 +0800

    refactor build.ps1

--- build.ps1
+++ build.ps1
@@ -6,29 +6,32 @@ function exec([string] $cmd) {
     }
 }
 
+function getBranchName() {
+    $branchPrefix = "refs/heads/"
+    $branch = & {git rev-parse --abbrev-ref HEAD}
+    if ([string]::IsNullOrEmpty($branch)) {
+        #https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=vsts&tabs=yaml%2Cbatch#working-with-variables
+        $branch = $env:BUILD_SOURCEBRANCH
+    }
+    if ($branch.StartsWith($branchPrefix)) {
+        $branch = $branch.SubString($branchPrefix.length)
+    }
+    return $branch
+}
 # running tests
 exec "dotnet run -p tools/CreateJsonSchema"
 exec "dotnet test test\docfx.Test"
 exec "dotnet test test\docfx.Test -c Release"
 
 # packing
-$branchPrefix = "refs/heads/"
 $commitSha = & { git describe --always }
 $commitCount = & { git rev-list --count HEAD }
 $revision = $commitCount.ToString().PadLeft(5, '0')
-$branch = & {git rev-parse --abbrev-ref HEAD}
-if ([string]::IsNullOrEmpty($branch)) {
-    #https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=vsts&tabs=yaml%2Cbatch#working-with-variables
-    $branch = $env:BUILD_SOURCEBRANCH
-}
+$branch = getBranchName
 
-if ($branch -eq "refs/heads/v3") {
+if ($branch -eq "v3") {
     # CI triggered by v3
     $version = "3.0.0-beta-$revision-$commitSha"
-} elseif ($branch.StartsWith($branchPrefix)) {
-    # CI triggered by any other branch
-    $branch = $branch.SubString($branchPrefix.length).Replace('/', '-')
-    $version = "3.0.0-alpha-$branch-$revision-$commitSha"
 } else {
     # local run
     $branch = $branch.Replace('/', '-')
