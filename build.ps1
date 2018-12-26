60daac348433da607b13aa956d3b1bb078c5335b
LIU Mingwei <42199316+mingwli@users.noreply.github.com>
2018-12-26 13:54:17 +0800

    new CI CD process (#3821)
    run test jobs when is pr targeting `v3`/`feature/*`  
    run deploy jobs when Individual CI happened on `v3`/`feature/*`, only `v3` will run impact

--- build.ps1
+++ build.ps1
@@ -6,14 +6,37 @@ function exec([string] $cmd) {
     }
 }
 
-$commitSha = & { git describe --always }
-$commitCount = & { git rev-list --count HEAD }
-$revision = $commitCount.ToString().PadLeft(5, '0')
-$version = "3.0.0-alpha-$revision-$commitSha"
-
-Remove-Item ./drop -Force -Recurse -ErrorAction Ignore
-
+function getBranchName() {
+    $branchPrefix = "refs/heads/"
+    $branch = & {git rev-parse --abbrev-ref HEAD}
+    if ([string]::IsNullOrEmpty($branch)) {
+        #https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=vsts&tabs=yaml%2Cbatch#working-with-variables
+        $branch = $env:BUILD_SOURCEBRANCH
+    }
+    if ($branch.StartsWith($branchPrefix)) {
+        $branch = $branch.SubString($branchPrefix.length)
+    }
+    return $branch
+}
+# running tests
 exec "dotnet run -p tools/CreateJsonSchema"
 exec "dotnet test test\docfx.Test"
 exec "dotnet test test\docfx.Test -c Release"
+
+# packing
+$commitSha = & { git describe --always }
+$commitCount = & { git rev-list --count HEAD }
+$revision = $commitCount.ToString().PadLeft(5, '0')
+$branch = getBranchName
+
+if ($branch -eq "v3") {
+    # CI triggered by v3
+    $version = "3.0.0-beta-$revision-$commitSha"
+} else {
+    # local run
+    $branch = $branch.Replace('/', '-')
+    $version = "3.0.0-alpha-$branch-$revision-$commitSha"
+}
+
+Remove-Item ./drop -Force -Recurse -ErrorAction Ignore
 exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version"
