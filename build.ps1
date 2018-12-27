75b59f402bb35d67e82553449804429569edeee2
Yufei Huang <yufeih@live.com>
2018-12-27 11:07:04 +0800

    Check code coverage

--- build.ps1
+++ build.ps1
@@ -18,12 +18,21 @@ function getBranchName() {
     }
     return $branch
 }
-# running tests
-exec "dotnet run -p tools/CreateJsonSchema"
+
+# Running tests
 exec "dotnet test test\docfx.Test"
 exec "dotnet test test\docfx.Test -c Release --logger trx"
 
-# packing
+# Check test coverage
+$coverage = Select-Xml -Path 'test\docfx.Test\coverage.cobertura.xml' -XPath "//package[@name='docfx']" | select -exp Node | select -exp line-rate
+if ($coverage -lt 0.8) {
+    throw ("Test code coverage MUST be > 0.8, but is now only $coverage")
+}
+
+# Check schema
+exec "dotnet run -p tools/CreateJsonSchema"
+
+# Packing
 $commitSha = & { git describe --always }
 $commitCount = & { git rev-list --count HEAD }
 $revision = $commitCount.ToString().PadLeft(5, '0')
@@ -33,7 +42,7 @@ if ($branch -eq "v3") {
     # CI triggered by v3
     $version = "3.0.0-beta-$revision-$commitSha"
 } else {
-    # local run
+    # Local run
     $branch = $branch.Replace('/', '-')
     $version = "3.0.0-alpha-$branch-$revision-$commitSha"
 }
