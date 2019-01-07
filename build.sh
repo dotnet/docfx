668efcf732a544b946268fe82726bb78a563e9f7
Yufei Huang <yufeih@live.com>
2019-01-07 14:52:25 +0800

    Test dotnet tool install (#3868)

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ set -e
 dotnet run -p tools/CreateJsonSchema
 dotnet test test/docfx.Test
 dotnet test test/docfx.Test -c Release
-dotnet pack src/docfx -c Release
+
+
+rm -rf drop
+dotnet pack src/docfx -c Release -o $PWD/drop /p:Version=3.0.0
+dotnet tool install docfx --version 3.0.0-* --add-source drop --tool-path drop
