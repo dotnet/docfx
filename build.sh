b8a43b4676194b2f082561dfbfe4539ece02abb4
Yufei Huang <yufeih@live.com>
2019-01-07 11:13:10 +0800

    fix ci

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ dotnet test test/docfx.Test -c Release
 rm -rf drop
 dotnet pack src/docfx -c Release -o $PWD/drop /p:Version=3.0.0
 dotnet tool install docfx --version 3.0.0-* --add-source drop --tool-path drop
-drop/docfx --version
