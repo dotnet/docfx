5b98c9f7e1ba8e699a87deb6aa6b94bcb41c4e6d
Wei Hu <huwe@microsoft.com>
2019-02-11 11:43:33 +0800

    Add dependencies to legacy aggregated file map

--- src/docfx/build/legacy/LegacyAggregatedFileMap.cs
+++ src/docfx/build/legacy/LegacyAggregatedFileMap.cs
@@ -4,12 +4,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Docs.Build
 {
     internal static class LegacyAggregatedFileMap
     {
-        public static void Convert(Docset docset, Context context, IEnumerable<(string legacyFilePathRelativeToBaseFolder, LegacyFileMapItem fileMapItem)> items)
+        public static void Convert(Docset docset, Context context, IEnumerable<(string legacyFilePathRelativeToBaseFolder, LegacyFileMapItem fileMapItem)> items, Dictionary<string, List<LegacyDependencyMapItem>> dependencyMap)
         {
             var aggregatedFileMapItems = new Dictionary<string, object>();
 
@@ -22,6 +25,9 @@ namespace Microsoft.Docs.Build
 
                 var aggregatedFileMapItem = new
                 {
+                    dependencies = dependencyMap.ContainsKey(legacyFilePathRelativeToBaseFolder)
+                                    ? dependencyMap[legacyFilePathRelativeToBaseFolder].Select(x => new DependencyItem { FromFilePath = PathUtility.NormalizeFile(Path.Combine(docset.Config.DocumentId.SourceBasePath, x.From)), ToFilePath = PathUtility.NormalizeFile(Path.Combine(docset.Config.DocumentId.SourceBasePath, x.To)), DependencyType = x.Type })
+                                    : new List<DependencyItem>(),
                     aggregated_monikers = Array.Empty<string>(), // todo
                     docset_names = new[] { docset.Config.Name },
                     has_non_moniker_url = true, // todo
@@ -44,6 +50,16 @@ namespace Microsoft.Docs.Build
                         },
                     },
                 }, "op_aggregated_file_map_info.json");
+        }
+
+        [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+        private class DependencyItem
+        {
+            public string FromFilePath { get; set; }
+
+            public string ToFilePath { get; set; }
+
+            public LegacyDependencyMapType DependencyType { get; set; }
         }
     }
 }
