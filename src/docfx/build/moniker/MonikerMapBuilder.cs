ed5311d056ea45bb5bc4725ea0dba773c827c15a
Yufei Huang <yufeih@live.com>
2019-02-20 15:29:06 +0800

    Fix build

--- src/docfx/build/moniker/MonikerMapBuilder.cs
+++ src/docfx/build/moniker/MonikerMapBuilder.cs
@@ -1,17 +1,18 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace Microsoft.Docs.Build
 {
     internal class MonikerMapBuilder
     {
-        private readonly Dictionary<Document, List<string>> _documentToMonikers;
+        private readonly ConcurrentDictionary<Document, List<string>> _documentToMonikers = new ConcurrentDictionary<Document, List<string>>();
 
         public void Add(Document file, List<string> monikers)
         {
-            _documentToMonikers.Add(file, monikers);
+            _documentToMonikers.TryAdd(file, monikers);
         }
 
         public MonikerMap Build()
