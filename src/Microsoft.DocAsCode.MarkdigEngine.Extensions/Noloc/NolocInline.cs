6375541889bc9559b7991961260f0d10938edfd1
Shuang Jiang <jiangshuang007@126.com>
2019-02-21 10:28:47 +0800

    add noloc extension (#3964)
    * add noloc extension
    
    * update syntax details

---
+++ src/Microsoft.DocAsCode.MarkdigEngine.Extensions/Noloc/NolocInline.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
+{
+    using Markdig.Syntax.Inlines;
+
+    public class NolocInline : LeafInline
+    {
+        public string Text { get; set; }
+    }
+}
