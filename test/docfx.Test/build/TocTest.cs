22d5cbb2e9f8bb6b2962e9daf124b70a8df36258
Osmond Jiang <xinjiang@microsoft.com>
2019-03-01 15:51:38 +0800

    only find up-level toc, same behavior like v2  (#4097)
    * fix toc rel bug
    
    * address pr comments

--- test/docfx.Test/build/TocTest.cs
+++ test/docfx.Test/build/TocTest.cs
@@ -20,40 +20,48 @@ namespace Microsoft.Docs.Build
 
         [Theory]
         // same level
-        [InlineData(new[] { "TOC.md" }, "b.md", "toc.json")]
-        [InlineData(new[] { "TOC.md", "a/TOC.md" }, "b.md", "toc.json")]
-        [InlineData(new[] { "TOC.md", "a/TOC.md" }, "a/b.md", "toc.json")]
-        [InlineData(new[] { "b/TOC.md", "a/TOC.md" }, "a/b.md", "toc.json")]
-        [InlineData(new[] { "TOC.md", "a/b/TOC.md" }, "a/b/b.md", "toc.json")]
-        [InlineData(new[] { "c/a/d/TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "toc.json")]
+        [InlineData(new[] { "TOC.md" }, "b.md", "toc.json", "toc.json")]
+        [InlineData(new[] { "TOC.md", "a/TOC.md" }, "b.md", "toc.json", "toc.json")]
+        [InlineData(new[] { "TOC.md", "a/TOC.md" }, "a/b.md", "toc.json", "toc.json")]
+        [InlineData(new[] { "b/TOC.md", "a/TOC.md" }, "a/b.md", "toc.json", "toc.json")]
+        [InlineData(new[] { "TOC.md", "a/b/TOC.md" }, "a/b/b.md", "toc.json", "toc.json")]
+        [InlineData(new[] { "c/a/d/TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "toc.json", "toc.json")]
 
         // next level(nearest)
-        [InlineData(new[] { "b/c/TOC.md", "a/TOC.md" }, "b.md", "a/toc.json")]
-        [InlineData(new[] { "b/TOC.md", "a/b/TOC.md" }, "b.md", "b/toc.json")]
-        [InlineData(new[] { "b/./TOC.md", "a/b/TOC.md" }, "b.md", "b/toc.json")]
-        [InlineData(new[] { "b/../b/./TOC.md", "a/b/TOC.md" }, "b.md", "b/toc.json")]
-        [InlineData(new[] { "b/../b/./TOC.md", "a/b/TOC.md" }, "a/../b.md", "b/toc.json")]
-        [InlineData(new[] { "a/TOC.md", "a/b/TOC.md" }, "a/../b.md", "a/toc.json")]
+        [InlineData(new[] { "b/c/TOC.md", "a/TOC.md" }, "b.md", "a/toc.json", null)]
+        [InlineData(new[] { "b/TOC.md", "a/b/TOC.md" }, "b.md", "b/toc.json", null)]
+        [InlineData(new[] { "b/./TOC.md", "a/b/TOC.md" }, "b.md", "b/toc.json", null)]
+        [InlineData(new[] { "b/../b/./TOC.md", "a/b/TOC.md" }, "b.md", "b/toc.json", null)]
+        [InlineData(new[] { "b/../b/./TOC.md", "a/b/TOC.md" }, "a/../b.md", "b/toc.json", null)]
+        [InlineData(new[] { "a/TOC.md", "a/b/TOC.md" }, "a/../b.md", "a/toc.json", null)]
 
         // order by folder name
-        [InlineData(new[] { "b/c/TOC.md", "b/d/TOC.md" }, "b.md", "b/c/toc.json")]
+        [InlineData(new[] { "b/c/TOC.md", "b/d/TOC.md" }, "b.md", "b/c/toc.json", null)]
+        [InlineData(new[] { "b/c/TOC.md", "b/d/TOC.md" }, "b/e/b.md", "../c/toc.json", null)]
 
         // up level(nearest)
-        [InlineData(new[] { "TOC.md", "a/TOC.md" }, "b/b.md", "../toc.json")]
-        [InlineData(new[] { "TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "../toc.json")]
-        [InlineData(new[] { "c/b/TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "../toc.json")]
+        [InlineData(new[] { "TOC.md", "a/TOC.md" }, "b/b.md", "../toc.json", "../toc.json")]
+        [InlineData(new[] { "TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "../toc.json", "../toc.json")]
+        [InlineData(new[] { "c/b/TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "../toc.json", "../toc.json")]
+
+        // mix level(nearst)
+        [InlineData(new[] { "c/b/TOC.md", "TOC.md" }, "c/a.md", "../toc.json", "../toc.json")]
+        [InlineData(new[] { "c/b/TOC.md", "TOC.md" }, "c/b/a.md", "toc.json", "toc.json")]
+        [InlineData(new[] { "c/b/TOC.md", "c/d/e/TOC.md" }, "c/f/h/a.md", "../../b/toc.json", null)]
 
         // order by levenshtein distance
-        [InlineData(new[] { "a/TOC.md", "b/TOC.md" }, "b.md", "b/toc.json")]
-        [InlineData(new[] { "c/b/TOC.md", "c/a/TOC.md" }, "c/e/b.md", "../b/toc.json")]
+        [InlineData(new[] { "a/TOC.md", "b/TOC.md" }, "b.md", "b/toc.json", null)]
+        [InlineData(new[] { "c/b/TOC.md", "c/a/TOC.md" }, "c/e/b.md", "../b/toc.json", null)]
         [InlineData(new[] { "a/multi-factor-authentication/TOC.md", "a/active-directory-domain-services/toc.md" },
                     "a/multi-factor-authentication.md",
-                    "multi-factor-authentication/toc.json")]
-        public static void FindTocRelativePath(string[] tocFiles, string file, string expectedTocPath)
+                    "multi-factor-authentication/toc.json", null)]
+
+        public static void FindTocRelativePath(string[] tocFiles, string file, string expectedTocPath, string expectedOrphanTocPath)
         {
             var builder = new TableOfContentsMapBuilder();
             var (_, document) = Document.TryCreate(s_docset, file);
 
+            // test multiple reference case
             foreach (var tocFile in tocFiles)
             {
                 var (_, toc) = Document.TryCreate(s_docset, tocFile);
@@ -62,6 +70,16 @@ namespace Microsoft.Docs.Build
 
             var tocMap = builder.Build();
             Assert.Equal(expectedTocPath, tocMap.FindTocRelativePath(document));
+
+            // test orphan case
+            builder = new TableOfContentsMapBuilder();
+            foreach (var tocFile in tocFiles)
+            {
+                var (_, toc) = Document.TryCreate(s_docset, tocFile);
+                builder.Add(toc, Array.Empty<Document>(), Array.Empty<Document>());
+            }
+            tocMap = builder.Build();
+            Assert.Equal(expectedOrphanTocPath, tocMap.FindTocRelativePath(document));
         }
     }
 }
