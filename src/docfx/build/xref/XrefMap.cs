e615f2876e60ca16cd3c4d1595482fec96906aae
osmondjiang <xinjiang@microsoft.com>
2019-03-04 17:10:11 +0800

    remove unused using

--- src/docfx/build/xref/XrefMap.cs
+++ src/docfx/build/xref/XrefMap.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -10,6 +9,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
 {
--- test/docfx.Test/restore/RestoreTest.cs
+++ test/docfx.Test/restore/RestoreTest.cs
@@ -3,7 +3,6 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 
