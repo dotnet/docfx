e3a0d1fd74cfdc74b9dce205b184df8471f5952a
Yufei Huang <yufeih@live.com>
2019-03-08 16:32:24 +0800

    clean

--- src/docfx/lib/log/Telemetry.cs
+++ src/docfx/lib/log/Telemetry.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Metrics;
