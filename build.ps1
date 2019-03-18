7cb2b8e54664644f4a24b9c098ee2d8bf74ea3f4
Yufei Huang <yufeih@live.com>
2019-03-18 15:08:09 +0800

    Update build.ps1 to generate correct version number (#4188)

--- build.ps1
+++ build.ps1
@@ -6,20 +6,6 @@ function exec([string] $cmd) {
     }
 }
 
-function getBranchName() {
-    $branchPrefix = "refs/heads/"
-    # https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=vsts&tabs=yaml%2Cbatch#working-with-variables
-    $branch = $env:BUILD_SOURCEBRANCH
-    if ([string]::IsNullOrEmpty($branch)) {
-        # Local detached head
-        return & {git rev-parse --abbrev-ref HEAD}
-    }
-    if ($branch.StartsWith($branchPrefix)) {
-        return $branch.SubString($branchPrefix.length)
-    }
-    return $branch
-}
-
 function runTests() {
     try {
         pushd test/docfx.Test
@@ -49,16 +35,9 @@ function createNuGetPackage() {
     $commitSha = & { git describe --always }
     $commitCount = & { git rev-list --count HEAD }
     $revision = $commitCount.ToString().PadLeft(5, '0')
-    $branch = getBranchName
 
-    if ($branch -eq "v3") {
-        # CI triggered by v3
-        $version = "3.0.0-beta-$revision-$commitSha"
-    } else {
-        # Local run
-        $branch = $branch.Replace('/', '-')
-        $version = "3.0.0-alpha-$branch-$revision-$commitSha"
-    }
+    # CI triggered by v3
+    $version = "3.0.0-beta-$revision-$commitSha"
 
     Remove-Item ./drop -Force -Recurse -ErrorAction Ignore
     exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version"
