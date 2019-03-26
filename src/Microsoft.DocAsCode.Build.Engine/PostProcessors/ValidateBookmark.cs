d67b84bac8c72e16096461c17c55f5dafe702d0b
Peican Gao <v-pegao@microsoft.com>
2019-03-26 11:11:26 +0000

    add rawTitle to ManifestProperties

--- src/Microsoft.DocAsCode.Build.Engine/PostProcessors/ValidateBookmark.cs
+++ src/Microsoft.DocAsCode.Build.Engine/PostProcessors/ValidateBookmark.cs
@@ -97,6 +97,12 @@ namespace Microsoft.DocAsCode.Build.Engine
                      TargetLineNumber = node.Line
                  }).ToList();
             var anchors = GetNodeAttribute(document, "id").Concat(GetNodeAttribute(document, "name"));
+            if (manifestItem.Metadata.ContainsKey("rawTitle"))
+            {               
+                var rawTitle = new HtmlDocument();
+                rawTitle.LoadHtml(manifestItem.Metadata["rawTitle"].ToString());
+                anchors = anchors.Concat(GetNodeAttribute(rawTitle, "id"));
+            }
             _registeredBookmarks[outputFile] = new HashSet<string>(anchors);
         }
 
