611e336cb75493dad8a5979306148d8ccabdcdb9
Yufei Huang <yufeih@live.com>
2019-04-03 11:50:22 +0800

    Remove CreateJsonSchema due to json schema.net license issues. (#4296)

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-dotnet run -p tools/CreateJsonSchema
 dotnet test test/docfx.Test 2>&1
 dotnet test test/docfx.Test -c Release 2>&1
-
 
 rm -rf drop
 dotnet pack src/docfx -c Release -o $PWD/drop /p:Version=3.0.0
