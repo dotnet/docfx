df73989922e89bf39915d985bcba607b31702e93
osmondjiang <xinjiang@microsoft.com>
2019-04-09 15:02:15 +0800

    add more metadata black list

--- src/docfx/build/metadata/MetadataValidator.cs
+++ src/docfx/build/metadata/MetadataValidator.cs
@@ -77,7 +77,18 @@ namespace Microsoft.Docs.Build
 
         private static HashSet<string> GetReservedMetadata()
         {
-            var blackList = new HashSet<string>(JsonUtility.GetPropertyNames(typeof(PageModel)));
+            var legacyBlackList = new[]
+            {
+                "content_type", "document_id", "ms.documentid", "internal_document_id", "locale", "ms.contentlang", "ms.locale", "product_family", "product_version",
+                "search.ms_sitename", "search.ms_product", "search.ms_docsetname", "updated_at", "ms.publishtime", "toc_asset_id", "original_content_git_url", "ms.giturl",
+                "original_ref_skeleton_git_url", "ms.gitsourceurl", "toc_rel", "site_name", "ms.sitename", "area", "theme", "theme_branch", "theme_url", "is_active",
+                "gitcommit", "ms.gitcommit", "ref_skeleton_gitcommit", "ms.gitsourcecommit", "Product", "TopicType", "APIType", "APILocation", "APIName" , "APIExtraInfo",
+                "TargetOS", "sitemap_priority", "AmbientContext", "MN", "ms.auth", "ms.lang", "ms.loc", "ms.prodver", "ms.puidhash", "ms.contentsource", "depot_name",
+                "ms.depotname", "pagetype", "ms.opspagetype", "word_count", "content_uri", "publish_version", "canonical_url", "relative_path_to_theme_resources",
+                "is_dynamic_rendering", "need_preview_pull_request", "moniker_type", "is_significant_update", "document_version_independent_id", "serviceData", "is_hidden",
+            };
+
+            var blackList = new HashSet<string>(JsonUtility.GetPropertyNames(typeof(PageModel)).Concat(legacyBlackList));
 
             foreach (var name in JsonUtility.GetPropertyNames(typeof(FileMetadata)))
             {
