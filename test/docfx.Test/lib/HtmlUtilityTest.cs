4c3e6945720d25e23f484af1eb80357ff0058f31
Wei Hu <huwe@microsoft.com>
2019-04-16 10:00:15 +0800

    Fix raw

--- test/docfx.Test/lib/HtmlUtilityTest.cs
+++ test/docfx.Test/lib/HtmlUtilityTest.cs
@@ -73,8 +73,8 @@ namespace Microsoft.Docs.Build
         [InlineData("", "a", "a", "")]
         [InlineData("<a href='hello'>", "a", "a", "<a href='hello'>")]
         [InlineData("<xref href='hello'>", "a", "b", "<a href='a'>b</a>")]
-        [InlineData(@"<xref href='hello' data-raw-html='higher&amp;' data-raw-source='lower'>", "", "", @"@higher&")]
-        [InlineData(@"<xref href='hello' data-raw-source='lower&amp;'>", "", "", @"@lower&")]
+        [InlineData(@"<xref href='hello' data-raw-html='@higher&amp;' data-raw-source='@lower'>", "", "", @"@higher&")]
+        [InlineData(@"<xref href='hello' data-raw-source='@lower&amp;'>", "", "", @"@lower&")]
         [InlineData(@"<xref href='hello'>", "", "", @"@hello")]
         public void TransformXrefs(string input, string xref, string display, string output)
         {
