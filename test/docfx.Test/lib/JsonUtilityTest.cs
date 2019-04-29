25dbdc3141a429435f2f52e1951e7e77e9fbf35a
Wei Hu <huwe@microsoft.com>
2019-04-29 14:04:19 +0800

    cleanup

--- test/docfx.Test/lib/JsonUtilityTest.cs
+++ test/docfx.Test/lib/JsonUtilityTest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
