3565e636c3eefe7beb1c3321477a0d4072e31ca0
osmondjiang <xinjiang@microsoft.com>
2019-05-07 15:19:45 +0800

    change output model to template consuming raw model

--- src/docfx/template/TemplateEngine.cs
+++ src/docfx/template/TemplateEngine.cs
@@ -123,28 +123,11 @@ namespace Microsoft.Docs.Build
             var docset = file.Docset;
             var rawMetadata = JsonUtility.ToJObject(pageModel);
 
-            rawMetadata["depot_name"] = $"{docset.Config.Product}.{docset.Config.Name}";
-
             rawMetadata["search.ms_docsetname"] = docset.Config.Name;
             rawMetadata["search.ms_product"] = docset.Config.Product;
             rawMetadata["search.ms_sitename"] = "Docs";
 
             rawMetadata["__global"] = Global;
-            rawMetadata.Remove("content");
-
-            var path = PathUtility.NormalizeFile(Path.GetRelativePath(file.Docset.SiteBasePath, file.SitePath));
-
-            rawMetadata["_path"] = path;
-            rawMetadata["wordCount"] = pageModel.WordCount;
-
-            rawMetadata["_op_canonicalUrlPrefix"] = $"{docset.HostName}/{docset.Locale}/{docset.SiteBasePath}/";
-
-            if (docset.Config.Output.Pdf)
-            {
-                rawMetadata["_op_pdfUrlPrefixTemplate"] = $"{docset.HostName}/pdfstore/{pageModel.Locale}/{docset.Config.Product}.{docset.Config.Name}/{{branchName}}";
-            }
-
-            rawMetadata.Remove("schema_type");
 
             return rawMetadata;
         }
