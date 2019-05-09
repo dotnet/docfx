c7fbc1eb24b1ebd175d80a372b0814b7d09c8b8f
Peican Gao <v-pegao@microsoft.com>
2019-05-09 05:52:07 +0000

    schema-not-found

--- src/docfx/Errors.cs
+++ src/docfx/Errors.cs
@@ -383,7 +383,7 @@ namespace Microsoft.Docs.Build
         ///   - defined a an unknown schema type(other than conceptual, contextObject, landingData)
         /// </summary>
         public static Error SchemaNotFound(SourceInfo<string> source)
-            => new Error(ErrorLevel.Error, "schema-not-found", !string.IsNullOrEmpty(source) ? $"Unknown schema '{source}', object model is missing." : $"Unknown schema '{source}'", source);
+            => new Error(ErrorLevel.Error, "schema-not-found", !string.IsNullOrEmpty(source) ? $"Schema '{source}' not found." : $"Unknown schema '{source}'", source);
 
         /// <summary>
         /// Build errors is larger than <see cref="OutputConfig.MaxErrors"/>.
