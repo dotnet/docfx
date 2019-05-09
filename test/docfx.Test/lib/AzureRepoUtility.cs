18cc40943912458dea4f00bf3f3aa309fd37c427
osmondjiang <xinjiang@microsoft.com>
2019-05-09 13:27:08 +0800

    support new azure devops url

--- test/docfx.Test/lib/AzureRepoUtility.cs
+++ test/docfx.Test/lib/AzureRepoUtility.cs
@@ -14,6 +14,12 @@ namespace Microsoft.Docs.Build
         [InlineData("http://ceapex.visualstudio.com/project/repo", false, null, null)]
         [InlineData("http://ceapex.visualstudio.com/project/_git/repo/unknown", false, null, null)]
         [InlineData("http://ceapex.visualstudio.com/project/_git/repo#", false, null, null)]
+        [InlineData("https://dev.azure.com/ceapex", false, null, null)]
+        [InlineData("http://dev.azure.com/ceapex/project", false, null, null)]
+        [InlineData("http://dev.azure.com/ceapex/project/_git", false, null, null)]
+        [InlineData("http://dev.azure.com/ceapex/project/repo", false, null, null)]
+        [InlineData("http://dev.azure.com/ceapex/project/_git/repo/unknown", false, null, null)]
+        [InlineData("http://dev.azure.com/ceapex/project/_git/repo#", false, null, null)]
         [InlineData("http://ceapex.visualstudio.com/project/_git/repo/", true, "project", "repo")]
         [InlineData("https://ceapex.visualstudio.com/project/_git/repo", true, "project", "repo")]
         [InlineData("http://ceapex.visualstudio.com/project/_git/repo", true, "project", "repo")]
@@ -23,6 +29,10 @@ namespace Microsoft.Docs.Build
         [InlineData("http://ceapex.visualstudio.com/DefaultCollection/project/_git/repo/", true, "project", "repo")]
         [InlineData("https://ceapex.visualstudio.com/DefaultCollection/project/_git/repo#branch", true, "project", "repo")]
         [InlineData("http://ceapex.visualstudio.com/DefaultCollection/project/_git/repo#branch", true, "project", "repo")]
+        [InlineData("https://dev.azure.com/ceapex/project/_git/repo", true, "project", "repo")]
+        [InlineData("http://dev.azure.com/ceapex/project/_git/repo/", true, "project", "repo")]
+        [InlineData("https://dev.azure.com/ceapex/project/_git/repo#branch", true, "project", "repo")]
+        [InlineData("http://dev.azure.com/ceapex/project/_git/repo#branch", true, "project", "repo")]
         public static void ParseAzureRepoRemote(string remote, bool parsed, string expectedProject, string expectedName)
         {
             if (AzureRepoUtility.TryParse(remote, out var owner, out var name))
