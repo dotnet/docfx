4c02164fdcb48cc2fdea77d071d56f12b39a8b1c
JiayinPei <jipe@microsoft.com>
2019-05-21 11:35:54 +0800

    address comments

--- src/docfx/publish/PublishModel.cs
+++ src/docfx/publish/PublishModel.cs
@@ -1,10 +1,13 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Collections.Generic;
 
 namespace Microsoft.Docs.Build
 {
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     internal class PublishModel
     {
         public PublishItem[] Files { get; set; }
