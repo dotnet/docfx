69bb6f6f4535d6f137b4b25a49f9ff81ad78d3d2
Yufei Huang <yufeih@live.com>
2019-05-27 16:41:48 +0800

    revert

--- src/docfx/Errors.cs
+++ src/docfx/Errors.cs
@@ -294,7 +294,7 @@ namespace Microsoft.Docs.Build
         /// Used docfx output model property which are not defined in input model.
         /// </summary>
         public static Error AttributeReserved(SourceInfo source, string name)
-            => new Error(ErrorLevel.Warning, "attribute-reserved", $"Attribute '{name}' is reserved for use by Docs. Remove it from your file metadata.", source);
+            => new Error(ErrorLevel.Warning, "attribute-reserved", $"Attribute {name} is reserved for use by Docs. Remove it from your file metadata.", source);
 
         /// <summary>
         /// Metadata value must be scalar or arrays of scalars.
