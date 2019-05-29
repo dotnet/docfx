bd00f9c69dbaa6160fa875dde332e0ade2e8d9cc
LowEntropyBody <yazhao@microsoft.com>
2019-05-29 14:21:26 +0800

    Add metadata validation rule: dependencies (#4654)

--- test/docfx.Test/lib/JsonSchemaTest.cs
+++ test/docfx.Test/lib/JsonSchemaTest.cs
@@ -20,7 +20,6 @@ namespace Microsoft.Docs.Build
             "const",
             "contains",
             "definitions",
-            "dependencies",
             "exclusiveMaximum",
             "exclusiveMinimum",
             "if-then-else",
@@ -45,6 +44,11 @@ namespace Microsoft.Docs.Build
             "items and subitems",
             "with boolean schema",
             "patternProperties",
+
+            //dependencies
+            "dependencies with boolean subschemas",
+            "multiple dependencies subschema",
+            "dependencies with escaped characters",
 
              // ref
             "relative pointer ref to object",
@@ -188,6 +192,16 @@ namespace Microsoft.Docs.Build
         [InlineData("{'required': ['a']}", "{'a': 1}", "")]
         [InlineData("{'required': ['a']}", "{'b': 1}",
             "['warning','field-required','Missing required field 'a'','file',1,1]")]
+
+        // dependencies validation
+        [InlineData("{'dependencies': {}}", "{}", "")]
+        [InlineData("{'dependencies': {'key1': ['key2']}}", "{'key1' : 1, 'key2' : 2}", "")]
+        [InlineData("{'dependencies': {'key1': ['key2']}}", "{}", "")]
+        [InlineData("{'dependencies': {'key1': ['key2']}}", "{'key1' : 1}",
+            "['warning','lack-dependency','Missing field: 'key2'. If you specify 'key1', you must also specify 'key2'','file',1,1]")]
+        [InlineData("{'properties': {'keys': {'dependencies': {'key1': ['key2']}}}}", "{'keys' : {'key1' : 1, 'key2': 2}}", "")]
+        [InlineData("{'properties': {'keys': {'dependencies': {'key1': ['key2']}}}}", "{'keys' : {'key1' : 1}}",
+            "['warning','lack-dependency','Missing field: 'key2'. If you specify 'key1', you must also specify 'key2'','file',1,11]")]
         public void TestJsonSchemaValidation(string schema, string json, string expectedErrors)
         {
             var jsonSchema = JsonUtility.Deserialize<JsonSchema>(schema.Replace('\'', '"'), null);
