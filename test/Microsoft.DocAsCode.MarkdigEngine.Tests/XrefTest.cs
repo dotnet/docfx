b62c48aca84510d11cab06a81091a45f4295c7ad
Yufei Huang <yufeih@live.com>
2019-06-12 12:39:33 +0800

    Add more test cases

--- test/Microsoft.DocAsCode.MarkdigEngine.Tests/XrefTest.cs
+++ test/Microsoft.DocAsCode.MarkdigEngine.Tests/XrefTest.cs
@@ -17,7 +17,12 @@ namespace Microsoft.DocAsCode.MarkdigEngine.Tests
 @Microsoft.Build.Tasks
 ""@""a[test](link)
 @hehe
+
 ![img @hehe a](src ""title @hehe b"")
+![img @hehe a
+![img @hehe a](src ""title @hehe b""
+![this **img @hehe a** haha](src ""title @hehe b"")
+
 @""Microsoft.Build.Tasks?text=Tasks""
 [link_text](xref:Microsoft.Build.Tasks)
 <xref:Microsoft.Build.Tasks#Anchor_1>
@@ -34,9 +39,12 @@ namespace Microsoft.DocAsCode.MarkdigEngine.Tests
 \<xref href=""Microsoft.Build.Tasks"" data-throw-if-not-resolved=""True"" data-raw-source=""&lt;xref:Microsoft.Build.Tasks&gt;""></xref>
 <xref href=""Microsoft.Build.Tasks"" data-throw-if-not-resolved=""False"" data-raw-source=""@Microsoft.Build.Tasks""></xref>
 &quot;@&quot;a<a href=""link"">test</a>
-<xref href=""hehe"" data-throw-if-not-resolved=""False"" data-raw-source=""@hehe""></xref>
-<img src=""src"" alt=""img @hehe a"" title=""title @hehe b"" />
-<xref href=""Microsoft.Build.Tasks?text=Tasks"" data-throw-if-not-resolved=""False"" data-raw-source=""@&quot;Microsoft.Build.Tasks?text=Tasks&quot;""></xref>
+<xref href=""hehe"" data-throw-if-not-resolved=""False"" data-raw-source=""@hehe""></xref></p>
+<p><img src=""src"" alt=""img @hehe a"" title=""title @hehe b"" />
+![img @hehe a
+![img @hehe a](src &quot;title @hehe b&quot;
+<img src=""src"" alt=""this img @hehe a haha"" title=""title @hehe b"" /></p>
+<p><xref href=""Microsoft.Build.Tasks?text=Tasks"" data-throw-if-not-resolved=""False"" data-raw-source=""@&quot;Microsoft.Build.Tasks?text=Tasks&quot;""></xref>
 <a href=""xref:Microsoft.Build.Tasks"">link_text</a>
 <xref href=""Microsoft.Build.Tasks#Anchor_1"" data-throw-if-not-resolved=""True"" data-raw-source=""&lt;xref:Microsoft.Build.Tasks#Anchor_1&gt;""></xref>
 <xref href=""Microsoft.Build.Tasks?alt=ImmutableArray""/>
