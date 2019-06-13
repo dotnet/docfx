00e9cd5e28aa0ec23a386e7e2078630f855d2b79
osmondjiang <xinjiang@microsoft.com>
2019-06-13 12:08:44 +0800

    homepage only shows when topic href is set

--- src/docfx/build/toc/parser/TableOfContentsParser.cs
+++ src/docfx/build/toc/parser/TableOfContentsParser.cs
@@ -137,7 +137,7 @@ namespace Microsoft.Docs.Build
                 // set resolved href back
                 tocModelItem.Href = resolvedTocHref ?? resolvedTopicHref ?? resolvedTopicItemFromTocHref?.Href;
                 tocModelItem.TocHref = resolvedTocHref;
-                tocModelItem.Homepage = resolvedTopicHref;
+                tocModelItem.Homepage = tocModelItem.TopicHref != null ? resolvedTopicHref : null;
                 tocModelItem.Name = tocModelItem.Name ?? resolvedTopicName;
                 tocModelItem.Items = subChildren?.Items ?? tocModelItem.Items;
 
