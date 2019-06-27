add4d971438ad964a3e154dddae88a383e01f628
Yufei Huang <yufeih@live.com>
2019-06-27 13:45:31 +0800

    remove null check

--- src/docfx/lib/log/ErrorLog.cs
+++ src/docfx/lib/log/ErrorLog.cs
@@ -84,9 +84,6 @@ namespace Microsoft.Docs.Build
 
         public bool Write(string file, Error error, bool isException = false)
         {
-            if (error is null)
-                return false;
-
             return Write(
                 file == error.File || !string.IsNullOrEmpty(error.File)
                     ? error
