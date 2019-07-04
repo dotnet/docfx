b340bd13aba036b1a787086089aa50c2e55140e2
Osmond Jiang <xinjiang@microsoft.com>
2019-07-04 15:05:30 +0800

    remove some unused code

--- src/docfx/lib/schema/JsonSchemaTransformer.cs
+++ src/docfx/lib/schema/JsonSchemaTransformer.cs
@@ -6,8 +6,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using DotLiquid.Tags;
-using Microsoft.Graph;
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
