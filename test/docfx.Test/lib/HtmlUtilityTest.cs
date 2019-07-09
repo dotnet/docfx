3730649fb74cc44edac5569d9eca8a454b00c30f
Renze Yu <renzeyu@microsoft.com>
2019-07-09 12:05:36 +0800

    only HTML encode required chars

--- test/docfx.Test/lib/HtmlUtilityTest.cs
+++ test/docfx.Test/lib/HtmlUtilityTest.cs
@@ -100,5 +100,14 @@ namespace Microsoft.Docs.Build
         {
             Assert.Equal(expected, string.Join(", ", HtmlUtility.GetBookmarks(HtmlUtility.LoadHtml(html))));
         }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("rétablir", "rétablir")]
+        [InlineData("&<>\"'/", "&amp;&lt;&gt;&quot;&#x27;&#x2F;")]
+        public static void Encode(string input, string expected)
+        {
+            Assert.Equal(expected, HtmlUtility.Encode(input));
+        }
     }
 }
