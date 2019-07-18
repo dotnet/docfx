e1dad0e6f078b362760527b0fe89de658ce68edb
JiayinPei <jipe@microsoft.com>
2019-07-18 09:59:30 +0800

    address comments

--- src/docfx/publish/PublishModel.cs
+++ src/docfx/publish/PublishModel.cs
@@ -13,6 +13,6 @@ namespace Microsoft.Docs.Build
     {
         public PublishItem[] Files { get; set; }
 
-        public SortedDictionary<string, List<string>> MonikerGroups { get; set; } = new SortedDictionary<string, List<string>>();
+        public IReadOnlyDictionary<string, List<string>> MonikerGroups { get; set; } = new Dictionary<string, List<string>>();
     }
 }
