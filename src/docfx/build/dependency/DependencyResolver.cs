e66a4d7b8d9f9881120de13cb7dd732d5bcbeaec
live1206 <huwe@microsoft.com>
2019-07-19 14:56:00 +0800

    improve

--- src/docfx/build/dependency/DependencyResolver.cs
+++ src/docfx/build/dependency/DependencyResolver.cs
@@ -179,14 +179,11 @@ namespace Microsoft.Docs.Build
                         return (error, href, fragment, linkType, null, false);
                     }
 
+                    // in v2, the non-reoslved link will be relocated to a relative path, v3 tries to avoid the behavior change here
                     // the relocation of relative path can be removed when migration from v2 to v3 is done
                     var relativePath = PathUtility.GetRelativePathToFile(declaringFile.FilePath, pathToDocset);
-                    return (error,
-                        Document.PathToRelativeUrl(relativePath, ContentType.Page, null, declaringFile.Docset.Config.Output.Json, true),
-                        fragment,
-                        linkType,
-                        null,
-                        false);
+                    var relativeUrl = Document.PathToRelativeUrl(relativePath, ContentType.Page, null, declaringFile.Docset.Config.Output.Json, true);
+                    return (error, relativeUrl, fragment, linkType, null, false);
                 }
 
                 // set file to resource got from histroy, reset the error
