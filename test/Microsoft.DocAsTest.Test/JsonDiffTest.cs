0d3f3e8702fa0173dd29f96d648d729553f42af7
Yufei Huang <yufeih@live.com>
2019-08-16 15:03:51 +0800

    refactor test

--- test/Microsoft.DocAsTest.Test/JsonDiffTest.cs
+++ test/Microsoft.DocAsTest.Test/JsonDiffTest.cs
@@ -8,18 +8,13 @@ namespace Microsoft.DocAsTest
     public class JsonDiffTest
     {
         [Theory]
-
-        // none
         [InlineData("{}", "{}", "")]
-
         [InlineData("{'a': null}", "{'a': null}", @"")]
         [InlineData("{'a': null}", "{}", @"
 -  'a': null")]
-
         [InlineData("{'a': undefined}", "{}", "")]
         [InlineData("{'a': undefined}", "{'a': null}", @"
 +  'a': null")]
-
         [InlineData("1", "2", @"
 -1
 +2")]
@@ -30,27 +25,48 @@ namespace Microsoft.DocAsTest
 -  1
 +  1,
 +  2")]
-
         [InlineData("{'a': 1, 'b': 2}", "{'b': 2, 'a': 1}", "")]
         [InlineData("{'a': 1, 'b': { 'c': 2 }}", "{'a': 1, 'b': { 'c': 2 }}", "")]
         [InlineData("{'a': 1, 'b': { 'c': 2 }}", "{'a': 1, 'b': { 'c': 3 }}", @"
 -    'c': 2
 +    'c': 3")]
-        [InlineData("{'a': 1}", "{'a': 1, 'b': 2}", "")]
+        [InlineData("{'a': 1}", "{'a': 1, 'b': 2}", @"
+-  'a': 1
++  'a': 1,
++  'b': 2")]
+        public static void Basic(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder());
+        }
 
-        // negate
+        [Theory]
+        [InlineData("{'a': 1}", "{'a': 1, 'b': 2}", "")]
+        public static void AdditionalProperties(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseAdditionalProperties());
+        }
+
+        [Theory]
         [InlineData("'!a'", "'b'", "")]
         [InlineData("'!a'", "'a'", @"
 -'!a'
 +'a'")]
+        public static void Negate(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseNegate());
+        }
 
-        // ignore null
+        [Theory]
         [InlineData("{'ignore-null': null}", "{'ignore-null': 'a'}", "")]
         [InlineData("{'ignore-null': null}", "{'ignore-null': true}", "")]
         [InlineData("{'ignore-null': null}", "{}", @"
 -  'ignore-null': null")]
+        public static void IgnoreNull(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseIgnoreNull());
+        }
 
-        // wildcard
+        [Theory]
         [InlineData("'a*b'", "'a123b'", "")]
         [InlineData("'a*b'", "'ab'", "")]
         [InlineData("'a*b'", "'abc'", @"
@@ -64,16 +80,24 @@ namespace Microsoft.DocAsTest
         [InlineData("[1,'*',2]", "[1,'2',3]", @"
 -  2
 +  3")]
+        public static void Wildcard(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseWildcard());
+        }
 
-        // regex
+        [Theory]
         [InlineData("'/.*/'", "''", "")]
         [InlineData("'/.*/'", "'a'", "")]
         [InlineData("'/^.?$/'", "'a'", "")]
         [InlineData("'/^.?$/'", "'aa'", @"
 -'/^.?$/'
 +'aa'")]
+        public static void Regex(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseRegex());
+        }
 
-        // jsonfile
+        [Theory]
         [InlineData("{'a.json': 'true'}", "{'a.json': 'true'}", "")]
 
         [InlineData(@"{'a.json': '{\'a\':undefined}'}", @"{'a.json': '{}'}", "")]
@@ -81,33 +105,37 @@ namespace Microsoft.DocAsTest
 +  \'a\': null")]
 
         [InlineData(@"{'a/b.json': '{\'a\':1}'}", @"{'a/b.json': '{\'a\':1}'}", "")]
-        [InlineData(@"{'a/b.json': '{\'a\':1}'}", @"{'a/b.json': '{\'a\':1, \'b\':2}'}", "")]
         [InlineData(@"{'a/b.json': '{\'a\':1}'}", @"{'a/b.json': '{\'a\':2}'}", @"
 -  \'a\': 1
 +  \'a\': 2")]
+        public static void Json(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseJson());
+        }
 
-        [InlineData(@"{'a.json': '{\'a\':\'*\'}'}", @"{'a.json': '{\'a\':\'anything\'}'}", "")]
-
-        // htmlfile
+        [Theory]
         [InlineData("{'a.html': 'a'}", "{'a.html': 'b'}", @"
 -  'a.html': 'a'
 +  'a.html': 'b'")]
         [InlineData("{'a.html': '<p> a </p>'}", "{'a.html': '<p>a</p>'}", "")]
         [InlineData("{'a.html': '<!-- a -->'}", "{'a.html': '<!-- b -->'}", "")]
         [InlineData(@"{'a.html': '<div a=\'1\' b= \'2\'></div>'}", @"{'a.html': '<div b=\'2\' a =\'1\'></div>'}", "")]
-        public static void Run(string expected, string actual, string diff)
+        public static void Html(string expected, string actual, string diff)
         {
-            var jsonDiff = new JsonDiffBuilder()
-                .UseAdditionalProperties()
-                .UseIgnoreNull((e, a, name) => name == "ignore-null")
-                .UseNegate()
-                .UseRegex()
-                .UseWildcard()
-                .UseJson()
-                .UseHtml()
-                .Build();
+            Run(expected, actual, diff, new JsonDiffBuilder().UseHtml());
+        }
 
-            var actualDiff = jsonDiff.Diff(
+        [Theory]
+        [InlineData(@"{'a/b.json': '{\'a\':1}'}", @"{'a/b.json': '{\'a\':1, \'b\':2}'}", "")]
+        [InlineData(@"{'a.json': '{\'a\':\'*\'}'}", @"{'a.json': '{\'a\':\'anything\'}'}", "")]
+        public static void NestedRules(string expected, string actual, string diff)
+        {
+            Run(expected, actual, diff, new JsonDiffBuilder().UseAdditionalProperties().UseWildcard().UseJson());
+        }
+
+        private static void Run(string expected, string actual, string diff, JsonDiffBuilder builder)
+        {
+            var actualDiff = builder.Build().Diff(
                     JToken.Parse(expected.Replace('\'', '\"')),
                     JToken.Parse(actual.Replace('\'', '\"')));
 
@@ -116,7 +144,7 @@ namespace Microsoft.DocAsTest
                 .Split('\n', StringSplitOptions.RemoveEmptyEntries)
                 .Where(line => line.StartsWith("+") || line.StartsWith("-"));
 
-            jsonDiff.Verify(
+            new JsonDiff().Verify(
                 diff.Trim().Replace("\r", "").Replace('\'', '\"'),
                 string.Join('\n', actualChanges));
         }
