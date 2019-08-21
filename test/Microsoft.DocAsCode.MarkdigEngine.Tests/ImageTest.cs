d19ba9c48ee5c55e2723836ad70b002e3310bacc
bharney <bharney0@gmail.com>
2019-08-21 14:05:13 -0700

    fixed unit test for ImageTestBlockGeneral.

--- test/Microsoft.DocAsCode.MarkdigEngine.Tests/ImageTest.cs
+++ test/Microsoft.DocAsCode.MarkdigEngine.Tests/ImageTest.cs
@@ -24,7 +24,7 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 :::image-end:::
 ";
 
-            var expected = @"<img src=""example.jpg"" alt=""example"" aria-describedby=""88850"">
+            var expected = @"<img alt=""example"" aria-describedby=""88850"" src=""example.jpg"">
 <div id=""88850"" class=""visually-hidden"">
 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 </div>
