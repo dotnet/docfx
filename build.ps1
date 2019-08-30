1ef689eb320a07c3b08a7d27ba5c9ba2ce20e765
Yufei Huang <yufeih@live.com>
2019-08-30 17:14:16 +0800

    Add DocAsTest nuget package test

--- build.ps1
+++ build.ps1
@@ -32,14 +32,32 @@ function publish() {
     Remove-Item ./drop -Force -Recurse -ErrorAction Ignore
     exec "dotnet pack src\docfx -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version"
     exec "dotnet pack src\Microsoft.DocAsTest -c Release -o $PSScriptRoot\drop /p:Version=$version /p:InformationalVersion=$version"
+}
+
+function testNuGet() {
+    if ($noTest) {
+        return
+    }
+
     exec "dotnet tool install docfx --version $version --add-source drop --tool-path drop"
     exec "drop\docfx --version"
+
+    Remove-Item $env:USERPROFILE/.nuget/packages/microsoft.docastest -Force -Recurse -ErrorAction Ignore
+
+    Remove-Item $PSScriptRoot\test\Microsoft.DocAsTest.NuGetTest\bin\Debug\netcoreapp2.2\foo -Force -Recurse -ErrorAction Ignore
+    exec "dotnet restore test/Microsoft.DocAsTest.NuGetTest --no-cache --force --source $PSScriptRoot\drop"
+    exec "dotnet test test/Microsoft.DocAsTest.NuGetTest --no-restore"
+
+    if (-not (Test-Path -Path "$PSScriptRoot\test\Microsoft.DocAsTest.NuGetTest\bin\Debug\netcoreapp2.2\foo")) {
+        throw 'Microsoft.DocAsTest.NuGetTest failed'
+    }
 }
 
 try {
     pushd $PSScriptRoot
     test
     publish
+    testNuGet
 } finally {
     popd
 }
---
+++ test/Microsoft.DocAsTest.NuGetTest/NuGetTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Microsoft.DocAsTest.NuGetTest
+{
+    public class NuGetTest
+    {
+        [MarkdownTest("data/**/*.md")]
+        public void Foo(string filename)
+        {
+            File.WriteAllText(filename, "");
+        }
+    }
+}
