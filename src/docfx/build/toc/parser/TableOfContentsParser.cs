bc0273b62b5bbcee2003546afb0275899160f516
mingwli <mingwei.liu@microsoft.com>
2019-09-24 15:43:14 +0800

    nearest toc calculation
    Parent toc including child toc by relative path won't participate in child toc calculation

--- src/docfx/build/toc/parser/TableOfContentsParser.cs
+++ src/docfx/build/toc/parser/TableOfContentsParser.cs
@@ -291,8 +291,9 @@ namespace Microsoft.Docs.Build
                 context, filePath, referencedTocs, tocHrefType, new SourceInfo<string>(hrefPath, tocHref), errors);
             if (referencedTocContent != null)
             {
-                var (subErrors, nestedToc) = LoadInternal(
-                    context, referenceTocFilePath, rootPath, referencedFiles, referencedTocs, referencedTocContent);
+                var (subErrors, nestedToc) = tocHrefType == TocHrefType.RelativeFolder
+                    ? LoadInternal(context, referenceTocFilePath, rootPath, new List<Document>(), referencedTocs, referencedTocContent)
+                    : LoadInternal(context, referenceTocFilePath, rootPath, referencedFiles, referencedTocs, referencedTocContent);
                 errors.AddRange(subErrors);
 
                 if (tocHrefType == TocHrefType.RelativeFolder)
