f5939227b19b93d229cf9a9e2fdb4763a76ac80e
Yufei Huang <yufeih@live.com>
2019-09-26 16:53:34 +0800

    DependencyResolver --> LinkResolver (#5150)

--- src/docfx/lib/markdown/MarkdownEngine.cs
+++ src/docfx/lib/markdown/MarkdownEngine.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Docs.Build
         // URLs starting with this magic string are transformed into relative URL after markup.
         private const string RelativeUrlMarker = "//////";
 
-        private readonly DependencyResolver _dependencyResolver;
+        private readonly LinkResolver _linkResolver;
         private readonly XrefResolver _xrefResolver;
         private readonly MonikerProvider _monikerProvider;
         private readonly TemplateEngine _templateEngine;
@@ -32,12 +32,12 @@ namespace Microsoft.Docs.Build
         public MarkdownEngine(
             Config config,
             RestoreFileMap restoreFileMap,
-            DependencyResolver dependencyResolver,
+            LinkResolver linkResolver,
             XrefResolver xrefResolver,
             MonikerProvider monikerProvider,
             TemplateEngine templateEngine)
         {
-            _dependencyResolver = dependencyResolver;
+            _linkResolver = linkResolver;
             _xrefResolver = xrefResolver;
             _monikerProvider = monikerProvider;
             _templateEngine = templateEngine;
@@ -182,7 +182,7 @@ namespace Microsoft.Docs.Build
         private (string content, object file) ReadFile(string path, object relativeTo, MarkdownObject origin)
         {
             var status = t_status.Value.Peek();
-            var (error, content, file) = _dependencyResolver.ResolveContent(new SourceInfo<string>(path, origin.ToSourceInfo()), (Document)relativeTo);
+            var (error, content, file) = _linkResolver.ResolveContent(new SourceInfo<string>(path, origin.ToSourceInfo()), (Document)relativeTo);
             status.Errors.AddIfNotNull(error);
             return (content, file);
         }
@@ -190,7 +190,7 @@ namespace Microsoft.Docs.Build
         private string GetLink(SourceInfo<string> href)
         {
             var status = t_status.Value.Peek();
-            var (error, link, file) = _dependencyResolver.ResolveAbsoluteLink(
+            var (error, link, file) = _linkResolver.ResolveAbsoluteLink(
                 href, (Document)InclusionContext.File);
 
             if (file != null)
