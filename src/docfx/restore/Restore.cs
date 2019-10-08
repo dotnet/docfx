47bbaf03794b794b6b6484f63662913a5829542c
Wei Hu <huwe@microsoft.com>
2019-10-08 17:09:32 +0800

    log non-docfx exception to console

--- src/docfx/restore/Restore.cs
+++ src/docfx/restore/Restore.cs
@@ -87,10 +87,13 @@ namespace Microsoft.Docs.Build
 
                     DependencyLockProvider.SaveGitLock(docsetPath, locale, extendedConfig.DependencyLock, restoredGitLock);
                 }
-                catch (Exception ex) when (DocfxException.IsDocfxException(ex, out var dex))
+                catch (Exception ex)
                 {
-                    Log.Write(dex);
-                    errorLog.Write(dex.Error, isException: true);
+                    Log.Write(ex);
+                    if (DocfxException.IsDocfxException(ex, out var dex))
+                    {
+                        errorLog.Write(dex.Error, isException: true);
+                    }
                 }
                 finally
                 {
