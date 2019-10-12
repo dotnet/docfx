618942df0d2424fdca9587692fbc7bd09c1a1e15
juchen <juchen@microsoft.com>
2019-10-12 10:02:55 +0800

    fix the test case.

--- src/docfx/lib/path/PathUtility.cs
+++ src/docfx/lib/path/PathUtility.cs
@@ -269,7 +269,7 @@ namespace Microsoft.Docs.Build
         {
             return siteBasePath == "." ? string.Empty : siteBasePath;
         }
-        
+
         // For azure blob url, url without sas token should identify if the content has changed
         // https://docs.microsoft.com/en-us/azure/storage/common/storage-dotnet-shared-access-signature-part-1#how-a-shared-access-signature-works
         private static string RemoveQueryForBlobUrl(string url)
