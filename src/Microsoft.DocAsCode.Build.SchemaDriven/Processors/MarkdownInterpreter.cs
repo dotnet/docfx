6dcdc3b8af5c7b6f291101f797668f45664c6fae
Jingkan He <hejingkan2005@hotmail.com>
2019-11-13 11:32:36 +0800

    Switch off schema driven document validation (#5286)
    - turn off schema driven document validation

--- src/Microsoft.DocAsCode.Build.SchemaDriven/Processors/MarkdownInterpreter.cs
+++ src/Microsoft.DocAsCode.Build.SchemaDriven/Processors/MarkdownInterpreter.cs
@@ -35,7 +35,7 @@ namespace Microsoft.DocAsCode.Build.SchemaDriven.Processors
         {
             var host = context.Host;
 
-            var mr = host.Markup(content, context.GetOriginalContentFile(path), false, true);
+            var mr = host.Markup(content, context.GetOriginalContentFile(path));
             (context.FileLinkSources).Merge(mr.FileLinkSources);
             (context.UidLinkSources).Merge(mr.UidLinkSources);
             (context.Dependency).UnionWith(mr.Dependency);
