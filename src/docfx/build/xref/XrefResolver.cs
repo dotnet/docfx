f817e45edbf919908c6fa6d2be4b06cb6348c134
Wei Hu <huwe@microsoft.com>
2019-11-14 13:53:43 +0800

    Exclude non-build toc files from .links.json

--- src/docfx/build/xref/XrefResolver.cs
+++ src/docfx/build/xref/XrefResolver.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
-using Microsoft.DocAsCode.MarkdigEngine.Extensions;
 
 namespace Microsoft.Docs.Build
 {
