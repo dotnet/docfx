57741867fd47c0bdcaac49f1f0646cf1799c8272
Yufei Huang <yufeih@live.com>
2019-12-27 12:35:33 +0800

    Add comments

--- src/docfx/restore/PackageFetchOptions.cs
+++ src/docfx/restore/PackageFetchOptions.cs
@@ -8,8 +8,21 @@ namespace Microsoft.Docs.Build
     [Flags]
     internal enum PackageFetchOptions
     {
+        /// <summary>
+        /// Default fetch option.
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Include full commit history if the package is git.
+        /// Git contributor calculate depend on git commit history.
+        /// </summary>
         FullHistory = 0b0001,
+
+        /// <summary>
+        /// Don't fail if the package does not exist.
+        /// Fallback branch for localized builds are typically optional.
+        /// </summary>
         Optional = 0b0010,
     }
 }
