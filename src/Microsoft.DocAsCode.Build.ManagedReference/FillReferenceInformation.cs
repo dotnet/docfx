aca594f15ad08a087105b06aa3da3cc2f137a386
SimonCropp <simon.cropp@gmail.com>
2020-01-14 19:39:52 +1100

    Update FillReferenceInformation.cs

--- src/Microsoft.DocAsCode.Build.ManagedReference/FillReferenceInformation.cs
+++ src/Microsoft.DocAsCode.Build.ManagedReference/FillReferenceInformation.cs
@@ -219,6 +219,7 @@ namespace Microsoft.DocAsCode.Build.ManagedReference
             public List<string> Platform { get; set; }
             public string File { get; set; }
             public Dictionary<string, object> Metadata { get; set; }
+            public bool IsExplicitInterfaceImplementation { get; set; }
         }
     }
 }
