10c6847fe2e8bd73be1ba9fb6c37a8495aabb016
Osmond Jiang <xinjiang@microsoft.com>
2020-03-27 09:39:54 +0800

    remove incorrect h1 line number

--- src/docfx/validation/ContentValidator.cs
+++ src/docfx/validation/ContentValidator.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Docs.Build
                         Content = title,
 
                         // todo: get title precise line info
-                        SourceInfo = new SourceInfo(file.FilePath, 1, 1),
+                        SourceInfo = new SourceInfo(file.FilePath, 0, 0),
                     },
                 };
 
