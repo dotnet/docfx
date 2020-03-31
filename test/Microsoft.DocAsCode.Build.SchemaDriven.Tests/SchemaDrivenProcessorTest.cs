5d35904032c5f6015a7d1780e678b7ab3c89fb0e
Renze Yu <renzeyu@microsoft.com>
2020-03-31 17:22:37 +0800

    log error for InvalidSchemaException

--- test/Microsoft.DocAsCode.Build.SchemaDriven.Tests/SchemaDrivenProcessorTest.cs
+++ test/Microsoft.DocAsCode.Build.SchemaDriven.Tests/SchemaDrivenProcessorTest.cs
@@ -515,9 +515,8 @@ metadata: Web Apps Documentation
             FileCollection files = new FileCollection(_defaultFiles);
             files.Add(DocumentType.Article, new[] { inputFile }, _inputFolder);
             BuildDocument(files);
-            var errors = listener.Items.Where(s => s.Code == "InvalidInputFile").ToList();
+            var errors = listener.Items.Where(s => s.Code == "ViolateSchema").ToList();
             Assert.Single(errors);
-            Assert.Equal($"Unable to load file '{inputFile}' via processor 'MetadataReferenceTest': Schema validation failed. Please validate the file and make sure it conforms to schema 'MetadataReferenceTest' (https://contoso.com/template/schemas/mta.reference.test.schema.json): \nInvalid type. Expected Object but got String. Path 'metadata'.", errors[0].Message);
         }
 
         [Fact]
