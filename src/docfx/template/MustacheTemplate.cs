e335f5053cd8fd6fb0afe11eb20899e7b3557bf9
mingwli <mingwei.liu@microsoft.com>
2020-04-13 17:40:48 +0800

    try using stubble compiler expression pipeline

--- src/docfx/template/MustacheTemplate.cs
+++ src/docfx/template/MustacheTemplate.cs
@@ -4,67 +4,82 @@
 using System;
 using System.Collections.Concurrent;
 using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
-using Stubble.Core.Builders;
+using Stubble.Compilation;
+using Stubble.Compilation.Builders;
+using Stubble.Compilation.Settings;
 using Stubble.Core.Interfaces;
-using Stubble.Core.Settings;
 
 namespace Microsoft.Docs.Build
 {
     internal class MustacheTemplate
     {
         private readonly string _templateDir;
-        private readonly IStubbleRenderer _renderer;
-        private readonly ConcurrentDictionary<string, Lazy<string?>> _templates = new ConcurrentDictionary<string, Lazy<string?>>();
+        private readonly MethodInfo? _method;
+        private readonly JObject? _global;
+        private readonly StubbleCompilationRenderer _renderer;
+        private readonly ConcurrentDictionary<string, Lazy<Func<JToken, string>?>> _templates = new ConcurrentDictionary<string, Lazy<Func<JToken, string>?>>();
 
         public MustacheTemplate(string templateDir, JObject? global = null)
         {
             _templateDir = templateDir;
-            _renderer = new StubbleBuilder().Configure(
-                settings => UseJson(settings, global).SetPartialTemplateLoader(new PartialLoader(templateDir))).Build();
+            _method = GetType().GetMethod(nameof(GetJObjectValue), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            _global = global;
+            _renderer = new StubbleCompilationBuilder().Configure(
+                settings => UseJson(settings).SetPartialTemplateLoader(new PartialLoader(templateDir))).Build();
         }
 
         public string Render(string templateFileName, JToken model)
         {
             var template = _templates.GetOrAdd(
                 templateFileName,
-                key => new Lazy<string?>(() =>
+                key => new Lazy<Func<JToken, string>?>(() =>
                 {
                     var fileName = Path.Combine(_templateDir, templateFileName);
                     return File.Exists(fileName)
-                    ? MustacheXrefTagParser.ProcessXrefTag(File.ReadAllText(fileName).Replace("\r", ""))
+                    ? _renderer.Compile<JToken>(MustacheXrefTagParser.ProcessXrefTag(File.ReadAllText(fileName).Replace("\r", "")))
                     : null;
                 })).Value;
 
-            return template == null ? JsonUtility.Serialize(model) : _renderer.Render(template, model);
+            return template == null ? JsonUtility.Serialize(model) : template(model);
         }
 
-        private static RendererSettingsBuilder UseJson(RendererSettingsBuilder settings, JObject? global)
+        private CompilerSettingsBuilder UseJson(CompilerSettingsBuilder settings)
         {
             // JObject implements IEnumerable, stubble treats IEnumerable as array,
             // need to put it to section blacklist and overwrite the truthy check method.
-            return settings.AddValueGetter(typeof(JObject), GetJObjectValue)
-                           .AddValueGetter(typeof(JValue), GetJValueValue)
+            return settings.AddValueGetter<JObject>(GetValue)
+                           .AddValueGetter<JValue>(GetValue)
                            .AddTruthyCheck<JObject>(value => value != null)
                            .AddTruthyCheck<JValue>(value => value.Type != JTokenType.Null)
                            .AddSectionBlacklistType(typeof(JObject))
                            .AddSectionBlacklistType(typeof(JValue));
+        }
 
-            object? GetJObjectValue(object value, string key, bool ignoreCase)
+        private Expression GetValue(Type type, Expression instance, string key, bool ignoreCase)
+        {
+            return Expression.Call(Expression.Constant(this), _method, instance, Expression.Constant(key), Expression.Constant(ignoreCase));
+        }
+
+        private object? GetJObjectValue(JToken token, string key, bool ignoreCase)
+        {
+            switch (token)
             {
-                if (key == "__global" && global != null)
-                {
-                    return global;
-                }
-                var token = (JObject)value;
-                var childToken = token.GetValue(key, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+                case JObject obj:
+                    if (key == "__global" && _global != null)
+                    {
+                        return _global;
+                    }
+                    var childToken = obj.GetValue(key, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
 
-                return childToken is JValue scalar ? scalar.Value ?? JValue.CreateNull() : childToken;
+                    return childToken is JValue scalar ? scalar.Value ?? JValue.CreateNull() : childToken;
+                case JValue val:
+                    return key.Trim() == "." ? ((JValue)val).Value : null;
             }
-
-            object? GetJValueValue(object value, string key, bool ignoreCase)
-                => key.Trim() == "." ? ((JValue)value).Value : null;
+            return default;
         }
 
         private class PartialLoader : IStubbleLoader
