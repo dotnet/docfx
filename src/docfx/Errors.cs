6db434d1b7f95a196a17c0510128154dd8739061
fkpwolf <fkpwolf@gmail.com>
2020-04-24 17:05:45 +0800

    code lint

--- src/docfx/Errors.cs
+++ src/docfx/Errors.cs
@@ -689,13 +689,13 @@ namespace Microsoft.Docs.Build
             /// <summary>
             /// Html Tag value must be in allowed list
             /// </summary>
-            public static Error UnSafeHTMLTag(Document file, string tag)
+            public static Error DisallowedHTMLTag(Document file, string tag)
                 => new Error(ErrorLevel.Warning, "disallowed-html", $"HTML tag '{tag}' isn't allowed. Disallowed HTML poses a security risk and must be replaced with approved Docs Markdown syntax.", file.FilePath);
 
             /// <summary>
             /// Html Attribute value must be in allowed list
             /// </summary>
-            public static Error UnSafeHTMLAttribute(Document file, string tag, string attribute)
+            public static Error DisallowedHTMLAttribute(Document file, string tag, string attribute)
                 => new Error(ErrorLevel.Warning, "disallowed-html", $"HTML attribute '{attribute}' on tag '{tag}' isn't allowed. Disallowed HTML poses a security risk and must be replaced with approved Docs Markdown syntax.", file.FilePath);
         }
     }
