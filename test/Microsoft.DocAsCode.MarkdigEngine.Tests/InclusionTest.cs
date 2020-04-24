3aef1eaf38a462a6d372b82c72249c4622d74704
adunndevster <adsbg6@gmail.com>
2020-04-24 12:17:40 -0600

    made minor fix to a test.

--- test/Microsoft.DocAsCode.MarkdigEngine.Tests/InclusionTest.cs
+++ test/Microsoft.DocAsCode.MarkdigEngine.Tests/InclusionTest.cs
@@ -858,7 +858,7 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
             TestUtility.WriteToFile("r/r.md", r);
             TestUtility.WriteToFile("r/b/token.md", token);
             var marked = TestUtility.MarkupWithoutSourceInfo(r, "r/r.md");
-            var expected = @"<img role=""presentation"" src=""~/r/b/example.svg"">
+            var expected = @"<img src=""~/r/b/example.svg"" role=""presentation"">
 ";
             Assert.Equal(expected.Replace("\r\n", "\n"), marked.Html);
         }
