7ddcd75e0204e6cfdb9701e1352970075f597204
Jiayin Pei <jipe@microsoft.com>
2020-05-19 11:00:01 +0800

    File reference in fallback origin should also be resolved to docs site URL in static output mode (#5922)
    * File reference in fallback origin should also be resolved to docs site URL in static output mode
    
    * Update src/docfx/build/link/LinkResolver.cs
    
    Co-authored-by: Yufei Huang <yufeih@live.com>
    
    Co-authored-by: Yufei Huang <yufeih@live.com>

--- src/docfx/build/link/LinkResolver.cs
+++ src/docfx/build/link/LinkResolver.cs
@@ -145,6 +145,13 @@ namespace Microsoft.Docs.Build
                 return (Errors.Link.LinkOutOfScope(href, file), href, fragment, linkType, null, false);
             }
 
+            // For static hosting, reference file in fallback repo should be resolved to docs site URL
+            if (file.FilePath.Origin == FileOrigin.Fallback && file.ContentType == ContentType.Page && _config.OutputUrlType != OutputUrlType.Docs)
+            {
+                var siteUrl = _documentProvider.GetDocsSiteUrl(file.FilePath);
+                return (error, UrlUtility.MergeUrl($"https://{_config.HostName}{siteUrl}", query, fragment), fragment, linkType, file, false);
+            }
+
             return (error, UrlUtility.MergeUrl(file.SiteUrl, query, fragment), fragment, linkType, file, false);
         }
 
