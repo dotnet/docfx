b395ed2614e8b976fbf3b0d1f36d880da6742662
Yufei Huang <yufeih@live.com>
2020-05-22 11:24:17 +0800

    Link definition before h1 is treated as invisible (#5950)

--- src/docfx/lib/markdown/MarkdigUtility.cs
+++ src/docfx/lib/markdown/MarkdigUtility.cs
@@ -210,7 +210,7 @@ namespace Microsoft.Docs.Build
                         visible = visible || VisibleHtml(htmlInline.Tag);
                         break;
                     case HeadingBlock headingBlock when headingBlock.Inline is null || !headingBlock.Inline.Any():
-                        // empty heading
+                    case LinkReferenceDefinition _:
                     case ThematicBreakBlock _:
                     case YamlFrontMatterBlock _:
                         break;
