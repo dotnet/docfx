0328ab345f9b1a9bb6abf6fdb423b34b6a9769f6
LIU Mingwei <42199316+mingwli@users.noreply.github.com>
2020-05-28 19:20:59 +0800

    Retire IntegrationTest with RegressionTest (#5984)

--- test/docfx.RegressionTest/Options.cs
+++ test/docfx.RegressionTest/Options.cs
@@ -13,26 +13,8 @@ namespace Microsoft.Docs.Build
         [Option("branch")]
         public string Branch { get; set; } = "master";
 
-        [Option("build-type")]
-        public string BuildType { get; set; } = "Commit";
-
-        [Option("pull-request-url")]
-        public string PullRequestUrl { get; set; } = string.Empty;
-
-        [Option("pull-request-branch")]
-        public string PullRequestBranch { get; set; } = string.Empty;
-
         [Option("locale")]
         public string Locale { get; set; } = "en-us";
-
-        [Option("need-publish")]
-        public bool NeedPublish { get; set; }
-
-        [Option("docset-name")]
-        public string DocsetName { get; set; }
-
-        [Option("product-name")]
-        public string ProductName { get; set; }
 
         [Option("timeout")]
         public int? Timeout { get; set; }
