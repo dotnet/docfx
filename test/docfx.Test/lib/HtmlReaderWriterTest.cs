e667f05a53f1afa6cdb9ce0c919fe49b6c891302
Yufei Huang <yufeih@live.com>
2020-05-29 12:54:46 +0800

    remove unused using

--- test/docfx.Test/lib/HtmlReaderWriterTest.cs
+++ test/docfx.Test/lib/HtmlReaderWriterTest.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.Graph;
 using Xunit;
 
 namespace Microsoft.Docs.Build
