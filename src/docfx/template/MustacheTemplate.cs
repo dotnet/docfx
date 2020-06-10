204a84ada0c60a2da75bd7461a72eac90068ec7e
Yufei Huang <yufeih@live.com>
2020-06-10 18:47:18 +0800

    Optimize MustachTemplate (#6047)

--- src/docfx/template/MustacheTemplate.cs
+++ src/docfx/template/MustacheTemplate.cs
@@ -1,96 +1,200 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
+using System.Net;
+using System.Text;
 using Newtonsoft.Json.Linq;
 using Stubble.Core.Builders;
-using Stubble.Core.Interfaces;
-using Stubble.Core.Settings;
+using Stubble.Core.Classes;
+using Stubble.Core.Parser;
+using Stubble.Core.Tokens;
 
 namespace Microsoft.Docs.Build
 {
     internal class MustacheTemplate
     {
+        private static readonly Tags s_defaultTags = new Tags("{{", "}}");
+
+        private readonly JObject? _global;
         private readonly string _templateDir;
-        private readonly IStubbleRenderer _renderer;
-        private readonly ConcurrentDictionary<string, Lazy<string?>> _templates = new ConcurrentDictionary<string, Lazy<string?>>();
+        private readonly ParserPipeline _parserPipeline;
+        private readonly ConcurrentDictionary<string, Lazy<BlockToken>> _templates = new ConcurrentDictionary<string, Lazy<BlockToken>>(PathUtility.PathComparer);
 
         public MustacheTemplate(string templateDir, JObject? global = null)
         {
+            _global = global;
             _templateDir = templateDir;
-            _renderer = new StubbleBuilder().Configure(
-                settings => UseJson(settings, global).SetPartialTemplateLoader(new PartialLoader(templateDir))).Build();
+            _parserPipeline = new ParserPipelineBuilder().Build();
         }
 
         public string Render(string templateFileName, JToken model)
         {
-            var template = _templates.GetOrAdd(
-                templateFileName,
-                key => new Lazy<string?>(() =>
-                {
-                    var fileName = Path.Combine(_templateDir, templateFileName);
-                    return File.Exists(fileName)
-                    ? MustacheXrefTagParser.ProcessXrefTag(File.ReadAllText(fileName).Replace("\r", ""))
-                    : null;
-                })).Value;
+            var context = new Stack<JToken>();
+            var template = GetTemplate(templateFileName);
 
-            return template == null ? JsonUtility.Serialize(model) : _renderer.Render(template, model);
+            var result = new StringBuilder(1024);
+            context.Push(model);
+            Render(template, result, context);
+            return result.ToString();
         }
 
-        private static RendererSettingsBuilder UseJson(RendererSettingsBuilder settings, JObject? global)
+        public void Render(BlockToken block, StringBuilder result, Stack<JToken> context)
         {
-            // JObject implements IEnumerable, stubble treats IEnumerable as array,
-            // need to put it to section blacklist and overwrite the truthy check method.
-            return settings.AddValueGetter(typeof(JObject), GetJObjectValue)
-                           .AddValueGetter(typeof(JValue), GetJValueValue)
-                           .AddTruthyCheck<string>(value => !string.IsNullOrEmpty(value))
-                           .AddTruthyCheck<JObject>(value => value != null)
-                           .AddTruthyCheck<JValue>(value => value.Type != JTokenType.Null && !(value.Type == JTokenType.String && string.IsNullOrEmpty(value.Value as string)))
-                           .AddSectionBlacklistType(typeof(JObject))
-                           .AddSectionBlacklistType(typeof(JValue));
-
-            object? GetJObjectValue(object value, string key, bool ignoreCase)
+            foreach (var child in block.Children)
             {
-                if (key == "__global" && global != null)
+                switch (child)
                 {
-                    return global;
-                }
-                var token = (JObject)value;
-                var childToken = token.GetValue(key, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+                    case LiteralToken literal:
+                        foreach (var content in literal.Content)
+                        {
+                            result.Append(content.Text, content.Start, content.Length);
+                        }
+                        break;
 
-                return childToken is JValue scalar ? scalar.Value ?? JValue.CreateNull() : childToken;
+                    case PartialToken partial:
+                        Render(GetTemplate(partial.Content.ToString()), result, context);
+                        break;
+
+                    case InvertedSectionToken invertedSection:
+                        switch (Lookup(context, invertedSection.SectionName))
+                        {
+                            case null:
+                            case JValue value when !IsTruthy(value):
+                            case JArray array when array.Count == 0:
+                                Render(invertedSection, result, context);
+                                break;
+                        }
+                        break;
+
+                    case SectionToken section:
+                        var property = Lookup(context, section.SectionName);
+                        switch (property)
+                        {
+                            case null:
+                            case JValue value when !IsTruthy(value):
+                                break;
+
+                            case JArray array:
+                                foreach (var item in array)
+                                {
+                                    context.Push(item);
+                                    Render(section, result, context);
+                                    context.Pop();
+                                }
+                                break;
+
+                            default:
+                                context.Push(property);
+                                Render(section, result, context);
+                                context.Pop();
+                                break;
+                        }
+                        break;
+
+                    case InterpolationToken interpolationToken:
+                        if (Lookup(context, interpolationToken.Content.ToString()) is JToken text)
+                        {
+                            var content = interpolationToken.EscapeResult
+                                ? WebUtility.HtmlEncode(text.ToString())
+                                : text.ToString();
+
+                            result.Append(content);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private JToken? Lookup(Stack<JToken> tokens, string name)
+        {
+            if (name == ".")
+            {
+                return tokens.Peek();
             }
 
-            object? GetJValueValue(object value, string key, bool ignoreCase)
-                => key.Trim() == "." ? ((JValue)value).Value : null;
-        }
-
-        private class PartialLoader : IStubbleLoader
-        {
-            private readonly string _dir;
-            private readonly ConcurrentDictionary<string, Lazy<string>> _cache = new ConcurrentDictionary<string, Lazy<string>>();
-
-            public PartialLoader(string dir) => _dir = dir;
-
-            public IStubbleLoader Clone() => new PartialLoader(_dir);
-
-            public ValueTask<string> LoadAsync(string name) => new ValueTask<string>(Load(name));
-
-            public string Load(string name) => _cache.GetOrAdd(
-                name,
-                key => new Lazy<string>(() =>
+            foreach (var token in tokens)
+            {
+                if (!name.Contains('.'))
                 {
-                    var fileName = Path.Combine(_dir, name);
-                    if (!File.Exists(fileName))
+                    var result = GetProperty(token, name);
+                    if (result != null)
                     {
-                        fileName = Path.Combine(_dir, name + ".tmpl.partial");
+                        return result;
+                    }
+                }
+                else
+                {
+                    var result = token;
+                    var keys = name.Split('.', StringSplitOptions.RemoveEmptyEntries);
+                    for (var i = 0; i < keys.Length; i++)
+                    {
+                        result = GetProperty(result, keys[i]);
+                        if (result is null)
+                        {
+                            // Skip looking up parent context for the last segment,
+                            // this is important for array length testing.
+                            if (i == keys.Length - 1)
+                            {
+                                return null;
+                            }
+                            break;
+                        }
                     }
 
-                    return MustacheXrefTagParser.ProcessXrefTag(File.ReadAllText(fileName).Replace("\r", ""));
-                })).Value;
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private JToken? GetProperty(JToken token, string key)
+        {
+            return token switch
+            {
+                JObject obj => key == "__global" && _global != null
+                    ? _global : obj.GetValue(key, StringComparison.Ordinal),
+                JArray array when int.TryParse(key, out var index) && index >= 0 && index < array.Count => array[index],
+                _ => null,
+            };
+        }
+
+        private static bool IsTruthy(JValue value)
+        {
+            return value.Value switch
+            {
+                null => false,
+                string stringValue => stringValue.Length > 0,
+                bool boolValue => boolValue,
+                int intValue => intValue != 0,
+                long longValue => longValue != 0,
+                float floatValue => floatValue != 0,
+                double doubleValue => doubleValue != 0,
+                _ => true,
+            };
+        }
+
+        private BlockToken GetTemplate(string name)
+        {
+            return _templates.GetOrAdd(name, key => new Lazy<BlockToken>(() =>
+            {
+                var fileName = Path.Combine(_templateDir, name);
+                if (!File.Exists(fileName))
+                {
+                    fileName = Path.Combine(_templateDir, name + ".tmpl.partial");
+                }
+
+                var template = MustacheXrefTagParser.ProcessXrefTag(File.ReadAllText(fileName).Replace("\r", ""));
+
+                return MustacheParser.Parse(template, s_defaultTags, 0, _parserPipeline);
+            })).Value;
         }
     }
 }
