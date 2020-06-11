fe4b99f7f3ca1611a06b400fbb833ecf92c1c665
Wei Hu <huwe@microsoft.com>
2020-06-11 12:37:44 +0800

    moniker order can be optional

--- src/docfx/lib/moniker/Moniker.cs
+++ src/docfx/lib/moniker/Moniker.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Docs.Build
 
         public string ProductName { get; set; } = "";
 
-        public int Order { get; set; } = 0;
+        public int Order { get; set; } = int.MinValue;
 
         [JsonExtensionData]
         public JObject ExtensionData { get; } = new JObject();
