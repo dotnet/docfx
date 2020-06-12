7a45b6c93120396cf5fbdd8fba8c27557c76713a
Jiayin Pei <jipe@microsoft.com>
2020-06-12 14:27:07 +0800

    disable `updateGhPage` temporarily

--- tools/Deployment/gulpfile.js
+++ tools/Deployment/gulpfile.js
@@ -225,7 +225,7 @@ gulp.task("dev", gulp.series("clean", "build", "e2eTest"));
 gulp.task("dev:build", gulp.series("clean", "build", "e2eTest"));
 gulp.task("dev:release", gulp.series("pack", "publish:azdevops-perf-login", "publish:azdevops-perf", "publish:azdevops-internal-login", "publish:azdevops-internal", "publish:azdevops-ppe-login", "publish:azdevops-ppe"));
 
-gulp.task("master:build", gulp.series("clean", "build:release", "e2eTest", "updateGhPage"));
+gulp.task("master:build", gulp.series("clean", "build:release", "e2eTest"));
 gulp.task("master:pack", gulp.series("pack"));
 gulp.task("master:release", gulp.series("packAssetZip", "publish:nuget", "publish:gh-release", "publish:gh-asset", "publish:chocolatey"));
 
