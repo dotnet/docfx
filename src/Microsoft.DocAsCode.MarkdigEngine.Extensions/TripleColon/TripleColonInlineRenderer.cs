8941cb14791e9704f2e3d368c53ec3f22928607d
Yufei Huang <yufeih@live.com>
2020-06-16 12:34:13 +0800

    Remove triple colon extension RenderDelegate

--- src/Microsoft.DocAsCode.MarkdigEngine.Extensions/TripleColon/TripleColonInlineRenderer.cs
+++ src/Microsoft.DocAsCode.MarkdigEngine.Extensions/TripleColon/TripleColonInlineRenderer.cs
@@ -2,14 +2,24 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
 {
+    using System;
     using Markdig.Renderers;
     using Markdig.Renderers.Html;
 
     public class TripleColonInlineRenderer : HtmlObjectRenderer<TripleColonInline>
     {
+        private readonly MarkdownContext _context;
+
+        public TripleColonInlineRenderer(MarkdownContext context)
+        {
+            _context = context;
+        }
+
         protected override void Write(HtmlRenderer renderer, TripleColonInline inline)
         {
-            if (inline.Extension.Render(renderer, inline))
+            var logWarning = new Action<string>(message => _context.LogWarning($"invalid-{inline.Extension.Name}", message, inline));
+
+            if (inline.Extension.Render(renderer, inline, logWarning))
             {
                 return;
             }
