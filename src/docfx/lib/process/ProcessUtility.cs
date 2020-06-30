10e38296b534129bc3a599af9d08c9cc785d4019
Yufei Huang <yufeih@live.com>
2020-06-30 09:52:55 +0800

    Fix ProcessUtility stream handling

--- src/docfx/lib/process/ProcessUtility.cs
+++ src/docfx/lib/process/ProcessUtility.cs
@@ -6,7 +6,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.Docs.Build
@@ -19,6 +18,7 @@ namespace Microsoft.Docs.Build
         /// <summary>
         /// Start a new process and wait for its execution to complete
         /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "MemoryStream properly disposed")]
         public static string Execute(string fileName, string commandLineArgs, string? cwd = null, bool stdout = true, string[]? secrets = null)
         {
             var sanitizedCommandLineArgs = secrets != null ? secrets.Aggregate(commandLineArgs, HideSecrets) : commandLineArgs;
@@ -35,32 +35,24 @@ namespace Microsoft.Docs.Build
                     RedirectStandardError = true,
                 };
 
-                var process = Process.Start(psi);
+                using var process = Process.Start(psi);
 
-                var errData = new StringBuilder();
-                Task.Run(
-                () =>
-                {
-                    var reader = process.StandardError;
-                    while (true)
-                    {
-                        var line = reader.ReadLine();
-                        if (line == null)
-                        {
-                            break;
-                        }
-                        errData.Append(line);
-                    }
-                    using var errStream = new MemoryStream(Encoding.UTF8.GetBytes(errData.ToString()));
-                    errStream.CopyTo(Console.OpenStandardOutput());
-                });
-                var sanitizedErrorData = secrets != null ? secrets.Aggregate(errData.ToString(), HideSecrets) : errData.ToString();
-
+                using var errorStream = new MemoryStream();
+                using var readError = Task.Run(() => PipeStream(process.StandardError.BaseStream, Console.OpenStandardOutput(), errorStream));
                 var result = stdout ? process.StandardOutput.ReadToEnd() : "";
+
                 process.WaitForExit();
 
                 if (process.ExitCode != 0)
                 {
+                    readError.GetAwaiter().GetResult();
+
+                    errorStream.Seek(0, SeekOrigin.Begin);
+
+                    using var errorReader = new StreamReader(errorStream);
+                    var errorData = errorReader.ReadToEnd();
+                    var sanitizedErrorData = secrets != null ? secrets.Aggregate(errorData, HideSecrets) : errorData;
+
                     throw new InvalidOperationException($"'\"{fileName}\" {sanitizedCommandLineArgs}' failed in directory '{cwd}' with exit code {process.ExitCode}: \nSTDOUT:'{result}': \nSTDERR:'{sanitizedErrorData}'");
                 }
 
@@ -70,6 +62,24 @@ namespace Microsoft.Docs.Build
             static string HideSecrets(string arg, string secret)
             {
                 return arg.Replace(secret, secret.Length > 5 ? "***" + secret.Substring(secret.Length - 5) : "***");
+            }
+
+            static void PipeStream(Stream input, Stream output1, Stream output2)
+            {
+                var console = Console.OpenStandardOutput();
+                var buffer = new byte[1024];
+
+                while (true)
+                {
+                    var bytesRead = input.Read(buffer, 0, buffer.Length);
+                    if (bytesRead <= 0)
+                    {
+                        break;
+                    }
+
+                    output1.Write(buffer, 0, bytesRead);
+                    output2.Write(buffer, 0, bytesRead);
+                }
             }
         }
 
