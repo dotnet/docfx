b2d68e97d080d4bf279c9e35dc5925b212074426
Yufei Huang <yufeih@live.com>
2020-07-01 13:44:25 +0800

    Fix uid unescape at wrong place

--- src/docfx/build/xref/XrefResolver.cs
+++ src/docfx/build/xref/XrefResolver.cs
@@ -58,6 +58,9 @@ namespace Microsoft.Docs.Build
             SourceInfo<string> href, Document referencingFile, Document inclusionRoot)
         {
             var (uid, query, fragment) = UrlUtility.SplitUrl(href);
+
+            uid = Uri.UnescapeDataString(uid);
+
             string? moniker = null;
             string? text = null;
             string? alt = null;
@@ -181,11 +184,10 @@ namespace Microsoft.Docs.Build
 
         private (Error?, IXrefSpec?, string? href) Resolve(SourceInfo<string> uid, Document referencingFile, Document inclusionRoot)
         {
-            var unescapedUid = Uri.UnescapeDataString(uid);
-            var (xrefSpec, href) = ResolveInternalXrefSpec(unescapedUid, referencingFile, inclusionRoot);
+            var (xrefSpec, href) = ResolveInternalXrefSpec(uid, referencingFile, inclusionRoot);
             if (xrefSpec is null)
             {
-                (xrefSpec, href) = ResolveExternalXrefSpec(unescapedUid);
+                (xrefSpec, href) = ResolveExternalXrefSpec(uid);
             }
 
             if (xrefSpec is null)
