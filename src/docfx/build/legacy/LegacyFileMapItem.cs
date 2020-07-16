63485524d0dbb2b51c0c312793493ad8fbe19428
Wei Hu <huwe@microsoft.com>
2020-07-16 15:39:19 +0800

    fix enum serialization

--- src/docfx/build/legacy/LegacyFileMapItem.cs
+++ src/docfx/build/legacy/LegacyFileMapItem.cs
@@ -3,6 +3,7 @@
 
 using System;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Docs.Build
@@ -10,6 +11,7 @@ namespace Microsoft.Docs.Build
     [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     internal class LegacyFileMapItem
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public LegacyItemType Type { get; set; }
 
         public string OutputRelativePath { get; set; }
--- src/docfx/build/legacy/LegacyItemType.cs
+++ src/docfx/build/legacy/LegacyItemType.cs
@@ -1,12 +1,19 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Runtime.Serialization;
+
 namespace Microsoft.Docs.Build
 {
     internal enum LegacyItemType
     {
+        [EnumMember(Value ="Toc")]
         Toc,
+
+        [EnumMember(Value = "Content")]
         Content,
+
+        [EnumMember(Value = "Resource")]
         Resource,
     }
 }
