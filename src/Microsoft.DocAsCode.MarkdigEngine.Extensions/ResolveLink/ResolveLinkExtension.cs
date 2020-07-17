cf0107ec1ac5df6e3866f3e1229c577a93ba5a46
Osmond Jiang <xinjiang@microsoft.com>
2020-07-17 14:43:56 +0800

    fix alt text for inline

--- src/Microsoft.DocAsCode.MarkdigEngine.Extensions/ResolveLink/ResolveLinkExtension.cs
+++ src/Microsoft.DocAsCode.MarkdigEngine.Extensions/ResolveLink/ResolveLinkExtension.cs
@@ -3,8 +3,11 @@
 
 namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
 {
+    using System.Linq;
+    using System.Text;
     using Markdig;
     using Markdig.Renderers;
+    using Markdig.Renderers.Html;
     using Markdig.Syntax;
     using Markdig.Syntax.Inlines;
 
@@ -35,14 +38,13 @@ namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
 
                 case LinkInline linkInline:
                     linkInline.Url = linkInline.IsImage
-                        ? _context.GetImageLink(linkInline.Url, linkInline, linkInline.Title)
+                        ? _context.GetImageLink(linkInline.Url, linkInline, GetAltText(linkInline))
                         : _context.GetLink(linkInline.Url, linkInline);
                     foreach (var subBlock in linkInline)
                     {
                         UpdateLinks(subBlock);
                     }
                     break;
-
                 case ContainerBlock containerBlock:
                     foreach (var subBlock in containerBlock)
                     {
@@ -65,6 +67,34 @@ namespace Microsoft.DocAsCode.MarkdigEngine.Extensions
                     break;
 
                 default:
+                    break;
+            }
+        }
+
+        private string GetAltText(Inline inline)
+        {
+            var stringBuilder = new StringBuilder();
+            GenerateAltTextInLinkInline(inline, stringBuilder);
+            return stringBuilder.ToString();
+        }
+
+        private void GenerateAltTextInLinkInline(Inline inline, StringBuilder altTextBuilder)
+        {
+            switch (inline)
+            {
+                case LiteralInline literal:
+                    var content = literal.Content;
+                    altTextBuilder.Append(content.Text, content.Start, content.Length);
+                    break;
+                case XrefInline xrefInline:
+                    var altText = xrefInline.GetAttributes().Properties.Where(p => p.Key == "data-raw-source").Select(p => p.Value).FirstOrDefault();
+                    altTextBuilder.Append(altText);
+                    break;
+                case ContainerInline containerInline:
+                    foreach (var subInline in containerInline)
+                    {
+                        GenerateAltTextInLinkInline(subInline, altTextBuilder);
+                    }
                     break;
             }
         }
