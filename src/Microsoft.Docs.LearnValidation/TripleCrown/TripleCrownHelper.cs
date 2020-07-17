29e903b643e3b952f2ba44abe973e5d602fd1468
LIU Mingwei <42199316+mingwli@users.noreply.github.com>
2020-07-17 10:17:32 +0800

    Initialize forking learn plugin (#6254)
    removed .ps1, App.config

---
+++ src/Microsoft.Docs.LearnValidation/TripleCrown/TripleCrownHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.OpenPublishing.PluginHelper;
+using Microsoft.TripleCrown.DataContract.ViewModel;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleCrownValidation.TripleCrown
+{
+    public class TripleCrownHelper
+    {
+        private enum CheckItemType
+        {
+            Unit,
+            Module
+        };
+
+        private RestClient _client;
+        private string _branch;
+        private bool _needBranchFallback;
+        private const string _defaultLocale = "en-us";
+        private static readonly string[] _nofallbackBranches = new[] { "master", "live" };
+
+        public TripleCrownHelper(string endpoint, string branch)
+        {
+            _client = new RestClient(endpoint);
+            _branch = branch;
+            _needBranchFallback = !_nofallbackBranches.Contains(_branch);
+        }
+
+        public bool IsModule(string uid)
+        {
+            return CheckItemExist(CheckItemType.Module, uid);
+        }
+
+        public bool IsUnit(string uid)
+        {
+            return CheckItemExist(CheckItemType.Unit, uid);
+        }
+
+        private bool CheckItemExist(CheckItemType type, string uid)
+        {
+            var requestString = type == CheckItemType.Module ? $"modules/{uid}" : $"units/{uid}";
+            var request = new RestRequest(requestString);
+
+            AppendHeaderAndParameter(request);
+
+            IRestResponse response = _client.Execute(request);
+
+            OPSLogger.LogToConsole("TripleCrownValidation check {0} call: {1}", type, response.ResponseUri);
+            OPSLogger.LogToConsole("TripleCrownValidation check {0} result: {1}", type, response.StatusCode);
+
+            if (response.StatusCode != HttpStatusCode.OK && _needBranchFallback)
+            {
+                request.AddOrUpdateParameter("branch", "master");
+                response = _client.Execute(request);
+
+                OPSLogger.LogToConsole("TripleCrownValidation check {0} call: {1}", type, response.ResponseUri);
+                OPSLogger.LogToConsole("TripleCrownValidation check {0} result: {1}", type, response.StatusCode);
+            }
+
+            return response.StatusCode == HttpStatusCode.OK;
+        }
+
+        private void AppendHeaderAndParameter(RestRequest request)
+        {
+            request.AddParameter("branch", _branch);
+            request.AddParameter("locale", _defaultLocale);
+            request.AddHeader("Referer", " https://tcexplorer.azurewebsites.net");
+        }
+    }
+}
---
+++ src/Microsoft.Docs.LearnValidation/Validators/TokenValidator.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.OpenPublishing.PluginHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripleCrownValidation.DependencyMap;
+using TripleCrownValidation.Models;
+
+namespace TripleCrownValidation.Validators
+{
+    public class TokenValidator
+    {
+        private List<IValidateModel> _hierarchyItems;
+        private string _docsetFolder;
+        private string _dependencyMapFile;
+        private List<string> _fallbackFolders;
+
+        public TokenValidator(string dependencyMapFile, List<IValidateModel> hierarchyItems, string docsetFolder, List<string> fallbackFolders)
+        {
+            _dependencyMapFile = dependencyMapFile;
+            _hierarchyItems = hierarchyItems;
+            _docsetFolder = docsetFolder;
+            _fallbackFolders = fallbackFolders;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            var dependencyItems = DependencyMapHelper.LoadDependentFileInfo(_dependencyMapFile);
+
+            var dependencyMapping = dependencyItems.Where(item => item.DependencyType == "include"
+                && item.ToFilePath != item.FromFilePath + ".md" // not fragment 
+                && (item.ToFilePath.EndsWith(".md") || item.ToFilePath.EndsWith(".yml")) // token extension 
+                && (_fallbackFolders == null || _fallbackFolders.All(ff => !item.ToFilePath.StartsWith(ff)))) // not fallback
+                .GroupBy(item => item.FromFilePath).ToDictionary(key => key.Key, value => value.Select(v=>v.ToFilePath).Distinct().ToList());
+
+            // LearningPath will not check token, required from Bodhi
+            // Refer to workitem https://ceapex.visualstudio.com/Engineering/_workitems/edit/64285/
+            foreach (var hierarchyItem in _hierarchyItems.Where(hi => !(hi is PathValidateModel)))
+            {
+                var file = Path.Combine(_docsetFolder, hierarchyItem.SourceRelativePath).BackSlashToForwardSlash();
+
+                if (dependencyMapping.ContainsKey(file))
+                {
+                    foreach(var tokenDependency in dependencyMapping[file])
+                    {
+                        if (!File.Exists(tokenDependency))
+                        {
+                            var tokenRelativePath = tokenDependency.StartsWith(_docsetFolder) ?
+                                "~" + tokenDependency.Substring(_docsetFolder.Length).ForwardSlashToBackSlash() 
+                                : tokenDependency;
+
+                            OPSLogger.LogUserError(LogCode.TripleCrown_Token_NotFound, LogMessageUtility.FormatMessage(LogCode.TripleCrown_Token_NotFound), hierarchyItem.SourceRelativePath);
+                            hierarchyItem.IsValid = false;
+                            isValid = false;
+                        }
+                    }
+                }
+            }
+
+            return isValid;
+        }
+    }
+}
