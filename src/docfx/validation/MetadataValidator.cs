b045b2b84113b3eab0a331df8ca66b46fa973a02
Kun Peng Fan <fkpwolf@gmail.com>
2020-08-24 17:22:52 +0800

    tune scope of metadata validate

--- src/docfx/validation/MetadataValidator.cs
+++ src/docfx/validation/MetadataValidator.cs
@@ -54,6 +54,12 @@ namespace Microsoft.Docs.Build
             {
                 if (file.PageType != null)
                 {
+                    if (file.PageType == "conceptual" &&
+                        metadata.ContainsKey("layout") &&
+                        !string.Equals(metadata.GetValue("layout")?.ToString(), "conceptual", StringComparison.OrdinalIgnoreCase))
+                    {
+                        continue;
+                    }
                     errors.AddRange(schemaValidator.Validate(metadata, file.FilePath));
                 }
             }
