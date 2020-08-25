9c0de9d8a034436856b9170e75eaef1e3f2dd213
Yufei Huang <yufeih@live.com>
2020-08-25 10:44:06 +0800

    Fix GetPageType

--- src/docfx/build/document/DocumentProvider.cs
+++ src/docfx/build/document/DocumentProvider.cs
@@ -193,30 +193,15 @@ namespace Microsoft.Docs.Build
 
         private string? GetPageType(ContentType contentType, FilePath path, string? mime)
         {
-            switch (contentType)
+            return contentType switch
             {
-                case ContentType.Page:
-                    if (mime != null && s_pageTypeMapping.TryGetValue(mime, out var type))
-                    {
-                        return type;
-                    }
-                    if (mime != "Conceptual")
-                    {
-                        return null;
-                    }
-                    var metadata = _metadataProvider.GetMetadata(_errors, path);
-                    if (metadata.Layout == "HubPage" || metadata.Layout == "LandingPage")
-                    {
-                        return null;
-                    }
-                    return "conceptual";
-                case ContentType.Redirection:
-                    return "redirection";
-                case ContentType.TableOfContents:
-                    return "toc";
-                default:
-                    return null;
-            }
+                ContentType.Page when mime != null => s_pageTypeMapping.TryGetValue(mime, out var type)
+                    ? type
+                    : (_metadataProvider.GetMetadata(_errors, path).Layout ?? mime).ToLowerInvariant(),
+                ContentType.Redirection => "redirection",
+                ContentType.TableOfContents => "toc",
+                _ => null,
+            };
         }
 
         private string FilePathToSitePath(FilePath filePath, ContentType contentType, OutputUrlType outputUrlType, bool isHtml)
