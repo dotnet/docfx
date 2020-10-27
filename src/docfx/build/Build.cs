811b637c975e8feb919470e6d2fcd9c3c2bc27a2
mingwli <mingwei.liu@microsoft.com>
2020-10-27 10:05:26 +0800

    clean

--- src/docfx/build/Build.cs
+++ src/docfx/build/Build.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Docs.Validation;
 
