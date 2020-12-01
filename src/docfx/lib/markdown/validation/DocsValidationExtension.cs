566df9837d8111a71d0c57ca825ea144942d8888
Validation Tool pipeline <you@example.com>
2020-12-01 16:33:39 +0800

    revert test

--- src/docfx/lib/markdown/validation/DocsValidationExtension.cs
+++ src/docfx/lib/markdown/validation/DocsValidationExtension.cs
@@ -27,11 +27,6 @@ namespace Microsoft.Docs.Build
             {
                 var currentFile = ((SourceInfo)InclusionContext.File).File;
 
-                if (currentFile.Format != FileFormat.Markdown)
-                {
-                    return;
-                }
-
                 var documentNodes = new List<ContentNode>();
                 var codeBlockNodes = new List<(bool isInclude, CodeBlockItem codeBlockItem)>();
 
