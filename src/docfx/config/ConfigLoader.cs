61ee9981457efb33e1dee34fedd752436c3a99e7
JiayinPei <jipe@microsoft.com>
2020-12-04 11:05:51 +0800

    update

--- src/docfx/config/ConfigLoader.cs
+++ src/docfx/config/ConfigLoader.cs
@@ -78,7 +78,7 @@ namespace Microsoft.Docs.Build
             PackageResolver? packageResolver = default;
             var fallbackDocsetPath = new Lazy<string?>(
                 () => LocalizationUtility.GetFallbackDocsetPath(docsetPath, repository, preloadConfig.FallbackRepository, packageResolver!));
-            var fileResolver = new FileResolver(docsetPath, fallbackDocsetPath, credentialProvider, configAdapter, fetchOptions);
+            var fileResolver = new FileResolver(docsetPackage, fallbackDocsetPath, credentialProvider, configAdapter, fetchOptions);
 
             packageResolver = new PackageResolver(docsetPath, preloadConfig, fetchOptions, fileResolver, repository);
 
