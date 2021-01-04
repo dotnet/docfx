45da9557bdf8c7356ff1478d1f463db6035bd07b
Yufei Huang <yufeih@live.com>
2021-01-04 05:22:41 -0600

    Add Watch<T> debug view (#6961)

---
+++ src/docfx/lib/watch/WatchDebugView{T}.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Docs.Build
+{
+    internal class WatchDebugView<T>
+    {
+        private readonly Watch<T> _watch;
+
+        public WatchDebugView(Watch<T> watch) => _watch = watch;
+
+        public T? Value => _watch.ValueForDebugDisplay;
+    }
+}
