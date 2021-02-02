112679a823a0a3b53a77d22e0085919325bd1b47
JiayinPei <jipe@microsoft.com>
2021-02-02 11:31:54 +0800

    Reset the verbose option since the SynchronizationContext doesn't flow by the ASPNetCore

--- src/docfx/serve/lsp/LanguageServerHost.cs
+++ src/docfx/serve/lsp/LanguageServerHost.cs
@@ -21,38 +21,40 @@ namespace Microsoft.Docs.Build
             ILanguageServerNotificationListener? notificationListener = null)
         {
             using var cts = new CancellationTokenSource();
+            using (Log.BeginScope(commandLineOptions.Verbose))
+            {
+                var languageServerPackage = new LanguageServerPackage(
+                    new(commandLineOptions.WorkingDirectory),
+                    package ?? new LocalPackage(commandLineOptions.WorkingDirectory));
 
-            var languageServerPackage = new LanguageServerPackage(
-                new(commandLineOptions.WorkingDirectory),
-                package ?? new LocalPackage(commandLineOptions.WorkingDirectory));
+                var server = await LanguageServer.From(options => options
+                    .WithInput(input)
+                    .WithOutput(output)
+                    .ConfigureLogging(x => x.AddLanguageProtocolLogging())
+                    .WithHandler<TextDocumentHandler>()
+                    .WithHandler<DidChangeWatchedFilesHandler>()
+                    .WithServices(services => services
+                        .AddSingleton(notificationListener ?? new LanguageServerNotificationListener())
+                        .AddSingleton(languageServerPackage)
+                        .AddSingleton(commandLineOptions)
+                        .AddSingleton<DiagnosticPublisher>()
+                        .AddSingleton<LanguageServerCredentialProvider>()
+                        .AddSingleton<LanguageServerBuilder>()
+                        .AddOptions()
+                        .AddLogging())
+                    .WithServices(x => x.AddLogging(b => b.SetMinimumLevel(LogLevel.Trace)))
+                    .OnExit(_ =>
+                    {
+                        cts.Cancel();
+                        return Task.CompletedTask;
+                    }));
 
-            var server = await LanguageServer.From(options => options
-                .WithInput(input)
-                .WithOutput(output)
-                .ConfigureLogging(x => x.AddLanguageProtocolLogging())
-                .WithHandler<TextDocumentHandler>()
-                .WithHandler<DidChangeWatchedFilesHandler>()
-                .WithServices(services => services
-                    .AddSingleton(notificationListener ?? new LanguageServerNotificationListener())
-                    .AddSingleton(languageServerPackage)
-                    .AddSingleton(commandLineOptions)
-                    .AddSingleton<DiagnosticPublisher>()
-                    .AddSingleton<LanguageServerCredentialProvider>()
-                    .AddSingleton<LanguageServerBuilder>()
-                    .AddOptions()
-                    .AddLogging())
-                .WithServices(x => x.AddLogging(b => b.SetMinimumLevel(LogLevel.Trace)))
-                .OnExit(_ =>
-                {
-                    cts.Cancel();
-                    return Task.CompletedTask;
-                }));
+                var builder = server.GetRequiredService<LanguageServerBuilder>();
 
-            var builder = server.GetRequiredService<LanguageServerBuilder>();
-
-            await Task.WhenAll(
-                server.WaitForExit,
-                Task.Run(() => builder.Run(cts.Token)));
+                await Task.WhenAll(
+                    server.WaitForExit,
+                    Task.Run(() => builder.Run(cts.Token)));
+            }
         }
     }
 }
