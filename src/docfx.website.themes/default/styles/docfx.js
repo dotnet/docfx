cffdcdc9bc177a569fd5da8c1f39db56af6c7149
Andrii Vitiv <cyberandrii@gmail.com>
2021-02-07 21:30:59 +0200

    Fix comparing URLs

--- src/docfx.website.themes/default/styles/docfx.js
+++ src/docfx.website.themes/default/styles/docfx.js
@@ -557,6 +557,9 @@ $(function () {
           tocrel = tocPath.substr(0, index + 1);
         }
         var currentHref = util.getCurrentWindowAbsolutePath();
+        if(!currentHref.endsWith('.html')) {
+          currentHref += '.html';
+        }
         $('#sidetoc').find('a[href]').each(function (i, e) {
           var href = $(e).attr("href");
           if (util.isRelativePath(href)) {
