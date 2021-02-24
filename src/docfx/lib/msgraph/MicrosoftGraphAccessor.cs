8922249080c7832d8a8aec03ace7b72fac1ac8c7
Yufei Huang <yufeih@live.com>
2021-02-24 14:05:29 +0800

    Remove unused usings

--- src/docfx/lib/msgraph/MicrosoftGraphAccessor.cs
+++ src/docfx/lib/msgraph/MicrosoftGraphAccessor.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Graph;
