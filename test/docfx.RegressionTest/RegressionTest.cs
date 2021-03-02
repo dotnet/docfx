13eb964da5c8569afec67eafea7fb5acdd72d39a
Yufei Huang <yufeih@live.com>
2021-03-02 18:04:49 +0800

    a

--- test/docfx.RegressionTest/RegressionTest.cs
+++ test/docfx.RegressionTest/RegressionTest.cs
@@ -13,6 +13,7 @@ namespace Microsoft.Docs.Build
         {
             using var http = new HttpClient();
             http.DefaultRequestHeaders.Authorization = new("Bearer", Environment.GetEnvironmentVariable("System.AccessToken"));
+            http.DefaultRequestHeaders.Accept.Add(new("application/json"));
             var endpoints = await (await http.GetAsync(
                 "https://dev.azure.com/ceapex/Engineering/_apis/serviceendpoint/endpoints?endpointNames=Engineering&api-version=6.1-preview.4"))
                 .EnsureSuccessStatusCode().Content.ReadAsStringAsync();
