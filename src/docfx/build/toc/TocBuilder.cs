cb86939a76bd803763a1d15b37c3bf604b00b686
Shichao Zhang <Shichao.Zhang@microsoft.com>
2021-04-26 11:04:43 +0800

    refactor

--- src/docfx/build/toc/TocBuilder.cs
+++ src/docfx/build/toc/TocBuilder.cs
@@ -85,15 +85,13 @@ namespace Microsoft.Docs.Build
                     var output = _templateEngine.RunJavaScript("toc.json.js", JsonUtility.ToJObject(model));
                     _output.WriteJson(Path.ChangeExtension(outputPath, ".json"), output);
                 }
-
-                if (_config.OutputType == OutputType.Json)
+                else if (_config.OutputType == OutputType.Json)
                 {
                     var outputModel = JsonUtility.ToJObject(model);
                     outputModel["schema"] = "toc";
                     _output.WriteJson(outputPath, outputModel);
                 }
-
-                if (_config.OutputType == OutputType.PageJson)
+                else
                 {
                     var output = _templateEngine.RunJavaScript("toc.json.js", JsonUtility.ToJObject(model));
                     _output.WriteJson(outputPath, output);
