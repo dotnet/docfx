438e9b94f927df59d6be1b2637ca06820017bdde
Yufei Huang <yufeih@live.com>
2021-08-25 22:54:08 -0500

    refactor: Merge docs specific stuff to Microsoft.Docs.Specalized project (#7577)

--- src/Microsoft.Docs.Specialized/template/LandingDataRowValue.cs
+++ src/Microsoft.Docs.Specialized/template/LandingDataRowValue.cs
@@ -1,10 +1,10 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.Docs.Build
 {
     public sealed class LandingDataRowValue
     {
-        public string Href { get; set; }
+        public string? Href { get; set; }
     }
 }
