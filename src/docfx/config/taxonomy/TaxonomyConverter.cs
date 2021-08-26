ee705277da82789ea471469d6028d6bc6b234b48
Jingkan He <jahe@microsoft.com>
2021-08-26 16:23:14 +0800

    - add file header

--- src/docfx/config/taxonomy/TaxonomyConverter.cs
+++ src/docfx/config/taxonomy/TaxonomyConverter.cs
@@ -1,7 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Collections.Generic;
 using Microsoft.Docs.Validation;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
 {
