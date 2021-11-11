6f1f5a264ba9dbc007cc56a168f4c212c825e190
mingwli <mingwei.liu@microsoft.com>
2021-11-11 10:57:00 +0800

    Keep link starting with /_themes/

--- src/docfx/lib/HtmlUtility.cs
+++ src/docfx/lib/HtmlUtility.cs
@@ -406,8 +406,9 @@ namespace Microsoft.Docs.Build
 
         private static string AddLocaleIfMissingForAbsolutePath(string href, string locale)
         {
-            // should not add locale for api links
-            if (href.StartsWith("/api/", StringComparison.OrdinalIgnoreCase))
+            // should not add locale for api, _themes links
+            if (href.StartsWith("/api/", StringComparison.OrdinalIgnoreCase) ||
+                href.StartsWith("/_themes/", StringComparison.OrdinalIgnoreCase))
             {
                 return href;
             }
