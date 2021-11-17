771e1129a73bf6217d930b89f852caf8d03c75e0
Peican Gao (Beyondsoft Consulting Inc) <v-pegao@microsoft.com>
2021-11-17 16:41:20 +0800

    revert changes

--- src/docfx/lib/HtmlUtility.cs
+++ src/docfx/lib/HtmlUtility.cs
@@ -319,24 +319,7 @@ namespace Microsoft.Docs.Build
                 }
                 else
                 {
-                    switch (value.Type)
-                    {
-                        case JTokenType.Boolean:
-                        {
-                            content = (bool)value ? "true" : "false";
-                            break;
-                        }
-                        case JTokenType.Date:
-                        {
-                            content = JsonUtility.Serialize(value).Trim('"');
-                            break;
-                        }
-                        default:
-                        {
-                            content = value.ToString();
-                            break;
-                        }
-                    }
+                    content = value.Type == JTokenType.Boolean ? (bool)value ? "true" : "false" : value.ToString();
                 }
 
                 result.AppendLine($"<meta name=\"{Encode(name)}\" content=\"{Encode(content)}\" />");
