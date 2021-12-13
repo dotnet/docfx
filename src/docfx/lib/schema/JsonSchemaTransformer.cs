15a4a0b18f820e9a9fd99aaed3c547a9a90a66b5
Yufei Huang <yufeih@live.com>
2021-12-13 17:10:59 +0800

    Remove SchemaMap lambda capture

--- src/docfx/lib/schema/JsonSchemaTransformer.cs
+++ src/docfx/lib/schema/JsonSchemaTransformer.cs
@@ -230,8 +230,7 @@ internal class JsonSchemaTransformer
                 }
 
                 xref.XrefProperties[xrefProperty] = new Lazy<JToken>(
-                    () => LoadXrefProperty(
-                        schemaMap, file, uid, value, rootSchema, uidCount, JsonUtility.AddToPropertyPath(propertyPath, xrefProperty)),
+                    () => LoadXrefProperty(file, uid, value, rootSchema, JsonUtility.AddToPropertyPath(propertyPath, xrefProperty)),
                     LazyThreadSafetyMode.PublicationOnly);
             }
         }
@@ -289,12 +288,10 @@ internal class JsonSchemaTransformer
     }
 
     private JToken LoadXrefProperty(
-        JsonSchemaMap schemaMap,
         FilePath file,
         SourceInfo<string> uid,
         JToken value,
         JsonSchema rootSchema,
-        int uidCount,
         string propertyPath)
     {
         var recursionDetector = s_recursionDetector.Value!;
@@ -306,6 +303,7 @@ internal class JsonSchemaTransformer
         try
         {
             recursionDetector.Push(uid);
+            var (token, schema, schemaMap, uidCount) = ValidateContent(ErrorBuilder.Null, file);
             return TransformContentCore(
                 _errors,
                 schemaMap,
