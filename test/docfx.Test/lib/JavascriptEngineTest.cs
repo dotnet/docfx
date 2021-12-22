5b543a8b23f08f488893e10deb2bb78bd8e19061
Yufei Huang <yufeih@live.com>
2021-12-22 15:32:52 +0800

    Free JS engine memory after use (#7782)

--- test/docfx.Test/lib/JavascriptEngineTest.cs
+++ test/docfx.Test/lib/JavascriptEngineTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -48,5 +49,26 @@ public class JavascriptEngineTest
             Assert.Equal("foo", engine.Run("module.js", "foo", JValue.CreateUndefined()));
             Assert.Equal("bar", engine.Run("module.js", "bar", JValue.CreateUndefined()));
         }
+    }
+
+    [SkippableFact]
+    public void ReleaseMemoryOnDispose()
+    {
+        Skip.IfNot(OperatingSystem.IsWindows());
+
+        var js = new ChakraCoreJsEngine(new LocalPackage("data/javascript"));
+        var before = Process.GetCurrentProcess().WorkingSet64;
+        var size = 1000_000_000;
+        var epsilon = 100_000_000;
+
+        js.Run("memory.js", "main", size);
+
+        var after = Process.GetCurrentProcess().WorkingSet64;
+        Assert.True(after - before >= size - epsilon, $"before {before}, after {after}");
+
+        js.Dispose();
+
+        var afterDispose = Process.GetCurrentProcess().WorkingSet64;
+        Assert.True(after - afterDispose >= size - epsilon, $"before {before}, after {after}, after dispose {afterDispose}");
     }
 }
