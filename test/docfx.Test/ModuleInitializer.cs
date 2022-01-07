9876c31694d3f78dcb1ced585e781a7f0dc91c1f
Yufei Huang <yufeih@live.com>
2022-01-07 14:08:34 +0800

    Fix test hang

---
+++ test/docfx.Test/ModuleInitializer.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+using Xunit;
+
+// By default xunit limit max parallel threads to the number of CPU counts,
+// this causes test hang on hosted CI servers where CPU count is small.
+// Changing it to -1 to remove this limit.
+[assembly: CollectionBehavior(MaxParallelThreads = -1)]
+
+namespace Microsoft.Docs.Build;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        Environment.SetEnvironmentVariable("DOCFX_APPDATA_PATH", Path.GetFullPath("appdata"));
+    }
+}
