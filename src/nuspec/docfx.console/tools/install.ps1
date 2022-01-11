6d5d0dd6b7fffc09b29601defbfd872888b15e26
Ying Hua <herohua@users.noreply.github.com>
2022-01-11 10:05:35 +0800

    fix: http to https (#7826)

--- src/nuspec/docfx.console/tools/install.ps1
+++ src/nuspec/docfx.console/tools/install.ps1
@@ -5,7 +5,7 @@ param($installPath, $toolsPath, $package, $project)
 
 try
 {
-  $url = "http://dotnet.github.io/docfx/install.html?version=" + $package.Version
+  $url = "https://dotnet.github.io/docfx/install.html?version=" + $package.Version
   $dte2 = Get-Interface $dte ([EnvDTE80.DTE2])
 
   if ($dte2.ActiveWindow.Caption -eq "Package Manager Console")
