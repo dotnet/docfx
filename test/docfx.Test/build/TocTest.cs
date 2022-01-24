42dfc54b1ec12f6ce744473ea4a57d1a1f41e3a0
Yufei Huang <yufeih@live.com>
2022-01-24 17:11:06 +0800

    TOC -> toc

--- test/docfx.Test/build/TocTest.cs
+++ test/docfx.Test/build/TocTest.cs
@@ -10,29 +10,29 @@ public static class TocTest
     [Theory]
 
     // same level
-    [InlineData(new[] { "TOC.md" }, "b.md", "TOC.md", "TOC.md")]
-    [InlineData(new[] { "TOC.md", "a/TOC.md" }, "b.md", "TOC.md", "TOC.md")]
-    [InlineData(new[] { "TOC.md", "a/TOC.md" }, "a/b.md", "a/TOC.md", "a/TOC.md")]
-    [InlineData(new[] { "b/TOC.md", "a/TOC.md" }, "a/b.md", "a/TOC.md", "a/TOC.md")]
-    [InlineData(new[] { "TOC.md", "a/b/TOC.md" }, "a/b/b.md", "a/b/TOC.md", "a/b/TOC.md")]
-    [InlineData(new[] { "c/a/d/TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "c/a/d/TOC.md", "c/a/d/TOC.md")]
+    [InlineData(new[] { "toc.md" }, "b.md", "toc.md", "toc.md")]
+    [InlineData(new[] { "toc.md", "a/toc.md" }, "b.md", "toc.md", "toc.md")]
+    [InlineData(new[] { "toc.md", "a/toc.md" }, "a/b.md", "a/toc.md", "a/toc.md")]
+    [InlineData(new[] { "b/toc.md", "a/toc.md" }, "a/b.md", "a/toc.md", "a/toc.md")]
+    [InlineData(new[] { "toc.md", "a/b/toc.md" }, "a/b/b.md", "a/b/toc.md", "a/b/toc.md")]
+    [InlineData(new[] { "c/a/d/toc.md", "c/a/toc.md" }, "c/a/d/b.md", "c/a/d/toc.md", "c/a/d/toc.md")]
 
     // next level(nearest)
-    [InlineData(new[] { "b/c/TOC.md", "a/TOC.md" }, "b.md", "a/TOC.md", null)]
-    [InlineData(new[] { "b/TOC.md", "a/b/TOC.md" }, "b.md", "b/TOC.md", null)]
+    [InlineData(new[] { "b/c/toc.md", "a/toc.md" }, "b.md", "a/toc.md", null)]
+    [InlineData(new[] { "b/toc.md", "a/b/toc.md" }, "b.md", "b/toc.md", null)]
 
     // order by folder name
-    [InlineData(new[] { "b/c/TOC.md", "b/d/TOC.md" }, "b.md", "b/c/TOC.md", null)]
+    [InlineData(new[] { "b/c/toc.md", "b/d/toc.md" }, "b.md", "b/c/toc.md", null)]
 
     // up level(nearest)
-    [InlineData(new[] { "TOC.md", "a/TOC.md" }, "b/b.md", "TOC.md", "TOC.md")]
-    [InlineData(new[] { "TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "c/a/TOC.md", "c/a/TOC.md")]
-    [InlineData(new[] { "c/b/TOC.md", "c/a/TOC.md" }, "c/a/d/b.md", "c/a/TOC.md", "c/a/TOC.md")]
+    [InlineData(new[] { "toc.md", "a/toc.md" }, "b/b.md", "toc.md", "toc.md")]
+    [InlineData(new[] { "toc.md", "c/a/toc.md" }, "c/a/d/b.md", "c/a/toc.md", "c/a/toc.md")]
+    [InlineData(new[] { "c/b/toc.md", "c/a/toc.md" }, "c/a/d/b.md", "c/a/toc.md", "c/a/toc.md")]
 
     // mix level(nearest)
-    [InlineData(new[] { "c/b/TOC.md", "TOC.md" }, "c/a.md", "TOC.md", "TOC.md")]
-    [InlineData(new[] { "c/b/TOC.md", "TOC.md" }, "c/b/a.md", "c/b/TOC.md", "c/b/TOC.md")]
-    [InlineData(new[] { "c/b/TOC.md", "c/d/e/TOC.md" }, "c/f/h/a.md", "c/b/TOC.md", null)]
+    [InlineData(new[] { "c/b/toc.md", "toc.md" }, "c/a.md", "toc.md", "toc.md")]
+    [InlineData(new[] { "c/b/toc.md", "toc.md" }, "c/b/a.md", "c/b/toc.md", "c/b/toc.md")]
+    [InlineData(new[] { "c/b/toc.md", "c/d/e/toc.md" }, "c/f/h/a.md", "c/b/toc.md", null)]
 
     public static void FindNearestToc(string[] tocs, string file, string expectedTocPath, string expectedOrphanTocPath)
     {
