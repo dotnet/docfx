7548116a051ccb3ec04b00c04d93ab5f44afefe5
bharney0 <brharney@microsoft.com>
2022-05-25 16:07:26 -0500

    fixed unit test for complex image src resolution

--- test/Microsoft.DocAsCode.MarkdigEngine.Tests/InclusionTest.cs
+++ test/Microsoft.DocAsCode.MarkdigEngine.Tests/InclusionTest.cs
@@ -782,7 +782,7 @@ body";
 ";
             var token = @"
 :::image source=""example.jpg"" type=""complex"" alt-text=""example"":::
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Lorem Ipsum is simply dummy text `code` of the printing and [link](https://microsoft.com) typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 :::image-end:::
 ";
             TestUtility.WriteToFile("r/r.md", r);
@@ -791,9 +791,8 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 
             var expected = @"<p class=""mx-imgBorder"">
 <img src=""~/r/b/example.jpg"" alt=""example"" aria-describedby=""1-0"">
-<div id=""1-0"" class=""visually-hidden"">
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-</div>
+<div id=""1-0"" class=""visually-hidden""><p>
+Lorem Ipsum is simply dummy text `code` of the printing and [link](https://microsoft.com) typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
 </p>
 ";
             Assert.Equal(expected.Replace("\r\n", "\n"), marked.Html);
