98831c68ec6b1ef9e5fd6b5755357ff9b6e135c0
Jiapeng Xu <jiapengxu@microsoft.com>
2022-06-07 11:59:55 +0800

    update

--- src/docfx/config/ops/OpsConfigAdapter.cs
+++ src/docfx/config/ops/OpsConfigAdapter.cs
@@ -82,6 +82,7 @@ internal class OpsConfigAdapter
             repository = queries["repository_url"] ?? "";
         }
         var branch = (queries["publish_repository_url"] != queries["repository_url"]) ? "main" : queries["repository_branch"] ?? "";
+        Console.WriteLine($"repository: {repository}, branch: {branch}");
         var locale = queries["locale"] ?? "";
         var xrefEndpoint = queries["xref_endpoint"] ?? "";
         var xrefQueryTags =
