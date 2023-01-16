fc564ad8dd4c1e903cf3e40edd28752aa50659ff
Yufei Huang <yufeih@live.com>
2023-01-16 14:16:09 +0800

    test: add C# 10 test cases
    Unsupported C# 10 features:
    - record struct
    
    #7737

---
+++ samples/seed/src/DotnetNew/CSharp10.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharp10;
+
+public readonly record struct ReadOnlyRecordStruct(double X, double Y, double Z);
+
+public record struct RecordStruct(DateTime TakenAt, double Measurement);
+
+public record class RecordClass(string FirstName, string LastName);
+
+public struct ParameterlessStructConstructors
+{
+    public int X;
+    public int Y { get; set; }
+    public string Description { get; init; } = "Default description";
+
+    public ParameterlessStructConstructors()
+    {
+        X = 10;
+    }
+}
+
+public class ConstantInterpolatedStrings
+{
+    public const string S1 = $"Hello world";
+    public const string S2 = $"Hello{" "}World";
+    public const string S3 = $"{S1} Kevin, welcome to the team!";
+}
+
+public class Issue7737
+{
+    /// <summary>
+    /// <see cref="Exception"/>
+    /// </summary>
+    public void Foo() { }
+}
