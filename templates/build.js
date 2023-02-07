8a9ed9c928c9963e069b23f559a6b7cd0db910f0
Yufei Huang <yufeih@live.com>
2023-02-07 14:47:22 +0800

    build: bundle vendor js using esbuild

--- templates/build.js
+++ templates/build.js
@@ -1,20 +1,18 @@
+const esbuild = require('esbuild')
 const { minify } = require('terser')
 const CleanCSS = require('clean-css')
 const { readFileSync, writeFileSync, cpSync } = require('fs')
 
 async function build() {
-  await minifyJs(
-    'default/styles/docfx.vendor.js', [
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/highlightjs/highlight.pack.min.js',
-    'node_modules/@websanova/url/dist/url.min.js',
-    'node_modules/twbs-pagination/jquery.twbsPagination.min.js',
-    'node_modules/mark.js/dist/jquery.mark.min.js',
-    'node_modules/anchor-js/anchor.min.js'])
 
-  await minifyJs('default/styles/lunr.min.js', [
-    'node_modules/lunr/lunr.js'])
+  esbuild.build({
+    bundle: true,
+    minify: true,
+    outfile: 'default/styles/docfx.vendor.js',
+    entryPoints: [
+      'src/docfx.vendor.js'
+    ]
+  })
 
   await minifyCss('default/styles/docfx.vendor.css', [
     'node_modules/bootstrap/dist/css/bootstrap.css',
---
+++ templates/src/docfx.vendor.js
@@ -0,0 +1,11 @@
+window.$ = window.jQuery = require('jquery')
+
+require('bootstrap')
+require('twbs-pagination')
+require('mark.js/src/jquery.js')
+
+const AnchorJS = require('anchor-js')
+window.anchors = new AnchorJS()
+
+window.hljs = require('highlightjs/highlight.pack.min.js')
+require('@websanova/url/src/url.js')
