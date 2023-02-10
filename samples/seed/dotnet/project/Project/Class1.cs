52aa30ff577338f4bea7b11122ebc011876b12e6
Yufei Huang <yufeih@live.com>
2023-02-10 20:45:34 +0800

    test: verify #896

--- samples/seed/dotnet/project/Project/Class1.cs
+++ samples/seed/dotnet/project/Project/Class1.cs
@@ -6,8 +6,14 @@ public class Class1
     /// This method should do something...
     /// </summary>
     /// <include file='../../docs.xml' path='docs/members[@name="MyTests"]/Test/*'/>
-    public void XmlCommentIncludeTag()
-    {
+    public void XmlCommentIncludeTag() { }
+    
+    /// <summary>
+    /// Test
+    /// </summary>
+    /// <param name="args"></param>
+    /// <seealso cref="Test{T}"/>
+    public void Issue896() { }
 
-    }
+    public class Test<T> { }
 }
