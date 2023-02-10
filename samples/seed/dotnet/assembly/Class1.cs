0eedf93238fbed5e0abff6f3b1ade03450dedb81
Yufei Huang <yufeih@live.com>
2023-02-10 17:32:16 +0800

    test: verify #7815 fixed
    fixes #7815

--- samples/seed/dotnet/assembly/Class1.cs
+++ samples/seed/dotnet/assembly/Class1.cs
@@ -1,12 +1,14 @@
-﻿namespace BuildFromAssembly;
+﻿using System.ComponentModel;
+
+namespace BuildFromAssembly;
 
 /// <summary>
 /// This is a test class.
 /// </summary>
 public class Class1
 {
-    public static void HelloWorld()
-    {
+    public static void HelloWorld() { }
 
-    }
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public void HiddenAPI() { }
 }
