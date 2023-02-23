f9cb70a48805795f4f7bd2c0978802a7e6253b39
Yufei Huang <yufeih@live.com>
2023-02-23 17:45:27 +0800

    Make in this article a list

--- templates/src/nav.ts
+++ templates/src/nav.ts
@@ -2,8 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import $ from 'jquery'
+import { render, html } from 'lit-html'
 import {
-  formList,
   getAbsolutePath,
   getCurrentWindowAbsolutePath,
   getDirectory,
@@ -86,103 +86,15 @@ export function renderNavbar() {
   }
 }
 
-export function renderAffix() {
-  const hierarchy = getHierarchy()
-  if (!hierarchy || hierarchy.length <= 0) {
-    $('#affix').hide()
-  } else {
-    const html = formList(hierarchy, ['nav', 'bs-docs-sidenav'])
-    $('#affix>div').empty().append(html)
-    if ($('footer').is(':visible')) {
-      $('.sideaffix').css('bottom', '70px')
-    }
-    $('#affix a').click(function(e) {
-      const scrollspy = $('[data-spy="scroll"]').data()['bs.scrollspy']
-      const target = e.target.hash
-      if (scrollspy && target) {
-        scrollspy.activate(target)
-      }
-    })
+export function renderInThisArticle() {
+  const h2s = document.querySelectorAll<HTMLHeadingElement>('article h2')
+  if (h2s.length <= 0) {
+    return
   }
 
-  function getHierarchy() {
-    // supported headers are h1, h2, h3, and h4
-    const $headers = $(
-      $.map(['h1', 'h2', 'h3', 'h4'], function(h) {
-        return '.article article ' + h
-      }).join(', ')
-    )
+  const dom = html`
+    <h5>In this article</h5>
+    <ul class="nav bs-docs-sidenav">${Array.from(h2s).map(h2 => html`<li><a href="#${h2.id}">${h2.innerText}</a></li>`)}</ul>`
 
-    // a stack of hierarchy items that are currently being built
-    const stack = []
-    $headers.each(function(i, e) {
-      if (!e.id) {
-        return
-      }
-
-      const item = {
-        name: htmlEncode($(e).text()),
-        href: '#' + e.id,
-        items: []
-      }
-
-      if (!stack.length) {
-        stack.push({ type: e.tagName, siblings: [item] })
-        return
-      }
-
-      const frame = stack[stack.length - 1]
-      if (e.tagName === frame.type) {
-        frame.siblings.push(item)
-      } else if (e.tagName[1] > frame.type[1]) {
-        // we are looking at a child of the last element of frame.siblings.
-        // push a frame onto the stack. After we've finished building this item's children,
-        // we'll attach it as a child of the last element
-        stack.push({ type: e.tagName, siblings: [item] })
-      } else {
-        // e.tagName[1] < frame.type[1]
-        // we are looking at a sibling of an ancestor of the current item.
-        // pop frames from the stack, building items as we go, until we reach the correct level at which to attach this item.
-        while (e.tagName[1] < stack[stack.length - 1].type[1]) {
-          buildParent()
-        }
-        if (e.tagName === stack[stack.length - 1].type) {
-          stack[stack.length - 1].siblings.push(item)
-        } else {
-          stack.push({ type: e.tagName, siblings: [item] })
-        }
-      }
-    })
-    while (stack.length > 1) {
-      buildParent()
-    }
-
-    function buildParent() {
-      const childrenToAttach = stack.pop()
-      const parentFrame = stack[stack.length - 1]
-      const parent = parentFrame.siblings[parentFrame.siblings.length - 1]
-      $.each(childrenToAttach.siblings, function(i, child) {
-        parent.items.push(child)
-      })
-    }
-    if (stack.length > 0) {
-      const topLevel = stack.pop().siblings
-      if (topLevel.length === 1) {
-        // if there's only one topmost header, dump it
-        return topLevel[0].items
-      }
-      return topLevel
-    }
-    return undefined
-  }
-
-  function htmlEncode(str) {
-    if (!str) return str
-    return str
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-  }
+  render(dom, document.getElementById('affix'))
 }
