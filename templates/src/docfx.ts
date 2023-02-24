695c1c6bdebfee1d586a668805152f62d8eb34ad
Yufei Huang <yufeih@live.com>
2023-02-24 17:55:39 +0800

    fix: incorrect anchor position

--- templates/src/docfx.ts
+++ templates/src/docfx.ts
@@ -8,7 +8,7 @@ import '../node_modules/highlight.js/scss/github.scss'
 import './docfx.scss'
 
 import { highlight } from './highlight'
-import { breakWord } from './helper'
+import { breakText } from './helper'
 import { renderMarkdown } from './markdown'
 import { enableSearch } from './search'
 import { renderToc } from './toc'
@@ -32,7 +32,6 @@ require('bootstrap')
 require('twbs-pagination')
 
 document.addEventListener('DOMContentLoaded', function() {
-  workAroundFixedHeaderForAnchors()
   highlight()
   enableSearch()
 
@@ -43,82 +42,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
   breakText()
 
-  function breakText() {
-    $('.xref').addClass('text-break')
-    const texts = $('.text-break')
-    texts.each(function() {
-      breakWord($(this))
-    })
-  }
-
-  // adjusted from https://stackoverflow.com/a/13067009/1523776
-  function workAroundFixedHeaderForAnchors() {
-    const HISTORY_SUPPORT = !!(history && history.pushState)
-    const ANCHOR_REGEX = /^#[^ ]+$/
-
-    function getFixedOffset() {
-      return $('header').first().height()
-    }
-
-    /**
-     * If the provided href is an anchor which resolves to an element on the
-     * page, scroll to it.
-     * @param  {String} href
-     * @return {Boolean} - Was the href an anchor.
-     */
-    function scrollIfAnchor(href, pushToHistory) {
-      let rect, anchorOffset
-
-      if (!ANCHOR_REGEX.test(href)) {
-        return false
-      }
-
-      const match = document.getElementById(href.slice(1))
-
-      if (match) {
-        rect = match.getBoundingClientRect()
-        anchorOffset = window.pageYOffset + rect.top - getFixedOffset()
-        window.scrollTo(window.pageXOffset, anchorOffset)
-
-        // Add the state to history as-per normal anchor links
-        if (HISTORY_SUPPORT && pushToHistory) {
-          history.pushState({}, document.title, location.pathname + href)
-        }
-      }
-
-      return !!match
-    }
-
-    /**
-     * Attempt to scroll to the current location's hash.
-     */
-    function scrollToCurrent() {
-      scrollIfAnchor(window.location.hash, false)
-    }
-
-    /**
-     * If the click event's target was an anchor, fix the scroll position.
-     */
-    function delegateAnchors(e) {
-      const elem = e.target
-
-      if (scrollIfAnchor(elem.getAttribute('href'), true)) {
-        e.preventDefault()
-      }
-    }
-
-    $(window).on('hashchange', scrollToCurrent)
-
-    $(window).on('load', function() {
-      // scroll to the anchor if present, offset by the header
-      scrollToCurrent()
-    })
-
-    $(document).ready(function() {
-      // Exclude tabbed content case
-      $('a:not([data-tab])').click(function(e) { delegateAnchors(e) })
-    })
-
-    window.docfx.ready = true
-  }
+  window.docfx.ready = true
 })
