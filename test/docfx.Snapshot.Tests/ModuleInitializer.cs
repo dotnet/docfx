438b8d0f72dbc29d53eb7acd5ca5c350e6623fe9
Yufei Huang <yufeih@live.com>
2023-03-10 10:58:08 +0800

    update build server detection logic

--- test/docfx.Snapshot.Tests/ModuleInitializer.cs
+++ test/docfx.Snapshot.Tests/ModuleInitializer.cs
@@ -13,8 +13,7 @@ namespace Microsoft.DocAsCode.Tests
         [ModuleInitializer]
         public static void Initialize()
         {
-            // Disable for PR build to allow commiting diff files
-            BuildServerDetector.Detected = Environment.GetEnvironmentVariable("GITHUB_EVENT_NAME") == "push";
+            BuildServerDetector.Detected = Environment.GetEnvironmentVariable("BUILD_SERVER") == "true";
             VerifyDiffPlex.Initialize(VerifyTests.DiffPlex.OutputType.Compact);
         }
     }
