9a8a2abc13c595472cac70121177ed5f9f55237e
Yufei Huang <yufeih@live.com>
2023-03-20 13:21:48 +0800

    feat: adjust .NET API display for modern template (#8525)

--- templates/modern/src/helper.test.ts
+++ templates/modern/src/helper.test.ts
@@ -6,6 +6,6 @@ import { breakWord } from './helper'
 test('break-text', () => {
   expect(breakWord('Other APIs')).toEqual(['Other APIs'])
   expect(breakWord('System.CodeDom')).toEqual(['System.', 'Code', 'Dom'])
-  expect(breakWord('System.Collections.Dictionary<string, object>')).toEqual(['System.', 'Collections.', 'Dictionary<', 'string, object>'])
+  expect(breakWord('System.Collections.Dictionary<string, object>')).toEqual(['System.', 'Collections.', 'Dictionary<', 'string,', ' object>'])
   expect(breakWord('https://github.com/dotnet/docfx')).toEqual(['https://github.', 'com/', 'dotnet/', 'docfx'])
 })
