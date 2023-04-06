64985dff7199b273b188cd8c043d6341d6cf34ed
dpvreony <dpvreony@users.noreply.github.com>
2023-04-06 15:41:05 +0100

    add ability to run PDF generation via Docset API

--- src/Microsoft.DocAsCode.App/Docset.cs
+++ src/Microsoft.DocAsCode.App/Docset.cs
@@ -37,9 +37,57 @@ public static class Docset
         {
             var configDirectory = Path.GetDirectoryName(Path.GetFullPath(configPath));
 
+            var defaultSerializer = JsonUtility.DefaultSerializer.Value;
+
             var config = JObject.Parse(File.ReadAllText(configPath));
-            if (config.TryGetValue("build", out var value))
-                RunBuild.Exec(value.ToObject<BuildJsonConfig>(JsonUtility.DefaultSerializer.Value), options, configDirectory);
+            if (config.TryGetValue("build", out var build))
+                RunBuild.Exec(build.ToObject<BuildJsonConfig>(defaultSerializer), options, configDirectory);
+
+            return Task.CompletedTask;
+        }
+        finally
+        {
+            Logger.Flush();
+            Logger.PrintSummary();
+            Logger.UnregisterAllListeners();
+        }
+    }
+
+    /// <summary>
+    /// Builds a pdf specified by docfx.json config.
+    /// </summary>
+    /// <param name="configPath">The path to docfx.json config file.</param>
+    /// <param name="options">The build options.</param>
+    /// <returns>A task to await for build completion.</returns>
+    public static Task Pdf(string configPath, BuildOptions options)
+    {
+        return Exec<PdfJsonConfig>(
+            configPath,
+            options,
+            "pdf",
+            RunPdf.Exec);
+    }
+
+    private static Task Exec<TConfig>(
+        string configPath,
+        BuildOptions options,
+        string elementKey,
+        Action<TConfig, BuildOptions, string, string?> execAction)
+    {
+        var consoleLogListener = new ConsoleLogListener();
+        Logger.RegisterListener(consoleLogListener);
+
+        try
+        {
+            var configDirectory = Path.GetDirectoryName(Path.GetFullPath(configPath));
+
+            var defaultSerializer = JsonUtility.DefaultSerializer.Value;
+
+            var config = JObject.Parse(File.ReadAllText(configPath));
+
+            if (config.TryGetValue(elementKey, out var pdf))
+                execAction(pdf.ToObject<TConfig>(defaultSerializer), options, configDirectory, null);
+
             return Task.CompletedTask;
         }
         finally
