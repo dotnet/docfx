30db3ad7af487fbb66eef2a415a4ca3ad6dfaf0a
Yufei Huang <yufeih@live.com>
2023-04-28 11:00:38 +0800

    feat: support navbar dropdown button (#8694)

--- templates/modern/src/nav.ts
+++ templates/modern/src/nav.ts
@@ -11,6 +11,11 @@ export type NavItem = {
   href: URL
 }
 
+export type NavItemContainer = {
+  name: string
+  items: NavItem[]
+}
+
 /**
  * @returns active navbar items
  */
@@ -23,13 +28,27 @@ export async function renderNavbar(): Promise<NavItem[]> {
   const navItems = await loadNavItems()
   const activeItem = findActiveItem(navItems)
 
+  const menuItem = item => {
+    const current = (item === activeItem ? 'page' : false)
+    const active = (item === activeItem ? 'active' : null)
+    return html`<li class='nav-item'><a class='nav-link ${active}' aria-current=${current} href=${item.href}>${breakWordLit(item.name)}</a></li>`
+  }
+
   const menu = html`
     <ul class='navbar-nav'>${
       navItems.map(item => {
-        const current = (item === activeItem ? 'page' : false)
-        const active = (item === activeItem ? 'active' : null)
-        return html`
-          <li class='nav-item'><a class='nav-link ${active}' aria-current=${current} href=${item.href}>${breakWordLit(item.name)}</a></li>`
+        if ('items' in item) {
+          const active = item.items.some(i => i === activeItem) ? 'active' : null
+          return html`
+            <li class='nav-item dropdown'>
+              <a class='nav-link dropdown-toggle ${active}' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+                ${breakWordLit(item.name)}
+              </a>
+              <ul class='dropdown-menu'>${item.items.map(menuItem)}</ul>
+            </li>`
+        } else {
+          return menuItem(item)
+        }
       })
     }</ul>`
 
@@ -43,7 +62,7 @@ export async function renderNavbar(): Promise<NavItem[]> {
 
   return activeItem ? [activeItem] : []
 
-  async function loadNavItems(): Promise<NavItem[]> {
+  async function loadNavItems(): Promise<(NavItem | NavItemContainer)[]> {
     const navrel = meta('docfx:navrel')
     if (!navrel) {
       return []
@@ -51,7 +70,12 @@ export async function renderNavbar(): Promise<NavItem[]> {
 
     const navUrl = new URL(navrel.replace(/.html$/gi, '.json'), window.location.href)
     const { items } = await fetch(navUrl).then(res => res.json())
-    return items.map(a => ({ name: a.name, href: new URL(a.href, navUrl) }))
+    return items.map((a: NavItem | NavItemContainer) => {
+      if ('items' in a) {
+        return { name: a.name, items: a.items.map(i => ({ name: i.name, href: new URL(i.href, navUrl) })) }
+      }
+      return { name: a.name, href: new URL(a.href, navUrl) }
+    })
   }
 
   function githubLink() {
@@ -113,11 +137,11 @@ function inThisArticleForManagedReference(): TemplateResult {
   }
 }
 
-function findActiveItem(items: NavItem[]): NavItem {
+function findActiveItem(items: (NavItem | NavItemContainer)[]): NavItem {
   const url = new URL(window.location.href)
   let activeItem: NavItem
   let maxPrefix = 0
-  for (const item of items) {
+  for (const item of items.map(i => 'items' in i ? i.items : i).flat()) {
     const prefix = commonUrlPrefix(url, item.href)
     if (prefix > maxPrefix) {
       maxPrefix = prefix
