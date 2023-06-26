cc78772cbd1739b2b5932e8ca6c5a412ed16b3b5
filzrev <103790468+filzrev@users.noreply.github.com>
2023-06-26 22:28:34 +0900

    fix-CS0612-and-CS0618

--- samples/seed/dotnet/assembly/Class1.cs
+++ samples/seed/dotnet/assembly/Class1.cs
@@ -7,8 +7,14 @@ namespace BuildFromAssembly;
 /// </summary>
 public class Class1
 {
+    /// <summary>
+    /// HelloWorld method.
+    /// </summary>
     public static void HelloWorld() { }
 
+    /// <summary>
+    /// HiddenAPI method.
+    /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
     public void HiddenAPI() { }
 }
