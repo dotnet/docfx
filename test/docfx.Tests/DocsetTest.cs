451797717bb4bb10d8441fc7d6e45191da66757b
dpvreony <dpvreony@users.noreply.github.com>
2023-07-05 12:25:12 +0100

    housekeeping on tests

--- test/docfx.Tests/DocsetTest.cs
+++ test/docfx.Tests/DocsetTest.cs
@@ -83,43 +83,6 @@ public class DocsetTest : TestBase
     }
 
     [Fact]
-    public static async Task CustomLogo_Override_LogoFromTemplate_Pdf()
-    {
-        var outputs = await Pdf(new()
-        {
-            ["docfx.json"] =
-                """
-                {
-                "pdf": {
-                    "content": [
-                        {
-                            "files": [
-                                "**/**.md"
-                            ]
-                        },
-                        {
-                            "files": "**/toc.yml"
-                        }
-                    ],
-                    "wkhtmltopdf": {
-                        "additionalArguments": "--enable-local-file-access"
-                    },
-                    "dest": "_pdf",
-                    "cleanupCacheHistory": false,
-                    "force": true
-                }
-                }
-                """,
-            ["logo.svg"] = "<svg>my svg</svg>",
-            ["pdf/toc.yml"] = "- name: Introduction\r\n  href: intro.md\r\n- name: Another Page\r\n  href: anotherpage.md\r\n",
-            ["pdf/intro.md"] = "# Introduction\r\n\r\n",
-            ["pdf/anotherpage.md"] = "# Another Page\r\n\r\n"
-        });
-
-        Assert.Equal("<svg>my svg</svg>", outputs["logo.svg"]());
-    }
-
-    [Fact]
     public static async Task Load_Custom_Plugin_From_Template()
     {
         var outputs = await Build(new()
@@ -142,73 +105,9 @@ public class DocsetTest : TestBase
     }
 
     [Fact]
-    public static async Task Load_Custom_Plugin_From_Template_Pdf()
-    {
-        var outputs = await Pdf(new()
-        {
-            ["docfx.json"] =
-                """
-                {
-                    "build": {
-                        "content": [{ "files": [ "*.md" ] }],
-                        "template": ["default", "../../Assets/template"],
-                        "dest": "_site",
-                        "postProcessors": ["CustomPostProcessor"]
-                    }
-                }
-                """,
-            ["index.md"] = ""
-        });
-
-        Assert.Equal("customPostProcessor", outputs["customPostProcessor.txt"]());
-    }
-
-    [Fact]
     public static async Task Build_With_Global_Metadata_Files()
     {
         var outputs = await Build(new()
-        {
-            ["docfx.json"] =
-                """
-                {
-                    "build": {
-                        "content": [{ "files": [ "*.md" ] }],
-                        "dest": "_site",
-                        "exportRawModel": true,
-                        "globalMetadataFiles": ["projectMetadata1.json", "projectMetadata2.json"],
-                        "globalMetadata": {
-                            "meta1": "docfx.json",
-                            "meta3": "docfx.json"
-                        }
-                    }
-                }
-                """,
-            ["projectMetadata1.json"] =
-                """
-                {
-                    "meta1": "projectMetadata1.json",
-                    "meta2": "projectMetadata2.json"
-                }
-                """,
-            ["projectMetadata2.json"] =
-                """
-                {
-                    "meta2": "projectMetadata2.json"
-                }
-                """,
-            ["index.md"] = ""
-        });
-
-        var metadata = JsonDocument.Parse(outputs["index.raw.json"]()).RootElement;
-        Assert.Equal("projectMetadata1.json", metadata.GetProperty("meta1").GetString());
-        Assert.Equal("projectMetadata2.json", metadata.GetProperty("meta2").GetString());
-        Assert.Equal("docfx.json", metadata.GetProperty("meta3").GetString());
-    }
-
-    [Fact]
-    public static async Task Build_With_Global_Metadata_Files_Pdf()
-    {
-        var outputs = await Pdf(new()
         {
             ["docfx.json"] =
                 """
@@ -295,18 +194,128 @@ public class DocsetTest : TestBase
         Assert.Equal("fileMetadata2.json", b.GetProperty("meta1").GetString());
     }
 
-
     [Fact]
-    public static async Task Build_With_File_Metadata_Files_Pdf()
+    public static async Task Pdf_Basic()
     {
         var outputs = await Pdf(new()
         {
             ["docfx.json"] =
                 """
                 {
-                    "build": {
-                        "content": [{ "files": [ "*.md" ] }],
-                        "dest": "_site",
+                "pdf": {
+                    "content": [
+                        {
+                            "files": [
+                                "**/**.md"
+                            ]
+                        },
+                        {
+                            "files": "**/toc.yml"
+                        }
+                    ],
+                    "wkhtmltopdf": {
+                        "additionalArguments": "--enable-local-file-access"
+                    },
+                    "dest": "_pdf"
+                }
+                }
+                """,
+            ["pdf/toc.yml"] = "- name: Introduction\r\n  href: intro.md\r\n- name: Another Page\r\n  href: anotherpage.md\r\n",
+            ["pdf/intro.md"] = "# Introduction\r\n\r\n",
+            ["pdf/anotherpage.md"] = "# Another Page\r\n\r\n"
+        });
+
+        Assert.True(outputs.ContainsKey("Pdf_Basic_pdf.pdf"));
+    }
+
+    [Fact]
+    public static async Task Pdf_With_Cover_Page()
+    {
+        var outputs = await Pdf(new()
+        {
+            ["docfx.json"] =
+                """
+                {
+                "pdf": {
+                    "content": [
+                        {
+                            "files": [
+                                "**/**.md"
+                            ]
+                        },
+                        {
+                            "files": "**/toc.yml"
+                        }
+                    ],
+                    "wkhtmltopdf": {
+                        "additionalArguments": "--enable-local-file-access"
+                    },
+                    "dest": "_pdf"
+                }
+                }
+                """,
+            ["pdf/toc.yml"] = "- name: Introduction\r\n  href: intro.md\r\n- name: Another Page\r\n  href: anotherpage.md\r\n",
+            ["pdf/cover.md"] = "# My Basic Cover Page\r\n\r\n",
+            ["pdf/intro.md"] = "# Introduction\r\n\r\n",
+            ["pdf/anotherpage.md"] = "# Another Page\r\n\r\n"
+        });
+
+        Assert.True(outputs.ContainsKey("Pdf_With_Cover_Page_pdf.pdf"));
+    }
+
+    [Fact]
+    public static async Task Pdf_With_Global_Metadata_Files()
+    {
+        var outputs = await Pdf(new()
+        {
+            ["docfx.json"] =
+                """
+                {
+                    "pdf": {
+                        "content": [{ "files": [ "*.md" ] },{ "files": "toc.yml" }],
+                        "dest": "_pdf",
+                        "exportRawModel": true,
+                        "globalMetadataFiles": ["projectMetadata1.json", "projectMetadata2.json"],
+                        "globalMetadata": {
+                            "meta1": "docfx.json",
+                            "meta3": "docfx.json"
+                        }
+                    }
+                }
+                """,
+            ["projectMetadata1.json"] =
+                """
+                {
+                    "meta1": "projectMetadata1.json",
+                    "meta2": "projectMetadata2.json"
+                }
+                """,
+            ["projectMetadata2.json"] =
+                """
+                {
+                    "meta2": "projectMetadata2.json"
+                }
+                """,
+            ["toc.yml"] = "- name: Introduction\r\n  href: intro.md\r\n- name: Another Page\r\n  href: anotherpage.md\r\n",
+            ["cover.md"] = "# My Basic Cover Page\r\n\r\n",
+            ["intro.md"] = "# Introduction\r\n\r\n",
+            ["anotherpage.md"] = "# Another Page\r\n\r\n"
+        });
+
+        Assert.True(outputs.ContainsKey("Pdf_With_Global_Metadata_Files.pdf"));
+    }
+
+    [Fact]
+    public static async Task Pdf_With_File_Metadata_Files()
+    {
+        var outputs = await Pdf(new()
+        {
+            ["docfx.json"] =
+                """
+                {
+                    "pdf": {
+                        "content": [{ "files": [ "*.md" ] },{ "files": "toc.yml" }],
+                        "dest": "_pdf",
                         "exportRawModel": true,
                         "fileMetadataFiles": ["fileMetadata1.json", "fileMetadata2.json"],
                         "fileMetadata": {
@@ -335,12 +344,13 @@ public class DocsetTest : TestBase
                 }
                 """,
             ["a.md"] = "",
-            ["b.md"] = ""
+            ["b.md"] = "",
+            ["toc.yml"] = "- name: Introduction\r\n  href: intro.md\r\n- name: Another Page\r\n  href: anotherpage.md\r\n",
+            ["cover.md"] = "# My Basic Cover Page\r\n\r\n",
+            ["intro.md"] = "# Introduction\r\n\r\n",
+            ["anotherpage.md"] = "# Another Page\r\n\r\n"
         });
 
-        var a = JsonDocument.Parse(outputs["a.raw.json"]()).RootElement;
-        var b = JsonDocument.Parse(outputs["b.raw.json"]()).RootElement;
-        Assert.Equal("fileMetadata1.json", a.GetProperty("meta1").GetString());
-        Assert.Equal("fileMetadata2.json", b.GetProperty("meta1").GetString());
+        Assert.True(outputs.ContainsKey("Pdf_With_File_Metadata_Files.pdf"));
     }
 }
