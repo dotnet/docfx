7ab1ad673c2455d9fb129da9a1a95500f7d1d657
Yufei Huang <yufeih@users.noreply.github.com>
2023-07-16 10:32:12 +0800

    chore: drop ftp, embedded from xrefmap downloader

--- src/Docfx.Build.Engine/XRefMaps/XRefMapDownloader.cs
+++ src/Docfx.Build.Engine/XRefMaps/XRefMapDownloader.cs
@@ -76,7 +76,7 @@ public class XRefMapDownloader
     }
 
     /// <remarks>
-    /// Support scheme: http, https, ftp, file, embedded.
+    /// Support scheme: http, https, file.
     /// </remarks>
     protected virtual async Task<IXRefContainer> DownloadBySchemeAsync(Uri uri)
     {
@@ -86,18 +86,13 @@ public class XRefMapDownloader
             result = DownloadFromLocal(uri);
         }
         else if (uri.Scheme == Uri.UriSchemeHttp ||
-            uri.Scheme == Uri.UriSchemeHttps ||
-            uri.Scheme == Uri.UriSchemeFtp)
+            uri.Scheme == Uri.UriSchemeHttps)
         {
             result = await DownloadFromWebAsync(uri);
         }
-        else if (uri.Scheme == "embedded")
-        {
-            result = DownloadFromAssembly(uri);
-        }
         else
         {
-            throw new ArgumentException($"Unsupported scheme {uri.Scheme}, expected: http, https, ftp, file, embedded.", nameof(uri));
+            throw new ArgumentException($"Unsupported scheme {uri.Scheme}, expected: http, https, file.", nameof(uri));
         }
         if (result == null)
         {
@@ -135,23 +130,6 @@ public class XRefMapDownloader
         map.BaseUrl = baseUrl;
         UpdateHref(map, null);
         return map;
-    }
-
-    private XRefMap DownloadFromAssembly(Uri uri)
-    {
-        var path = uri.GetComponents(UriComponents.Path, UriFormat.Unescaped);
-        var index = path.IndexOf('/');
-        if (index == -1)
-        {
-            throw new ArgumentException($"Invalid uri {uri.OriginalString}, expect: {uri.Scheme}:{{assemblyName}}/{{resourceName}}", nameof(uri));
-        }
-        var assemblyName = path.Remove(index);
-        var resourceName = assemblyName + "." + path.Substring(index + 1);
-
-        var assembly = AppDomain.CurrentDomain.Load(assemblyName);
-        using var stream = assembly.GetManifestResourceStream(resourceName);
-        using var sr = new StreamReader(stream);
-        return YamlUtility.Deserialize<XRefMap>(sr);
     }
 
     public static void UpdateHref(XRefMap map, Uri uri)
