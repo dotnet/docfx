a33973d5aae638e4ba8f6d578dbf3b21a5dfc85e
dpvreony <dpvreony@users.noreply.github.com>
2023-07-17 20:12:04 +0100

    fix last few tests around log counts

--- test/docfx.Tests/DocsetPdfTest.cs
+++ test/docfx.Tests/DocsetPdfTest.cs
@@ -2,8 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.CompilerServices;
+using Docfx;
 using docfx.Tests.Attributes;
-using Microsoft.DocAsCode.Tests.Common;
+using Docfx.Tests.Common;
 
 using Xunit;
 
