a609767b2f61c1e29f50cdb0a71d685913a2d858
Yufei Huang <yufeih@live.com>
2023-08-11 10:22:32 +0800

    feat: add type definition to REST API pages

--- templates/common/RestApi.common.js
+++ templates/common/RestApi.common.js
@@ -81,6 +81,17 @@ exports.transform = function (model) {
             model.children = model.children.filter(function (o) { return o; });
         }
     }
+    model.definitions = [];
+    model.tags.forEach(function(tag) {
+        tag.children.forEach(function(child) {
+            child.parameters.forEach(function(parameter) { addComplexTypeMetadata(parameter.schema, model.definitions); });
+            child.responses.forEach(function(response) { addComplexTypeMetadata(response.schema, model.definitions); });
+        });
+    });
+    model.children.forEach(function(child) {
+        child.parameters.forEach(function(parameter) { addComplexTypeMetadata(parameter.schema, model.definitions); });
+        child.responses.forEach(function(response) { addComplexTypeMetadata(response.schema, model.definitions); });
+    });
 
     return model;
 
@@ -256,6 +267,72 @@ exports.transform = function (model) {
             path += parameters[i].name;
         }
         return path;
+    }
+
+    function addDefinition(definition, definitions) {
+
+        if (!definition) {
+            return;
+        }
+
+        var xRefName = definition.items && definition.items['x-internal-ref-name']
+            ? definition.items['x-internal-ref-name']
+            : definition['x-internal-ref-name'];
+
+        // Not complex type.
+        if (!xRefName) {
+            return;
+        }
+
+        // Definition already exists return.
+        if (definitions.some(function(d) { return d['x-internal-ref-name'] == xRefName; })) {
+            return;
+        }
+
+        // Create clone to not affect object structure used in original location
+        definition = JSON.parse(JSON.stringify(definition));
+
+        // Unify different object structure to be the same
+
+        // Sometimes properties is under items sometimes not
+        if (definition.items && definition.items.properties) {
+            definition.properties = definition.items.properties;
+        }
+
+        // Sometimes ref-name is under items sometimes not
+        definition['x-internal-ref-name'] = xRefName;
+
+        // Sometimes properties are key/value pairs sometimes not
+        if (definition.properties && !Array.isArray(definition.properties)) {
+            definition.properties = Object.keys(definition.properties).map(function(key) {
+                return {
+                    key: key,
+                    value: definition.properties[key]
+                }
+            });
+        }
+
+        // Add definition to definitions list.
+        definitions.push(definition);
+
+        // Loop through properties that refer to other definitions.
+        (definition.properties || []).forEach(function(property) {
+            addComplexTypeMetadata(property.value, definitions);
+        });
+    }
+
+    function addComplexTypeMetadata(child, definitions) {
+        // Add variations of x-internal-ref-name to support 
+        if (child && child['x-internal-ref-name']) {
+            child.cTypeId = child['x-internal-ref-name'].replace(/\./g, '_');
+            child.cType = child['x-internal-ref-name'].replace(/([A-Z])/g, '<wbr>$1');
+        }
+        if (child && child.items && child.items['x-internal-ref-name']) {
+            child.cTypeId = child.items['x-internal-ref-name'].replace(/\./g, '_');
+            child.cType = child.items['x-internal-ref-name'].replace(/([A-Z])/g, '<wbr>$1');
+            child.cTypeIsArray = true;
+        }
+        addDefinition(child, definitions);
     }
 }
 
