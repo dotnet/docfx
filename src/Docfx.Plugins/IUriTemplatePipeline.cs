c824aa2842a01b08b4defc56b9a213ed6d45bee6
Yufei Huang <yufeih@users.noreply.github.com>
2023-09-30 20:53:54 +0800

    chore: drop url template

--- src/Docfx.Plugins/IUriTemplatePipeline.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace Docfx.Plugins;
-
-public interface IUriTemplatePipeline<T>
-{
-    T Handle(T value, string[] parameters);
-}
