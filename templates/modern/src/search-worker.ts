2f575b282cc46be353455307eceda1462a0c8bd0
Yufei Huang <yufeih@live.com>
2023-10-20 17:12:04 +0800

    feat: support multiple search languages (#9313)

--- templates/modern/src/search-worker.ts
+++ templates/modern/src/search-worker.ts
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 import lunr from 'lunr'
+import stemmer from 'lunr-languages/lunr.stemmer.support'
+import multi from 'lunr-languages/lunr.multi'
 import { get, set, createStore } from 'idb-keyval'
 
 type SearchHit = {
@@ -31,14 +33,25 @@ async function loadIndexCore() {
     }
   }
 
-  const { configureLunr } = await import('./main.js').then(m => m.default) as DocfxOptions
+  const { lunrLanguages, configureLunr } = await import('./main.js').then(m => m.default) as DocfxOptions
+
+  if (lunrLanguages && lunrLanguages.length > 0) {
+    multi(lunr)
+    stemmer(lunr)
+    await Promise.all(lunrLanguages.map(initLanguage))
+  }
 
   const index = lunr(function() {
+    lunr.tokenizer.separator = /[\s\-.()]+/
+
     this.ref('href')
     this.field('title', { boost: 50 })
     this.field('keywords', { boost: 20 })
 
-    lunr.tokenizer.separator = /[\s\-.()]+/
+    if (lunrLanguages && lunrLanguages.length > 0) {
+      this.use(lunr.multiLanguage(...lunrLanguages))
+    }
+
     configureLunr?.(this)
 
     for (const key in data) {
@@ -58,5 +71,47 @@ loadIndex().catch(console.error)
 onmessage = function(e) {
   if (search) {
     postMessage({ e: 'query-ready', d: search(e.data.q) })
+  }
+}
+
+const langMap = {
+  ar: () => import('lunr-languages/lunr.ar.js'),
+  da: () => import('lunr-languages/lunr.da.js'),
+  de: () => import('lunr-languages/lunr.de.js'),
+  du: () => import('lunr-languages/lunr.du.js'),
+  el: () => import('lunr-languages/lunr.el.js'),
+  es: () => import('lunr-languages/lunr.es.js'),
+  fi: () => import('lunr-languages/lunr.fi.js'),
+  fr: () => import('lunr-languages/lunr.fr.js'),
+  he: () => import('lunr-languages/lunr.he.js'),
+  hi: () => import('lunr-languages/lunr.hi.js'),
+  hu: () => import('lunr-languages/lunr.hu.js'),
+  hy: () => import('lunr-languages/lunr.hy.js'),
+  it: () => import('lunr-languages/lunr.it.js'),
+  ja: () => import('lunr-languages/lunr.ja.js'),
+  jp: () => import('lunr-languages/lunr.jp.js'),
+  kn: () => import('lunr-languages/lunr.kn.js'),
+  ko: () => import('lunr-languages/lunr.ko.js'),
+  nl: () => import('lunr-languages/lunr.nl.js'),
+  no: () => import('lunr-languages/lunr.no.js'),
+  pt: () => import('lunr-languages/lunr.pt.js'),
+  ro: () => import('lunr-languages/lunr.ro.js'),
+  ru: () => import('lunr-languages/lunr.ru.js'),
+  sa: () => import('lunr-languages/lunr.sa.js'),
+  sv: () => import('lunr-languages/lunr.sv.js'),
+  ta: () => import('lunr-languages/lunr.ta.js'),
+  te: () => import('lunr-languages/lunr.te.js'),
+  th: () => import('lunr-languages/lunr.th.js'),
+  tr: () => import('lunr-languages/lunr.tr.js'),
+  vi: () => import('lunr-languages/lunr.vi.js')
+
+  // zh is currently not supported due to dependency on NodeJS.
+  // zh: () => import('lunr-languages/lunr.zh.js')
+}
+
+async function initLanguage(lang: string) {
+  if (lang !== 'en') {
+    const { default: init } = await langMap[lang]()
+    init(lunr)
   }
 }
