b272259f924667f5697b0fd630db10d8ff0744ac
Yufei Huang <yufeih@users.noreply.github.com>
2023-11-06 15:44:10 +0800

    chore: remove uneeded RunBuild exception handling

--- src/Docfx.App/RunBuild.cs
+++ src/Docfx.App/RunBuild.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Docfx.Build.Engine;
-using Docfx.Exceptions;
 using Docfx.Plugins;
 
 namespace Docfx;
@@ -26,44 +25,24 @@ internal static class RunBuild
 
         var templateManager = new TemplateManager(config.Template, config.Theme, configDirectory);
 
-        EnvironmentContext.SetGitFeaturesDisabled(config.DisableGitFeatures);
-        EnvironmentContext.SetBaseDirectory(Path.GetFullPath(string.IsNullOrEmpty(configDirectory) ? Directory.GetCurrentDirectory() : configDirectory));
-
-        // TODO: remove BaseDirectory from Config, it may cause potential issue when abused
-        var baseDirectory = EnvironmentContext.BaseDirectory;
+        var baseDirectory = Path.GetFullPath(string.IsNullOrEmpty(configDirectory) ? Directory.GetCurrentDirectory() : configDirectory);
         var outputFolder = Path.GetFullPath(Path.Combine(
             string.IsNullOrEmpty(outputDirectory) ? Path.Combine(baseDirectory, config.Output ?? "") : outputDirectory,
             config.Dest ?? ""));
 
+        EnvironmentContext.SetGitFeaturesDisabled(config.DisableGitFeatures);
+        EnvironmentContext.SetBaseDirectory(baseDirectory);
+
         try
         {
             DocumentBuilderWrapper.BuildDocument(config, options, templateManager, baseDirectory, outputFolder, null, null);
-        }
-        catch (AggregateException agg) when (agg.InnerException is DocfxException)
-        {
-            throw new DocfxException(agg.InnerException.Message);
-        }
-        catch (AggregateException agg) when (agg.InnerException is DocumentException)
-        {
-            throw new DocumentException(agg.InnerException.Message);
-        }
-        catch (DocfxException e)
-        {
-            throw new DocfxException(e.Message);
-        }
-        catch (DocumentException)
-        {
-            throw;
-        }
-        catch (Exception e)
-        {
-            throw new DocfxException(e.ToString());
-        }
 
-        templateManager.ProcessTheme(outputFolder, true);
-        // TODO: SEARCH DATA
-
-        EnvironmentContext.Clean();
+            templateManager.ProcessTheme(outputFolder, true);
+        }
+        finally
+        {
+            EnvironmentContext.Clean();
+        }
 
         return outputFolder;
     }
