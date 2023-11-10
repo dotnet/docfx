e6e390532ba8555237ec88b5185893bd110ec370
Yufei Huang <yufeih@users.noreply.github.com>
2023-11-10 12:21:49 +0800

    feat: add --verbose command line args for verbose logging, drops --repositoryRoot

--- src/Docfx.Common/Loggers/ReportLogListener.cs
+++ src/Docfx.Common/Loggers/ReportLogListener.cs
@@ -1,20 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
-
 namespace Docfx.Common;
 
 public sealed class ReportLogListener : ILoggerListener
 {
-    private readonly string _repoRoot;
-    private readonly string _root;
     private readonly StreamWriter _writer;
 
-    private const LogLevel LogLevelThreshold = LogLevel.Diagnostic;
-
-    public ReportLogListener(string reportPath, string repoRoot, string root)
+    public ReportLogListener(string reportPath)
     {
         var dir = Path.GetDirectoryName(reportPath);
         if (!string.IsNullOrEmpty(dir))
@@ -22,31 +15,21 @@ public sealed class ReportLogListener : ILoggerListener
             Directory.CreateDirectory(dir);
         }
         _writer = new StreamWriter(reportPath, true);
-        _repoRoot = repoRoot;
-        _root = root;
     }
 
     public void WriteLine(ILogItem item)
     {
         ArgumentNullException.ThrowIfNull(item);
 
-        var level = item.LogLevel;
-        var message = item.Message;
-        var file = item.File;
-        var line = item.Line;
-        if (level < LogLevelThreshold) return;
-
-        var reportItem = new ReportItem
+        _writer.WriteLine(JsonUtility.Serialize(new
         {
-            Severity = GetSeverity(level),
-            Message = message,
-            File = TransformFile(file),
-            Line = line,
-            DateTime = DateTime.UtcNow,
-            Code = item.Code,
-        };
-
-        _writer.WriteLine(JsonUtility.Serialize(reportItem));
+            severity = item.LogLevel,
+            message = item.Message,
+            file = item.File,
+            line = item.Line,
+            date_time = DateTime.UtcNow,
+            code = item.Code,
+        }));
     }
 
     public void Dispose()
@@ -54,83 +37,8 @@ public sealed class ReportLogListener : ILoggerListener
         _writer.Dispose();
     }
 
-    private static MessageSeverity GetSeverity(LogLevel level)
-    {
-        switch (level)
-        {
-            case LogLevel.Diagnostic:
-                return MessageSeverity.Diagnostic;
-            case LogLevel.Verbose:
-                return MessageSeverity.Verbose;
-            case LogLevel.Info:
-                return MessageSeverity.Info;
-            case LogLevel.Suggestion:
-                return MessageSeverity.Suggestion;
-            case LogLevel.Warning:
-                return MessageSeverity.Warning;
-            case LogLevel.Error:
-                return MessageSeverity.Error;
-            default:
-                throw new NotSupportedException(level.ToString());
-        }
-    }
-
-    private string TransformFile(string fileFromRoot)
-    {
-        if (fileFromRoot == null)
-        {
-            return null;
-        }
-        if (string.IsNullOrEmpty(_repoRoot))
-        {
-            return fileFromRoot;
-        }
-
-        string file = ((RelativePath)fileFromRoot).RemoveWorkingFolder();
-        string basePath = Path.GetFullPath(_repoRoot);
-        string fullPath = Path.GetFullPath(Path.Combine(_root, file));
-        return PathUtility.MakeRelativePath(basePath, fullPath);
-    }
-
     public void Flush()
     {
         _writer.Flush();
-    }
-
-    public class ReportItem
-    {
-        [JsonProperty("message")]
-        [JsonPropertyName("message")]
-        public string Message { get; set; }
-
-        [JsonProperty("file")]
-        [JsonPropertyName("file")]
-        public string File { get; set; }
-
-        [JsonProperty("line")]
-        [JsonPropertyName("line")]
-        public string Line { get; set; }
-
-        [JsonProperty("date_time")]
-        [JsonPropertyName("date_time")]
-        public DateTime DateTime { get; set; }
-
-        [JsonProperty("message_severity")]
-        [JsonPropertyName("message_severity")]
-        public MessageSeverity Severity { get; set; }
-
-        [JsonProperty("code")]
-        [JsonPropertyName("code")]
-        public string Code { get; set; }
-    }
-
-    public enum MessageSeverity
-    {
-        Error,
-        Warning,
-        Suggestion,
-        Info,
-        Verbose,
-        Diagnostic
     }
 }
