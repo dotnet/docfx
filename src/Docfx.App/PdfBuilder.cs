7e2f5ef25bf07044066f2582f4fee0356e799d33
David Vreony <dpvreony@users.noreply.github.com>
2023-12-01 00:16:52 +0000

    FIX: Docset Pdf call terminating early (#9493)
    * add logging around toc search
    
    * fix exec not waiting for async pdf builder

--- src/Docfx.App/PdfBuilder.cs
+++ src/Docfx.App/PdfBuilder.cs
@@ -47,16 +47,19 @@ static class PdfBuilder
         var outputFolder = Path.GetFullPath(Path.Combine(
             string.IsNullOrEmpty(outputDirectory) ? Path.Combine(configDirectory, config.Output ?? "") : outputDirectory,
             config.Dest ?? ""));
-
         return CreatePdf(outputFolder);
     }
 
     public static async Task CreatePdf(string outputFolder)
     {
         var stopwatch = Stopwatch.StartNew();
+        Logger.LogInfo($"Searching for manifest in {outputFolder}");
         var pdfTocs = GetPdfTocs().ToDictionary(p => p.url, p => p.toc);
         if (pdfTocs.Count == 0)
+        {
+            Logger.LogWarning($"No PDF TOC's found in: {outputFolder}");
             return;
+        }
 
         Program.Main(new[] { "install", "chromium" });
 
