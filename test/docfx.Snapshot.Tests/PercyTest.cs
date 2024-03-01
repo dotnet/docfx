0d638b53e6fcb134f5dd371696f7b732fcc96e76
Yufei Huang <yufeih@live.com>
2024-03-01 10:27:57 +0800

    feat: percy snapshot (#9747)

---
+++ test/docfx.Snapshot.Tests/PercyTest.cs
@@ -0,0 +1,138 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using Docfx.Common;
+using Microsoft.Playwright;
+
+namespace Docfx.Tests;
+
+[Trait("Stage", "Percy")]
+public class PercyTest
+{
+    private class PercyFactAttribute : FactAttribute
+    {
+        public PercyFactAttribute()
+        {
+            Skip = IsActiveLocalTcpPort(5338) ? null : "Run percy tests with `percy exec`";
+        }
+    }
+
+    private static readonly string s_samplesDir = Path.GetFullPath("../../../../../samples");
+
+    private static readonly string[] s_screenshotUrls =
+    [
+        "index.html",
+        "articles/markdown.html?tabs=windows%2Ctypescript#markdown-extensions",
+        "articles/markdown.html?dark",
+        "articles/csharp_coding_standards.html",
+        "api/BuildFromProject.Class1.html",
+        "api/CatLibrary.html?dark",
+        "api/CatLibrary.html?term=cat",
+        "api/CatLibrary.Cat-2.html?q=cat",
+        "restapi/petstore.html",
+    ];
+
+    static PercyTest()
+    {
+        Microsoft.Playwright.Program.Main(["install", "chromium"]);
+    }
+
+    [PercyFact]
+    public async Task SeedHtml()
+    {
+        var samplePath = $"{s_samplesDir}/seed";
+        Clean(samplePath);
+
+        var docfxPath = Path.GetFullPath(OperatingSystem.IsWindows() ? "docfx.exe" : "docfx");
+        Assert.Equal(0, Exec(docfxPath, $"metadata {samplePath}/docfx.json"));
+        Assert.Equal(0, Exec(docfxPath, $"build {samplePath}/docfx.json"));
+
+        const int port = 8089;
+        var _ = Task.Run(() => Program.Main(["serve", "--port", $"{port}", $"{samplePath}/_site"]))
+                    .ContinueWith(x =>
+                    {
+                        Logger.LogError("Failed to run `dotnet serve` command. " + x.Exception.ToString());
+                    }, TaskContinuationOptions.OnlyOnFaulted);
+
+        // Wait until web server started.
+        bool isStarted = SpinWait.SpinUntil(() => { Thread.Sleep(100); return IsActiveLocalTcpPort(port); }, TimeSpan.FromSeconds(10));
+
+        using var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync(new());
+
+        foreach (var url in s_screenshotUrls)
+        {
+            await page.GotoAsync($"http://localhost:{port}/{url}");
+            await page.WaitForFunctionAsync("window.docfx.ready");
+            await page.WaitForFunctionAsync("window.docfx.searchReady");
+
+            if (url.Contains("?dark"))
+            {
+                await page.EvaluateAsync($"() => document.documentElement.setAttribute('data-bs-theme', 'dark')");
+            }
+
+            await Task.Delay(200);
+
+            if (url.Contains("?term=cat"))
+            {
+                await (await page.QuerySelectorAsync("#search-query")).FillAsync("cat");
+                await page.WaitForFunctionAsync("window.docfx.searchResultReady");
+            }
+
+            var name = $"{Regex.Replace(url, "[^a-zA-Z0-9-_.]", "-")}";
+            await PercySnapshot(page, name);
+        }
+
+        await page.CloseAsync();
+    }
+
+    private static async Task PercySnapshot(IPage page, string name)
+    {
+        using (var http = new HttpClient())
+        {
+            // https://www.browserstack.com/docs/percy/integrate/build-your-sdk
+            // # Step 2: Fetch and inject the DOM JavaScript into the browser
+            var domjs = await http.GetStringAsync("http://localhost:5338/percy/dom.js");
+            await page.AddScriptTagAsync(new() { Content = domjs });
+            var domSnapshot = await page.EvaluateAsync("PercyDOM.serialize()");
+            var res = await http.PostAsJsonAsync("http://localhost:5338/percy/snapshot", new
+            {
+                domSnapshot,
+                url = page.Url,
+                name
+            });
+        }
+    }
+
+    private static int Exec(string filename, string args, string workingDirectory = null)
+    {
+        var psi = new ProcessStartInfo(filename, args);
+        psi.EnvironmentVariables.Add("DOCFX_SOURCE_BRANCH_NAME", "main");
+        if (workingDirectory != null)
+            psi.WorkingDirectory = Path.GetFullPath(workingDirectory);
+        var process = Process.Start(psi);
+        process.WaitForExit();
+        return process.ExitCode;
+    }
+
+    private static void Clean(string samplePath)
+    {
+        if (Directory.Exists($"{samplePath}/_site"))
+            Directory.Delete($"{samplePath}/_site", recursive: true);
+
+        if (Directory.Exists($"{samplePath}/_site_pdf"))
+            Directory.Delete($"{samplePath}/_site_pdf", recursive: true);
+    }
+
+    private static bool IsActiveLocalTcpPort(int port)
+    {
+        var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+        var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+        return tcpConnInfoArray.Any(x => x.Port == port);
+    }
+}
--- test/docfx.Snapshot.Tests/SamplesTest.cs
+++ test/docfx.Snapshot.Tests/SamplesTest.cs
@@ -1,19 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Net.NetworkInformation;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
-using Docfx.Common;
 using Docfx.Dotnet;
-using ImageMagick;
-using Microsoft.Playwright;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Actions;
 using UglyToad.PdfPig.Annotations;
@@ -34,29 +28,8 @@ public class SamplesTest
 
     private static readonly string s_samplesDir = Path.GetFullPath("../../../../../samples");
 
-    private static readonly string[] s_screenshotUrls = new[]
-    {
-        "index.html",
-        "articles/markdown.html?tabs=windows%2Ctypescript#markdown-extensions",
-        "articles/csharp_coding_standards.html",
-        "api/BuildFromProject.Class1.html",
-        "api/CatLibrary.html",
-        "api/CatLibrary.html?term=cat",
-        "api/CatLibrary.Cat-2.html?q=cat",
-        "restapi/petstore.html",
-    };
-
-    private static readonly (int width, int height, string theme, bool fullPage)[] s_viewports = new[]
-    {
-        (1920, 1080, "light", true),
-        (1152, 648, "light", false),
-        (768, 600, "dark", false),
-        (375, 812, "dark", true),
-    };
-
     static SamplesTest()
     {
-        Microsoft.Playwright.Program.Main(new[] { "install", "chromium" });
         Process.Start("dotnet", $"build \"{s_samplesDir}/seed/dotnet/assembly/BuildFromAssembly.csproj\"").WaitForExit();
     }
 
@@ -120,114 +93,6 @@ public class SamplesTest
                     UriBookmarkNode d => new { node.Title, Children = ToBookmarks(node.Children), d.Uri },
                 }).ToArray();
             }
-        }
-    }
-
-    [SnapshotFact]
-    public async Task SeedHtml()
-    {
-        if (!OperatingSystem.IsLinux())
-            return;
-
-        var samplePath = $"{s_samplesDir}/seed";
-        Clean(samplePath);
-
-        var docfxPath = Path.GetFullPath(OperatingSystem.IsWindows() ? "docfx.exe" : "docfx");
-        Assert.Equal(0, Exec(docfxPath, $"metadata {samplePath}/docfx.json"));
-        Assert.Equal(0, Exec(docfxPath, $"build {samplePath}/docfx.json"));
-
-        const int port = 8089;
-        var _ = Task.Run(() => Program.Main(new[] { "serve", "--port", $"{port}", $"{samplePath}/_site" }))
-                    .ContinueWith(x =>
-                    {
-                        Logger.LogError("Failed to run `dotnet serve` command. " + x.Exception.ToString());
-                    }, TaskContinuationOptions.OnlyOnFaulted);
-
-        // Wait until web server started.
-        bool isStarted = SpinWait.SpinUntil(() => { Thread.Sleep(100); return IsActiveLocalTcpPort(port); }, TimeSpan.FromSeconds(10));
-
-        using var playwright = await Playwright.CreateAsync();
-        var browser = await playwright.Chromium.LaunchAsync();
-        var htmlUrls = new ConcurrentDictionary<string, string>();
-
-        await Parallel.ForEachAsync(s_viewports, async (viewport, _) =>
-        {
-            var (width, height, theme, fullPage) = viewport;
-            var isMobile = width < 500;
-            var page = await browser.NewPageAsync(new()
-            {
-                ViewportSize = new() { Width = width, Height = height },
-                IsMobile = isMobile,
-                HasTouch = isMobile,
-                ReducedMotion = ReducedMotion.Reduce,
-            });
-
-            foreach (var url in s_screenshotUrls)
-            {
-                await page.GotoAsync($"http://localhost:{port}/{url}");
-                await page.WaitForFunctionAsync("window.docfx.ready");
-                await page.WaitForFunctionAsync("window.docfx.searchReady");
-                await page.EvaluateAsync($"() => document.documentElement.setAttribute('data-bs-theme', '{theme}')");
-                await Task.Delay(200);
-
-                if (url.Contains("?term=cat"))
-                {
-                    if (isMobile)
-                    {
-                        await (await page.QuerySelectorAsync("[data-bs-target='#navpanel']")).ClickAsync();
-                        await page.WaitForSelectorAsync("#navpanel.show");
-                    }
-
-                    await (await page.QuerySelectorAsync("#search-query")).FillAsync("cat");
-                    await page.WaitForFunctionAsync("window.docfx.searchResultReady");
-                }
-
-                var directory = $"{nameof(SamplesTest)}.{nameof(SeedHtml)}/{width}x{height}";
-                var fileName = $"{Regex.Replace(url, "[^a-zA-Z0-9-_.]", "-")}";
-
-                // Verify HTML files once
-                if (theme is "light" && htmlUrls.TryAdd(url, url))
-                {
-                    var html = await page.ContentAsync();
-                    await
-                        Verify(new Target("html", NormalizeHtml(html)))
-                        .UseDirectory($"{nameof(SamplesTest)}.{nameof(SeedHtml)}/html")
-                        .UseFileName(fileName)
-                        .AutoVerify(includeBuildServer: false);
-                }
-
-                var bytes = await page.ScreenshotAsync(new() { FullPage = fullPage });
-                await
-                    Verify(new Target("png", new MemoryStream(bytes)))
-                    .UseStreamComparer((received, verified, _) => CompareImage(received, verified, fileName))
-                    .UseDirectory(directory)
-                    .UseFileName(fileName)
-                    .AutoVerify(includeBuildServer: false);
-            }
-
-            await page.CloseAsync();
-        });
-
-        static Task<CompareResult> CompareImage(Stream received, Stream verified, string fileName)
-        {
-            using var receivedImage = new MagickImage(received);
-            using var verifiedImage = new MagickImage(verified);
-            using var diffImage = new MagickImage();
-            var diff = receivedImage.Compare(verifiedImage, ErrorMetric.Fuzz, diffImage);
-            if (diff <= 0.001)
-            {
-                return Task.FromResult(CompareResult.Equal);
-            }
-
-            return Task.FromResult(CompareResult.NotEqual($"Image diff: {diff}"));
-        }
-
-        static string NormalizeHtml(string html)
-        {
-            html = Regex.Replace(html, "<!--.*?-->", "");
-            html = Regex.Replace(html, @"mermaid-\d+", "");
-            html = Regex.Replace(html, @"flowchart-\w+-\d", "");
-            return html;
         }
     }
 
@@ -322,12 +187,5 @@ public class SamplesTest
                 Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
             }));
         }
-    }
-
-    private static bool IsActiveLocalTcpPort(int port)
-    {
-        var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
-        var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
-        return tcpConnInfoArray.Any(x => x.Port == port);
     }
 }
