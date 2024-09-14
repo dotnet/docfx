cf7bf6c6a93cf806d3f15b06853775faeee76072
filzrev <103790468+filzrev@users.noreply.github.com>
2024-09-14 13:11:47 +0900

    chore: switch XRefMap serialization logics to use System.Text.Json

---
+++ test/docfx.Tests/SerializationTests/YamlSerializationTest.XRefMap.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Docfx.Build.ApiPage;
+using Docfx.Build.Engine;
+using Docfx.Build.ManagedReference;
+using Docfx.DataContracts.ManagedReference;
+using Docfx.Plugins;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Xunit.Sdk;
+
+namespace docfx.Tests;
+
+public partial class YamlSerializationTest
+{
+    [Theory]
+    [TestData<XRefMap>]
+    public void YamlSerializationTest_XRefMap(string path)
+    {
+        // Arrange
+        var model = TestData.Load<XRefMap>(path);
+
+        // Act/Assert
+        ValidateYamlRoundTrip(model);
+        ValidateYamlJsonRoundTrip(model);
+    }
+}
