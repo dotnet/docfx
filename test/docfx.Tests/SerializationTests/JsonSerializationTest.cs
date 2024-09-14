6fe004a4059c8cd1f7fa91499b523da49e87169c
filzrev <103790468+filzrev@users.noreply.github.com>
2024-09-14 13:09:55 +0900

    chore: add json/yaml serialization test infrastructure

---
+++ test/docfx.Tests/SerializationTests/JsonSerializationTest.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json;
+using Docfx;
+using Docfx.Common;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
+
+namespace docfx.Tests;
+
+public partial class JsonSerializationTest
+{
+    /// <summary>
+    /// Helper method to validate serialize/deserialize results.
+    /// </summary>
+    protected void ValidateJsonRoundTrip<T>(T model)
+    {
+        // 1. Validate serialized result.
+        var newtonsoftJson = NewtonsoftJsonUtility.Serialize(model);
+        var systemTextJson = SystemTextJsonUtility.Serialize(model);
+        systemTextJson.Should().Be(newtonsoftJson);
+
+        // 2. Validate deserialized result.
+        var json = systemTextJson;
+        var systemTextJsonModel = SystemTextJsonUtility.Deserialize<T>(json);
+        var newtonsoftJsonModel = NewtonsoftJsonUtility.Deserialize<T>(new StringReader(json));
+
+        // Assert
+        systemTextJsonModel.Should().BeEquivalentTo(model, assertionOptions);
+        newtonsoftJsonModel.Should().BeEquivalentTo(model, assertionOptions);
+
+        // Helper local function
+        static EquivalencyAssertionOptions<T> assertionOptions(EquivalencyAssertionOptions<T> opt)
+        {
+            // By default. JsonElement is compared by reference because JsonElement don't override Equals.
+            return opt.ComparingByMembers<JsonElement>();
+        }
+    }
+}
---
+++ test/docfx.Tests/SerializationTests/TestData/TestData.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Text.Json;
+using Docfx.Build.ApiPage;
+using Docfx.Common;
+using Docfx.Tests;
+using YamlDotNet.Serialization;
+
+namespace docfx.Tests;
+
+public static class TestData
+{
+    /// <summary>
+    /// Load test data from specified path.
+    /// </summary>
+    public static T Load<T>(string path)
+    {
+        if (typeof(T) == typeof(ApiPage))
+            throw new NotSupportedException(); // It should be handled separately.
+
+        var testDataPath = PathHelper.ResolveTestDataPath(path);
+
+        switch (Path.GetExtension(path))
+        {
+            case ".yml":
+                return YamlUtility.Deserialize<T>(testDataPath);
+            case ".json":
+                return JsonUtility.Deserialize<T>(testDataPath);
+            default:
+                throw new NotSupportedException();
+        }
+    }
+
+    /// <summary>
+    /// Gets test data relative file paths.
+    /// </summary>
+    public static string[] GetTestDataFilePaths(string key)
+    {
+        var testDataRootDir = PathHelper.GetTestDataDirectory();
+        var basePathLength = testDataRootDir.Length + 1;
+
+        var testDataDir = Path.Combine(testDataRootDir, key);
+        var directoryInfo = new DirectoryInfo(testDataDir);
+
+        // Gets TestData directory relative paths
+        var relativePaths = directoryInfo.EnumerateFiles()
+                                         .Select(x => x.FullName)
+                                         .Select(x => x.Substring(basePathLength));
+
+        return relativePaths.ToArray();
+    }
+}
