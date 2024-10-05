657b118b6b8213e492d70c76af13ea2429656117
filzrev <103790468+filzrev@users.noreply.github.com>
2024-10-05 15:18:25 +0900

    chore: add ListWithStringFallback converters and JSON roundtrip test

---
+++ test/docfx.Tests/SerializationTests/JsonSerializationTest.ListWithStringFallback.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Docfx;
+using Docfx.Common;
+using FluentAssertions;
+
+namespace docfx.Tests;
+
+public partial class JsonSerializationTest
+{
+    [Theory]
+    [TestData<ListWithStringFallback>]
+    public void JsonSerializationTest_ListWithStringFallback(string path)
+    {
+        // Arrange
+        var model = TestData.Load<ListWithStringFallback>(path);
+
+        // Act/Assert
+        ValidateJsonRoundTrip(model);
+    }
+}
