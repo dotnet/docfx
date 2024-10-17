01dcf696f12b504c074fe34241dc99c7a58ab402
Simon Cropp <simon.cropp@gmail.com>
2024-10-17 20:57:51 +1100

    replace TryGetValue+Remove with Remove+out

--- src/Docfx.Build.ManagedReference/SplitClassPageToMemberLevel.cs
+++ src/Docfx.Build.ManagedReference/SplitClassPageToMemberLevel.cs
@@ -66,9 +66,8 @@ public class SplitClassPageToMemberLevel : BaseDocumentBuildStep
 
         foreach (var dupeModel in dupeModels)
         {
-            if (modelsDict.TryGetValue(dupeModel.File, out var dupe))
+            if (modelsDict.Remove(dupeModel.File, out var dupe))
             {
-                modelsDict.Remove(dupeModel.File);
                 RenewDupeFileModels(dupe, newFilePaths, modelsDict);
             }
             RenewDupeFileModels(dupeModel, newFilePaths, modelsDict);
@@ -120,7 +119,7 @@ public class SplitClassPageToMemberLevel : BaseDocumentBuildStep
             }
             else
             {
-                // new file path already exist but doesn't have suffix (special case) 
+                // new file path already exist but doesn't have suffix (special case)
                 newFileName += "_1";
                 newFilePaths[newFilePath] = 2;
             }
