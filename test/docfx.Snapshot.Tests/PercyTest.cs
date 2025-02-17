b49e42a31fde1ebf9f2f37a72ad3e8639766f0c8
filzrev <103790468+filzrev@users.noreply.github.com>
2025-02-17 14:39:47 +0900

    chore: update xunit version to v3

--- test/docfx.Snapshot.Tests/PercyTest.cs
+++ test/docfx.Snapshot.Tests/PercyTest.cs
@@ -43,13 +43,13 @@ public class PercyTest
     }
 
     [PercyFact]
+    [UseCustomBranchName("main")]
     public async Task SeedHtml()
     {
         var samplePath = $"{s_samplesDir}/seed";
         Clean(samplePath);
 
-        using var process = Process.Start("dotnet", $"build \"{s_samplesDir}/seed/dotnet/assembly/BuildFromAssembly.csproj\"");
-        await process.WaitForExitAsync();
+        Exec("dotnet", $"build \"{s_samplesDir}/seed/dotnet/assembly/BuildFromAssembly.csproj\"");
 
         var docfxPath = Path.GetFullPath(OperatingSystem.IsWindows() ? "docfx.exe" : "docfx");
         Assert.Equal(0, Exec(docfxPath, $"metadata {samplePath}/docfx.json"));
@@ -115,13 +115,14 @@ public class PercyTest
 
     private static int Exec(string filename, string args, string workingDirectory = null)
     {
-        var psi = new ProcessStartInfo(filename, args);
-        psi.EnvironmentVariables.Add("DOCFX_SOURCE_BRANCH_NAME", "main");
-        if (workingDirectory != null)
-            psi.WorkingDirectory = Path.GetFullPath(workingDirectory);
-        using var process = Process.Start(psi);
-        process.WaitForExit();
-        return process.ExitCode;
+        var execTask = ProcessHelper.ExecAsync(
+            filename,
+            args,
+            workingDirectory,
+            environmentVariables: [],
+            TestContext.Current.CancellationToken);
+
+        return execTask.GetAwaiter().GetResult();
     }
 
     private static void Clean(string samplePath)
