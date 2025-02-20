f70ab076cfe91143c800c8b0b5a8ef347cae89fb
filzrev <103790468+filzrev@users.noreply.github.com>
2025-02-20 12:23:57 +0900

    chore: Update YamlDotNet version to 16.3.0 (#10541)
    * chore: update yamldotnet version
    
    * chore: update ITypeInspector's derived method
    
    * chore: update IPropertyDescriptor derived methods
    
    * chore: update YamlDeserializer
    
    * chore: update YamlSerializer
    
    * chore: update IObjectGraphTraversalStrategy derived methods
    
    * chore: update INodeDeserializer derived methods
    
    * chore: update IObjectGraphVisitor derived methods

--- src/Docfx.YamlSerialization/YamlSerializer.cs
+++ src/Docfx.YamlSerialization/YamlSerializer.cs
@@ -11,6 +11,7 @@ using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.EventEmitters;
 using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization.ObjectFactories;
 using YamlDotNet.Serialization.ObjectGraphVisitors;
 using YamlDotNet.Serialization.TypeInspectors;
 using YamlDotNet.Serialization.TypeResolvers;
@@ -23,11 +24,15 @@ public class YamlSerializer
     private readonly SerializationOptions _options;
     private readonly INamingConvention _namingConvention;
     private readonly ITypeResolver _typeResolver;
+    private readonly ITypeInspector _typeDescriptor;
+    private readonly IObjectFactory _objectFactory;
+    private readonly Settings _settings;
 
-    public YamlSerializer(SerializationOptions options = SerializationOptions.None, INamingConvention? namingConvention = null)
+    public YamlSerializer(SerializationOptions options = SerializationOptions.None, INamingConvention? namingConvention = null, Settings? settings = null)
     {
         _options = options;
         _namingConvention = namingConvention ?? NullNamingConvention.Instance;
+        _settings = settings ?? new Settings();
 
         Converters = new List<IYamlTypeConverter>();
         foreach (IYamlTypeConverter yamlTypeConverter in YamlTypeConverters.BuiltInConverters)
@@ -38,6 +43,9 @@ public class YamlSerializer
         _typeResolver = IsOptionSet(SerializationOptions.DefaultToStaticType)
             ? new StaticTypeResolver()
             : new DynamicTypeResolver();
+
+        _typeDescriptor = CreateTypeInspector();
+        _objectFactory = new DefaultObjectFactory();
     }
 
     private bool IsOptionSet(SerializationOptions option)
@@ -66,7 +74,7 @@ public class YamlSerializer
         emitter.Emit(new StreamStart());
         emitter.Emit(new DocumentStart());
 
-        traversalStrategy.Traverse(graph, emittingVisitor, emitter);
+        traversalStrategy.Traverse(graph, emittingVisitor, emitter, GetObjectSerializer(emitter));
 
         emitter.Emit(new DocumentEnd(true));
         emitter.Emit(new StreamEnd());
@@ -76,6 +84,8 @@ public class YamlSerializer
     {
         IObjectGraphVisitor<IEmitter> emittingVisitor = new EmittingObjectGraphVisitor(eventEmitter);
 
+        emittingVisitor = new CustomSerializationObjectGraphVisitor(emittingVisitor, Converters, GetObjectSerializer(emitter));
+
         void nestedObjectSerializer(object? v, Type? t = null) => SerializeValue(emitter, v, t);
 
         emittingVisitor = new CustomSerializationObjectGraphVisitor(emittingVisitor, Converters, nestedObjectSerializer);
@@ -83,7 +93,8 @@ public class YamlSerializer
         if (!IsOptionSet(SerializationOptions.DisableAliases))
         {
             var anchorAssigner = new AnchorAssigner(Converters);
-            traversalStrategy.Traverse(graph, anchorAssigner, default);
+
+            traversalStrategy.Traverse(graph, anchorAssigner, default, GetObjectSerializer(emitter));
 
             emittingVisitor = new AnchorAssigningObjectGraphVisitor(emittingVisitor, eventEmitter, anchorAssigner);
         }
@@ -110,7 +121,12 @@ public class YamlSerializer
             graph
         );
 
-        traversalStrategy.Traverse(graph, emittingVisitor, emitter);
+        traversalStrategy.Traverse(graph, emittingVisitor, emitter, GetObjectSerializer(emitter));
+    }
+
+    private ObjectSerializer GetObjectSerializer(IEmitter emitter)
+    {
+        return (object? v, Type? t = null) => SerializeValue(emitter, v, t);
     }
 
     private IEventEmitter CreateEventEmitter()
@@ -119,7 +135,7 @@ public class YamlSerializer
 
         if (IsOptionSet(SerializationOptions.JsonCompatible))
         {
-            return new JsonEventEmitter(writer, YamlFormatter.Default, NullNamingConvention.Instance);
+            return new JsonEventEmitter(writer, YamlFormatter.Default, NullNamingConvention.Instance, _typeDescriptor);
         }
         else
         {
@@ -130,29 +146,28 @@ public class YamlSerializer
                 quoteYaml1_1Strings: false,
                 defaultScalarStyle: ScalarStyle.Any,
                 formatter: YamlFormatter.Default,
-                enumNamingConvention: NullNamingConvention.Instance);
+                enumNamingConvention: NullNamingConvention.Instance,
+                typeInspector: _typeDescriptor);
         }
     }
 
     private IObjectGraphTraversalStrategy CreateTraversalStrategy()
     {
+        return IsOptionSet(SerializationOptions.Roundtrip)
+            ? new RoundtripObjectGraphTraversalStrategy(Converters, this, _typeDescriptor, _typeResolver, 50, _namingConvention, _settings, _objectFactory)
+            : new FullObjectGraphTraversalStrategy(this, _typeDescriptor, _typeResolver, 50, _namingConvention, _objectFactory);
+    }
+
+    private ITypeInspector CreateTypeInspector()
+    {
         ITypeInspector typeDescriptor = new EmitTypeInspector(_typeResolver);
         if (IsOptionSet(SerializationOptions.Roundtrip))
-        {
             typeDescriptor = new ReadableAndWritablePropertiesTypeInspector(typeDescriptor);
-        }
 
         typeDescriptor = new NamingConventionTypeInspector(typeDescriptor, _namingConvention);
         typeDescriptor = new YamlAttributesTypeInspector(typeDescriptor);
 
-        if (IsOptionSet(SerializationOptions.Roundtrip))
-        {
-            return new RoundtripObjectGraphTraversalStrategy(this, typeDescriptor, _typeResolver, 50);
-        }
-        else
-        {
-            return new FullObjectGraphTraversalStrategy(this, typeDescriptor, _typeResolver, 50, _namingConvention);
-        }
+        return typeDescriptor;
     }
 }
 
