dotnet/docfx

commit ec959b5c537ea52b9772d788bebbb78d98d53799

feat: expose type through the toc (#10090)

templates/default/toc.html.primary.js
@@ -51,6 +51,11 @@ function transformMemberPage(model) {
       "event":       { key: "eventsInSubtitle" },
       "operator":    { key: "operatorsInSubtitle" },
       "eii":         { key: "eiisInSubtitle" },
+      "class":       { key: "classesInSubtitle" },
+      "struct":      { key: "structsInSubtitle" },
+      "enum":        { key: "enumsInSubtitle" },
+      "interface":   { key: "interfacesInSubtitle" },
+      "namespace":   { key: "namespacesInSubtitle" },
   };
 
   groupChildren(model);

templates/default/toc.json.js
@@ -36,6 +36,11 @@ exports.transform = function (model) {
         "event":       { key: "eventsInSubtitle" },
         "operator":    { key: "operatorsInSubtitle" },
         "eii":         { key: "eiisInSubtitle" },
+        "class":       { key: "classesInSubtitle" },
+        "struct":      { key: "structsInSubtitle" },
+        "enum":        { key: "enumsInSubtitle" },
+        "interface":   { key: "interfacesInSubtitle" },
+        "namespace":   { key: "namespacesInSubtitle" },
     };
   
     groupChildren(model);

src/Docfx.DataContracts.Common/TocItemViewModel.cs
@@ -92,6 +92,11 @@ public class TocItemViewModel
     [JsonPropertyName("order")]
     public int? Order { get; set; }
 
+    [YamlMember(Alias = Constants.PropertyName.Type)]
+    [JsonProperty(Constants.PropertyName.Type)]
+    [JsonPropertyName(Constants.PropertyName.Type)]
+    public string Type { get; set; }
+
     [YamlIgnore]
     [Newtonsoft.Json.JsonIgnore]
     [System.Text.Json.Serialization.JsonIgnore]

src/Docfx.Dotnet/YamlViewModelExtensions.cs
@@ -134,6 +134,7 @@ internal static class YamlViewModelExtensions
         {
             Uid = item.Name,
             Name = item.DisplayNames.GetLanguageProperty(SyntaxLanguage.Default),
+            Type = item.Type.ToString(),
         };
 
         if (item.Type is MemberType.Namespace)

test/docfx.Tests/Api.verified.cs
@@ -2690,6 +2690,10 @@ namespace Docfx.DataContracts.Common
         [System.Text.Json.Serialization.JsonPropertyName("topicUid")]
         [YamlDotNet.Serialization.YamlMember(Alias="topicUid")]
         public string TopicUid { get; set; }
+        [Newtonsoft.Json.JsonProperty("type")]
+        [System.Text.Json.Serialization.JsonPropertyName("type")]
+        [YamlDotNet.Serialization.YamlMember(Alias="type")]
+        public string Type { get; set; }
         [Newtonsoft.Json.JsonProperty("uid")]
         [System.Text.Json.Serialization.JsonPropertyName("uid")]
         [YamlDotNet.Serialization.YamlMember(Alias="uid")]

commit f75dba98c899326ac21051ee7916ab9d2ef45938

fix: toc grouping regression introduced by #10090

templates/default/toc.html.primary.js
@@ -59,6 +59,16 @@ function transformMemberPage(model) {
       "delegate":    { key: "delegatesInSubtitle" },
   };
 
+  var groupBy = [
+    "constructor",
+    "field",
+    "property",
+    "method",
+    "event",
+    "operator",
+    "eii",
+  ];
+
   groupChildren(model);
   transformItem(model, 1);
   return model;
@@ -71,7 +81,7 @@ function transformMemberPage(model) {
       var items = [];
       item.items.forEach(function (element) {
           groupChildren(element);
-          if (element.type) {
+          if (element.type && groupBy.includes(element.type)) {
               var type = element.isEii ? "eii" : element.type.toLowerCase();
               if (!grouped.hasOwnProperty(type)) {
                   if (!groupNames.hasOwnProperty(type)) {

templates/default/toc.json.js
@@ -15,18 +15,18 @@ exports.transform = function (model) {
     if (extension && extension.postTransform) {
       model = extension.postTransform(model);
     }
-  
+
     for (var key in model) {
       if (key[0] === '_') {
         delete model[key]
       }
     }
-  
+
     return {
       content: JSON.stringify(model)
     };
   }
-  
+
   function transformMemberPage(model) {
     var groupNames = {
         "constructor": { key: "constructorsInSubtitle" },
@@ -43,11 +43,21 @@ exports.transform = function (model) {
         "namespace":   { key: "namespacesInSubtitle" },
         "delegate":    { key: "delegatesInSubtitle" },
     };
-  
+
+    var groupBy = [
+      "constructor",
+      "field",
+      "property",
+      "method",
+      "event",
+      "operator",
+      "eii",
+    ];
+
     groupChildren(model);
     transformItem(model, 1);
     return model;
-  
+
     function groupChildren(item) {
         if (!item || !item.items || item.items.length == 0) {
             return;
@@ -56,7 +66,7 @@ exports.transform = function (model) {
         var items = [];
         item.items.forEach(function (element) {
             groupChildren(element);
-            if (element.type) {
+            if (element.type && groupBy.includes(element.type)) {
                 var type = element.isEii ? "eii" : element.type.toLowerCase();
                 if (!grouped.hasOwnProperty(type)) {
                     if (!groupNames.hasOwnProperty(type)) {
@@ -72,7 +82,7 @@ exports.transform = function (model) {
                 items.push(element);
             }
         }, this);
-  
+
         // With order defined in groupNames
         for (var key in groupNames) {
             if (groupNames.hasOwnProperty(key) && grouped.hasOwnProperty(key)) {
@@ -82,18 +92,18 @@ exports.transform = function (model) {
                 })
             }
         }
-  
+
         item.items = items;
     }
-  
+
     function transformItem(item, level) {
         // set to null in case mustache looks up
         item.topicHref = item.topicHref || null;
         item.tocHref = item.tocHref || null;
         item.name = item.name || null;
-  
+
         item.level = level;
-  
+
         if (item.items && item.items.length > 0) {
             item.leaf = false;
             var length = item.items.length;
@@ -106,4 +116,3 @@ exports.transform = function (model) {
         }
     }
   }
-  