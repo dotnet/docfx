f882667450a63ed1ad2dbf378e6d740ecb391ccd
filzrev <103790468+filzrev@users.noreply.github.com>
2026-01-14 19:36:21 +0900

    deps: update JsonSchema.Net dependency to 8.x

--- src/Docfx.Build.SchemaDriven/Validators/SchemaValidator.cs
+++ src/Docfx.Build.SchemaDriven/Validators/SchemaValidator.cs
@@ -5,40 +5,63 @@ using System.Text.Json;
 using Docfx.Common;
 using Json.Schema;
 
+#nullable enable
+
 namespace Docfx.Build.SchemaDriven;
 
 public class SchemaValidator
 {
     private readonly JsonSchema _schema;
 
-    private static readonly EvaluationOptions DefaultOptions = new()
+    private static readonly EvaluationOptions DefaultEvaluationOptions = new()
     {
-        ValidateAgainstMetaSchema = false,
         OutputFormat = OutputFormat.List,
+    };
+
+    private static readonly JsonDocumentOptions DefaultJsonDocumentOptions = new()
+    {
+        AllowTrailingCommas = true,
     };
 
     static SchemaValidator()
     {
-        SchemaRegistry.Global.Register(new("http://dotnet.github.io/docfx/schemas/v1.0/schema.json#"), MetaSchemas.Draft7);
-        SchemaRegistry.Global.Register(new("https://dotnet.github.io/docfx/schemas/v1.0/schema.json#"), MetaSchemas.Draft7);
+        Uri[] uris =
+        [
+            new Uri("http://dotnet.github.io/docfx/schemas/v1.0/schema.json#"),
+            new Uri("https://dotnet.github.io/docfx/schemas/v1.0/schema.json#"),
+        ];
+
+        foreach (var uri in uris)
+        {
+            SchemaRegistry.Global.Register(uri, MetaSchemas.Draft7);
+            DialectRegistry.Global.Register(Dialect.Draft07.With([], id: uri));
+        }
     }
 
     public SchemaValidator(string json)
     {
-        _schema = JsonSchema.FromText(json, new() { AllowTrailingCommas = true });
+        var builldOptions = new BuildOptions()
+        {
+            // Create SchemaRegistry instance to avoid exception.
+            // https://github.com/json-everything/json-everything/issues/957
+            SchemaRegistry = new SchemaRegistry(),
+            Dialect = Dialect.Draft07,
+        };
+
+        _schema = JsonSchema.FromText(json, builldOptions, jsonOptions: DefaultJsonDocumentOptions);
     }
 
     public void Validate(object obj)
     {
         var json = JsonSerializer.Serialize(obj);
-        var result = _schema.Evaluate(JsonDocument.Parse(json), DefaultOptions);
+        var result = _schema.Evaluate(JsonDocument.Parse(json, DefaultJsonDocumentOptions).RootElement, DefaultEvaluationOptions);
 
         if (result.IsValid)
             return;
 
-        foreach (var detail in result.Details)
+        foreach (var detail in result.Details ?? [])
         {
-            if (detail.HasErrors)
+            if (detail.Errors != null)
             {
                 foreach (var (type, message) in detail.Errors)
                 {
